a87dc151e9eaa56fab42f629ab933af9b1a6d5b8
Justin Cormack <justin@specialbusservice.com>
2011-05-13 11:37:57 +0100

    tidy up assert in tests

--- test.lua
+++ test.lua
@@ -5,14 +5,12 @@ local fd, fd0, fd1, fd2, n, s, err, errno
 S.gcollect(true)
 
 -- print uname info
-local u
-u, err = S.uname()
-assert(err == nil, err)
+local u = assert(S.uname())
 print(u.nodename .. " " .. u.sysname .. " " .. u.release .. " " .. u.version)
 
 -- test open non existent file
 fd, err, errno = S.open("/tmp/file/does/not/exist", S.O_RDONLY)
-assert(err ~= nil, "expected open to fail on file not found")
+assert(err, "expected open to fail on file not found")
 assert(S.symerror[errno] == 'ENOENT', "expect ENOENT from open non existent file")
 
 -- test close invalid fd
@@ -21,15 +19,12 @@ assert(err, "expected to fail on close invalid fd")
 assert(S.symerror[errno] == 'EBADF', "expect EBADF from invalid numberic fd")
 
 -- test open and close valid file
-fd, err, errno = S.open("/dev/null", S.O_RDONLY)
-assert(err == nil, "should be able to open /dev/null")
-assert(errno == nil, "errno should not be set opening /dev/null")
+fd = assert(S.open("/dev/null", S.O_RDONLY))
 assert(type(fd) == 'cdata', "should get a cdata object back from open")
 assert(fd.fd == 3, "should get file descriptor 3 back from first open")
 
 -- another open
-fd2, err, errno = S.open("/dev/zero", S.O_RDONLY)
-assert(err == nil, "should be able to open /dev/zero")
+fd2 = assert(S.open("/dev/zero", S.O_RDONLY))
 assert(fd2.fd == 4, "should get file descriptor 4 back from second open")
 
 -- normal close
@@ -47,8 +42,7 @@ local buf = S.t.buffer(size) -- allocate buffer for read
 
 for i = 0, size - 1 do buf[i] = 255 end -- make sure overwritten
 -- test read
-n, err, errno = S.read(fd2, buf, size)
-assert(err == nil, "should be able to read from /dev/zero")
+n = assert(S.read(fd2, buf, size))
 assert(n >= 0, "should not get error reading from /dev/zero")
 assert(n == size, "should not get truncated read from /dev/zero") -- technically allowed!
 for i = 0, size - 1 do assert(buf[i] == 0, "should read zero bytes from /dev/zero") end
@@ -63,51 +57,41 @@ collectgarbage("collect")
 
 -- test file has been closed after garbage collection
 n, err, errno = S.read(4, buf, size)
-assert(not n, "should not be able to read from fd 4 after gc")
+assert(err, "should not be able to read from fd 4 after gc")
 assert(S.symerror[errno] == 'EBADF', "expect EBADF from already closed fd")
 
 -- test with gc turned off
-fd, err, errno = S.open("/dev/zero", S.O_RDONLY)
-assert(err == nil, err)
+fd = assert(S.open("/dev/zero", S.O_RDONLY))
 assert(fd.fd == 3, "fd should be 3")
 fd:nogc()
 fd = nil
 collectgarbage("collect")
-n, err, errno = S.read(3, buf, size)
-assert(err == nil, "should be able to read as gc disabled")
+n = assert(S.read(3, buf, size))
 assert(S.close(3))
 
 -- another open
-fd, err, errno = S.open("/dev/zero", S.O_RDWR)
-assert(err == nil, "should be able to open /dev/zero read write")
+fd = assert(S.open("/dev/zero", S.O_RDWR))
 -- test write
-n, err, errno = S.write(fd, buf, size)
-assert(err == nil, "should be able to write to /dev/zero")
+n = assert(S.write(fd, buf, size))
 assert(n >= 0, "should not get error writing to /dev/zero")
 assert(n == size, "should not get truncated write to /dev/zero") -- technically allowed!
 
 local offset = 1
-n, err, errno = fd:pread(buf, size, offset)
-assert(err == nil, "should be able to pread /dev/zero")
-n, err, errno = fd:pwrite(buf, size, offset)
-assert(err == nil, "should be able to pwrite /dev/zero")
+n = assert(fd:pread(buf, size, offset))
+n = assert(fd:pwrite(buf, size, offset))
 
-fd2, err, errno = fd:dup()
-assert(err == nil, "should be able to dup fd")
+fd2 = assert(fd:dup())
 assert(fd2:close())
 
-fd2, err, errno = fd:dup2(17)
-assert(err == nil, "should be able to dup2 fd")
-assert(fd2.fd == 17, "dup2,3 should set file id as specified")
+fd2 = assert(fd:dup2(17))
+assert(fd2.fd == 17, "dup2 should set file id as specified")
 assert(S.close(17))
 
-fd2, err, errno = fd:dup3(17, S.O_CLOEXEC)
-assert(err == nil, "should be able to dup3 fd")
-assert(fd2.fd == 17, "dup2,3 should set file id as specified")
+fd2 = assert(fd:dup3(17, S.O_CLOEXEC))
+assert(fd2.fd == 17, "dup3 should set file id as specified")
 assert(S.close(17))
 
-fd2, err, errno = fd:dup(17, S.O_CLOEXEC)
-assert(err == nil, "should be able to use dup as dup3")
+fd2 = assert(fd:dup(17, S.O_CLOEXEC))
 assert(fd2.fd == 17, "dup2,3 should set file id as specified")
 assert(S.close(17))
 
@@ -118,8 +102,7 @@ assert(S.O_CREAT == 64, "wrong octal value for O_CREAT")
 local tmpfile = "./XXXXYYYYZZZ4521"
 local tmpfile2 = "./666666DDDDDFFFF"
 
-fd, err = S.creat(tmpfile, S.S_IRWXU)
-assert(err == nil, err)
+fd = assert(S.creat(tmpfile, S.S_IRWXU))
 
 assert(S.link(tmpfile, tmpfile2))
 assert(S.unlink(tmpfile2))
@@ -130,14 +113,12 @@ assert(S.chmod(tmpfile, S.S_IRUSR + S.S_IWUSR))
 assert(fd:fsync())
 assert(fd:fdatasync())
 
-n, err, errno = fd:lseek(offset, 'SEEK_SET')
-assert(err == nil, "should be able to seek file")
+n = assert(fd:lseek(offset, 'SEEK_SET'))
 assert(n == offset, "seek should position at set position")
-n, err, errno = fd:lseek(offset, 'SEEK_CUR')
-assert(err == nil, "should be able to seek file")
+n = assert(fd:lseek(offset, 'SEEK_CUR'))
 assert(n == offset + offset, "seek should position at set position")
 
-assert(S.unlink(tmpfile), "should be able to unlink file")
+assert(S.unlink(tmpfile))
 
 assert(S.mkdir(tmpfile, S.S_IRWXU))
 assert(S.rmdir(tmpfile))
@@ -145,76 +126,67 @@ assert(S.rmdir(tmpfile))
 assert(S.close(fd))
 
 fd, err = S.open(tmpfile, S.O_RDWR)
-assert(err ~= nil, "expected open to fail on file not found")
+assert(err, "expected open to fail on file not found")
 
-fd0, fd1, err, errno = S.pipe(99999) -- invalid flags to test error handling
+fd0, fd1, err, errno = S.pipe(99999) -- invalid flags
 assert(fd0 == nil and fd1 == nil and S.symerror[errno] == 'EINVAL', "should be EINVAL with bad flags to pipe2")
+
+-- cant use idiomatic assert here, should we return table?
 fd0, fd1, err, errno = S.pipe()
 assert(err == nil and errno == nil, "should be able to open pipe")
 assert(fd0.fd == 3 and fd1.fd == 4, "expect file handles 3 and 4 for pipe")
 fd0, fd1 = nil, nil
 
 assert(S.chdir("/"))
-fd, err = S.open("/")
-assert(err == nil, err)
+fd = assert(S.open("/"))
 assert(fd:fchdir())
 
 assert(S.getcwd(buf, size))
 assert(S.string(buf) == "/", "expect cwd to be /")
-s, err = S.getcwd()
-assert(err == nil, err)
+s= assert(S.getcwd())
 assert(s == "/", "expect cwd to be /")
 
 local rem
-rem, err, errno = S.nanosleep(S.t.timespec(0, 1000000))
-assert(err == nil, err)
+rem = assert(S.nanosleep(S.t.timespec(0, 1000000)))
 assert(rem.tv_sec == 0 and rem.tv_nsec == 0, "expect no elapsed time after nanosleep")
 
 local stat
 
-stat, err, errno = S.lstat("/dev/zero")
-assert(err == nil, err)
+stat = assert(S.lstat("/dev/zero"))
 assert(stat.st_nlink == 1, "expect link count on /dev/zero to be 1")
 
-stat, err, errno = S.fstat(fd) -- stat "/"
-assert(err == nil, err)
+stat = assert(fd:fstat(fd)) -- stat "/"
 assert(stat.st_size == 4096, "expect / to be size 4096") -- might not be
 assert(stat.st_gid == 0, "expect / to be gid 0 is " .. tonumber(stat.st_gid))
 assert(stat.st_uid == 0, "expect / to be uid 0 is " .. tonumber(stat.st_uid))
 assert(S.S_ISDIR(stat.st_mode), "expect / to be a directory")
 
-stat, err, errno = S.stat("/dev/zero")
-assert(err == nil, err)
+stat = assert(S.stat("/dev/zero"))
 assert(S.major(stat.st_rdev) == 1, "expect major number of /dev/zero to be 1")
 assert(S.minor(stat.st_rdev) == 5, "expect minor number of /dev/zero to be 5")
 assert(S.S_ISCHR(stat.st_mode), "expect /dev/zero to be a character device")
 
-stat, err, errno = S.lstat("/etc/passwd")
-assert(err == nil, err)
+stat = assert(S.lstat("/etc/passwd"))
 assert(S.S_ISREG(stat.st_mode), "expect /etc/passwd to be a regular file")
 
 -- mmap and related functions
 local mem, mem2
 size = 4096
-mem, err = S.mmap(nil, size, S.PROT_READ, S.MAP_PRIVATE + S.MAP_ANONYMOUS, -1, 0)
-assert(err == nil, err)
+mem = assert(S.mmap(nil, size, S.PROT_READ, S.MAP_PRIVATE + S.MAP_ANONYMOUS, -1, 0))
 assert(S.munmap(mem, size))
-mem, err = S.mmap(nil, size, S.PROT_READ, S.MAP_PRIVATE + S.MAP_ANONYMOUS, -1, 0)
-assert(err == nil, err)
+mem = assert(S.mmap(nil, size, S.PROT_READ, S.MAP_PRIVATE + S.MAP_ANONYMOUS, -1, 0))
 assert(S.msync(mem, size, S.MS_SYNC))
 assert(S.madvise(mem, size, "MADV_RANDOM"))
 mem = nil -- gc memory, should be munmapped
 collectgarbage("collect")
 mem, err = S.mmap(nil, size, S.PROT_READ, S.MAP_PRIVATE + S.MAP_ANONYMOUS + S.MAP_FIXED, -1, 0) -- MAP_FIXED should fail here!
-assert(mem == nil, "expect mmap MAP_FIXED to fail")
+assert(err, "expect mmap MAP_FIXED to fail")
 
 local size2 = size * 2
-mem, err = S.mmap(nil, size, S.PROT_READ, S.MAP_PRIVATE + S.MAP_ANONYMOUS, -1, 0)
-assert(err == nil, err)
+mem = assert(S.mmap(nil, size, S.PROT_READ, S.MAP_PRIVATE + S.MAP_ANONYMOUS, -1, 0))
 S.nogc(mem)
-mem2, err = S.mremap(mem, size, size2, S.MREMAP_MAYMOVE)
+mem2 = assert(S.mremap(mem, size, size2, S.MREMAP_MAYMOVE))
 mem = nil
-assert(err == nil, err)
 assert(S.munmap(mem2, size2))
 
 local mask
@@ -224,16 +196,13 @@ assert(mask == S.S_IWGRP + S.S_IWOTH, "umask not set correctly")
 
 -- sockets
 local s, fl
-s, err = S.socket("AF_INET", "SOCK_STREAM", 0)
-assert(err == nil, err)
-fl, err = s:fcntl("F_GETFL")
-assert(err == nil, err)
+s = assert(S.socket("AF_INET", "SOCK_STREAM", 0))
+fl = assert(s:fcntl("F_GETFL"))
 assert(s:fcntl("F_SETFL", bit.bor(fl, S.O_NONBLOCK)))
 
 assert(S.sizeof(S.t.sockaddr) == S.sizeof(S.t.sockaddr_in), "inet socket addresses should be padded to same as sockaddr")
 
-local a
-a = S.t.sockaddr_in(S.AF_INET, S.htons(1234), S.INADDR_ANY) -- fix constructor to be more useful
+local a = S.t.sockaddr_in(S.AF_INET, S.htons(1234), S.INADDR_ANY) -- fix constructor to be more useful
 assert(s:bind(a))
 
 assert(S.close(s))
@@ -242,25 +211,22 @@ assert(S.close(s))
 local pid, pid0, status
 pid0 = S.getpid()
 assert(pid0 > 1, "expecting my pid to be larger than 1")
-local ppid = S.getppid()
-assert(ppid > 1, "expecting my parent pid to be larger than 1")
+assert(S.getppid() > 1, "expecting my parent pid to be larger than 1")
 
-pid, err = S.fork()
-assert(err == nil, err)
+pid = assert(S.fork())
 if (pid == 0) then -- child
   assert(S.getppid() == pid0, "parent pid should be previous pid")
   S.exit()
 else -- parent
-  pid0, status, err = S.wait()
+  pid0, status, err = S.wait() -- non idiomatic return.
   assert(err == nil)
   assert(pid == pid0, "expect fork to return same pid as wait")
 end
-pid, err = S.fork()
-assert(err == nil, err)
+pid = assert(S.fork())
 if (pid == 0) then -- child
   S.exit()
 else -- parent
-  pid0, status, err = S.waitpid(-1)
+  pid0, status, err = S.waitpid(-1) -- non idiomatic return.
   assert(err == nil)
   assert(pid == pid0, "expect fork to return same pid as wait")
 end
