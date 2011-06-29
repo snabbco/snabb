5153fed4ddbabf508f6e1766ac406a5ecf4f4fbe
Justin Cormack <justin@specialbusservice.com>
2011-06-29 17:08:35 +0100

    add readahead

--- syscall.lua
+++ syscall.lua
@@ -1160,6 +1160,7 @@ typedef int32_t clockid_t;
 // 64 bit
 typedef uint64_t dev_t;
 typedef uint64_t loff_t;
+typedef uint64_t off64_t;
 
 // posix standards
 typedef unsigned short int sa_family_t;
@@ -1780,6 +1781,7 @@ void *mremap(void *old_address, size_t old_size, size_t new_size, int flags, voi
 int madvise(void *addr, size_t length, int advice);
 int posix_fadvise(int fd, off_t offset, off_t len, int advice);
 int fallocate(int fd, int mode, off_t offset, off_t len);
+ssize_t readahead(int fd, off64_t offset, size_t count);
 
 int pipe(int pipefd[2]);
 int pipe2(int pipefd[2], int flags);
@@ -2536,9 +2538,12 @@ function S.posix_fadvise(fd, advice, offset, len) -- note argument order
   return retbool(C.posix_fadvise(getfd(fd), offset or 0, len or 0, stringflag(advice, "POSIX_FADV_")))
 end
 function S.fallocate(fd, mode, offset, len)
-  return retbool(getfd(fd), stringflag(mode, "FALLOC_FL_"), offset or 0, len)
+  return retbool(C.fallocate(getfd(fd), stringflag(mode, "FALLOC_FL_"), offset or 0, len))
 end
 function S.posix_fallocate(fd, offset, len) return S.fallocate(fd, 0, offset, len) end
+function S.readahead(fd, offset, count)
+  return retbool(C.readahead(getfd(fd), offset, count))
+end
 
 local sproto
 function sproto(domain, protocol) -- helper function to lookup protocol type depending on domain
@@ -3795,7 +3800,7 @@ local fdmethods = {'nogc', 'nonblock', 'block', 'sendfds', 'sendcred',
                    'inotify_add_watch', 'inotify_rm_watch', 'inotify_read', 'flistxattr',
                    'fsetxattr', 'fgetxattr', 'fremovexattr', 'fxattr', 'splice', 'vmsplice', 'tee',
                    'signalfd_read', 'timerfd_gettime', 'timerfd_settime', 'timerfd_read',
-                   'posix_fadvise', 'fallocate', 'posix_fallocate'
+                   'posix_fadvise', 'fallocate', 'posix_fallocate', 'readahead'
                    }
 local fmeth = {}
 for _, v in ipairs(fdmethods) do fmeth[v] = S[v] end
