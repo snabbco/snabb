e5b0296e7dd37950a92fcdd7bd75a61854681a17
Justin Cormack <justin@specialbusservice.com>
2012-07-07 18:53:55 +0100

    split test as failing on one test machine: different messages

--- test.lua
+++ test.lua
@@ -1028,9 +1028,9 @@ test_netlink = {
       local ok, err = S.unshare("newnet")
       if err then S.exit("failure") end
       local i = fork_assert(S.interfaces())
-      fork_assert(#i == 1, "expect new network ns only has down lo interface")
-      fork_assert(i.lo, "expect new network ns only has down lo interface")
-      fork_assert(not i.lo.flags.up, "expect new network ns only has down lo interface")
+      fork_assert(#i == 1, "expect new network ns only has down one interface")
+      fork_assert(i.lo, "expect new network ns has lo interface")
+      fork_assert(not i.lo.flags.up, "expect new network lo is down")
       fork_assert(i.lo:setflags("up"))
       local lo = fork_assert(S.interface("lo"))
       fork_assert(lo.flags.up, "expect lo up now")
