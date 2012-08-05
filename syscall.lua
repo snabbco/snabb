04ea66add6e90eb7124f241479c93636fd26cf93
Justin Cormack <justin@specialbusservice.com>
2012-08-05 10:42:56 +0100

    remove fixes to work around luajit bug now fixed in head

--- syscall.lua
+++ syscall.lua
@@ -5510,10 +5510,10 @@ local function ifla_getmsg(args, messages, values, tab, lookup, af)
     if not value then error("not enough arguments") end
   end
 
-  if type(tp) == "string" and tp == "asciiz" then
+  if tp == "asciiz" then
     tp = t.buffer(#value + 1)
   else
-    if type(tp) == "string" and tp == "address" then
+    if tp == "address" then
       tp = S.addrtype[af]
     end
     if not ffi.istype(tp, value) then
