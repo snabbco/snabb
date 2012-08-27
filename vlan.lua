a27d06b722715a13577052e2a1c89b0bd32f4caa
Justin Cormack <justin@specialbusservice.com>
2012-08-27 15:35:24 +0100

    switch to modular netlink code

--- vlan.lua
+++ vlan.lua
@@ -1,19 +1,19 @@
 -- example of how to create a vlan using ljsyscall
 -- not in the tests as it will mess up your interfaces and as far as I know you can only create vlans on physical interfaces
 
-local S = require "syscall"
+local nl = require "nl"
 
 local interface = "eth0"
 local vlan = 40
 local name = interface .. "." .. tostring(vlan)
 
-local i = assert(S.interfaces())
+local i = assert(nl.interfaces())
 
 local ii = i[interface]
 
 if not ii then
   print("cannot find underlying interface")
-  S.exit("failure")
+  os.exit(1)
 end
 
 -- create
@@ -21,25 +21,25 @@ end
 -- equivalent to
 -- ip link add link eth0 name eth0.42 type vlan id 40
 
-ok, err = S.create_interface{name = name, link = ii.index, type = "vlan", id = vlan}
+ok, err = nl.create_interface{name = name, link = ii.index, type = "vlan", id = vlan}
 
 --Equivalent using newlink
---ok, err = S.newlink(0, S.NLM_F_CREATE, 0, 0, "link", ii.index, "ifname", name, "linkinfo", {"kind", "vlan", "data", {"id", vlan}})
+--ok, err = nl.newlink(0, "create", 0, 0, "link", ii.index, "ifname", name, "linkinfo", {"kind", "vlan", "data", {"id", vlan}})
 
 if not ok then
   print(err)
-  S.exit("failure")
+  os.exit(1)
 end
 
 i:refresh()
 
 print(i)
 
-ok, err = S.dellink(0, "ifname", name)
+ok, err = nl.dellink(0, "ifname", name)
 
 if not ok then
   print(err)
-  S.exit("failure")
+  os.exit(1)
 end
 
 
