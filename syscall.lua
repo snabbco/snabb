5c31bcdbfa16c61aeda819ca97b99e85b67e01d9
Justin Cormack <justin@specialbusservice.com>
2012-08-27 20:33:43 +0100

    acct not in Musl

--- syscall.lua
+++ syscall.lua
@@ -3669,6 +3669,7 @@ end
 -- if not in libc replace
 -- not in Musl TODO patch so they are
 if not pcall(inlibc, "fallocate") then C.fallocate = CC.fallocate end
+if not pcall(inlibc, "acct") then C.acct = CC.acct end
 
 -- not in eglibc
 if not pcall(inlibc, "mknod") then C.mknod = CC.mknod end
