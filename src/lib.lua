f35a40fa6f72330314bfb58a9687ec86f3610e81
Luke Gorrie <luke@snabb.co>
2012-12-12 14:48:49 +0000

    lib.lua: Added comma_value() function. credit http://richard.warburton.it

--- src/lib.lua
+++ src/lib.lua
@@ -15,3 +15,9 @@ end
 function bitset (value, n)
    return bit.band(value, bit.lshift(1, n)) ~= 0
 end
+
+function comma_value(n) -- credit http://richard.warburton.it
+   local left,num,right = string.match(n,'^([^%d]*%d)(%d*)(.-)$')
+   return left..(num:reverse():gsub('(%d%d%d)','%1,'):reverse())..right
+end
+
