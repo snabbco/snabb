ac4a6e8f5d64d18d27b8ac769a907c6780e14862
Luke Gorrie <luke@snabb.co>
2012-12-20 15:48:53 +0000

    main.lua: dump JIT trace to file, require strict, more backtrace on error.

--- src/main.lua
+++ src/main.lua
@@ -1,12 +1,20 @@
 module(...,package.seeall)
 
 function main ()
+   require "strict"
+
+   local jit_dump = require "jit.dump"
+   local jit_v = require "jit.v"
+
+   jit_v.start("snabbswitch-jit-dump.txt")
+
    require "selftest"
+
 end
 
 function handler (reason)
    print(reason)
-   -- print(debug.traceback())
+   print(debug.traceback())
    -- debug.debug()
    os.exit(1)
 end
