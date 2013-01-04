859e977f6e6e603dd094ebd81fb6bbf2a1396b4f
Luke Gorrie <luke@snabb.co>
2013-01-04 09:54:55 +0000

    Automatically find and take over available PCI ethernet adapters.
    selftest.lua now tests every suitable NIC automatically. No config needed.
    
    pci.lua now contains logic to scan /sys/bus/pci/devices/* to see which
    PCI devices are available and which ones are suitable for use by the
    switch.
    
    Devices are suitable if:
      They use an ethernet controller supported by our driver.
      The device is not used by the OS *OR* the interface is in 'down' state.
    
    The selftest code unbinds interfaces from the OS and doesn't rebind them.
    That means you won't see them in 'ifconfig' anymore, for example.
    
    If you want to give a device back to the kernel driver then do this:
      echo '0000:00:04.0' > /sys/bus/pci/drivers/e1000e/bind
    (with the right PCI address, of course.)
    
    Here is a transcript of how the scanning process looks:
    
    selftest: pci
    Scanning PCI devices:
    pciaddr         vendor  device  iface   status
    0000:00:00.0    0x8086  0x1237  -       -
    0000:00:01.0    0x8086  0x7000  -       -
    0000:00:01.1    0x8086  0x7010  -       -
    0000:00:01.3    0x8086  0x7113  -       -
    0000:00:02.0    0x1013  0x00b8  -       -
    0000:00:03.0    0x10ec  0x8139  eth0    up
    0000:00:04.0    0x8086  0x10d3  eth1    down
    0000:00:05.0    0x1af4  0x1001  -       -
    Suitable devices:
      0000:00:04.0
    Testing device: 0000:00:04.0
    Unbinding PCI device 0000:00:04.0 (eth1) from the operating system driver.

--- src/pci.lua
+++ src/pci.lua
@@ -10,18 +10,71 @@ local lib = require("lib")
 require("clib_h")
 require("snabb_h")
 
--- Return true if 'device' is an Intel 82574 ethernet controller.
-function pcidev_is_82574 (device)
-   return io.open(path(device,"config"), "ro"):read(4) == "\x86\x80\xd3\x10"
+function suitable_devices ()
+   list = {}
+   for _,info in ipairs(devices()) do
+      if info.vendor == "0x8086" and info.device == "0x10d3" and
+         (info.interface == nil or info.status == 'down') then
+         list[#list + 1] = info
+      end
+   end
+   return list
 end
 
-local function path(pcidev, file)
-   return "/sys/bus/pci/devices/"..pcidev.."/"..file
+function prepare_device (pciaddress)
+   local device = device_info(pciaddress)
+   if device.status == 'down' then
+      print("Unbinding PCI device "..pciaddress.." ("..device.interface..") from the operating system driver.")
+      local file = io.open(path(pciaddress).."/driver/unbind", "w")
+      file:write(pciaddress)
+      file.close()
+   end
 end
+
+function devices ()
+   info = {}
+   for _,device in ipairs(scandir("/sys/bus/pci/devices")) do
+      info[#info + 1] = device_info(device)
+   end
+   return info
+end
+
+function device_info (pciaddress)
+   local info = {}
+   local p = path(pciaddress)
+   info.pciaddress = pciaddress
+   info.vendor = firstline(p.."/vendor")
+   info.device = firstline(p.."/device")
+   info.interface = firstfile(p.."/net")
+   if info.interface then
+      info.status = firstline(p.."/net/"..info.interface.."/operstate")
+   end
+   return info
+end
+
+function firstline(filename)
+   local file = io.open(filename, "r")
+   if file then
+      local line = file:read("*l")
+      file.close()
+      return line
+   end
+end
+
+function firstfile(dir)
+   for _,file in ipairs(scandir(dir)) do return file end
+end
+
+-- Return true if 'device' is an Intel 82574 ethernet controller.
+function pcidev_is_82574 (device)
+   return io.open(path(device).."/config", "ro"):read(4) == "\x86\x80\xd3\x10"
+end
+
+function path(pcidev) return "/sys/bus/pci/devices/"..pcidev end
 
 -- Return a pointer for MMIO access to 'device' resource 'n'.
 function map_pci_memory (device, n)
-   local filepath = path(device,"resource")..n
+   local filepath = path(device).."/resource"..n
    local addr = C.map_pci_resource(filepath)
    assert( addr ~= 0 )
    return addr
@@ -29,7 +82,7 @@ end
 
 -- Return a file descriptor for accessing PCI configuration space.
 function open_config (device)
-   return C.open_pcie_config(path(device, "config"))
+   return C.open_pcie_config(path(device).."/config")
 end
 
 -- Close the file descriptor for PCI configuration space access.
@@ -58,3 +111,36 @@ function set_bus_master (fd, flag)
    end
    write_config(fd, 0x04, control)
 end
+
+function selftest ()
+   print("selftest: pci")
+   print("Scanning PCI devices:")
+   list_devices()
+   local suitable = suitable_devices()
+   if #suitable == 0 then
+      print("No suitable PCI Ethernet devices found.")
+   else
+      print("Suitable devices: ")
+      for _,device in ipairs(suitable) do
+         print("  "..device.pciaddress)
+      end
+   end
+end
+
+function list_devices ()
+   print("pciaddr", "", "vendor", "device", "iface", "status")
+   for _,d in ipairs(devices()) do
+      print(d.pciaddress, d.vendor, d.device, d.interface or "-", d.status or "-")
+   end
+end
+
+-- Return the names of all files in dir.
+-- XXX Consider doing this with luafilesystem (lfs) instead of popen.
+function scandir (dir)
+   local files = {}
+   for filename in io.popen('ls -1 "'..dir..'" 2>/dev/null'):lines() do
+      files[#files + 1] = filename
+   end
+   return files
+end
+
--- src/selftest.lua
+++ src/selftest.lua
@@ -11,12 +11,18 @@ memory.selftest({verbose = true})
 print "selftest"
 assert(C.lock_memory() == 0)
 
-local nic = intel.new("0000:00:04.0")
+pci.selftest()
 
-print("Initializing controller..")
-nic.init()
-test.waitfor("linkup", nic.linkup, 20, 250000)
-nic.selftest2()
--- nic.enable_mac_loopback()
--- nic.selftest({packets=10000000})
+for _,device in ipairs(pci.suitable_devices()) do
+   local pciaddress = device.pciaddress
+   print("Testing device: "..pciaddress)
+   pci.prepare_device(pciaddress)
+   local nic = intel.new(pciaddress)
+   print("Initializing controller..")
+   nic.init()
+   test.waitfor("linkup", nic.linkup, 20, 250000)
+   nic.selftest2()
+   -- nic.enable_mac_loopback()
+   -- nic.selftest({packets=10000000})
+end
 
