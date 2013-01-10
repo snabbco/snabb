03c248c1a9a9e1f563f4fb923a2b4c5f7920bf06
Luke Gorrie <luke@snabb.co>
2013-01-10 13:19:29 +0000

    Bad idea: Lookup actual HugeTLB page size via /proc/self/pagemap.
    Bad idea because it doesn't work: the file reports 4096 even for huge
    pages. Committed just in case this code should be dug up some day for
    some other purpose.
    
    The next commits will rollback these changes and add a better solution.

--- src/memory.lua
+++ src/memory.lua
@@ -3,7 +3,8 @@ module(...,package.seeall)
 local ffi = require("ffi")
 local C = ffi.C
 
-local hugepagesize = 2 * 1024 * 1024
+local base_page_size = 4096
+local huge_page_size = 2 * 1024 * 1024
 
 -- Allocate physically contiguous memory that is suitable for DMA.
 -- Return a pointer (or nil on failure) and the total number of bytes
@@ -11,13 +12,14 @@ local hugepagesize = 2 * 1024 * 1024
 function dma_alloc (size)
    local page = allocate_huge_page()
    if page == nil then return nil, 0 end
-   return page, math.min(hugepagesize, size)
+   return page, math.min(huge_page_size, size)
 end
 
 -- Try hard to allocate a huge page and return its address.
 function allocate_huge_page ()
    for i = 1,3 do
-      local page = C.allocate_huge_page(hugepagesize)
+      local page = C.allocate_huge_page(huge_page_size)
+      map(page)
       if page ~= nil then  return page  else  reserve_new_page()  end
    end
 end
@@ -41,23 +43,41 @@ end
 
 -- From virtual page to physical page
 local cache = {}
-local page_size = 4096
+local sizes = {}
 
 -- Return the 64-bit physical address of virt_addr.
 function map (virt_addr)
    virt_addr = ffi.cast("uint64_t", virt_addr)
-   local virt_page = tonumber(virt_addr / page_size)
-   local offset   = tonumber(virt_addr % page_size)
+   local virt_page = tonumber(virt_addr / base_page_size)
+   local offset   = tonumber(virt_addr % base_page_size)
    local phys_page = cache[virt_page] or resolve(virt_page)
-   return ffi.cast("uint64_t", phys_page * page_size + offset)
+   return ffi.cast("uint64_t", phys_page * base_page_size + offset)
 end
 
 -- Return (and cache) the physical page number of virtpage.
 function resolve (virt_page)
-   local phys_page = C.phys_page(virt_page)
-   if phys_page == 0 then error("Unable to resolve page " .. virt_page) end
+   local phys_page = physical_page(virt_page)
+   if phys_page == nil then error("Unable to resolve page " .. virt_page) end
    cache[virt_page] = phys_page
+   local size = page_size(virt_page)
+   sizes[size] = (sizes[size] or 0) + 1
    return phys_page
+end
+
+--- ## Extracting information about memory from /proc/self/pagemap
+
+function physical_page (virt_page) return pagemap_info(virt_page) % 2^56 end
+
+function page_size (virt_page)
+   local info = pagemap_info(virt_page)
+   local pageshift = tonumber((info / 2^55) % 2^6)
+   return 2^pageshift
+end
+
+function pagemap_info (virt_page)
+   local info = C.pagemap_info(virt_page)
+   local mapped = info >= 2^63
+   if mapped then return info else return nil end
 end
 
 function selftest (options)
@@ -65,7 +85,7 @@ function selftest (options)
    local verbose = options.verbose or false
    print("Kernel HugeTLB pages (/proc/sys/vm/nr_hugepages): " .. get_hugepages())
    for i = 1, 4 do
-      io.write("  Allocating 1MB from a HugeTLB page: ") io.flush()
+      io.write("  Allocating a "..(huge_page_size/1024/1024).."MB HugeTLB page: ") io.flush()
       local dmaptr, dmalen = dma_alloc(1024*1024)
       print(tostring(dmaptr)..", "..tostring(dmalen))
       if dmaptr == nil or dmalen ~= 1024*1024 then
@@ -103,5 +123,12 @@ function selftest (options)
       if (verbose and i % 4 == 0) then print() end
    end
    print("Virtual->Physical mapping OK.")
+   if verbose then
+      print("Page size tallys:")
+      for size,count in pairs(sizes) do
+         print(size,count)
+      end
+   end
+
 end
 
