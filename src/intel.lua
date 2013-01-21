de334f35d36d17365460b8d8cd9c667f04e7638d
Pete Kazmier <pete@kazmier.com>
2013-01-21 12:38:56 -0500

    Fixed crash in selftest when multiple PCI devices are available.
    The selftest executes for each suitable PCI device found. As part of
    the test, an instance of the intel NIC is instantiated, which caused
    the FFI cdefs to be executed more than once resulting in a crash due
    to the attempt to redefine the cdefs.
    
    See
    https://github.com/SnabbCo/snabbswitch/issues/34#issuecomment-12409461
    for an example of the stack trace.

--- src/intel.lua
+++ src/intel.lua
@@ -20,6 +20,62 @@ local bits, bitset = lib.bits, lib.bitset
 require("clib_h")
 require("snabb_h")
 
+-- FFI definitions for receive and transmit descriptors
+
+ffi.cdef[[
+         // RX descriptor written by software.
+         struct rx_desc {
+            uint64_t address;    // 64-bit address of receive buffer
+            uint64_t dd;         // low bit must be 0, otherwise reserved
+         } __attribute__((packed));
+
+         // RX writeback descriptor written by hardware.
+         struct rx_desc_wb {
+            // uint32_t rss;
+            uint16_t checksum;
+            uint16_t id;
+            uint32_t mrq;
+            uint32_t status;
+            uint16_t length;
+            uint16_t vlan;
+         } __attribute__((packed));
+
+         union rx {
+            struct rx_desc data;
+            struct rx_desc_wb wb;
+         } __attribute__((packed));
+   ]]
+
+ffi.cdef[[
+         // TX Extended Data Descriptor written by software.
+         struct tx_desc {
+            uint64_t address;
+            uint64_t options;
+         } __attribute__((packed));
+
+         struct tx_context_desc {
+            unsigned int tucse:16,
+                         tucso:8,
+                         tucss:8,
+                         ipcse:16,
+                         ipcso:8,
+                         ipcss:8,
+                         mss:16,
+                         hdrlen:8,
+                         rsv:2,
+                         sta:4,
+                         tucmd:8,
+                         dtype:4,
+                         paylen:20;
+         } __attribute__((packed));
+
+         union tx {
+            struct tx_desc data;
+            struct tx_context_desc ctx;
+         };
+   ]]
+
+
 function new (pciaddress)
 
    -- Method dictionary for Intel NIC objects.
@@ -207,30 +263,6 @@ function new (pciaddress)
 
    -- Receive functionality
 
-   ffi.cdef[[
-         // RX descriptor written by software.
-         struct rx_desc {
-            uint64_t address;    // 64-bit address of receive buffer
-            uint64_t dd;         // low bit must be 0, otherwise reserved
-         } __attribute__((packed));
-
-         // RX writeback descriptor written by hardware.
-         struct rx_desc_wb {
-            // uint32_t rss;
-            uint16_t checksum;
-            uint16_t id;
-            uint32_t mrq;
-            uint32_t status;
-            uint16_t length;
-            uint16_t vlan;
-         } __attribute__((packed));
-
-         union rx {
-            struct rx_desc data;
-            struct rx_desc_wb wb;
-         } __attribute__((packed));
-   ]]
-
    local rxnext = 0
    local rxbuffers = {}
 
@@ -314,35 +346,6 @@ function new (pciaddress)
    end
 
    -- Transmit functionality
-
-   ffi.cdef[[
-         // TX Extended Data Descriptor written by software.
-         struct tx_desc {
-            uint64_t address;
-            uint64_t options;
-         } __attribute__((packed));
-
-         struct tx_context_desc {
-            unsigned int tucse:16,
-                         tucso:8,
-                         tucss:8,
-                         ipcse:16,
-                         ipcso:8,
-                         ipcss:8,
-                         mss:16,
-                         hdrlen:8,
-                         rsv:2,
-                         sta:4,
-                         tucmd:8,
-                         dtype:4,
-                         paylen:20;
-         } __attribute__((packed));
-
-         union tx {
-            struct tx_desc data;
-            struct tx_context_desc ctx;
-         };
-   ]]
 
    function init_transmit ()
       regs[TCTL]        = 0x3103f0f8
