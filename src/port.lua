ba8c99d4b9f64fba17c1e096c15bc3dc897efebc
Luke Gorrie <luke@snabb.co>
2013-04-15 15:43:12 +0200

    port.lua: New high-level abstraction for a logical ethernet port.
    Ports are the layer above device drivers. Currently driver-independent selftest code is consolidated here. This is also where we can have code that feeds packets into switch objects.
    
    Early days!

---
+++ src/port.lua
@@ -0,0 +1,97 @@
+-- port.lua -- Ethernet port abstraction
+
+module(...,package.seeall)
+
+local memory = require("memory")
+
+-- Dictionary of ports that exist.
+ports = {}
+
+--- A port is a logical ethernet port.
+
+Port = {}
+
+function new (name, input, output, coroutine)
+   local o = {name = name, input = input, output = output,
+	      coroutine = coroutine, state = "start"}
+   setmetatable(o, {__index = Port,
+		    __tostring = Port.tostring})
+   ports[name] = o
+   return o
+end
+
+function Port:tostring () return "Port<"..self.name..">" end
+
+function Port:selftest ()
+   self:spam()
+end
+
+function Port:run (...)
+   if self.coroutine then
+      if self.coroutine(...) == nil then self.coroutine = nil end
+   end
+end
+
+--- Spamming is sending and receiving in a tight loop using only one
+--- packet buffer.
+
+function Port:spam ()
+   local input, output = self.input, self.output
+   -- Keep it simple: use one buffer for everything.
+   local buffer, len = memory.allocate()
+   repeat
+      input.sync_receive()
+      while input.can_receive() do
+	 input.receive()
+      end
+      while output.can_transmit() do
+	 output.transmit(buffer, len)
+      end
+      while input.can_add_receive_buffer() do
+	 input.add_receive_buffer(buffer, len)
+      end
+      while output.can_reclaim_buffer() do
+	 output.reclaim_buffer()
+      end
+      output.sync_transmit()
+   until coroutine.yield("spam") == nil
+   memory.free(buffer)
+end
+
+-- Echo receives packets and transmits the same packets back onto the
+-- network. The receive queue is only processed when the transmit
+-- queue has available space. Buffers are allocated and freed
+-- dynamically.
+
+function Port:echo ()
+   local input, output = self.input, self.output
+   repeat
+      self.input.sync_receive()
+      while input.can_receive() and output.can_transmit() do
+	 local addr, len = input.receive()
+	 output.transmit(addr, len)
+      end
+      while input.can_add_receive_buffer() do
+	 input.add_receive_buffer(memory.allocate())
+      end
+      while output.can_reclaim_buffer() do
+	 memory.free(output.reclaim_buffer())
+      end
+      output.sync_transmit()
+   until coroutine.yield("echo") == nil
+end
+
+function selftest (options)
+   print("selftest: port")
+   options = options or {}
+   local virtio = require("virtio").new("snabb%d")
+   virtio.init()
+   local port = port.new("test", virtio, virtio)
+   local program = options.program or Port.spam
+   port.coroutine = coroutine.wrap(program)
+   local finished = lib.timer((options.secs or 1) * 1e9)
+   repeat
+      port:run(port)
+   until finished()
+end
+
