afbcaf119e2998c2168f88ac7fafbc30d1f125d0
Justin Cormack <justin@specialbusservice.com>
2013-04-17 22:33:06 +0100

    add ucontext_t for x64

--- syscall/x64/headers.lua
+++ syscall/x64/headers.lua
@@ -34,5 +34,36 @@ struct statfs64 {
 ]]
 end
 
+arch.ucontext = function()
+ffi.cdef[[
+typedef long long greg_t, gregset_t[23];
+typedef struct _fpstate {
+  unsigned short cwd, swd, ftw, fop;
+  unsigned long long rip, rdp;
+  unsigned mxcsr, mxcr_mask;
+  struct {
+    unsigned short significand[4], exponent, padding[3];
+  } _st[8];
+  struct {
+    unsigned element[4];
+  } _xmm[16];
+  unsigned padding[24];
+} *fpregset_t;
+typedef struct {
+  gregset_t gregs;
+  fpregset_t fpregs;
+  unsigned long long __reserved1[8];
+} mcontext_t;
+typedef struct __ucontext {
+  unsigned long uc_flags;
+  struct __ucontext *uc_link;
+  stack_t uc_stack;
+  mcontext_t uc_mcontext;
+  sigset_t uc_sigmask;
+  unsigned long __fpregs_mem[64];
+} ucontext_t;
+]]
+end
+
 return arch
 
--- syscall/x86/headers.lua
+++ syscall/x86/headers.lua
@@ -8,24 +8,24 @@ arch.ucontext = function()
 ffi.cdef [[
 typedef int greg_t, gregset_t[19];
 typedef struct _fpstate {
-        unsigned long cw, sw, tag, ipoff, cssel, dataoff, datasel;
-        struct {
-                unsigned short significand[4], exponent;
-        } _st[8];
-        unsigned long status;
+  unsigned long cw, sw, tag, ipoff, cssel, dataoff, datasel;
+  struct {
+    unsigned short significand[4], exponent;
+  } _st[8];
+  unsigned long status;
 } *fpregset_t;
 typedef struct {
-        gregset_t gregs;
-        fpregset_t fpregs;
-        unsigned long oldmask, cr2;
+  gregset_t gregs;
+  fpregset_t fpregs;
+  unsigned long oldmask, cr2;
 } mcontext_t;
 typedef struct __ucontext {
-        unsigned long uc_flags;
-        struct __ucontext *uc_link;
-        stack_t uc_stack;
-        mcontext_t uc_mcontext;
-        sigset_t uc_sigmask;
-        unsigned long __fpregs_mem[28];
+  unsigned long uc_flags;
+  struct __ucontext *uc_link;
+  stack_t uc_stack;
+  mcontext_t uc_mcontext;
+  sigset_t uc_sigmask;
+  unsigned long __fpregs_mem[28];
 } ucontext_t;
 ]]
 end
