220b5b508b479c70ae3ec2252d09891c53c1d3c8
Justin Cormack <justin@specialbusservice.com>
2013-04-17 22:41:56 +0100

    add ucontext_t for ppc

--- syscall/ppc/headers.lua
+++ syscall/ppc/headers.lua
@@ -18,5 +18,27 @@ struct termio {
 ]]
 end
 
+arch.ucontext = function()
+ffi.cdef[[
+typedef unsigned long greg_t, gregset_t[48];
+typedef struct {
+  double fpregs[32];
+  double fpscr;
+  unsigned _pad[2];
+} fpregset_t;
+typedef struct {
+  unsigned vrregs[32][4];
+  unsigned vrsave;
+  unsigned _pad[2];
+  unsigned vscr;
+} vrregset_t;
+typedef struct {
+  gregset_t gregs;
+  fpregset_t fpregs;
+  vrregset_t vrregs __attribute__((__aligned__(16)));
+} mcontext_t;
+]]
+end
+
 return arch
 
