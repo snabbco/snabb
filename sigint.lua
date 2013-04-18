a74664c594fd4ab13925a91a8da7d9c2b2ccf26c
Justin Cormack <justin@specialbusservice.com>
2013-04-18 09:03:30 +0100

    change to sigpipe as sigint is handled by interpreter anyway

--- sigint.lua
+++ sigint.lua
@@ -1,4 +1,4 @@
--- example of a complex signal handler, in this case produce a Lua backtrace on sigint
+-- example of a complex signal handler, in this case produce a Lua backtrace on sigpipe
 
 local S = require "syscall"
 local t = S.t
@@ -10,16 +10,16 @@ if ffi.arch == "x86" then ip = c.REG.EIP
 elseif ffi.arch == "x64" then ip = c.REG.RIP
 else error "unsupported architecture" end
  
-local g = ffi.cast("void (*)(void)", function() error("sigint") end)
+local g = ffi.cast("void (*)(void)", function() error("sigpipe") end)
  
 local f = t.sa_sigaction(function(s, info, ucontext)
   ucontext.uc_mcontext.gregs[ip] = ffi.cast("intptr_t", ffi.cast("void *", g)) -- set instruction pointer to g
 end)
-assert(S.sigaction("int", {sigaction = f}))
+assert(S.sigaction("pipe", {sigaction = f}))
  
 -- example code to get interesting stack trace
 function bb(x)
-  assert(S.kill(S.getpid(), "int"))
+  assert(S.kill(S.getpid(), "pipe"))
   return x + 1
 end
  
