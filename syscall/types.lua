d9985ef60925fe0e2e2145e55a59b756f712a9ca
Justin Cormack <justin@specialbusservice.com>
2013-04-29 08:58:47 +0100

    cleanup

--- syscall/types.lua
+++ syscall/types.lua
@@ -1170,22 +1170,6 @@ end
 
 -- ip, udp types. Need endian conversions
 
---[[
-struct iphdr {
-  uint8_t  ihl:4,
-           version:4;
-  uint8_t  tos;
-  uint16_t tot_len;
-  uint16_t id;
-  uint16_t frag_off;
-  uint8_t  ttl;
-  uint8_t  protocol;
-  uint16_t check;
-  uint32_t saddr;
-  uint32_t daddr;
-};
-]]
-
 local function ip_checksum(buf, size, c, notfinal)
   c = c or 0
   local b8 = pt.char(buf)
