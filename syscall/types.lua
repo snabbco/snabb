adadc9ce6d81e01c5417bc3d6e20a00677e0a002
Justin Cormack <justin@specialbusservice.com>
2013-05-10 19:27:09 +0100

    inet_pton etc to generic code

--- syscall/types.lua
+++ syscall/types.lua
@@ -57,6 +57,50 @@ local function istype(tp, x)
   if ffi.istype(tp, x) then return x else return false end
 end
 
+-- convert strings to inet addresses and the reverse
+local function inet_ntop(af, src)
+  af = c.AF[af] -- TODO do not need, in fact could split into two functions if no need to export.
+  if af == c.AF.INET then
+    local b = pt.uchar(src)
+    return b[0] .. "." .. b[1] .. "." .. b[2] .. "." .. b[3]
+  end
+  if af ~= c.AF.INET6 then return end
+  local a = src.s6_addr
+  local parts = {256*a[0] + a[1], 256*a[2] + a[3],   256*a[4] + a[5],   256*a[6] + a[7],
+                 256*a[8] + a[9], 256*a[10] + a[11], 256*a[12] + a[13], 256*a[14] + a[15]}
+
+  for i = 1, #parts do parts[i] = string.format("%x", parts[i]) end
+
+  local start, max = 0, 0
+  for i = 1, #parts do
+    if parts[i] == "0" then
+      local count = 0
+      for j = i, #parts do
+        if parts[j] == "0" then count = count + 1 else break end
+      end
+      if count > max then max, start = count, i end
+    end
+  end
+
+  if max > 2 then
+    parts[start] = ""
+    if start == 1 or start + max == 9 then parts[start] = ":" end
+    if start == 1 and start + max == 9 then parts[start] = "::" end 
+    for i = 1, max - 1 do table.remove(parts, start + 1) end
+  end
+
+  return table.concat(parts, ":")
+end
+
+local function inet_pton(af, src, addr)
+  af = c.AF[af]
+  if not addr then addr = t.addrtype[af]() end
+  local ret = C.inet_pton(af, src, addr) -- TODO redo in pure Lua
+  if ret == -1 then return nil, t.error() end
+  if ret == 0 then return nil end -- maybe return string
+  return addr
+end
+
 -- generic types
 
 local voidp = ffi.typeof("void *")
@@ -93,6 +137,33 @@ local addstructs = {
 
 for k, v in pairs(addtypes) do addtype(k, v) end
 for k, v in pairs(addstructs) do addtype(k, v, lenmt) end
+
+-- TODO add generic address type that works out which to take? basically inet_name, except without netmask
+
+addtype("in_addr", "struct in_addr", {
+  __tostring = function(a) return inet_ntop(c.AF.INET, a) end,
+  __new = function(tp, s)
+    local addr = ffi.new(tp)
+    if s then
+      if ffi.istype(tp, s) then addr.s_addr = s.s_addr else addr = inet_pton(c.AF.INET, s, addr) end
+    end
+    return addr
+  end
+})
+
+addtype("in6_addr", "struct in6_addr", {
+  __tostring = function(a) return inet_ntop(c.AF.INET6, a) end,
+  __new = function(tp, s)
+    local addr = ffi.new(tp)
+    if s then addr = inet_pton(c.AF.INET6, s, addr) end
+    return addr
+  end
+})
+
+t.addrtype = {
+  [c.AF.INET] = t.in_addr,
+  [c.AF.INET6] = t.in6_addr,
+}
 
 -- include OS specific types
 local hh = {ptt = ptt, addtype = addtype, lenfn = lenfn, lenmt = lenmt, newfn = newfn, istype = istype}
