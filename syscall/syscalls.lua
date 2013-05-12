c4ed9f68bbd8bb139066c475e0f847fc64ec971c
Justin Cormack <justin@specialbusservice.com>
2013-05-12 16:45:29 +0100

    initial split out of fcntl into new file

--- syscall/syscalls.lua
+++ syscall/syscalls.lua
@@ -9,6 +9,7 @@ local C = require "syscall.c"
 local types = require "syscall.types"
 local abi = require "syscall.abi"
 local h = require "syscall.helpers"
+local fcntl = require "syscall.linux.fcntl" -- TODO not Linux obviously!
 
 local t, pt, s = types.t, types.pt, types.s
 
@@ -218,43 +219,6 @@ function S.getpgrp() return retnum(C.getpgrp()) end
 
 function S._exit(status) C._exit(c.EXIT[status]) end
 
--- TODO maybe in separate config file?
--- TODO change to type
-local function getflock(arg)
-  arg = arg or t.flock()
-  if not ffi.istype(t.flock, arg) then
-    for _, v in pairs {"type", "whence", "start", "len", "pid"} do -- allow use of short names
-      if arg[v] then
-        arg["l_" .. v] = arg[v] -- TODO cleanup this to use table?
-        arg[v] = nil
-      end
-    end
-    arg.l_type = c.FCNTL_LOCK[arg.l_type]
-    arg.l_whence = c.SEEK[arg.l_whence]
-    arg = t.flock(arg)
-  end
-  return arg
-end
-local fcntl = {
-  commands = {
-    [c.F.SETFL] = function(arg) return c.O[arg] end,
-    [c.F.SETFD] = function(arg) return c.FD[arg] end,
-    [c.F.GETLK] = getflock,
-    [c.F.SETLK] = getflock,
-    [c.F.SETLKW] = getflock,
-  },
-  ret = {
-    [c.F.DUPFD] = function(ret) return t.fd(ret) end,
-    [c.F.DUPFD_CLOEXEC] = function(ret) return t.fd(ret) end,
-    [c.F.GETFD] = function(ret) return tonumber(ret) end,
-    [c.F.GETFL] = function(ret) return tonumber(ret) end,
-    [c.F.GETLEASE] = function(ret) return tonumber(ret) end,
-    [c.F.GETOWN] = function(ret) return tonumber(ret) end,
-    [c.F.GETSIG] = function(ret) return tonumber(ret) end,
-    [c.F.GETPIPE_SZ] = function(ret) return tonumber(ret) end,
-    [c.F.GETLK] = function(ret, arg) return arg end,
-  }
-}
 function S.fcntl(fd, cmd, arg)
   cmd = c.F[cmd]
   if fcntl.commands[cmd] then arg = fcntl.commands[cmd](arg) end
