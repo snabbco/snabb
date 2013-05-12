b76bf5134d46a912c82f24345564f46aed85f96c
Justin Cormack <justin@specialbusservice.com>
2013-05-12 21:46:28 +0100

    bsd seems to mess up stat syscalls, so call directly

--- syscall/netbsd/c.lua
+++ syscall/netbsd/c.lua
@@ -1,5 +1,6 @@
 -- This sets up the table of C functions
 -- For BSD we hope we do not need many overrides
+-- however stat appears to be "fixed" up in libc to return incorrect results TODO why?
 
 local ffi = require "ffi"
 
@@ -8,9 +9,21 @@ local t, pt, s = types.t, types.pt, types.s
 
 local C = setmetatable({}, {__index = ffi.C})
 
--- SYS___stat50    439
+-- TODO could move to constants
+local stat50 = 439
+local fstat50 = 440
+local lstat50 = 441
+
 C.stat = function(path, buf)
-  return C.syscall(439, pt.void(path), pt.void(buf))
+  return C.syscall(stat50, pt.void(path), pt.void(buf))
+end
+
+C.fstat = function(fd, path, buf)
+  return C.syscall(fstat50, t.int(fd), pt.void(path), pt.void(buf))
+end
+
+C.lstat = function(path, buf)
+  return C.syscall(lstat50, pt.void(path), pt.void(buf))
 end
 
 return C
