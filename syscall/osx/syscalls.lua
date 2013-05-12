30d5d7f636e97a251876d25b1d1401ba87e63cb9
Justin Cormack <justin@specialbusservice.com>
2013-05-12 20:21:21 +0100

    mkfifo support for bsd, osx - missing includes

--- syscall/osx/syscalls.lua
+++ syscall/osx/syscalls.lua
@@ -7,6 +7,9 @@ local C = require "syscall.c"
 local types = require "syscall.types"
 local abi = require "syscall.abi"
 
+local istype, mktype, getfd = hh.istype, hh.mktype, hh.getfd
+local ret64, retnum, retfd, retbool, retptr = hh.ret64, hh.retnum, hh.retfd, hh.retbool, hh.retptr
+
 function S.exit(status) C.exit(c.EXIT[status]) end
 function S.mkfifo(pathname, mode) return retbool(C.mkfifo(pathname, c.S_I[mode])) end
 
