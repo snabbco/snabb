cb4f9f8dc9f0935642d7cc7de2038093e69c465d
Justin Cormack <justin@specialbusservice.com>
2013-05-14 22:24:45 +0100

    remove arch calls in ioctl

--- syscall/netbsd/ioctl.lua
+++ syscall/netbsd/ioctl.lua
@@ -9,8 +9,6 @@ local strflag = require("syscall.helpers").strflag
 local abi = require "syscall.abi"
 
 local ffi = require "ffi"
-local ok, arch = pcall(require, "linux." .. abi.arch .. ".ioctl") -- architecture specific definitions
-if not ok then arch = {} end
 
 local bit = require "bit"
 
