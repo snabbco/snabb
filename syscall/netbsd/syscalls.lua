642be3d0ee84fc10ff118dc67a9312c4fdcf0b2b
Justin Cormack <justin@specialbusservice.com>
2013-05-14 23:14:46 +0100

    netbsd needs t defined

--- syscall/netbsd/syscalls.lua
+++ syscall/netbsd/syscalls.lua
@@ -7,6 +7,8 @@ local C = require "syscall.c"
 local types = require "syscall.types"
 local abi = require "syscall.abi"
 
+local t, pt, s = types.t, types.pt, types.s
+
 local istype, mktype, getfd = hh.istype, hh.mktype, hh.getfd
 local ret64, retnum, retfd, retbool, retptr = hh.ret64, hh.retnum, hh.retfd, hh.retbool, hh.retptr
 
