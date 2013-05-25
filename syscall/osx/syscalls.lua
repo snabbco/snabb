0a48cfd8a2b547f7e3b605def9bc3efb79696415
Justin Cormack <justin@specialbusservice.com>
2013-05-25 13:01:52 +0100

    remove exit from osx syscalls

--- syscall/osx/syscalls.lua
+++ syscall/osx/syscalls.lua
@@ -1,11 +1,10 @@
--- BSD specific syscalls
+-- OSX specific syscalls
 
 return function(S, hh, abi, c, C, types, ioctl)
 
 local istype, mktype, getfd = hh.istype, hh.mktype, hh.getfd
 local ret64, retnum, retfd, retbool, retptr = hh.ret64, hh.retnum, hh.retfd, hh.retbool, hh.retptr
 
-function S.exit(status) C.exit(c.EXIT[status]) end
 function S.mkfifo(pathname, mode) return retbool(C.mkfifo(pathname, c.S_I[mode])) end
 
 return S
