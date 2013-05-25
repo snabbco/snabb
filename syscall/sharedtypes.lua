336dee523af5d0749ec38e992d18275d78c2f473
Justin Cormack <justin@specialbusservice.com>
2013-05-25 23:06:53 +0100

    split up inet_pton functions by addr type

--- syscall/sharedtypes.lua
+++ syscall/sharedtypes.lua
@@ -33,13 +33,12 @@ local function lenfn(tp) return ffi.sizeof(tp) end
 -- TODO add generic address type that works out which to take? basically inet_name, except without netmask
 
 -- convert strings to inet addresses and the reverse
-local function inet_ntop(af, src)
-  af = c.AF[af] -- TODO do not need, in fact could split into two functions if no need to export.
-  if af == c.AF.INET then
-    local b = pt.uchar(src)
-    return b[0] .. "." .. b[1] .. "." .. b[2] .. "." .. b[3]
-  end
-  if af ~= c.AF.INET6 then return end
+local function inet4_ntop(src)
+  local b = pt.uchar(src)
+  return b[0] .. "." .. b[1] .. "." .. b[2] .. "." .. b[3]
+end
+
+local function inet6_ntop(src)
   local a = src.s6_addr
   local parts = {256*a[0] + a[1], 256*a[2] + a[3],   256*a[4] + a[5],   256*a[6] + a[7],
                  256*a[8] + a[9], 256*a[10] + a[11], 256*a[12] + a[13], 256*a[14] + a[15]}
@@ -67,18 +66,17 @@ local function inet_ntop(af, src)
   return table.concat(parts, ":")
 end
 
-local function inet_pton(af, src, addr)
-  af = c.AF[af]
-  addr = addr or t.addrtype[af]()
-  if af == c.AF.INET then
-    local ip4 = split("%.", src)
-    if #ip4 ~= 4 then return nil end
-    addr = addr or t.in_addr()
-    addr.s_addr = ip4[4] * 0x1000000 + ip4[3] * 0x10000 + ip4[2] * 0x100 + ip4[1]
-    return addr
-  end
+local function inet4_pton(src, addr)
+  local ip4 = split("%.", src)
+  if #ip4 ~= 4 then return nil end
+  addr = addr or t.in_addr()
+  addr.s_addr = ip4[4] * 0x1000000 + ip4[3] * 0x10000 + ip4[2] * 0x100 + ip4[1]
+  return addr
+end
+
+local function inet6_pton(src, addr)
 -- TODO ipv6 implementation
-  local ret = ffi.C.inet_pton(af, src, addr) -- TODO redo in pure Lua
+  local ret = ffi.C.inet_pton(c.AF.INET6, src, addr) -- TODO redo in pure Lua
   if ret == -1 then return nil, t.error() end
   if ret == 0 then return nil end -- maybe return string
   return addr
@@ -96,7 +94,7 @@ local in6addr = strflag {
 }
 
 addtype("in_addr", "struct in_addr", {
-  __tostring = function(a) return inet_ntop(c.AF.INET, a) end,
+  __tostring = inet4_ntop,
   __new = function(tp, s)
     local addr = ffi.new(tp)
     if s then
@@ -104,7 +102,7 @@ addtype("in_addr", "struct in_addr", {
         addr.s_addr = s.s_addr
       else
         if inaddr[s] then s = inaddr[s] end
-        addr = inet_pton(c.AF.INET, s, addr)
+        addr = inet4_pton(s, addr)
         if not addr then return nil end
       end
     end
@@ -114,12 +112,12 @@ addtype("in_addr", "struct in_addr", {
 })
 
 addtype("in6_addr", "struct in6_addr", {
-  __tostring = function(a) return inet_ntop(c.AF.INET6, a) end,
+  __tostring = inet6_ntop,
   __new = function(tp, s)
     local addr = ffi.new(tp)
     if s then
       if in6addr[s] then s = in6addr[s] end
-      addr = inet_pton(c.AF.INET6, s, addr)
+      addr = inet6_pton(s, addr)
     end
     return addr
   end,
