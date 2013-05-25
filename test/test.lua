87c058945634e3b399752222408fa2e51967f599
Justin Cormack <justin@specialbusservice.com>
2013-05-25 13:06:24 +0100

    osx does not have nonblock flag on sockets, do not use in tests

--- test/test.lua
+++ test/test.lua
@@ -656,7 +656,8 @@ test_sockets_pipes = {
     assert(not sa, "expect nil socket address from invalid ip string")
   end,
   test_inet_socket = function() -- TODO break this test up
-    local s = assert(S.socket("inet", "stream, nonblock"))
+    local s = assert(S.socket("inet", "stream"))
+    assert(s:nonblock())
     local sa = assert(t.sockaddr_in(1234, "loopback"))
     assert(sa.sin_family == 2, "expect family on inet socket to be 2")
     -- find a free port
