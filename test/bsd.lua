28af363088592fd90ff9518b5d7b681b53ea692e
Justin Cormack <justin@specialbusservice.com>
2013-05-27 20:58:55 +0100

    fix tmpfs mount data

--- test/bsd.lua
+++ test/bsd.lua
@@ -62,7 +62,7 @@ test_mount_bsd_root = {
   test_mount_tmpfs = function()
     assert(S.mkdir(tmpfile))
     local data = {ta_version = 1, ta_nodes_max=100, ta_size_max=1048576, ta_root_mode=helpers.octal("0700")}
-    assert(S.mount{dir=tmpfile, type="tmpfs", data})
+    assert(S.mount{dir=tmpfile, type="tmpfs", data=data})
     assert(S.unmount(tmpfile))
     assert(S.rmdir(tmpfile))
   end,
