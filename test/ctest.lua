1b988af6d8802a5ab340e6d64ddaed3b1ed63a2a
Justin Cormack <justin@specialbusservice.com>
2013-06-05 19:13:19 +0100

    split out osx ctest

--- test/ctest.lua
+++ test/ctest.lua
@@ -1,8 +1,4 @@
 local abi = require "syscall.abi"
 
-local os = abi.os
-
-if os == "osx" or os == "netbsd" then os = "bsd" end -- use same tests for now
-
-require("test.ctest-" .. os)
+require("test.ctest-" .. abi.os)
 
