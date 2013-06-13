39594324ede3bdf67cbcabd1a64c489d0fe86925
Justin Cormack <justin@specialbusservice.com>
2013-06-13 23:43:43 +0100

    add DT flags to osx

--- syscall/osx/constants.lua
+++ syscall/osx/constants.lua
@@ -516,5 +516,17 @@ c.SHUT = strflag {
   RDWR = 2,
 }
 
+c.DT = strflag {
+  UNKNOWN = 0,
+  FIFO = 1,
+  CHR = 2,
+  DIR = 4,
+  BLK = 6,
+  REG = 8,
+  LNK = 10,
+  SOCK = 12,
+  WHT = 14,
+}
+
 return c
 
