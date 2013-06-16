88d1d34d2ee9345f25fc92dd76cfd793c73a20c2
Justin Cormack <justin@specialbusservice.com>
2013-06-16 22:33:46 +0100

    osx fixes

--- syscall/osx/ffifunctions.lua
+++ syscall/osx/ffifunctions.lua
@@ -5,10 +5,12 @@ local cdef = require "ffi".cdef
 cdef[[
 int lchmod(const char *path, mode_t mode);
 int fchroot(int fd);
+int utimes(const char *filename, const struct timeval times[2]);
+
+/* might just use syscalls */
 int stat64(const char *path, struct stat *sb);
 int lstat64(const char *path, struct stat *sb);
 int fstat64(int fd, struct stat *sb);
-int utimes(const char *filename, const struct timeval times[2]);
-int getdirentries(int fd, char *buf, int nbytes, long *basep);
+int getdirentries64(int fd, char *buf, int nbytes, long *basep);
 ]]
 
