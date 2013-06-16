d30c73e5f5617d60ee352bfc356f9716af620e84
Justin Cormack <justin@specialbusservice.com>
2013-06-16 22:58:55 +0100

    test if need syscall

--- syscall/osx/c.lua
+++ syscall/osx/c.lua
@@ -5,12 +5,20 @@ local function init(abi, c, types)
 
 local ffi = require "ffi"
 
+local t, pt, s = types.t, types.pt, types.s
+
 local C = setmetatable({}, {__index = ffi.C})
 
 -- new stat structure, else get legacy one; could use syscalls instead
 C.stat = C.stat64
 C.fstat = C.fstat64
 C.lstat = C.lstat64
+
+local getdirentries = 196
+
+function C.getdirentries(fd, buf, len, basep)
+  return C.syscall(getdirentries, t.int(fd), pt.void(buf), t.int(len), pt.void(basep))
+end
 
 return C
 
