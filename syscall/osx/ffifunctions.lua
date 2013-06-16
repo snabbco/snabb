609c6f6047b3afad3acf8540de04e099133115fb
Justin Cormack <justin@specialbusservice.com>
2013-06-16 22:51:55 +0100

    futimes decl for osx

--- syscall/osx/ffifunctions.lua
+++ syscall/osx/ffifunctions.lua
@@ -7,6 +7,7 @@ int lchmod(const char *path, mode_t mode);
 int fchroot(int fd);
 int utimes(const char *filename, const struct timeval times[2]);
 int getdirentries(int fd, char *buf, int nbytes, long *basep);
+int futimes(int, const struct timeval *);
 
 /* might just use syscalls */
 int stat64(const char *path, struct stat *sb);
