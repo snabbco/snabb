8112ae69b410f04374e9ec491a4cdc2b1580987a
Justin Cormack <justin@specialbusservice.com>
2013-07-01 22:41:31 +0100

    add ifcreate test

---
+++ test/bsd.lua
@@ -0,0 +1,95 @@
+-- BSD specific tests
+
+local S = require "syscall"
+local helpers = require "syscall.helpers"
+local types = S.types
+local c = S.c
+local abi = S.abi
+local features = require "syscall.features"
+local util = S.util
+
+local bit = require "bit"
+local ffi = require "ffi"
+
+local t, pt, s = types.t, types.pt, types.s
+
+local oldassert = assert
+local function assert(cond, s)
+  collectgarbage("collect") -- force gc, to test for bugs
+  return oldassert(cond, tostring(s)) -- annoyingly, assert does not call tostring!
+end
+
+local function fork_assert(cond, str) -- if we have forked we need to fail in main thread not fork
+  if not cond then
+    print(tostring(str))
+    print(debug.traceback())
+    S.exit("failure")
+  end
+  return cond, str
+end
+
+local function assert_equal(...)
+  collectgarbage("collect") -- force gc, to test for bugs
+  return assert_equals(...)
+end
+
+local teststring = "this is a test string"
+local size = 512
+local buf = t.buffer(size)
+local tmpfile = "XXXXYYYYZZZ4521" .. S.getpid()
+local tmpfile2 = "./666666DDDDDFFFF" .. S.getpid()
+local tmpfile3 = "MMMMMTTTTGGG" .. S.getpid()
+local longfile = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890" .. S.getpid()
+local efile = "./tmpexXXYYY" .. S.getpid() .. ".sh"
+local largeval = math.pow(2, 33) -- larger than 2^32 for testing
+local mqname = "ljsyscallXXYYZZ" .. S.getpid()
+
+local clean = function()
+  S.rmdir(tmpfile)
+  S.unlink(tmpfile)
+  S.unlink(tmpfile2)
+  S.unlink(tmpfile3)
+  S.unlink(longfile)
+  S.unlink(efile)
+end
+
+test_mount_bsd_root = {
+  test_mount_kernfs = function()
+    assert(S.mkdir(tmpfile))
+    assert(S.mount{dir=tmpfile, type="kernfs"})
+    assert(S.unmount(tmpfile))
+    assert(S.rmdir(tmpfile))
+  end,
+  test_mount_tmpfs = function()
+    assert(S.mkdir(tmpfile))
+    local data = {ta_version = 1, ta_nodes_max=100, ta_size_max=1048576, ta_root_mode=helpers.octal("0700")}
+    assert(S.mount{dir=tmpfile, type="tmpfs", data=data})
+    assert(S.unmount(tmpfile))
+    assert(S.rmdir(tmpfile))
+  end,
+}
+
+test_filesystem_bsd = {
+-- BSD utimensat as same specification as Linux, but some functionality missing, so test simpler
+  test_utimensat = function()
+    local fd = assert(S.creat(tmpfile, "RWXU"))
+    local dfd = assert(S.open("."))
+    assert(S.utimensat(nil, tmpfile))
+    local st1 = fd:stat()
+    assert(S.utimensat("fdcwd", tmpfile, {"omit", "omit"}))
+    local st2 = fd:stat()
+    assert(st1.atime == st2.atime and st1.mtime == st2.mtime, "atime and mtime unchanged")
+    assert(S.unlink(tmpfile))
+    assert(fd:close())
+    assert(dfd:close())
+  end,
+}
+
+test_network_utils_bsd_root = {
+  test_ifcreate = function()
+    local ifname = "lo99" .. tostring(S.getpid())
+    assert(util.ifcreate(ifname))
+    assert(util.ifdestroy(ifname))
+  end,
+}
+
