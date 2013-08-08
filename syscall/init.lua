edb8e78d52c7db7702fd47f13680b9fd23ba239e
Justin Cormack <justin@specialbusservice.com>
2013-08-08 21:59:58 +0100

    move ffitypes to shared

--- syscall/init.lua
+++ syscall/init.lua
@@ -11,7 +11,7 @@ local function init(abi)
 
 local ffi = require "ffi"
 
-require "syscall.ffitypes"
+require "syscall.shared.ffitypes"
 require("syscall." .. (abi.types or abi.os) .. ".ffitypes").init(abi)
 
 if not abi.rump then require "syscall.ffifunctions" end
