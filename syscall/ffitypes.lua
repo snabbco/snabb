f0e3acdbbe5694d8e433b9064a5f3b4c71f04429
Justin Cormack <justin@specialbusservice.com>
2013-08-08 22:06:59 +0100

    split string defintion of abi from ffitypes

---
+++ syscall/ffitypes.lua
@@ -0,0 +1,15 @@
+-- these are types which are currently the same for all ports
+-- in a module so rump does not import twice
+-- note that even if type is same (like pollfd) if the metatype is different cannot be here due to ffi
+
+local require, print, error, assert, tonumber, tostring,
+setmetatable, pairs, ipairs, unpack, rawget, rawset,
+pcall, type, table, string, math, bit = 
+require, print, error, assert, tonumber, tostring,
+setmetatable, pairs, ipairs, unpack, rawget, rawset,
+pcall, type, table, string, math, bit
+
+local ffi = require "ffi"
+
+ffi.cdef(require "syscall.shared.ffitypes")
+
