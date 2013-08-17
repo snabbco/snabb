a2060cedb4829e46d54950e8439ae582c5a16bb9
Luke Gorrie <luke@snabb.co>
2013-08-17 11:59:13 +0000

    link.lua: Added a real selftest() function.

--- src/link.h
+++ src/link.h
@@ -1,8 +1,10 @@
-enum { LINK_MAX_PACKETS = 10000 };
+enum { LINK_RING_SIZE         = 10001,
+       LINK_RING_MAX_PACKETS  = 10000
+};
 
 struct link {
   int head, tail;
-  struct packet* packets[LINK_MAX_PACKETS];
+  struct packet* packets[LINK_RING_SIZE];
   struct {
     uint64_t tx, rx, drop;
   } stats;
--- src/link.lua
+++ src/link.lua
@@ -1,12 +1,14 @@
 module(...,package.seeall)
 
+local packet = require("packet")
 local ffi = require("ffi")
 local C = ffi.C
 
 require("packet_h")
 require("link_h")
 
-max = C.LINK_MAX_PACKETS;
+size = C.LINK_RING_SIZE
+max  = C.LINK_RING_MAX_PACKETS
 
 function new ()
    return ffi.new("struct link")
@@ -15,8 +17,9 @@ end
 function receive (l)
    assert(not empty(l), "receive on empty link")
    local p = l.packets[l.tail]
-   l.tail = (l.tail + 1) % max
-   return l.packets[index]
+   l.tail = (l.tail + 1) % size
+   l.stats.rx = l.stats.rx + 1
+   return p
 end
 
 function transmit (l, p)
@@ -24,7 +27,8 @@ function transmit (l, p)
       l.stats.drop = l.stats.drop + 1
    else
       l.packets[l.head] = p
-      l.head = (l.head + 1) % max
+      l.head = (l.head + 1) % size
+      l.stats.tx = l.stats.tx + 1
    end
 end
 
@@ -33,14 +37,29 @@ function empty (l)
 end
 
 function full (l)
-   return (l.head + 1) % max == l.tail
+   return (l.head + 1) % size == l.tail
 end
 
 function selftest ()
    print("selftest: link")
    l = new()
-   assert(empty(l))
-   
-   print("link:", new())
+   p = packet.allocate()
+   assert(l.stats.tx == 0 and empty(l) == true  and full(l) == false)
+   transmit(l, p)
+   assert(l.stats.tx == 1 and empty(l) == false and full(l) == false)
+   for i = 1, max-2 do
+      transmit(l, p)
+   end
+   assert(l.stats.tx == max-1 and empty(l) == false and full(l) == false)
+   transmit(l, p)
+   assert(l.stats.tx == max   and empty(l) == false and full(l) == true)
+   transmit(l, p)
+   assert(l.stats.tx == max and l.stats.drop == 1)
+   assert(not empty(l) and full(l))
+   while not empty(l) do
+      receive(l)
+   end
+   assert(l.stats.rx == max)
+   print("selftest OK")
 end
 
