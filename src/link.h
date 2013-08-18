2fff300a0e6fa39c5dbfb07463655dc8283b114b
Luke Gorrie <luke@snabb.co>
2013-08-18 13:18:16 +0000

    link.h: Added link to ring buffer description on wikipedia.

--- src/link.h
+++ src/link.h
@@ -3,6 +3,8 @@ enum { LINK_RING_SIZE         = 10001,
 };
 
 struct link {
+  // link is a circular ring buffer, as described at:
+  //   http://en.wikipedia.org/wiki/Circular_buffer
   int head, tail;
   struct packet* packets[LINK_RING_SIZE];
   struct {
