71a856ec85777acaa93995bd66723c3cadf150f6
Luke Gorrie <luke@snabb.co>
2013-08-21 10:54:22 +0000

    packet.lua: Trivial numeric constant update.

--- src/packet.lua
+++ src/packet.lua
@@ -9,7 +9,7 @@ local freelist = require("freelist")
 require("packet_h")
 
 initial_fuel = 1000
-max_packets = 10e5
+max_packets = 1e6
 packets_fl = freelist.new("struct packet *", max_packets)
 packets    = ffi.new("struct packet[?]", max_packets)
 
