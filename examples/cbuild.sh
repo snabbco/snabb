d081e4223a500d4c0976df1110f43873c885aa93
Justin Cormack <justin@specialbusservice.com>
2013-09-08 18:14:12 +0100

    move bytecode script into cbuild

--- examples/cbuild.sh
+++ examples/cbuild.sh
@@ -1,21 +1,34 @@
 #!/bin/sh
 
-# fix as appropriate for your setup
-LIBDIR=/usr/local/lib
-INCDIR=/usr/local/include/luajit-2.0
+mkdir -p obj
+
+cd include/luajit-2.0 && make && cd ../..
+
+LIBDIR=include/luajit-2.0/src
+INCDIR=include/luajit-2.0/src
 
 # example of how to build a C executable
 
 [ ! -f syscall.lua ] && echo "This script is designed to be run from top level directory" && exit
 
 rm -f ./obj/cbuild
+rm -f ./obj/*.{o,a}
 
-./examples/bytecode.sh || ("Bytecode build failed" && exit)
+# bc.lua  bcsave.lua  dis_arm.lua  dis_mipsel.lua  dis_mips.lua  dis_ppc.lua  dis_x64.lua  dis_x86.lua  dump.lua  v.lua  vmdef.lua
+
+FILES=`find syscall.lua syscall -name '*.lua'`
+
+for f in $FILES
+do
+  NAME=`echo ${f} | sed 's/\.lua//'`
+  MODNAME=`echo ${NAME} | sed 's@/@.@g'`
+  luajit -b -t o -n ${MODNAME} ${f} obj/${MODNAME}.o
+done
 
 # we will link in hello world
 luajit -b -t o -n hello examples/hello.lua obj/hello.o
 
-cc -Wl,-E -o obj/cbuild -I ${INCDIR} examples/cstub.c obj/hello.o ${LIBDIR}/libluajit-5.1.a obj/syscall*.o -ldl -lm
+cc -Wl,-E -o obj/cbuild -I ${INCDIR} examples/cstub.c obj/hello.o ${LIBDIR}/libluajit.a obj/syscall*.o -ldl -lm
 
 ./obj/cbuild
 
--- examples/hello.lua
+++ examples/hello.lua
@@ -9,18 +9,5 @@ local stdout = S.stdout
 
 stdout:write("Hello world from " .. S.abi.os .. "\n")
 
--- this requires rump kernel included...
-
-local R = require "syscall.rump.init".init{"vfs", "fs.kernfs"}
-
-assert(R.mkdir("/kern", "0755"))
-assert(R.mount("kernfs", "/kern"))
-
-local fd = assert(R.open("/kern/version"))
-
-local str = assert(fd:read(nil, 1024))
-print("hello world from " .. str .. "\n")
-assert(fd:close())
-
 S.exit("success")
 
