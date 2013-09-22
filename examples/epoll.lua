35b860d711c19af86446fb948b42d951da5463b9
Justin Cormack <justin@specialbusservice.com>
2013-09-22 19:40:17 +0100

    starting to rework epoll example to add kqueue support

--- examples/epoll.lua
+++ examples/epoll.lua
@@ -1,13 +1,47 @@
 -- simple epoll-based socket example. Serves up http responses, but is of course not a proper server
--- you can test performance iwth ab -n 100000 -c 100 http://localhost:8000/ although ab may be the limiting factor
+-- you can test performance with ab -n 100000 -c 100 http://localhost:8000/ although ab may be the limiting factor
 
-local S = require "syscall"
+local S
+if arg[1] == "rump" then
+  S = require "syscall.rump.init".init{"net", "net.net", "net.local", "net.netinet"}
+else
+  S = require "syscall"
+end
 
 local t, c = S.t, S.c
 
 local oldassert = assert
 function assert(c, s)
-  return oldassert(c, tostring(s)) -- annoyingly, assert does not call tostring!
+  return oldassert(c, tostring(s))
+end
+
+local poll
+
+if S.epoll_create then
+  local maxevents = 1024
+  poll = {
+    init = function(this)
+      return setmetatable({fd = assert(S.epoll_create())}, {__index = this})
+    end,
+    event = t.epoll_event(),
+    add = function(this, s)
+      local event = this.event
+      event.events = c.EPOLL.IN
+      event.data.fd = s:getfd()
+      return this.fd:epoll_ctl("add", s, event)
+    end,
+    events = t.epoll_events(maxevents),
+    get = function(this)
+      return this.fd:epoll_wait(this.events, maxevents)
+    end,
+    eof = function(ev) return ev.HUP or ev.ERR end
+  }
+elseif S.kqueue then
+  poll = {
+
+  }
+else
+  error("no epoll or kqueue support")
 end
 
 local s = assert(S.socket("inet", "stream, nonblock"))
@@ -20,9 +54,9 @@ assert(s:bind(sa))
 
 assert(s:listen(128))
 
-local ep = assert(S.epoll_create())
+ep = poll:init()
 
-assert(ep:epoll_ctl("add", s, "in"))
+assert(ep:add(s))
 
 local w = {}
 
@@ -46,8 +80,6 @@ local reply = table.concat({
 "",
 }, "\r\n") .. msg
 
-local maxevents = 1024
-local events = t.epoll_events(maxevents)
 
 local bufsize = 4096
 local buffer = t.buffer(bufsize)
@@ -55,39 +87,33 @@ local buffer = t.buffer(bufsize)
 local ss = t.sockaddr_storage()
 local addrlen = t.socklen1(#ss)
 
-local event = t.epoll_event()
-
 while true do
 
-local r = assert(ep:epoll_wait(events, maxevents))
+local r = ep:get()
 
 for i = 1, #r do
   local ev = r[i]
+
+  if ep.eof(ev) then
+    fd:close()
+    w[ev.fileno] = nil
+  end
+
   if ev.fd == s.filenum then -- server socket, accept
     repeat
       local a, err = s:accept("nonblock", ss, addrlen)
       if a then
-        event.events = c.EPOLL.IN
-        event.data.fd = a.fd:getfd()
-        assert(ep:epoll_ctl("add", a.fd, event))
+        assert(ep:add(a.fd))
         w[a.fd:getfd()] = a.fd
       end
     until not a
   else
     local fd = w[ev.fd]
-    if ev.HUP or ev.ERR then -- closed or error
-      fd:close()
-      w[ev.fileno] = nil
-    else
-      if ev.IN then
-        local n
-        fd:read(buffer, bufsize)
-        n = fd:write(reply)
-        assert(n == #reply)
-        assert(fd:close())
-        w[ev.fd] = nil
-      end
-    end
+    fd:read(buffer, bufsize)
+    local n = fd:write(reply)
+    assert(n == #reply)
+    assert(fd:close())
+    w[ev.fd] = nil
   end
 end
 
