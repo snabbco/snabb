7234e8154a88aaf8e6c46d1881a6b13b14d845d8
Justin Cormack <justin@specialbusservice.com>
2013-10-12 11:47:18 +0100

    abi.lua minor cleanups

--- syscall/abi.lua
+++ syscall/abi.lua
@@ -11,7 +11,7 @@ pcall, type, table, string, math
 local ffi = require "ffi"
 
 local abi = {
-  arch = ffi.arch, -- ppc, x86, arm, x64
+  arch = ffi.arch, -- ppc, x86, arm, x64, mips
   abi32 = ffi.abi("32bit"), -- boolean
   abi64 = ffi.abi("64bit"), -- boolean
   le = ffi.abi("le"), -- boolean
@@ -20,18 +20,19 @@ local abi = {
   os = ffi.os:lower(), -- bsd, osx, linux
 }
 
+-- Makes no difference to us I believe
+if abi.arch == "ppcspe" then abi.arch = "ppc" end
+
 -- At the moment we only support NetBSD but do not attempt to detect it
 -- If you want to support eg FreeBSD then will have to detect it
 
 if abi.os == "bsd" then abi.os = "netbsd" end
 
--- Makes no difference to us I believe
-if abi.arch == "ppcspe" then abi.arch = "ppc" end
-
 if abi.os == "netbsd" then -- we default to version 6, as stable target; you can monkeypatch to 7 which is also supported
   abi.netbsd = {version = 6}
 end
 
+-- perhaps this should be in test suite not here
 local function inlibc_fn(k) return ffi.C[k] end
 
 ffi.cdef[[
