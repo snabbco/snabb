1068dab726c0a8fc47033618edafb90f9cb4e41a
Justin Cormack <justin@specialbusservice.com>
2013-12-15 14:40:59 +0000

    fix netbsd rump init

--- syscall/rump/init.lua
+++ syscall/rump/init.lua
@@ -79,7 +79,7 @@ end
 if abi.host == "netbsd" and abi.types == "netbsd" then -- running native (NetBSD on NetBSD)
   local SS = require "syscall"
   local C = require "syscall.rump.c"
-  S = require "syscall.syscalls".init(SS.c, C, SS.types, SS.c.IOCTL, SS.__fcntl)
+  S = require "syscall.syscalls".init(C, SS.types, SS.c.IOCTL, SS.__fcntl)
   S.abi, S.c, S.types, S.t = abi, SS.c, SS.types, SS.types.t
   S = require "syscall.compat".init(S)
   S = require "syscall.methods".init(S)
