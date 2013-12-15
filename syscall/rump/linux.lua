07b3450dec0ca75f001e3c999c92dfbd2c949588
Justin Cormack <justin@specialbusservice.com>
2013-12-15 22:54:55 +0000

    simplify rump/linux.lua

--- syscall/rump/linux.lua
+++ syscall/rump/linux.lua
@@ -11,29 +11,7 @@ pcall, type, table, string
 
 local function init(types, c)
 
-local ffi = require "ffi"
-
-ffi.cdef [[
-typedef uint32_t _netbsd_mode_t;
-typedef uint64_t _netbsd_ino_t;
-struct _netbsd_ufs_args {
-  char *fspec;
-};
-struct _netbsd_tmpfs_args {
-  int ta_version;
-  _netbsd_ino_t ta_nodes_max;
-  off_t ta_size_max;
-  uid_t ta_root_uid;
-  gid_t ta_root_gid;
-  _netbsd_mode_t ta_root_mode;
-};
-struct _netbsd_ptyfs_args {
-  int version;
-  gid_t gid;
-  _netbsd_mode_t mode;
-  int flags;
-};
-]]
+require "syscall.netbsd.ffitypes"
 
 local h = require "syscall.helpers"
 
