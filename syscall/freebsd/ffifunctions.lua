13cc19e931770d3b46211fca4b8c609de5121375
Justin Cormack <justin@specialbusservice.com>
2013-12-17 12:20:50 +0000

    more freebsd stubs

---
+++ syscall/freebsd/ffifunctions.lua
@@ -0,0 +1,15 @@
+-- define system calls for ffi, FreeBSD specific calls
+
+local require, error, assert, tonumber, tostring,
+setmetatable, pairs, ipairs, unpack, rawget, rawset,
+pcall, type, table, string = 
+require, error, assert, tonumber, tostring,
+setmetatable, pairs, ipairs, unpack, rawget, rawset,
+pcall, type, table, string
+
+local cdef = require "ffi".cdef
+
+cdef[[
+int ioctl(int d, unsigned long request, void *arg);
+]]
+
