541b1165822af5bda51270b1b2a34ff628682a09
Justin Cormack <justin@specialbusservice.com>
2013-12-17 11:29:25 +0000

    osx duplicated definition

--- syscall/osx/ffitypes.lua
+++ syscall/osx/ffitypes.lua
@@ -150,40 +150,6 @@ struct dirent {
   char      d_name[1024];
 };
 ]]
-
--- endian dependent TODO not really, define in independent way
-if abi.le then
-append [[
-struct iphdr {
-  uint8_t  ihl:4,
-           version:4;
-  uint8_t  tos;
-  uint16_t tot_len;
-  uint16_t id;
-  uint16_t frag_off;
-  uint8_t  ttl;
-  uint8_t  protocol;
-  uint16_t check;
-  uint32_t saddr;
-  uint32_t daddr;
-};
-]]
-else
-append [[
-struct iphdr {
-  uint8_t  version:4,
-           ihl:4;
-  uint8_t  tos;
-  uint16_t tot_len;
-  uint16_t id;
-  uint16_t frag_off;
-  uint8_t  ttl;
-  uint8_t  protocol;
-  uint16_t check;
-  uint32_t saddr;
-  uint32_t daddr;
-};
-]]
 end
 
 local ffi = require "ffi"
