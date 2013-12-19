3e104a138e8b54af9182ef8234bab24d3de68060
Justin Cormack <justin@specialbusservice.com>
2013-12-19 22:23:19 +0000

    add some constants for freebsd

--- syscall/freebsd/constants.lua
+++ syscall/freebsd/constants.lua
@@ -129,5 +129,127 @@ c.E = strflag {
   OWNERDEAD     = 96,
 }
 
+-- alternate names
+c.E.WOULDBLOCK    = c.E.AGAIN
+
+c.AF = strflag {
+  UNSPEC      = 0,
+  LOCAL       = 1,
+  INET        = 2,
+  IMPLINK     = 3,
+  PUP         = 4,
+  CHAOS       = 5,
+  NS          = 6,
+  ISO         = 7,
+  ECMA        = 8,
+  DATAKIT     = 9,
+  CCITT       = 10,
+  SNA         = 11,
+  DECNET      = 12,
+  DLI         = 13,
+  LAT         = 14,
+  HYLINK      = 15,
+  APPLETALK   = 16,
+  ROUTE       = 17,
+  LINK        = 18,
+-- #define pseudo_AF_XTP   19
+  COIP        = 20,
+  CNT         = 21,
+-- #define pseudo_AF_RTIP  22
+  IPX         = 23,
+  SIP         = 24,
+-- pseudo_AF_PIP   25
+  ISDN        = 26,
+-- pseudo_AF_KEY   27
+  AF_INET6    = 28,
+  NATM        = 29,
+  ATM         = 30,
+-- pseudo_AF_HDRCMPLT 31
+  NETGRAPH    = 32,
+  SLOW        = 33,
+  SCLUSTER    = 34,
+  ARP         = 35,
+  BLUETOOTH   = 36,
+  IEEE80211   = 37,
+  INET_SDP    = 40,
+  INET6_SDP   = 42,
+}
+
+c.O = multiflags {
+  RDONLY      = 0x0000,
+  WRONLY      = 0x0001,
+  RDWR        = 0x0002,
+  ACCMODE     = 0x0003,
+  NONBLOCK    = 0x0004,
+  APPEND      = 0x0008,
+  SHLOCK      = 0x0010,
+  EXLOCK      = 0x0020,
+  ASYNC       = 0x0040,
+  FSYNC       = 0x0080,
+  SYNC        = 0x0080,
+  NOFOLLOW    = 0x0100,
+  CREAT       = 0x0200,
+  TRUNC       = 0x0400,
+  EXCL        = 0x0800,
+  NOCTTY      = 0x8000,
+  DIRECT      = 0x00010000,
+  DIRECTORY   = 0x00020000,
+  EXEC        = 0x00040000,
+  TTY_INIT    = 0x00080000,
+  CLOEXEC     = 0x00100000,
+}
+
+c.EXIT = strflag {
+  SUCCESS = 0,
+  FAILURE = 1,
+}
+
+c.OK = charflags {
+  F = 0,
+  X = 0x01,
+  W = 0x02,
+  R = 0x04,
+}
+
+c.MODE = modeflags {
+  SUID = octal('04000'),
+  SGID = octal('02000'),
+  STXT = octal('01000'),
+  RWXU = octal('00700'),
+  RUSR = octal('00400'),
+  WUSR = octal('00200'),
+  XUSR = octal('00100'),
+  RWXG = octal('00070'),
+  RGRP = octal('00040'),
+  WGRP = octal('00020'),
+  XGRP = octal('00010'),
+  RWXO = octal('00007'),
+  ROTH = octal('00004'),
+  WOTH = octal('00002'),
+  XOTH = octal('00001'),
+}
+
+c.SEEK = strflag {
+  SET  = 0,
+  CUR  = 1,
+  END  = 2,
+  DATA = 3,
+  HOLE = 4,
+}
+
+c.SOCK = multiflags {
+  STREAM    = 1,
+  DGRAM     = 2,
+  RAW       = 3,
+  RDM       = 4,
+  SEQPACKET = 5,
+  CLOEXEC   = 0x10000000,
+  NONBLOCK  = 0x20000000,
+}
+
+c.SOL = strflag {
+  SOCKET    = 0xffff,
+}
+
 return c
 
