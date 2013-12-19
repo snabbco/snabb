c205097ed393e46091158303cb41386b8004b1f8
Justin Cormack <justin@specialbusservice.com>
2013-12-19 21:19:35 +0000

    freebsd types

--- syscall/freebsd/ffitypes.lua
+++ syscall/freebsd/ffitypes.lua
@@ -61,6 +61,39 @@ typedef struct fd_set {
 typedef struct __sigset {
   uint32_t __bits[4];
 } sigset_t;
+
+struct sockaddr {
+  uint8_t       sa_len;
+  sa_family_t   sa_family;
+  char          sa_data[14];
+};
+struct sockaddr_storage {
+  uint8_t       ss_len;
+  sa_family_t   ss_family;
+  char          __ss_pad1[6];
+  int64_t       __ss_align;
+  char          __ss_pad2[128 - 2 - 8 - 6];
+};
+struct sockaddr_in {
+  uint8_t         sin_len;
+  sa_family_t     sin_family;
+  in_port_t       sin_port;
+  struct in_addr  sin_addr;
+  int8_t          sin_zero[8];
+};
+struct sockaddr_in6 {
+  uint8_t         sin6_len;
+  sa_family_t     sin6_family;
+  in_port_t       sin6_port;
+  uint32_t        sin6_flowinfo;
+  struct in6_addr sin6_addr;
+  uint32_t        sin6_scope_id;
+};
+struct sockaddr_un {
+  uint8_t         sun_len;
+  sa_family_t     sun_family;
+  char            sun_path[104];
+};
 ]]
 
 local s = table.concat(defs, "")
