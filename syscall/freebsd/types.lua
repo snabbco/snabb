5ff5f13372c8b3189c137841feb40c9ecc04dff0
Justin Cormack <justin@specialbusservice.com>
2013-12-19 21:02:11 +0000

    stub freebsd types file

---
+++ syscall/freebsd/types.lua
@@ -0,0 +1,35 @@
+-- FreeBSD types
+
+local require, error, assert, tonumber, tostring,
+setmetatable, pairs, ipairs, unpack, rawget, rawset,
+pcall, type, table, string = 
+require, error, assert, tonumber, tostring,
+setmetatable, pairs, ipairs, unpack, rawget, rawset,
+pcall, type, table, string
+
+local function init(types)
+
+local abi = require "syscall.abi"
+
+local t, pt, s, ctypes = types.t, types.pt, types.s, types.ctypes
+
+local ffi = require "ffi"
+local bit = require "syscall.bit"
+
+local h = require "syscall.helpers"
+
+local addtype, addtype_var, addtype_fn, addraw2 = h.addtype, h.addtype_var, h.addtype_fn, h.addraw2
+local ptt, reviter, mktype, istype, lenfn, lenmt, getfd, newfn
+  = h.ptt, h.reviter, h.mktype, h.istype, h.lenfn, h.lenmt, h.getfd, h.newfn
+local ntohl, ntohl, ntohs, htons = h.ntohl, h.ntohl, h.ntohs, h.htons
+
+local c = require "syscall.freebsd.constants"
+
+local mt = {} -- metatables
+
+return types
+
+end
+
+return {init = init}
+
