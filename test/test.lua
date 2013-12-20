414ba257f4d1d818ecbf2f885ecffc60f5f008d9
Justin Cormack <justin@specialbusservice.com>
2013-12-20 00:00:46 +0000

    split out fdatasync - not available everywhere

--- test/test.lua
+++ test/test.lua
@@ -622,8 +622,14 @@ test_file_operations = {
   test_sync = function()
     local fd = assert(S.creat(tmpfile, "RWXU"))
     assert(fd:fsync())
-    assert(fd:fdatasync())
     assert(fd:sync()) -- synonym
+    assert(S.unlink(tmpfile))
+    assert(fd:close())
+  end,
+  test_fdatasync = function()
+    if not S.fdatasync then error "skipped" end
+    local fd = assert(S.creat(tmpfile, "RWXU"))
+    assert(fd:fdatasync())
     assert(fd:datasync()) -- synonym
     assert(S.unlink(tmpfile))
     assert(fd:close())
