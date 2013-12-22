ec58952f32ea67bd37bb8aec7c5048b8f9636846
Justin Cormack <justin@specialbusservice.com>
2013-12-22 21:19:09 +0000

    add freebsd rusage

--- syscall/freebsd/ffitypes.lua
+++ syscall/freebsd/ffitypes.lua
@@ -142,6 +142,24 @@ struct stat {
   unsigned int :(8 / 2) * (16 - (int)sizeof(struct timespec));
   unsigned int :(8 / 2) * (16 - (int)sizeof(struct timespec));
 };
+struct rusage {
+  struct timeval ru_utime;
+  struct timeval ru_stime;
+  long    ru_maxrss;
+  long    ru_ixrss;
+  long    ru_idrss;
+  long    ru_isrss;
+  long    ru_minflt;
+  long    ru_majflt;
+  long    ru_nswap;
+  long    ru_inblock;
+  long    ru_oublock;
+  long    ru_msgsnd;
+  long    ru_msgrcv;
+  long    ru_nsignals;
+  long    ru_nvcsw;
+  long    ru_nivcsw;
+};
 ]]
 
 local s = table.concat(defs, "")
