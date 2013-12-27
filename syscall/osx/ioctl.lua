7e4024cc16521bee44927018f1691660c8661027
Justin Cormack <justin@specialbusservice.com>
2013-12-27 18:15:31 +0000

    missing osx ioctl

--- syscall/osx/ioctl.lua
+++ syscall/osx/ioctl.lua
@@ -90,6 +90,7 @@ local ioctl = strflag {
   TIOCEXT        = _IOW('t', 96, "int"),
   TIOCSCTTY      =  _IO('t', 97),
   TIOCCONS       = _IOW('t', 98, "int"),
+  TIOCGSID	 = _IOR('t', 99, "int"),
   TIOCSTAT       =  _IO('t', 101),
   TIOCUCNTL      = _IOW('t', 102, "int"),
   TIOCSWINSZ     = _IOW('t', 103, "winsize"),
