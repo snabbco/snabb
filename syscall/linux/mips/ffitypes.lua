5f99dcfc82aeb2b57aaa377c50db6c8f4ea37e1d
Justin Cormack <justin@specialbusservice.com>
2013-12-30 18:10:16 +0000

    rename unused mips stat members see #12

--- syscall/linux/mips/ffitypes.lua
+++ syscall/linux/mips/ffitypes.lua
@@ -29,14 +29,14 @@ typedef struct __ucontext {
   stat = [[
 struct stat {
   unsigned long   st_dev;
-  unsigned long   st_pad0[3];
+  unsigned long   __st_pad0[3];
   unsigned long long      st_ino;
   mode_t          st_mode;
   nlink_t         st_nlink;
   uid_t           st_uid;
   gid_t           st_gid;
   unsigned long   st_rdev;
-  unsigned long   st_pad1[3];
+  unsigned long   __st_pad1[3];
   long long       st_size;
   time_t          st_atime;
   unsigned long   st_atime_nsec;
@@ -45,7 +45,7 @@ struct stat {
   time_t          st_ctime;
   unsigned long   st_ctime_nsec;
   unsigned long   st_blksize;
-  unsigned long   st_pad2;
+  unsigned long   __st_pad2;
   long long       st_blocks;
 };
 ]],
