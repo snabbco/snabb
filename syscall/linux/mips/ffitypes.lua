1b6d71ada1769b51b9ee4a0a808a06d4f34c5fd1
Justin Cormack <justin@specialbusservice.com>
2013-12-30 17:31:59 +0000

    fix mips stat

--- syscall/linux/mips/ffitypes.lua
+++ syscall/linux/mips/ffitypes.lua
@@ -25,27 +25,27 @@ typedef struct __ucontext {
   unsigned long uc_regspace[128];
 } ucontext_t;
 ]],
-  stat = [[
+  stat = [[ -- note this is struct stat64
 struct stat {
-  unsigned long long      st_dev;
-  unsigned char   __pad0[4];
-  unsigned long   __st_ino;
-  unsigned int    st_mode;
-  unsigned int    st_nlink;
-  unsigned long   st_uid;
-  unsigned long   st_gid;
-  unsigned long long      st_rdev;
-  unsigned char   __pad3[4];
-  long long       st_size;
-  unsigned long   st_blksize;
-  unsigned long long      st_blocks;
-  unsigned long   st_atime;
-  unsigned long   st_atime_nsec;
-  unsigned long   st_mtime;
-  unsigned int    st_mtime_nsec;
-  unsigned long   st_ctime;
-  unsigned long   st_ctime_nsec;
+  unsigned long   st_dev;
+  unsigned long   st_pad0[3];
   unsigned long long      st_ino;
+  mode_t          st_mode;
+  nlink_t         st_nlink;
+  uid_t           st_uid;
+  gid_t           st_gid;
+  unsigned long   st_rdev;
+  unsigned long   st_pad1[3];
+  long long       st_size;
+  time_t          st_atime;
+  unsigned long   st_atime_nsec;
+  time_t          st_mtime;
+  unsigned long   st_mtime_nsec;
+  time_t    st_ctime;
+  unsigned long   st_ctime_nsec;
+  unsigned long   st_blksize;
+  unsigned long   st_pad2;
+  long long       st_blocks;
 };
 ]],
 }
