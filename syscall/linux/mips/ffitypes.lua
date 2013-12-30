e6cca61a6383061d65df74b3da7667a57366e238
Justin Cormack <justin@specialbusservice.com>
2013-12-30 17:49:34 +0000

    use musl stat definition for mips. its confusing, and maybe it differs between o32 and n32 or maybe it does just for glibc?

--- syscall/linux/mips/ffitypes.lua
+++ syscall/linux/mips/ffitypes.lua
@@ -28,25 +28,23 @@ typedef struct __ucontext {
   -- note this is struct stat64
   stat = [[
 struct stat {
-  unsigned long   st_dev;
-  unsigned long   st_pad0[3];
-  unsigned long long      st_ino;
-  mode_t          st_mode;
-  nlink_t         st_nlink;
-  uid_t           st_uid;
-  gid_t           st_gid;
-  unsigned long   st_rdev;
-  unsigned long   st_pad1[3];
-  long long       st_size;
-  time_t          st_atime;
-  unsigned long   st_atime_nsec;
-  time_t          st_mtime;
-  unsigned long   st_mtime_nsec;
-  time_t          st_ctime;
-  unsigned long   st_ctime_nsec;
-  unsigned long   st_blksize;
-  unsigned long   st_pad2;
-  long long       st_blocks;
+  dev_t st_dev;
+  long __st_padding1[2];
+  ino_t st_ino;
+  mode_t st_mode;
+  nlink_t st_nlink;
+  uid_t st_uid;
+  gid_t st_gid;
+  dev_t st_rdev;
+  long __st_padding2[2];
+  off_t st_size;
+  struct timespec st_atim;
+  struct timespec st_mtim;
+  struct timespec st_ctim;
+  blksize_t st_blksize;
+  long __st_padding3;
+  blkcnt_t st_blocks;
+  long __st_padding4[14];
 };
 ]],
 }
