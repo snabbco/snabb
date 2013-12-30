cb5421272e0dd3748127d2fa9fc8bfe7153ee9e6
Justin Cormack <justin@specialbusservice.com>
2013-12-30 21:53:05 +0000

    revert to 32 bit dev_t see #12

--- syscall/linux/mips/ffitypes.lua
+++ syscall/linux/mips/ffitypes.lua
@@ -28,10 +28,8 @@ typedef struct __ucontext {
   -- note this is struct stat64
   stat = [[
 struct stat {
-  //unsigned long   st_dev;
-  //unsigned long   __st_pad0[3];
-  int64_t st_dev;
-  long __st_padding1[2];
+  unsigned long   st_dev;
+  unsigned long   __st_pad0[3];
   unsigned long long      st_ino;
   mode_t          st_mode;
   nlink_t         st_nlink;
