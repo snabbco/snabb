20fb0ba92a6946203e4b711a97f80e0a36cf6ce9
Justin Cormack <justin@specialbusservice.com>
2014-01-01 14:15:05 +0000

    add hopefully correct statfs64 for mips

--- syscall/linux/mips/ffitypes.lua
+++ syscall/linux/mips/ffitypes.lua
@@ -50,29 +50,21 @@ struct stat {
   long __st_padding4[14];
 };
 ]],
-}
-
---[[ -- this is what Musl uses, I think it is the n32 stat?
-
-struct stat {
-  dev_t st_dev;
-  long __st_padding1[2];
-  ino_t st_ino;
-  mode_t st_mode;
-  nlink_t st_nlink;
-  uid_t st_uid;
-  gid_t st_gid;
-  dev_t st_rdev;
-  long __st_padding2[2];
-  off_t st_size;
-  struct timespec st_atim;
-  struct timespec st_mtim;
-  struct timespec st_ctim;
-  blksize_t st_blksize;
-  long __st_padding3;
-  blkcnt_t st_blocks;
-  long __st_padding4[14];
+  statfs = [[
+struct statfs64 {
+  uint32_t   f_type;
+  uint32_t   f_bsize;
+  uint32_t   f_frsize;
+  uint32_t   __pad;
+  uint64_t   f_blocks;
+  uint64_t   f_bfree;
+  uint64_t   f_files;
+  uint64_t   f_ffree;
+  uint64_t   f_bavail;
+  kernel_fsid_t f_fsid;
+  uint32_t   f_namelen;
+  uint32_t   f_spare[6];
 };
-]]
-
+]],
+}
 
