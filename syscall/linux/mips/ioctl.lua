261aa97f9548a0004dbb23fe853d2feb1281fb23
Justin Cormack <justin@specialbusservice.com>
2014-01-28 08:32:58 +0000

    add more generic socket IOs - MIPS

--- syscall/linux/mips/ioctl.lua
+++ syscall/linux/mips/ioctl.lua
@@ -3,8 +3,15 @@
 return function(s)
 
 local arch = {
-  ioctl = {
-  }
+  ioctl = function(_IO, _IOR, _IOW, _IORW)
+    return {
+      FIOGETOWN       _IOR('f', 123, "int"),
+      FIOSETOWN       _IOW('f', 124, "int"),
+      SIOCATMARK      _IOR('s', 7, "int"),
+      SIOCSPGRP       _IOW('s', 8, "pid"),
+      SIOCGPGRP       _IOR('s', 9, "pid"),
+    }
+  end,
 }
 
 return arch
