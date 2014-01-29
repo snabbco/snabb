97c8e07ff44b3d5ec2717c3da6c503867c567ac4
Justin Cormack <justin@specialbusservice.com>
2014-01-29 23:07:30 +0000

    typos in ioctl

--- syscall/linux/mips/ioctl.lua
+++ syscall/linux/mips/ioctl.lua
@@ -5,11 +5,11 @@ return function(s)
 local arch = {
   ioctl = function(_IO, _IOR, _IOW, _IORW)
     return {
-      FIOGETOWN       _IOR('f', 123, "int"),
-      FIOSETOWN       _IOW('f', 124, "int"),
-      SIOCATMARK      _IOR('s', 7, "int"),
-      SIOCSPGRP       _IOW('s', 8, "pid"),
-      SIOCGPGRP       _IOR('s', 9, "pid"),
+      FIOGETOWN      = _IOR('f', 123, "int"),
+      FIOSETOWN      = _IOW('f', 124, "int"),
+      SIOCATMARK     = _IOR('s', 7, "int"),
+      SIOCSPGRP      = _IOW('s', 8, "pid"),
+      SIOCGPGRP      = _IOR('s', 9, "pid"),
     }
   end,
 }
