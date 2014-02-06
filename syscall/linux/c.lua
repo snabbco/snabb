4516db707b49b4e1ce30e2ecce2c026c7483c3f3
Justin Cormack <justin@specialbusservice.com>
2014-02-06 00:26:46 +0000

    more Linux syscall conversions

--- syscall/linux/c.lua
+++ syscall/linux/c.lua
@@ -533,6 +533,8 @@ function C.lremovexattr(path, name) return syscall(sys.lremovexattr, void(path),
 function C.fremovexattr(fd, name) return syscall(sys.fremovexattr, int(fd), void(name)) end
 function C.inotify_add_watch(fd, pathname, mask) return syscall(sys.inotify_add_watch, int(fd), void(pathname), uint(mask)) end
 function C.inotify_rm_watch(fd, wd) return syscall(sys.inotify_rm_watch, int(fd), int(wd)) end
+function C.unshare(flags) return syscall(sys.unshare, int(flags)) end
+
 
 -- defined in libc as a pair of longs, but lets by typed
 local pst = ffi.typeof("struct {void *sigmask; long size;}")
@@ -604,7 +606,7 @@ C.recvmsg = ffi.C.recvmsg
 -- sendmmsg missing
 
 -- these should be converted to syscalls
-local extra = {"waitid", "waitpid", "capget", "readahead", "munmap", "sched_yield", "poll", "sched_get_priority_min", "sched_get_priority_max", "sched_rr_get_interval", "mremap", "getgroups", "fcntl", "sysinfo", "klogctl", "msync", "madvise", "mlock", "munlock", "mlockall", "munlockall", "sigprocmask", "getitimer", "alarm", "setpriority", "wait4", "setitimer", "execve", "sigpending", "faccessat", "fchmodat", "mkdirat", "unlinkat", "unshare", "reboot", "sethostname", "setdomainname", "acct", "setgroups", "capset", "fchownat"}
+local extra = {"waitid", "waitpid", "capget", "readahead", "munmap", "sched_yield", "poll", "sched_get_priority_min", "sched_get_priority_max", "sched_rr_get_interval", "mremap", "getgroups", "fcntl", "sysinfo", "klogctl", "msync", "madvise", "mlock", "munlock", "mlockall", "munlockall", "sigprocmask", "getitimer", "alarm", "setpriority", "wait4", "setitimer", "execve", "sigpending", "faccessat", "fchmodat", "mkdirat", "unlinkat", "reboot", "sethostname", "setdomainname", "acct", "setgroups", "capset", "fchownat"}
 
 for _, v in ipairs(extra) do C[v] = ffi.C[v] end
 
