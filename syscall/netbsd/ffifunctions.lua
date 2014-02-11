ad7f4987c4a80aa2367f736384b901ef45422e3c
Justin Cormack <justin@specialbusservice.com>
2014-02-11 18:55:07 +0000

    add ksem prototypes for netbsd

--- syscall/netbsd/ffifunctions.lua
+++ syscall/netbsd/ffifunctions.lua
@@ -52,5 +52,16 @@ int __kevent50(int kq, const struct kevent *changelist, size_t nchanges, struct 
 int __getcwd(char *buf, size_t size);
 int __libc_sigaction14(int signum, const struct sigaction *act, struct sigaction *oldact);
 int __sysctl(const int *, unsigned int, void *, size_t *, const void *, size_t);
+
+int __ksem_init(unsigned int, intptr_t *);
+int __ksem_open(const char *, int, _netbsd_mode_t, unsigned int, intptr_t *);
+int __ksem_unlink(const char *);
+int __ksem_close(intptr_t);
+int __ksem_post(intptr_t);
+int __ksem_wait(intptr_t);
+int __ksem_trywait(intptr_t);
+int __ksem_getvalue(intptr_t, unsigned int *);
+int __ksem_destroy(intptr_t);
+int __ksem_timedwait(intptr_t, const struct _netbsd_timespec *);
 ]]
 
