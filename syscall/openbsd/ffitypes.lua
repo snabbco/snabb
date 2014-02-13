935850daa44d27166ebd7d89888f9b238bb3aafe
Justin Cormack <justin@specialbusservice.com>
2014-02-13 23:28:01 +0000

    fix openbsd sigset_t

--- syscall/openbsd/ffitypes.lua
+++ syscall/openbsd/ffitypes.lua
@@ -48,9 +48,7 @@ typedef uint32_t __fd_mask;
 typedef struct fd_set {
   __fd_mask fds_bits[32];
 } fd_set;
-typedef struct __sigset {
-  uint32_t sig[4]; // note renamed to match Linux
-} sigset_t;
+typedef unsigned int sigset_t;
 struct cmsghdr {
   socklen_t cmsg_len;
   int cmsg_level;
