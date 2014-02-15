07ac91f469ba7a2b5b03aaa55eafd40a92965385
Justin Cormack <justin@specialbusservice.com>
2014-02-15 22:44:47 +0000

    linux mips ioctls are just special

--- syscall/linux/mips/ioctl.lua
+++ syscall/linux/mips/ioctl.lua
@@ -13,6 +13,7 @@ local arch = {
       TCSETA	     = 0x5402,
       TCSETAW	     = 0x5403,
       TCSETAF	     = 0x5404,
+      TCSBRK	     = 0x5405,
       TCFLSH	     = 0x5407,
       TCSETS	     = 0x540e,
       TCSETSW	     = 0x540f,
@@ -21,11 +22,15 @@ local arch = {
       TCSBRKP	     = 0x5486,
       TIOCSERSWILD   = 0x548a,
       TIOCMIWAIT     = 0x5491,
+      TIOCSETD	     = 0x7401,
+      TIOCMSET	     = 0x741a,
+      TIOCOUTQ	     = 0x7472,
       FIOGETOWN      = _IOR('f', 123, "int"),
       FIOSETOWN      = _IOW('f', 124, "int"),
       SIOCATMARK     = _IOR('s', 7, "int"),
       SIOCSPGRP      = _IOW('s', 8, "pid"),
       SIOCGPGRP      = _IOR('s', 9, "pid"),
+      TIOCSPGRP	     = _IOW('t', 118, "int"),
     }
   end,
 }
