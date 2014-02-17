686d9044886459239ca070ba7e96ad774c881639
tedu <tedu@ox32.home.tedunangst.com>
2014-02-17 16:33:33 -0500

    actually, getdents only starting with 5.5

--- syscall/openbsd/ffifunctions.lua
+++ syscall/openbsd/ffifunctions.lua
@@ -9,6 +9,8 @@ pcall, type, table, string
 
 local cdef = require "ffi".cdef
 
+local abi = require "syscall.abi"
+
 cdef[[
 int reboot(int howto);
 int ioctl(int d, unsigned long request, void *arg);
@@ -17,6 +19,11 @@ int ioctl(int d, unsigned long request, void *arg);
 int grantpt(int fildes);
 int unlockpt(int fildes);
 char *ptsname(int fildes);
+]]
+
+if abi.openbsd >= 5.5 then
+cdef[[
 int getdents(int fd, void *buf, size_t nbytes);
 ]]
+end
 
