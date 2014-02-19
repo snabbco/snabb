dc89198ce92253c4dae67c88eab3aecbfe4cbc8e
Justin Cormack <justin@specialbusservice.com>
2014-02-19 16:17:05 +0000

    incorrect mips TCGETS ioctl number

--- syscall/linux/mips/ioctl.lua
+++ syscall/linux/mips/ioctl.lua
@@ -14,7 +14,7 @@ local arch = {
       TCSBRK	     = 0x5405,
       TCXONC	     = 0x5406,
       TCFLSH	     = 0x5407,
-      TCGETS	     = {number = 0x5401, read = true, type = "termios"},
+      TCGETS	     = {number = 0x540d, read = true, type = "termios"},
       TCSETS	     = 0x540e,
       TCSETSW	     = 0x540f,
       TCSETSF	     = 0x5410,
