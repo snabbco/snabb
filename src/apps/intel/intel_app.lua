c042671dccc4be5b4d80b09e1c1b997f141b1b70
Luke Gorrie <luke@snabb.co>
2014-03-06 17:28:45 +0100

    Migrated app.intel_app to new app interface.

--- src/apps/intel/intel_app.lua
+++ src/apps/intel/intel_app.lua
@@ -1,6 +1,7 @@
 module(...,package.seeall)
 
 local app      = require("core.app")
+local link     = require("core.link")
 local basic_apps = require("apps.basic.basic_apps")
 local buffer   = require("core.buffer")
 local freelist = require("core.freelist")
@@ -9,17 +10,16 @@ local lib      = require("core.lib")
 local register = require("lib.hardware.register")
 local intel10g = require("apps.intel.intel10g")
 local vfio     = require("lib.hardware.vfio")
+local config = require("core.config")
 
 Intel82599 = {}
 
 -- Create an Intel82599 App for the device with 'pciaddress'.
-function Intel82599:new (pciaddress,  rx_buffer_freelist)
-   local a = { dev = intel10g.new(pciaddress),
-               -- Optional special freelist to allocate RX buffers from.
-               rx_buffer_freelist = rx_buffer_freelist }
+function Intel82599:new (pciaddress) -- XXX rx_buffer_freelist
+   local a = { dev = intel10g.new(pciaddress) }
    setmetatable(a, {__index = Intel82599 })
    intel10g.open_for_loopback_test(a.dev)
-   return app.new(a)
+   return a
 end
 
 -- Pull in packets from the network and queue them on our 'tx' link.
@@ -27,8 +27,8 @@ function Intel82599:pull ()
    local l = self.output.tx
    if l == nil then return end
    self.dev:sync_receive()
-   while not app.full(l) and self.dev:can_receive() do
-      app.transmit(l, self.dev:receive())
+   while not link.full(l) and self.dev:can_receive() do
+      link.transmit(l, self.dev:receive())
    end
    self:add_receive_buffers()
 end
@@ -52,8 +52,8 @@ end
 function Intel82599:push ()
    local l = self.input.rx
    if l == nil then return end
-   while not app.empty(l) and self.dev:can_transmit() do
-      local p = app.receive(l)
+   while not link.empty(l) and self.dev:can_transmit() do
+      local p = link.receive(l)
       self.dev:transmit(p)
       packet.deref(p)
    end
@@ -72,15 +72,24 @@ function selftest ()
    --   Source --> Intel82599(loopback) --> Sink
    -- and push packets through it.
    vfio.bind_device_to_vfio("0000:01:00.0")
+   local c = config.new()
+   config.app(c, "intel10g", Intel82599, "0000:01:00.0")
+   config.app(c, "source", basic_apps.Source)
+   config.app(c, "sink", basic_apps.Sink)
+   config.link(c, "source.out -> intel10g.rx")
+   config.link(c, "intel10g.tx -> sink.in")
+   app.configure(c)
+--[[
    app.apps.intel10g = Intel82599:new("0000:01:00.0")
    app.apps.source = app.new(basic_apps.Source)
    app.apps.sink   = app.new(basic_apps.Sink)
    app.connect("source", "out", "intel10g", "rx")
    app.connect("intel10g", "tx", "sink", "in")
    app.relink()
+--]]
    buffer.preallocate(100000)
-   local deadline = lib.timer(1e9)
-   repeat app.breathe() until deadline()
-   app.report()
+   app.main({duration = 1})
+--   repeat app.breathe() until deadline()
+--   app.report()
 end
 
