84de62be6bcd4091ffb1af8ba8d9d626206dba15
Vladislav Naumov <vnaum@vnaum.com>
2014-03-19 14:41:44 +0700

    debian package build rules

---
+++ debian/snabbswitch.default.ex
@@ -0,0 +1,10 @@
+# Defaults for snabbswitch initscript
+# sourced by /etc/init.d/snabbswitch
+# installed at /etc/default/snabbswitch by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
