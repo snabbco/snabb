331807d0b8e5069b9cbac11727892dc341ced236
Justin Cormack <justin@specialbusservice.com>
2014-03-25 21:45:56 +0000

    note about global to hold modules

--- syscall/rump/init.lua
+++ syscall/rump/init.lua
@@ -18,6 +18,8 @@ local modules = {
   rump = ffi.load("rump", true),
 }
 
+_G[{}] = modules -- if you unload rump kernel crashes are likely, so hang on to them
+
 local unchanged = {
   char = true,
   int = true,
@@ -227,7 +229,6 @@ function S.rump.init(ms, ...) -- you must load the factions here eg dev, vfs, ne
   if ms then loadmodules(ms) end
   local ok = ffi.C.rump_init()
   if ok == -1 then return nil, t.error() end
-  _G[{}] = modules
   S.abi = abi
   return S
 end
