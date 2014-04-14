91788100530c8c35c71c87f33dc58cdca0a2b4bb
Justin Cormack <justin@specialbusservice.com>
2014-04-15 00:56:42 +0100

    do not try to call sysctl if not running on netbsd

--- syscall/netbsd/ffitypes.lua
+++ syscall/netbsd/ffitypes.lua
@@ -17,6 +17,8 @@ local helpers = require "syscall.helpers"
 
 -- TODO constants probably needs version, so may need moving
 -- NetBSD ABI version
+-- TODO if running rump on NetBSD the version detection is a bit flaky if the host and rump differ
+-- normally this is ok if you init netbsd first and have compat installed for rump, or do not use both...
 ffi.cdef[[
 int sysctl(const int *, unsigned int, void *, size_t *, const void *, size_t);
 int __sysctl(const int *, unsigned int, void *, size_t *, const void *, size_t);
@@ -25,7 +27,10 @@ int rump_getversion(void);
 local sc = ffi.new("int[2]", 1, 3) -- kern.osrev
 local osrevision = ffi.new("int[1]")
 local lenp = ffi.new("unsigned long[1]", ffi.sizeof("int"))
-local ok, res = pcall(ffi.C.sysctl, sc, 2, osrevision, lenp, nil, 0)
+local ok, res
+if abi.host == "netbsd" then
+  ok, res = pcall(ffi.C.sysctl, sc, 2, osrevision, lenp, nil, 0)
+end
 if not ok or res == -1 then ok, res = pcall(ffi.C.rump_getversion) end
 if not ok or res == -1 then 
   abi.netbsd = 7
