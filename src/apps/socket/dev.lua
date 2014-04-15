a43056ee415912bee2628d7886e5353268657ae2
Alexander Gall <alexander.gall@gmail.com>
2014-04-15 13:20:26 +0200

    Add app for raw socket access to regular network devices
    The RawSocket app provided in src/apps/socket can be used to read and
    write bare packets from regular network devices.  It can be used to
    access NICs for which no native Snabb Switch driver exists.  It can
    also facilitate debugging with, e.g., tcpdump, which is not straight
    forward to use with the native driver.  The app is not recommended for
    handling traffic with high demands on performance.

---
+++ src/apps/socket/dev.lua
@@ -0,0 +1,47 @@
+module(...,package.seeall)
+
+local ffi = require("ffi")
+local C = ffi.C
+
+local buffer = require("core.buffer")
+local packet = require("core.packet")
+require("lib.raw.raw_h")
+require("apps.socket.io_h")
+
+dev = {}
+
+function dev:new (ifname)
+   assert(ifname)
+   self.__index = self
+   local dev = {fd = C.open_raw(ifname)}
+   return setmetatable(dev, self)
+end
+
+function dev:transmit (p)
+   assert(self.fd)
+   assert(C.send_packet(self.fd, p) ~= -1)
+end
+
+function dev:can_transmit ()
+   return C.can_transmit(self.fd) == 1
+end
+
+function dev:receive ()
+   assert(self.fd)
+   local size = C.msg_size(self.fd)
+   assert(size ~= -1)
+   local p = packet.allocate()
+   local nbuffers = math.ceil(size/buffer.size)
+   assert(nbuffers <= C.PACKET_IOVEC_MAX)
+   for i = 1, nbuffers do
+      local b = buffer.allocate()
+      packet.add_iovec(p, b, 0)
+   end
+   local s = C.receive_packet(self.fd, p)
+   assert(s ~= -1)
+   return p
+end
+
+function dev:can_receive ()
+   return C.can_receive(self.fd) == 1
+end
---
+++ src/apps/socket/raw.lua
@@ -0,0 +1,74 @@
+module(...,package.seeall)
+
+local app    = require("core.app")
+local link   = require("core.link")
+local packet = require("core.packet")
+local dev    = require("apps.socket.dev").dev
+
+RawSocket = {}
+
+function RawSocket:new (ifname)
+   assert(ifname)
+   self.__index = self
+   return setmetatable({dev = dev:new(ifname)}, self)
+end
+
+function RawSocket:pull ()
+   local l = self.output.tx
+   if l == nil then return end
+   while not link.full(l) and self.dev:can_receive() do
+      link.transmit(l, self.dev:receive())
+   end
+end
+
+function RawSocket:push ()
+   local l = self.input.rx
+   if l == nil then return end
+   while not link.empty(l) and self.dev:can_transmit() do
+      local p = link.receive(l)
+      self.dev:transmit(p)
+      packet.deref(p)
+   end
+end
+
+function RawSocket.selftest ()
+   -- Send a packet over the loopback device and check
+   -- that it is received correctly.
+   -- XXX beware of a race condition with unrelated traffic over the
+   -- loopback device
+   local datagram = require("lib.protocol.datagram")
+   local ethernet = require("lib.protocol.ethernet")
+   local ipv6 = require("lib.protocol.ipv6")
+   local dg_tx = datagram:new()
+   local src = ethernet:pton("00:00:00:00:00:01")
+   local dst = ethernet:pton("00:00:00:00:00:02")
+   local localhost = ipv6:pton("0:0:0:0:0:0:0:1")
+   dg_tx:push(ethernet:new({src = src, dst = dst, type = 0x86dd}))
+   dg_tx:push(ipv6:new({src = localhost,
+			dst = localhost,
+			next_header = 59, -- no next header
+			hop_limit = 1}))
+
+   local link = require("core.link")
+   local lo = RawSocket:new("lo")
+   lo.input, lo.output = {}, {}
+   lo.input.rx, lo.output.tx = link.new(), link.new()
+   link.transmit(lo.input.rx, dg_tx:packet())
+   lo:push()
+   lo:pull()
+   local dg_rx = datagram:new(link.receive(lo.output.tx), ethernet)
+   assert(dg_rx:parse({ { ethernet, function(eth)
+				       return(eth:src_eq(src) and eth:dst_eq(dst)
+					and eth:type() == 0x86dd)
+				    end },
+			{ ipv6, function(ipv6)
+				   return(ipv6:src_eq(localhost) and
+				       ipv6:dst_eq(localhost))
+				end } }), "loopback test failed")
+
+   -- Another useful test would be to feed a pcap file with
+   -- pings to 127.0.0.1 and ::1 into lo and capture/compare
+   -- the responses with a pre-recorded pcap.
+end
+
+return RawSocket
