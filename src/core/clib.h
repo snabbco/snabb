a069d6e32622acd100ab8502f694a16a89f61300
Alexander Gall <alexander.gall@gmail.com>
2014-04-16 10:57:02 +0200

    Add utility functions for bitfield manipulation and string splitting to src/core/lib.lua

--- src/core/clib.h
+++ src/core/clib.h
@@ -36,3 +36,9 @@ char *basename(char *path);
 
 // inet_pton(3) - convert IPv4 and IPv6 addresses from text to binary form
 int inet_pton(int af, const char *src, void *dst);
+
+// Conversions between host and network byte order
+uint16_t htons(uint16_t);
+uint16_t ntohs(uint16_t);
+uint32_t htonl(uint32_t);
+uint32_t ntohl(uint32_t);
