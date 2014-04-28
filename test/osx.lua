2150f214d3fcbcdd5668474ad736fd9394415d64
Justin Cormack <justin@specialbusservice.com>
2014-04-29 00:23:04 +0100

    remove osx mount tests

--- test/osx.lua
+++ test/osx.lua
@@ -51,22 +51,6 @@ end
 
 local test = {}
 
-test.mount_bsd_root = {
-  test_mount_kernfs = function()
-    assert(S.mkdir(tmpfile))
-    assert(S.mount{dir=tmpfile, type="kernfs"})
-    assert(S.unmount(tmpfile))
-    assert(S.rmdir(tmpfile))
-  end,
-  test_mount_tmpfs = function()
-    assert(S.mkdir(tmpfile))
-    local data = {ta_version = 1, ta_nodes_max=100, ta_size_max=1048576, ta_root_mode=helpers.octal("0700")}
-    assert(S.mount{dir=tmpfile, type="tmpfs", data=data})
-    assert(S.unmount(tmpfile))
-    assert(S.rmdir(tmpfile))
-  end,
-}
-
 return test
 
 end
