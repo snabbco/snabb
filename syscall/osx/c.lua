c85506cc92da8ef0aea9fb7c61f7d23edad09c02
Justin Cormack <justin@specialbusservice.com>
2014-04-28 22:07:14 +0100

    mlockall on osx always fails, hide

--- syscall/osx/c.lua
+++ syscall/osx/c.lua
@@ -24,8 +24,14 @@ local uint, ulong = ffi.typeof("unsigned int"), ffi.typeof("unsigned long")
 
 local function inlibc_fn(k) return ffi.C[k] end
 
+-- Syscalls that just return ENOSYS but are in libc. Note these might vary by version in future
+local nosys_calls = {
+  mlockall = true,
+}
+
 local C = setmetatable({}, {
   __index = function(C, k)
+    if nosys_calls[k] then return nil end
     if pcall(inlibc_fn, k) then
       C[k] = ffi.C[k] -- add to table, so no need for this slow path again
       return C[k]
