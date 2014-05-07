e0816bab7244f9a205eb0ce63f3fa22755523ce1
Alexander Altshuler <altexy@gmail.com>
2014-05-07 21:02:31 +0400

    lib/macaddress: add error handling

--- src/lib/macaddress.lua
+++ src/lib/macaddress.lua
@@ -15,8 +15,15 @@ function mac_mt:new (m)
    local macobj = mac_t()
    local i = 0;
    for b in m:gmatch('[0-9a-fA-F][0-9a-fA-F]') do
+      if i == 6 then
+         -- avoid out of bound array index
+         return nil, "malformed MAC address: " .. m
+      end
       macobj.bytes[i] = tonumber(b, 16)
       i = i + 1
+   end
+   if i < 6 then
+      return nil, "malformed MAC address: " .. m
    end
    return macobj
 end
