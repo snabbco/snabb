2b6e39ef8eec345737035886ae87c5f5091a6d97
Alexander Gall <alexander.gall@gmail.com>
2014-05-08 14:44:42 +0200

    Add capability to recycle objects to class.lua
    The creation of instance objects should be avoided in packet
    processing loops to reduce garbage collection overhead.  class.lua now
    supports a simple mechanism where an app can declare an object to no
    longer be in use.  This object is then put on a list in the
    corresponding class and will be re-used by the constructor method
    instead of allocating a new object.
    
    The class API has been changed slightly to no longer require the
    declaration of constructor methods.  Any method can act as a
    constructor or extend the standard constructor by calling the new()
    method of the super class.

--- src/lib/lua/class.lua
+++ src/lib/lua/class.lua
@@ -1,74 +1,91 @@
--- Support for basic OO programming.  Copied from
--- http://lua-users.org/wiki/InheritanceTutorial with a few
--- modifications.
+-- Support for basic OO programming.  Apart from the usual
+-- incantations of setmetatable(), it implements a simple mechanism to
+-- avoid table allocations by recycling objects that have been
+-- explicitely declared to no longer be in use.
+--
+-- All objects are descendants from a simple "elementary class" that
+-- implements the basic functionality for the creation and recycling
+-- of instance objects through the new() and free() methods.
 --
 -- Usage:
--- local require("lib.lua.class")
--- local myclass = subClass(baseClass | nil, [constructor_method, ...])
+--   local require("lib.lua.class")
+--   local baseClass = require("baseClass")
+--   local myclass = subClass(baseClass)
+--   local instance = myclass:new()
+--   instance:free()
 --
--- myclass inherits all public methods and class variables from
--- baseClass as well as all constructor methods. The default
--- constructor method is called new().  Each constructor executes the
--- method called _init_<construcor> if it exists, where <constructor>
--- is the name of the constructor method.  All arguments supplied to
--- the constructor are passed unmodified to the corresponding _init
--- method.
+-- If baseClass is nil, myclass will be a direct descendant of
+-- elementaryClass
 --
-function subClass (baseClass, ... )
-   local new_class = {}
-   local class_mt = {__index = new_class}
-   local constructors = {...}
+-- The basic constructor new() either allocates a new instance or
+-- re-uses one that has been put on the class's freelist by a previous
+-- call of the free() instance method.
+--
+-- Calls to methods of the super class must use the 'dot' notation and
+-- pass the object as argument itself, e.g.
+--
+--   local myclass = subClass(someClass)
+--   function myclass:method(...)
+--      myclass:superClass().method(self, ...)
+--      -- Customization goes here
+--    end
+--
+-- Note that the superClass method must be called with reference to
+-- the class in which the function is defined.  Using
+-- self:superClass() would create a loop if the method itself was
+-- called from a derived class.
 
-   if baseClass ~= nil then
-      setmetatable(new_class, {__index = baseClass})
-      for c, _ in pairs(baseClass.constructors) do
-	 new_class.constructors[c] = true
-      end
+local elementaryClass = {}
+elementaryClass._name = "elementary class"
+
+-- Class methods
+
+-- Create a new instance of a class or re-use one from the free list.
+-- A recycled object has its instance variable _recycled set to true.
+-- A class can use this, for example, to perform clean-up on such an
+-- object before re-use.
+function elementaryClass:new ()
+   assert(self ~= elementaryClass, "Can't instantiate abstract class elementaryClass")
+   local instance
+   local freelist = self._freelist
+   local index = freelist.index
+   if index > 0 then
+      instance = freelist.list[index]
+      instance._recycled = true
+      freelist.index = index - 1
    else
-      new_class.constructors = {new = true}
+      instance = { _recycled = false }
+      setmetatable(instance, { __index = self })
    end
-   for _, c in ipairs(constructors) do
-      assert(not new_class.constructors[c],
-	     "duplicate declaration of constructor method "..c)
-      new_class.constructors[c] = true
+   return instance
+end
+
+-- Instance methods
+
+function elementaryClass:name()
+   return self._name or nil
+end
+
+-- Put an instance on the free list for recycling
+function elementaryClass:free ()
+   local freelist = self:class()._freelist
+   local index = freelist.index + 1
+   freelist.list[index] = self
+   freelist.index = index
+end
+
+function subClass (baseClass)
+   local baseClass = baseClass or elementaryClass
+   local class = { _freelist = { index = 0, list = {} } }
+   setmetatable(class, { __index = baseClass })
+
+   function class:class ()
+      return class
    end
 
-   for c, _ in pairs(new_class.constructors) do
-      new_class[c] =
-	 function (self, ...)
-	    local newinst = {}
-	    setmetatable(newinst, class_mt)
-	    if newinst['_init_'..c] then
-	       newinst['_init_'..c](newinst, ...)
-	    end
-	    return newinst
-	 end
-   end
-
-   -- Return the class object of the instance
-   function new_class:class ()
-      return new_class
-   end
-
-   -- Return the super class object of the instance
-   function new_class:superClass ()
+   function class:superClass ()
       return baseClass
    end
 
-   -- Return true if the caller is an instance of theClass
-   function new_class:isa (theClass)
-      local b_isa = false
-      local cur_class = new_class
-
-      while (cur_class ~= nil) and (b_isa == false) do
-	 if cur_class == theClass then
-	    b_isa = true
-	 else
-	    cur_class = cur_class:superClass()
-	 end
-      end
-      return b_isa
-   end
-
-   return new_class
+   return class
 end
