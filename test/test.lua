3de7a24e3105db42578cdc5c07ae1337b679ebb4
Justin Cormack <justin@specialbusservice.com>
2014-05-12 22:23:49 +0100

    vfs first, to stop openbsd errors

--- test/test.lua
+++ test/test.lua
@@ -24,8 +24,8 @@ if arg[1] == "rump" or arg[1] == "rumplinux" then
   if arg[1] == "rumplinux" then
     tmpabi.types = "linux" -- monkeypatch
   end
-  local modules = {"kern.tty", "dev", "net", "fs.tmpfs", "fs.kernfs", "fs.ptyfs",
-                   "net.net", "net.local", "net.netinet", "net.netinet6", "vfs"}
+  local modules = {"kern.tty", "dev", "net", "vfs", "fs.tmpfs", "fs.kernfs", "fs.ptyfs",
+                   "net.net", "net.local", "net.netinet", "net.netinet6"}
   S = require "syscall.rump.init".init(modules)
   table.remove(arg, 1)
 else
