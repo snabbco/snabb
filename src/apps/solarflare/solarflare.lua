33c572a7047525af687e5926a7ba9632e16c5a2f
Hans Huebner <hans.huebner@gmail.com>
2014-07-20 16:05:31 +0200

    polish and SolarFlareNic make into proper class

--- src/apps/solarflare/solarflare.lua
+++ src/apps/solarflare/solarflare.lua
@@ -33,24 +33,31 @@ SolarFlareNic.__index = SolarFlareNic
 SolarFlareNic.version = ef_vi_version
 
 function SolarFlareNic:new (args)
-   args = config.parse_app_args(args)
+   assert(args.ifname)
+   return setmetatable(args, { __index = SolarFlareNic })
 end
 
-function SolarFlareNic.open_device (interface_name)
-   print("open device " .. interface_name)
+function SolarFlareNic:open ()
    local try_ = try
    local function try (rc, message)
-      return try_(rc, string.format("%s (if=%s)", message, interface_name))
+      return try_(rc, string.format("%s (if=%s)", message, self.ifname))
    end
 
    local handle_p = ffi.new("ef_driver_handle[1]")
    try(ciul.ef_driver_open(handle_p), "ef_driver_open")
-   local driver_handle = handle_p[0]
-   print(string.format("driver handle %d", driver_handle));
-   local pd_p = ffi.new("ef_pd[1]")
-   try(ciul.ef_pd_alloc_by_name(pd_p, driver_handle, interface_name, C.EF_PD_DEFAULT + C.EF_PD_PHYS_MODE), "ef_pd_alloc_by_name")
-   local fi_p = ffi.new("ef_vi[1]")
-   try(ciul.ef_vi_alloc_from_pd(fi_p, driver_handle, pd_p, driver_handle,
+   self.driver_handle = handle_p[0]
+   self.pd_p = ffi.new("ef_pd[1]")
+   try(ciul.ef_pd_alloc_by_name(self.pd_p, self.driver_handle, self.ifname, C.EF_PD_DEFAULT + C.EF_PD_PHYS_MODE), "ef_pd_alloc_by_name")
+   self.ef_vi_p = ffi.new("ef_vi[1]")
+   try(ciul.ef_vi_alloc_from_pd(self.ef_vi_p, self.driver_handle, self.pd_p, self.driver_handle,
                                    -1, -1, -1, nil, -1, C.EF_VI_TX_PUSH_DISABLE))
+
+   self.mac_address = ffi.new("unsigned char[6]");
+   try(ciul.ef_vi_get_mac(self.ef_vi_p, self.driver_handle, self.mac_address))
+   self.mtu = try(ciul.ef_vi_mtu(self.ef_vi_p, self.driver_handle))
+   print(string.format("Opened SolarFlare interface %s (MAC address %02x:%02x:%02x:%02x:%02x:%02x, MTU %d)",
+                       self.ifname,
+                       self.mac_address[0], self.mac_address[1], self.mac_address[2], self.mac_address[3], self.mac_address[4], self.mac_address[5],
+                       self.mtu));
 end
    
