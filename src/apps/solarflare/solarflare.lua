93fc4115dc3f05d3633248962bb98fc8513f070d
Hans Huebner <hans.huebner@gmail.com>
2014-07-20 18:35:38 +0200

    refactor for short lines

--- src/apps/solarflare/solarflare.lua
+++ src/apps/solarflare/solarflare.lua
@@ -32,12 +32,12 @@ SolarFlareNic = {}
 SolarFlareNic.__index = SolarFlareNic
 SolarFlareNic.version = ef_vi_version
 
-function SolarFlareNic:new (args)
+function SolarFlareNic:new(args)
    assert(args.ifname)
    return setmetatable(args, { __index = SolarFlareNic })
 end
 
-function SolarFlareNic:open ()
+function SolarFlareNic:open()
    local try_ = try
    local function try (rc, message)
       return try_(rc, string.format("%s (if=%s)", message, self.ifname))
@@ -47,17 +47,42 @@ function SolarFlareNic:open ()
    try(ciul.ef_driver_open(handle_p), "ef_driver_open")
    self.driver_handle = handle_p[0]
    self.pd_p = ffi.new("ef_pd[1]")
-   try(ciul.ef_pd_alloc_by_name(self.pd_p, self.driver_handle, self.ifname, C.EF_PD_DEFAULT + C.EF_PD_PHYS_MODE), "ef_pd_alloc_by_name")
+   try(ciul.ef_pd_alloc_by_name(self.pd_p,
+                                self.driver_handle,
+                                self.ifname,
+                                C.EF_PD_DEFAULT + C.EF_PD_PHYS_MODE),
+       "ef_pd_alloc_by_name")
    self.ef_vi_p = ffi.new("ef_vi[1]")
-   try(ciul.ef_vi_alloc_from_pd(self.ef_vi_p, self.driver_handle, self.pd_p, self.driver_handle,
-                                   -1, -1, -1, nil, -1, C.EF_VI_TX_PUSH_DISABLE))
+   try(ciul.ef_vi_alloc_from_pd(self.ef_vi_p,
+                                self.driver_handle,
+                                self.pd_p,
+                                self.driver_handle,
+                                -1,
+                                -1,
+                                -1,
+                                nil,
+                                -1,
+                                C.EF_VI_TX_PUSH_DISABLE))
 
    self.mac_address = ffi.new("unsigned char[6]");
-   try(ciul.ef_vi_get_mac(self.ef_vi_p, self.driver_handle, self.mac_address))
+   try(ciul.ef_vi_get_mac(self.ef_vi_p,
+                          self.driver_handle,
+                          self.mac_address))
    self.mtu = try(ciul.ef_vi_mtu(self.ef_vi_p, self.driver_handle))
    print(string.format("Opened SolarFlare interface %s (MAC address %02x:%02x:%02x:%02x:%02x:%02x, MTU %d)",
                        self.ifname,
-                       self.mac_address[0], self.mac_address[1], self.mac_address[2], self.mac_address[3], self.mac_address[4], self.mac_address[5],
+                       self.mac_address[0],
+                       self.mac_address[1],
+                       self.mac_address[2],
+                       self.mac_address[3],
+                       self.mac_address[4],
+                       self.mac_address[5],
                        self.mtu));
 end
    
+function SolarFlareNic:test()
+   local b = buffer.allocate();
+   local p = packet.allocate();
+   packet.add_iovec(p, b, 100);
+   print(string.format("done testing"))
+end
