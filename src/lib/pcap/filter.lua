a0f2830f76de716dd44817e82336433c089ebdf8
Diego Pino Garcia <dpino@igalia.com>
2014-11-16 00:39:00 +0100

    Adapt function filter

--- src/lib/pcap/filter.lua
+++ src/lib/pcap/filter.lua
@@ -1,35 +1,52 @@
 module(..., package.seeall)
+
 local ffi = require("ffi")
 local C = ffi.C
-require ("lib.pcap.filter_h")
-
-ffi.load("pcap", true)
 
 local filter = subClass(nil)
 filter._name = "pcap packet filter"
 
--- Dummy pcap handle shared by all instances. Link type 1 corresponds to
--- Ethernet
-local pcap = C.pcap_open_dead(1, 0xffff)
-
 -- Create a filter with an arbitrary libpcap filter expression
 function filter:new(program)
    local o = filter:superClass().new(self)
-   o._bpf = ffi.new("struct bpf_program")
-   if C.pcap_compile(pcap, o._bpf, ffi.cast("char *", program), 1, 0xffffffff) ~= 0 then
-      o:free()
-      return nil, C.pcap_geterr(pcap)
+   if _G.pflua then
+      -- Include pflua
+      package.path = package.path .. ";../deps/pflua/src/?.lua"
+      local pflua = require("pf")
+
+      -- Compile filter
+      local opts = { }
+      o._filter = pflua.compile_filter(program, opts)
+   else
+      -- Include headers
+      require ("lib.pcap.filter_h")
+      ffi.load("pcap", true)
+
+      -- Dummy pcap handle shared by all instances. Link type 1 corresponds to
+      -- Ethernet
+      local pcap = C.pcap_open_dead(1, 0xffff)
+
+      -- Compile filter
+      o._bpf = ffi.new("struct bpf_program")
+      if C.pcap_compile(pcap, o._bpf, ffi.cast("char *", program), 1, 0xffffffff) ~= 0 then
+         o:free()
+         return nil, C.pcap_geterr(pcap)
+      end
+      o._header = ffi.new("struct pcap_pkthdr")
    end
-   o._header = ffi.new("struct pcap_pkthdr")
    return o
 end
 
 -- Apply the filter to a region of memory
 function filter:match(data, length)
-   local header = self._header
-   header.incl_len = length
-   header.orig_len = length
-   return C.pcap_offline_filter(self._bpf, header, ffi.cast("char *", data)) ~= 0
+   if _G.pflua then
+      return self._filter(data, length)
+   else
+      local header = self._header
+      header.incl_len = length
+      header.orig_len = length
+      return C.pcap_offline_filter(self._bpf, header, ffi.cast("char *", data)) ~= 0
+   end
 end
 
 return filter
