8a33db3a568662eca74d6074e5eae69b4a6c0a50
Alexander Gall <alexander.gall@gmail.com>
2014-11-28 15:27:08 +0100

    Add apps to provide multi-port Ethernet switches
    The abstract class lib.bridge.base provides a constructor for a
    multi-port Ethernet switch with split-horizon semantics.  The subclass
    lib.bridge.flooding implements a simple flooding bridge while
    lib.bridge.learning avoids flooding through MAC learning via Bloom
    filters.

---
+++ src/apps/bridge/flooding.lua
@@ -0,0 +1,43 @@
+-- This class derives from lib.bridge.base and implements the simplest
+-- possible bridge, which floods a packet arriving on a port to all
+-- destination ports within its scope according to the split-horizon
+-- topology.
+
+module(..., package.seeall)
+
+local bridge_base = require("apps.bridge.base")
+local packet = require("core.packet")
+local link = require("core.link")
+local empty, receive, transmit = link.empty, link.receive, link.transmit
+local cow_clone = packet.cow_clone
+
+local bridge = subClass(bridge_base)
+bridge._name = "flooding bridge"
+
+function bridge:new (config)
+   return bridge:superClass().new(self, config)
+end
+
+function bridge:push()
+   local src_ports = self._src_ports
+   local dst_ports = self._dst_ports
+   local output = self.output
+   local i = 1
+   while src_ports[i] do
+      local src_port = src_ports[i]
+      local l_in = self.input[src_port]
+      while not empty(l_in) do
+	 local ports = dst_ports[src_port]
+	 local p = receive(l_in)
+	 transmit(output[ports[1]], p)
+	 local j = 2
+	 while ports[j] do
+	    transmit(output[ports[j]], cow_clone(p))
+	    j = j + 1
+	 end
+      end
+      i = i + 1
+   end
+end
+
+return bridge
