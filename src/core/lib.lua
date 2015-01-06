f7cf2c1b2d08eee6c74f1d0f39381dd5671d0d92
Javier Guerra <javier@guerrag.com>
2015-01-06 03:55:04 -0500

    small core/lib.lua speedup by cacheing bit.* functions.  mostly needed for lib.update_checksum()

--- src/core/lib.lua
+++ src/core/lib.lua
@@ -5,6 +5,9 @@ local C = ffi.C
 local getopt = require("lib.lua.alt_getopt")
 require("core.clib_h")
 
+local lshift, rshift = bit.lshift, bit.rshift
+local band, bor, bnot = bit.band, bit.bor, bit.bnot
+
 -- Returns true if x and y are structurally similar (isomorphic).
 function equal (x, y)
    if type(x) ~= type(y) then return false end
@@ -158,14 +161,14 @@ end
 function bits (bitset, basevalue)
    local sum = basevalue or 0
    for _,n in pairs(bitset) do
-	 sum = bit.bor(sum, bit.lshift(1, n))
+	 sum = bit.bor(sum, lshift(1, n))
    end
    return sum
 end
 
 -- Return true if bit number 'n' of 'value' is set.
 function bitset (value, n)
-   return bit.band(value, bit.lshift(1, n)) ~= 0
+   return bit.band(value, lshift(1, n)) ~= 0
 end
 
 -- Manipulation of bit fields in uint{8,16,32)_t stored in network
@@ -189,17 +192,17 @@ function bitfield(size, struct, member, offset, nbits, value)
       field = struct[member]
    end
    local shift = size-(nbits+offset)
-   local mask = bit.lshift(2^nbits-1, shift)
-   local imask = bit.bnot(mask)
+   local mask = lshift(2^nbits-1, shift)
+   local imask = bnot(mask)
    if value then
-      field = bit.bor(bit.band(field, imask), bit.lshift(value, shift))
+      field = bor(band(field, imask), lshift(value, shift))
       if conv then
 	 struct[member] = conv.hton(field)
       else
 	 struct[member] = field
       end
    else
-      return bit.rshift(bit.band(field, mask), shift)
+      return rshift(band(field, mask), shift)
    end
 end
 
@@ -321,17 +324,17 @@ function update_csum (ptr, len,  csum0)
    ptr = ffi.cast("uint8_t*", ptr)
    local sum = csum0 or 0LL
    for i = 0, len-2, 2 do
-      sum = sum + bit.lshift(ptr[i], 8) + ptr[i+1]
+      sum = sum + lshift(ptr[i], 8) + ptr[i+1]
    end
-   if len % 2 == 1 then sum = sum + bit.lshift(ptr[len-1], 1) end
+   if len % 2 == 1 then sum = sum + lshift(ptr[len-1], 1) end
    return sum
 end
 
 function finish_csum (sum)
-   while bit.band(sum, 0xffff) ~= sum do
-      sum = bit.band(sum + bit.rshift(sum, 16), 0xffff)
+   while band(sum, 0xffff) ~= sum do
+      sum = band(sum + rshift(sum, 16), 0xffff)
    end
-   return bit.band(bit.bnot(sum), 0xffff)
+   return band(bnot(sum), 0xffff)
 end
 
 
@@ -382,7 +385,7 @@ if ffi.abi("be") then
    function htons(b) return b end
 else
    function htonl(b) return bit.bswap(b) end
-   function htons(b) return bit.rshift(bit.bswap(b), 16) end
+   function htons(b) return rshift(bit.bswap(b), 16) end
 end
 ntohl = htonl
 ntohs = htons
