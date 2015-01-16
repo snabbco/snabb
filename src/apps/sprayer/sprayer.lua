9aa28aae78f9af051af0d69860dbcce6aa114467
Jeff Loughridge <jeffl@alumni.duke.edu>
2015-01-16 08:24:30 -0800

    Added sprayer app for Getting Started Guide

---
+++ src/apps/sprayer/sprayer.lua
@@ -0,0 +1,36 @@
+module(..., package.seeall)
+
+local app = require("core.app")
+local config = require("core.config")
+local pcap = require("apps.pcap.pcap")
+local link = require("core.link")
+local packet = require("core.packet")
+
+Sprayer = {}
+
+function Sprayer:new(arg)
+   self.packet_counter = 1
+   return setmetatable({}, {__index = Sprayer})
+end
+
+function Sprayer:push()
+   local i = assert(self.input.input, "input port not found")
+   local o = assert(self.output.output, "output port not found")
+
+   while not link.empty(i) and not link.full(o) do
+      self:process_packet(i, o)
+      self.packet_counter = self.packet_counter + 1
+   end
+end
+
+function Sprayer:process_packet(i, o)
+   local p = link.receive(i)
+   local iov = p.iovecs[0]
+
+   -- drop every other packet
+   if self.packet_counter % 2 == 0 then
+      link.transmit(o, p)
+   else
+      packet.deref(p)
+   end
+end
