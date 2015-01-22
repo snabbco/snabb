ffb1ef8c278534954f0c0bdbfc3f6fc0c4b640b0
Alexander Gall <alexander.gall@gmail.com>
2015-01-22 10:49:53 +0100

    Add IPC method using file-backed shared memory
    lib/ipc/shmem/shmem.lua implements a class that allows the sharing of
    a collection of cdata objects with other processes through a region of
    shared memory.
    
    This commit includes two sub classes. The mib subclass implements data
    types based on the SMIv2 specification.  It is intended to provide an
    interface to a SNMP sub-agent running on the same host.
    
    The counter subclass provides a collection of unsigned 64-bit numbers,
    intended to be used as generic counters (e.g. byte or packet counters
    for interfaces or inter-app links).

---
+++ src/lib/ipc/shmem/shmem.c
@@ -0,0 +1,38 @@
+#include <sys/mman.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdbool.h>
+
+bool shmem_unmap(void *mem, size_t size) {
+  if (munmap(mem, size) == -1) {
+    perror("munmap");
+    return(false);
+  }
+  return(true);
+}
+
+// Note: the io.* file handle passed to us is converted to a Unix
+// filehandle (FILE *) by LuaJIT
+char *shmem_grow(void *fh, void *old_mem, size_t old_size, size_t new_size) {
+  int fd;
+  void *mem;
+
+  if (old_mem != NULL) {
+    if (shmem_unmap(old_mem, old_size) == false) {
+      return(NULL);
+    }
+  }
+  fd = fileno(fh);
+  if (ftruncate(fd, new_size) == -1) {
+    perror("ftruncate");
+    return(NULL);
+  }
+  if ((mem = mmap(old_mem, new_size, PROT_READ|PROT_WRITE, MAP_SHARED,
+		  fd, 0)) == MAP_FAILED) {
+    perror("mmap");
+    return(NULL);
+  }
+  return((char *)mem);
+}
+
