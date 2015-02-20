922465100e3fba9f1a8cac74ec3c5df29791b0c9
Luke Gorrie <luke@snabb.co>
2015-02-20 17:06:37 +0100

    gcc-preinclude.h: LuaJIT compatibility fix
    Detect when compiling an assembler file and suppress the contents of
    the header file. (It only compiles as C.)

--- gcc-preinclude.h
+++ gcc-preinclude.h
@@ -1,6 +1,8 @@
 // Force an old symbol version on memcpy.
 // See: http://www.win.tue.nl/~aeb/linux/misc/gcc-semibug.html
 //      https://rjpower9000.wordpress.com/2012/04/09/fun-with-shared-libraries-version-glibc_2-14-not-found/
-__asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
 
+#ifndef __ASSEMBLER__
+__asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
+#endif
 
