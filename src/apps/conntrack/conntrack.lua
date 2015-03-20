9a7d0369522cc1f6ff1f97f09d7bcc3101e5f822
Diego Pino Garcia <dpino@igalia.com>
2015-03-20 10:46:31 +0800

    Implement three-way handshake authentication

--- src/apps/conntrack/conntrack.lua
+++ src/apps/conntrack/conntrack.lua
@@ -9,21 +9,25 @@ local bit = require("bit")
 
 Conntrack = {}
 
-local conns = {}
-local conns_rev_ack = {}
-local three_way_hand = {}
-
 local new_conn_id = (function()
    local count = 0
    return function()
       count = count + 1
-      return "conn_"..id
+      return "conn_"..count
    end
 end)()
 
 function Conntrack:new(arg)
    self.packet_counter = 1
-   return setmetatable({}, {__index = Conntrack})
+   self.conns = {}   
+   self.conn_packs = {}
+   self.three_way_handshake = {}
+   return setmetatable({}, {__index = Conntrack, 
+      __gc = function() print("__gc") end })
+end
+
+function Conntrack:destroy()
+   print("Destroy!!")
 end
 
 function Conntrack:push()
@@ -67,7 +71,7 @@ local function length(p)
    return uint16(p[16], p[17])   
 end
 
-local function ip_src(p)
+local function src_ip(p)
    return uint32(p[26], p[27], p[28], p[29])
 end
 
@@ -75,15 +79,15 @@ local function ip_str(a, b, c, d)
    return ("%d.%d.%d.%d"):format(a, b, c, d)
 end
 
-local function ip_src_str(p)
+local function src_ip_str(p)
    return ip_str(p[26], p[27], p[28], p[29])
 end
 
-local function ip_dst(p)
+local function dst_ip(p)
    return uint32(p[30], p[31], p[32], p[33])
 end
 
-local function ip_dst_str(p)
+local function dst_ip_str(p)
    return ip_str(p[30], p[31], p[32], p[33])
 end
 
@@ -130,12 +134,21 @@ end
 
 -- TCP_FLAGS
 
-local TCP_SYN     =  0x02
-local TCP_ACK     =  0x10
-local TCP_SYN_ACK =  0x12
+local TCP_SYN     = 0x02
+local TCP_ACK     = 0x10
+local TCP_SYN_ACK = 0x12
+local TCP_FIN     = 0x20
 
 local function tcpflags(p, flag)
    return bit.band(p[47], 0x3F) == flag
+end
+
+-- TCP connection negotiation
+
+local function packet_id(p)
+   local src = src_ip_str(p)..":"..src_port(p)
+   local dst = dst_ip_str(p)..":"..dst_port(p)
+   return src.."-"..dst
 end
 
 local function is_syn(p)
@@ -150,97 +163,54 @@ local function is_syn_ack(p)
    return tcpflags(p, TCP_SYN_ACK)
 end
 
+local function is_fin(p)
+   return tcpflags(p, TCP_FIN)
+end
+
 function Conntrack:process_packet(i, o)
    local p = link.receive(i)
+   link.transmit(o, p)
 
-   -- print("Before: "..count_connections())
-   -- local src = ip_src(p.data)
-   -- local dst = ip_dst(p.data)
+   local p = p.data
 
-   -- add_connection(src, dst)
+   -- A packet id is defined by the tuple { src_ip; src_port; dst_ip; dst_port }
+   local p_id = packet_id(p) 
 
-   -- print(("src: %s; dst: %s"):format(src, dst))
-
-   -- drop every other packet
-   if self.packet_counter % 2 == 0 then
-      local p = p.data
-
-      local src = ip_src_str(p)
-      local dst = ip_dst_str(p)
-      local key = key(src, dst)
-
-      local seq = seq(p)
-      local ack = ack(p)
-
-      -- print("src: "..src.."; dst: "..dst.."; src_port: "..src_port(p).."; dst_port: "..dst_port(p).."; proto: "..protocol(p))
-      -- print("src: "..src.."; dst: "..dst.."; src_port: "..src_port(p).."; dst_port: "..dst_port(p).."; reserved: "..reserved(p))
-
-      -- print("src: "..src.."; dst: "..dst.."; reserved: "..reserved(p))
-
-      if is_syn(p) then
-         print("Is SYN! - src: "..src.."; dst: "..dst.."; seq: "..seq.."; ack: "..ack)
+   if self.three_way_handshake[p_id] then
+      if is_syn_ack(p) then
+         if (self.three_way_handshake[p_id] == ack(p)) then
+            self.three_way_handshake[p_id] = seq(p) + 1
+            return
+         end
       end
       if is_ack(p) then
-         print("Is ACK! - src: "..src.."; dst: "..dst.."; seq: "..seq.."; ack: "..ack)
-      end
-      if is_syn_ack(p) then
-         print("Is SYN_ACK! - src: "..src.."; dst: "..dst.."; seq: "..seq.."; ack: "..ack)
-      end
+         if (self.three_way_handshake[p_id] == seq(p)) then
+            -- The connection was established, create a new connection indexed by id
+            local conn_id = new_conn_id()
+            self.conns[p_id] = conn_id
+            self.conn_packs[conn_id] = 0
 
-      -- print("src: "..src.."; dst: "..dst.."; seq: "..seq.."; ack: "..ack)
-
-      --[[
-      if is_syn(p.data) then
-
-      elseif is_syn(p.data) and is_ack(p.data)
-
-      elseif is_ack(p.data) then
-
-      end
-      --]]
-
-      --[[
-      -- Only TCP packets with a successfully established connection
-      if seq(p.data) == 0 and ack(p.data) == 0 then
-         three_way_hand[key] = "syn"
-      -- elseif seq(p.data) == 0 and ack(p.data) == 1 then
-      elseif ack(p.data) == 0 then
-         print("Switch to ack")
-         assert(three_way_hand[key] == "syn", "Error: spoofing connection")
-         three_way_hand[key] = "ack"
-      elseif seq(p.data) == 1 and ack(p.data) == 1 then
-         print("Switch to syn-ack")
-         assert(three_way_hand[key] == "ack", "Error: spoofing connection")
-         three_way_hand[key] = "syn-ack"
-
-         local conn_id = new_conn_id()
-         local seq = seq(p.data)
-         local ack = ack(p.data)
-
-         conns[conn_id] = { src = src, dst = dst, seq = seq, ack = ack }
-         conns_rev_ack[key(src, dst, ack)] = conn_id
-      else
-         -- print("ack: "..ack(p.data))
-         if three_way_hand[key] then
-            -- print("src: "..ip_src_str(p.data).."; key: "..ip_dst_str(p.data).."; flag: "..three_way_hand[key])
+            self.three_way_handshake[p_id] = nil
+            return
          end
-         -- assert(three_way_hand[key] == "syn-ack")
-
-         -- local key = key(src, dst, seq(p.data))
-         -- assert(conn_rev_ack[key], "Error: sequence number has not associated ACK")
-         -- local conn_id = conn_rev_ack[key]
-         -- conn_rev_ack[key] = nil
       end
+      if is_fin(p) then
+         self.three_way_handshake[p_id] = nil
+         print("Destroyed connection: "..p_id)
+         return
+      end
+   end
 
-      -- Once the connection is established:
-      --    * Create a new entry in the table of connections.
-      --    * This table stores the last sequence number.
-      --    * Keep track of the connection number and associate each packet to its correspondent connection.
+   -- Is SYN
+   if is_syn(p) then
+      self.three_way_handshake[p_id] = seq(p) + 1
+      return
+   end
 
-      link.transmit(o, p)
-      --]]
-   else
-      packet.free(p)
+   if self.conns[p_id] then
+      conn_id = self.conns[p_id]
+      self.conn_packs[conn_id] = self.conn_packs[conn_id] + 1
+      -- print(conn_id..": "..self.conn_packs[conn_id])
    end
 
 end
