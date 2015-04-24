7f45eddceee0a7745fac93e2d795a9f1e1b54444
Katerina Barone-Adesi <kbarone@igalia.com>
2015-04-24 09:16:54 +0100

    Rewrites packets, but doesn't update checksums yet

--- src/apps/basicnat/basicnat.lua
+++ src/apps/basicnat/basicnat.lua
@@ -15,18 +15,13 @@ local function bytes_to_uint32(a, b, c, d)
    return a * 2^24 + b * 2^16 + c * 2^8 + d
 end
 
-local function get_src_ip(pkt)
-   local d = pkt.data
-   return bytes_to_uint32(d[26], d[27], d[28], d[29])
-end
-
-local function get_dst_ip(pkt)
-   local d = pkt.data
-   return bytes_to_uint32(d[30], d[31], d[32], d[33])
-end
-
---local function uint32_to_bytes(u)
---end 
+local function uint32_to_bytes(u)
+   a = bit.rshift(bit.band(u, 0xff000000) % 2^32, 24)
+   b = bit.rshift(bit.band(u, 0x00ff0000), 16)
+   c = bit.rshift(bit.band(u, 0x0000ff00), 8)
+   d = bit.band(u, 0x000000ff)
+   return a, b, c, d
+end 
 
 local function str_ip_to_uint32(ip)
    local a, b, c, d = ip:match("([0-9]+).([0-9]+).([0-9]+).([0-9]+)")
@@ -45,13 +40,37 @@ local function str_net_to_uint32(ip)
    return bit.band(base_ip, mask) % 2^32, mask
 end
 
-local function set_src_ip(pkt, ip)
-   return pkt -- TODO
+local function get_src_ip(pkt)
+   local d = pkt.data
+   return bytes_to_uint32(d[26], d[27], d[28], d[29])
 end
 
+local function get_dst_ip(pkt)
+   local d = pkt.data
+   return bytes_to_uint32(d[30], d[31], d[32], d[33])
+end
+
+-- TODO: fix the checksum
+local function set_src_ip(pkt, ip)
+   a, b, c, d = uint32_to_bytes(ip)
+   pkt.data[26] = a
+   pkt.data[27] = b
+   pkt.data[28] = c
+   pkt.data[29] = d
+   return pkt
+end
+
+-- TODO: fix the checksum
+local function set_dst_ip(pkt, ip)
+   a, b, c, d = uint32_to_bytes(ip)
+   pkt.data[30] = a
+   pkt.data[31] = b
+   pkt.data[32] = c
+   pkt.data[33] = d
+   return pkt
+end
 
 local function ip_in_net(ip, net, mask)
-  print(ip, net, mask)
   return net == bit.band(ip, mask) % 2^32
 end
 
@@ -61,13 +80,13 @@ end
 -- packets, the destination IP address and the checksums as listed above
 -- are translated.
 -- TODO: make this able to deal with a range of external IPs, not just one
-local function basic_rewrite(external_ip, internal_net, mask, pkt)
+local function basic_rewrite(pkt, external_ip, internal_net, mask)
    local src_ip, dst_ip = get_src_ip(pkt), get_dst_ip(pkt)
    if ip_in_net(src_ip, internal_net, mask) then
-      print("src", src_ip)
+      set_src_ip(pkt, external_ip)
    end
    if ip_in_net(dst_ip, internal_net, mask) then
-      print("dst", dst_ip)
+      set_dst_ip(pkt, external_ip)
    end
    return pkt
 end
@@ -77,6 +96,6 @@ function BasicNAT:push ()
    local pkt = link.receive(i)
    local external_ip = str_ip_to_uint32("10.0.0.1")
    local internal_net, mask = str_net_to_uint32("178.0.0.0/8")
-   local natted_pkt = basic_rewrite(external_ip, internal_net, mask, pkt)
+   local natted_pkt = basic_rewrite(pkt, external_ip, internal_net, mask)
    link.transmit(o, natted_pkt)
 end
