9e03abf3e90c4c197533613ac2a3d53dfef2a9eb
Justin Cormack <justin@specialbusservice.com>
2015-05-04 18:12:21 +0100

    fix include netbsd/c

--- syscall/netbsd/c.lua
+++ syscall/netbsd/c.lua
@@ -48,46 +48,43 @@ local C = setmetatable({}, {
 -- for NetBSD we use libc names not syscalls, as assume you will have libc linked or statically linked with all symbols exported.
 -- this is so we can use NetBSD libc even where syscalls have been redirected to rump calls.
 
--- was using __sysctl, but many sysctls provided by libc
-C.sysctl = ffi.C.sysctl
-
 -- use new versions
-C.mount = ffi.C.__mount50
-C.stat = ffi.C.__stat50
-C.fstat = ffi.C.__fstat50
-C.lstat = ffi.C.__lstat50
-C.getdents = ffi.C.__getdents30
-C.socket = ffi.C.__socket30
-C.select = ffi.C.__select50
-C.pselect = ffi.C.__pselect50
-C.fhopen = ffi.C.__fhopen40
-C.fhstat = ffi.C.__fhstat50
-C.fhstatvfs1 = ffi.C.__fhstatvfs140
-C.utimes = ffi.C.__utimes50
-C.posix_fadvise = ffi.C.__posix_fadvise50
-C.lutimes = ffi.C.__lutimes50
-C.futimes = ffi.C.__futimes50
-C.getfh = ffi.C.__getfh30
-C.kevent = ffi.C.__kevent50
-C.mknod = ffi.C.__mknod50
-C.pollts = ffi.C.__pollts50
+C.mount = C.__mount50
+C.stat = C.__stat50
+C.fstat = C.__fstat50
+C.lstat = C.__lstat50
+C.getdents = C.__getdents30
+C.socket = C.__socket30
+C.select = C.__select50
+C.pselect = C.__pselect50
+C.fhopen = C.__fhopen40
+C.fhstat = C.__fhstat50
+C.fhstatvfs1 = C.__fhstatvfs140
+C.utimes = C.__utimes50
+C.posix_fadvise = C.__posix_fadvise50
+C.lutimes = C.__lutimes50
+C.futimes = C.__futimes50
+C.getfh = C.__getfh30
+C.kevent = C.__kevent50
+C.mknod = C.__mknod50
+C.pollts = C.__pollts50
 
-C.gettimeofday = ffi.C.__gettimeofday50
-C.settimeofday = ffi.C.__settimeofday50
-C.adjtime = ffi.C.__adjtime50
-C.setitimer = ffi.C.__setitimer50
-C.getitimer = ffi.C.__getitimer50
-C.clock_gettime = ffi.C.__clock_gettime50
-C.clock_settime = ffi.C.__clock_settime50
-C.clock_getres = ffi.C.__clock_getres50
-C.nanosleep = ffi.C.__nanosleep50
-C.timer_settime = ffi.C.__timer_settime50
-C.timer_gettime = ffi.C.__timer_gettime50
+C.gettimeofday = C.__gettimeofday50
+C.settimeofday = C.__settimeofday50
+C.adjtime = C.__adjtime50
+C.setitimer = C.__setitimer50
+C.getitimer = C.__getitimer50
+C.clock_gettime = C.__clock_gettime50
+C.clock_settime = C.__clock_settime50
+C.clock_getres = C.__clock_getres50
+C.nanosleep = C.__nanosleep50
+C.timer_settime = C.__timer_settime50
+C.timer_gettime = C.__timer_gettime50
 
 -- use underlying syscall not wrapper
-C.getcwd = ffi.C.__getcwd
+C.getcwd = C.__getcwd
 
-C.sigaction = ffi.C.__libc_sigaction14 -- TODO not working I think need to use tramp_sigaction, see also netbsd pause()
+C.sigaction = C.__libc_sigaction14 -- TODO not working I think need to use tramp_sigaction, see also netbsd pause()
 
 return C
 
