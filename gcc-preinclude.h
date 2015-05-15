30505c667a8a4b3a9793ae413132d95a05c05f44
Kacper Wysocki <comotion@delta9.pl>
2015-05-15 17:09:39 +0200

    include a libc header to get the __GLIBC__ define

--- gcc-preinclude.h
+++ gcc-preinclude.h
@@ -2,7 +2,10 @@
 // See: http://www.win.tue.nl/~aeb/linux/misc/gcc-semibug.html
 //      https://rjpower9000.wordpress.com/2012/04/09/fun-with-shared-libraries-version-glibc_2-14-not-found/
 
-#if !defined(__ASSEMBLER__) && defined(__GLIBC__)
+#ifndef __ASSEMBLER__
+#include <string.h>
+#ifdef __GLIBC__
 __asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
+#endif
 #endif
 
