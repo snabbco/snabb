c96f1e806405eb2e4c07044eb0188e61a5dd82f7
Kacper Wysocki <comotion@delta9.pl>
2015-05-15 15:37:46 +0200

    only back-version memcpy on GLIBC platforms

--- gcc-preinclude.h
+++ gcc-preinclude.h
@@ -2,7 +2,7 @@
 // See: http://www.win.tue.nl/~aeb/linux/misc/gcc-semibug.html
 //      https://rjpower9000.wordpress.com/2012/04/09/fun-with-shared-libraries-version-glibc_2-14-not-found/
 
-#ifndef __ASSEMBLER__
+#if !defined(__ASSEMBLER__) && defined(__GLIBC__)
 __asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
 #endif
 
