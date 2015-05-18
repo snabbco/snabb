c71117ffd84730e934a4a6c3b62303e07739571e
Max Rottenkolber <max@mr.gy>
2015-05-18 11:20:48 +0200

    [neutron-sync-*] Don't print command outputs and print timestamped log messages instead.

--- src/program/snabbnfv/neutron_sync_agent/neutron_sync_agent.sh
+++ src/program/snabbnfv/neutron_sync_agent/neutron_sync_agent.sh
@@ -32,24 +32,29 @@ mkdir -p $TMP_DIR
 
 initial=true
 
+function log { echo "[$(date +"%F %T %Z")]" "$1"; }
+
 # Loop pulling/cloning the repo.
 while true; do
     if [ ! -d $NEUTRON_DIR ]; then
-    git clone git://$SYNC_HOST/$SYNC_PATH $NEUTRON_DIR
+        log "Initializing $NEUTRON_DIR"
+        git clone git://$SYNC_HOST/$SYNC_PATH $NEUTRON_DIR >/dev/null
     fi
     cd $NEUTRON_DIR
-    git fetch
-    git diff --quiet origin/master
+    git fetch >/dev/null
+    git diff --quiet origin/master >/dev/null 2>&1
     if [ $? != 0 -o $initial = true ]; then
-        git pull --rebase origin master
+        log "Generating new configuration"
+        git pull --rebase origin master >/dev/null 2>&1
         git reflog expire --expire-unreachable=0 --all
         git prune --expire 0
-        echo "Generating new configuration"
-        $NEUTRON2SNABB $NEUTRON_DIR $TMP_DIR
+        $NEUTRON2SNABB $NEUTRON_DIR $TMP_DIR >/dev/null
         # Only (atomically) replace configurations that have changed.
         for conf in $TMP_DIR/*; do
             dest=$SNABB_DIR/$(basename $conf)
-            if ! diff $conf $dest; then mv -f $conf $dest; fi
+            echo "$dest"
+            if ! diff $conf $dest; then mv -f $conf $dest;
+            else echo "Unchanged."; fi
         done
         initial=false
     fi
