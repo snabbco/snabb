ce1401addf6b9c3e063c387b7bf0b99b4eb1815f
Katerina Barone-Adesi <kbarone@igalia.com>
2015-05-23 16:07:16 +0100

    Fix up ip, tcp, and upd checksums

--- src/apps/basicnat/basicnat.lua
+++ src/apps/basicnat/basicnat.lua
@@ -1,6 +1,7 @@
 module(..., package.seeall)
 
 bit = require("bit")
+ffi = require("ffi")
 
 --- ### `basicnat` app: Implement http://www.ietf.org/rfc/rfc1631.txt Basic NAT
 
@@ -14,6 +15,11 @@ end
 local function bytes_to_uint32(a, b, c, d)
    return a * 2^24 + b * 2^16 + c * 2^8 + d
 end
+
+local ipv4_base = 14 -- Ethernet encapsulated ipv4
+local transport_base = 34 -- tranport layer (TCP/UDP/etc) header start
+local proto_tcp = 6
+local proto_udp = 17
 
 local function uint32_to_bytes(u)
    a = bit.rshift(bit.band(u, 0xff000000) % 2^32, 24)
@@ -50,23 +56,85 @@ local function get_dst_ip(pkt)
    return bytes_to_uint32(d[30], d[31], d[32], d[33])
 end
 
+local function csum_carry_and_not(checksum)
+   while checksum > 0xffff do -- process the carry nibbles
+      local carry = bit.rshift(checksum, 16)
+      checksum = bit.band(checksum, 0xffff) + carry
+   end
+   return bit.band(bit.bnot(checksum), 0xffff)
+end
+
+local function ipv4_checksum(pkt)
+   local checksum = 0
+   for i = ipv4_base, ipv4_base + 18, 2 do
+      if i ~= ipv4_base + 10 then -- The checksum bytes are assumed to be 0
+         checksum = checksum + pkt.data[i] * 0x100 + pkt.data[i+1]
+      end
+   end
+   return csum_carry_and_not(checksum)
+end
+
+local function transport_checksum(pkt)
+   local checksum = 0
+   -- First 64 bytes of the TCP pseudo-header: the ip addresses
+   for i = ipv4_base + 12, ipv4_base + 18, 2 do
+      checksum = checksum + pkt.data[i] * 0x100 + pkt.data[i+1]
+   end
+   -- Add the protocol field of the IPv4 header to the checksum
+   local protocol = pkt.data[ipv4_base + 9]
+   checksum = checksum + protocol
+   local tcplen = pkt.data[ipv4_base + 2] * 0x100 + pkt.data[ipv4_base + 3] - 20
+   checksum = checksum + tcplen -- end of pseudo-header
+
+   for i = transport_base, transport_base + tcplen - 2, 2 do
+      if i ~= transport_base + 16 then -- The checksum bytes are zero
+         checksum = checksum + pkt.data[i] * 0x100 + pkt.data[i+1]
+      end
+   end
+   if tcplen % 2 == 1 then
+      checksum = checksum + pkt.data[transport_base + tcplen - 1]
+   end
+   return csum_carry_and_not(checksum)
+end
+
+
+local function fix_checksums(pkt)
+   local ipchecksum = ipv4_checksum(pkt)
+   pkt.data[ipv4_base + 10] = bit.rshift(ipchecksum, 8)
+   pkt.data[ipv4_base + 11] = bit.band(ipchecksum, 0xff)
+   local transport_proto = pkt.data[ipv4_base + 9]
+   if transport_proto == proto_tcp then
+      local transport_checksum = transport_checksum(pkt)
+      pkt.data[transport_base + 16] = bit.rshift(transport_checksum, 8)
+      pkt.data[transport_base + 17] = bit.band(transport_checksum, 0xff)
+      return true
+   elseif transport_proto == proto_udp then
+      -- ipv4 udp checksums are optional
+      pkt.data[transport_base + 6] = 0
+      pkt.data[transport_base + 7] = 0
+      return true
+   else
+      return false -- didn't attempt to change a transport-layer checksum
+   end
+end
+
 -- TODO: fix the checksum
 local function set_src_ip(pkt, ip)
    a, b, c, d = uint32_to_bytes(ip)
-   pkt.data[26] = a
-   pkt.data[27] = b
-   pkt.data[28] = c
-   pkt.data[29] = d
+   pkt.data[ipv4_base + 12] = a
+   pkt.data[ipv4_base + 13] = b
+   pkt.data[ipv4_base + 14] = c
+   pkt.data[ipv4_base + 15] = d
    return pkt
 end
 
 -- TODO: fix the checksum
 local function set_dst_ip(pkt, ip)
    a, b, c, d = uint32_to_bytes(ip)
-   pkt.data[30] = a
-   pkt.data[31] = b
-   pkt.data[32] = c
-   pkt.data[33] = d
+   pkt.data[ipv4_base + 16] = a
+   pkt.data[ipv4_base + 17] = b
+   pkt.data[ipv4_base + 18] = c
+   pkt.data[ipv4_base + 19] = d
    return pkt
 end
 
@@ -81,6 +149,8 @@ end
 -- are translated.
 -- TODO: make this able to deal with a range of external IPs, not just one
 local function basic_rewrite(pkt, external_ip, internal_net, mask)
+   -- Only attempt to alter ipv4 packets. Assume an Ethernet encapsulation.
+   if pkt.data[12] ~= 8 or pkt.data[13] ~= 0 then return pkt end
    local src_ip, dst_ip = get_src_ip(pkt), get_dst_ip(pkt)
    if ip_in_net(src_ip, internal_net, mask) then
       set_src_ip(pkt, external_ip)
@@ -88,6 +158,7 @@ local function basic_rewrite(pkt, external_ip, internal_net, mask)
    if ip_in_net(dst_ip, internal_net, mask) then
       set_dst_ip(pkt, external_ip)
    end
+   fix_checksums(pkt)
    return pkt
 end
 
