f450847bdef0fdf3d0204ae847919f5b65183bc2
Katerina Barone-Adesi <kbarone@igalia.com>
2015-07-08 01:14:23 +0100

    Minimal wireshark-parseable ipv6 encapsulation

--- src/apps/ipv6_tunnel/ipv6_tunnel.lua
+++ src/apps/ipv6_tunnel/ipv6_tunnel.lua
@@ -1,6 +1,9 @@
 module(..., package.seeall)
 
+local datagram = require("lib.protocol.datagram")
+local ethernet = require("lib.protocol.ethernet")
 local ipv6 = require("lib.protocol.ipv6")
+local packet = require("core.packet")
 
 -- TODO: handle fragmentation
 --local IPv6HeaderSize = 40 -- bytes
@@ -14,14 +17,53 @@ function IPv6Tunnel:new (conf)
    return setmetatable(c, {__index=IPv6Tunnel})
 end
 
+local function print_pkt(pkt)
+   local fbytes = {}
+   for i=0,pkt.length - 1 do table.insert(fbytes, string.format("0x%x", pkt.data[i])) end
+   print(string.format("Len: %i: ", pkt.length) .. table.concat(fbytes, " "))
+end
+
+local function pp(t)
+   for k,v in pairs(t) do print(k,v) end
+end
+
 --TODO: is it better to modify or copy pkt?
 local function ipv6_encapsulate(pkt, src_ip, dst_ip)
-   local next_header_type = 58 -- FIXME
-   local ipv6_pkt = ipv6:new({next_header = next_header_type,
+   local dgram = datagram:new(pkt, ethernet)
+   print_pkt(pkt)
+   local ethernet_header_size = 14
+   -- TODO: verify that the last 2 bytes are 0x0800, IPv4
+   dgram:pop_raw(ethernet_header_size)
+   -- TODO: decrement the IPv4 ttl as this is part of forwarding
+   -- TODO: do not encapsulate if ttl was already 0; send icmp
+   local payload_len = pkt.length
+   print("popped")
+   print_pkt(pkt)
+   local next_header_type = 4 -- IPv4
+   local ipv6_hdr = ipv6:new({next_header = next_header_type,
                               hop_limit = 255,
                               src = src_ip,
                               dst = dst_ip})
-   return ipv6_pkt
+   pp(ipv6_hdr)
+ 
+   local ipv6_ethertype = 0x86dd
+   local eth_hdr = ethernet:new({src = ethernet:pton("77:77:77:77:77:77"),
+                                 dst = ethernet:pton("66:66:66:66:66:66"),
+                                 type = ipv6_ethertype})
+   
+   print_pkt(pkt)
+   print("eth-prepush")
+   print_pkt(pkt)
+   dgram:push(ipv6_hdr)
+   -- The API makes setting the payload length awkward; set it manually
+   pkt.data[4] = bit.band(payload_len, 0xff00)
+   pkt.data[5] = bit.band(payload_len, 0xff)
+   print("ipv6 pushed")
+   print_pkt(pkt)
+   dgram:push(eth_hdr)
+   print("eth-pushed")
+   print_pkt(pkt)
+   return pkt
 end
 
 -- TODO: revisit this and check on performance idioms
@@ -32,4 +74,5 @@ function IPv6Tunnel:push ()
    local encap_pkt = ipv6_encapsulate(pkt, self.local_ip, self.to_ip)
    print("encapsulated")
    link.transmit(o, encap_pkt)
+   print("tx'd")
 end
