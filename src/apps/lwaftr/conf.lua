39c40aba7f97b4e4450e84d6ce63c23f92b0391a
Katerina Barone-Adesi <kbarone@igalia.com>
2015-07-09 10:08:19 +0100

    Refactoring, starting policy stubs

--- src/apps/lwaftr/conf.lua
+++ src/apps/lwaftr/conf.lua
@@ -1,12 +1,18 @@
 module(..., package.seeall)
 
+local ethernet = require("lib.protocol.ethernet")
 local ipv6 = require("lib.protocol.ipv6")
 
 local bt = require("apps.lwaftr.binding_table")
 
+DROP_POLICY = 1
+
+
 local aftrconf = {
    aftr_ipv6_ip = ipv6:pton('8:9:a:b:c:d:e:f'),
-   binding_table = bt.get_binding_table()
+   aftr_mac = ethernet:pton("22:22:22:22:22:22"),
+   binding_table = bt.get_binding_table(),
+   icmp_policy = DROP_POLICY
 }
 
 function get_aftrconf()
