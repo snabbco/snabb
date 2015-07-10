e47f9118c3ae293d6b6c684e666fdde51b01e0f7
Katerina Barone-Adesi <kbarone@igalia.com>
2015-07-10 18:49:14 +0100

    Added (untested) code to optionally send icmp on binding lookup failure
    Also refactored ethernet handling.

--- src/apps/lwaftr/conf.lua
+++ src/apps/lwaftr/conf.lua
@@ -1,18 +1,24 @@
 module(..., package.seeall)
 
 local ethernet = require("lib.protocol.ethernet")
+local ipv4 = require("lib.protocol.ipv4")
 local ipv6 = require("lib.protocol.ipv6")
 
 local bt = require("apps.lwaftr.binding_table")
 
 DROP_POLICY = 1
-
+DISCARD_PLUS_ICMP_POLICY = 2
 
 local aftrconf = {
+   aftr_ipv4_ip = ipv4:pton("10.10.10.10"),
    aftr_ipv6_ip = ipv6:pton('8:9:a:b:c:d:e:f'),
-   aftr_mac = ethernet:pton("22:22:22:22:22:22"),
+   aftr_mac_b4_side = ethernet:pton("22:22:22:22:22:22"),
+   aftr_mac_inet_side = ethernet:pton("22:22:22:22:22:22"),
+   b4_mac = ethernet:pton("44:44:44:44:44:44"),
    binding_table = bt.get_binding_table(),
-   icmp_policy = DROP_POLICY
+   icmp_policy = DROP_POLICY,
+   inet_mac = ethernet:pton("68:68:68:68:68:68"),
+   ipv4_lookup_failed_policy = DISCARD_PLUS_ICMP_POLICY
 }
 
 function get_aftrconf()
