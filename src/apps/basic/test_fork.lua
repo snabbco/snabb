19ae0e558e9348029275f036087a00f6368efeee
Javier Guerra <javier@guerrag.com>
2015-07-13 02:29:29 -0500

    simplest test of two-fork network

---
+++ src/apps/basic/test_fork.lua
@@ -0,0 +1,15 @@
+module(..., package.seeall)
+
+basic_apps = require ('apps.basic.basic_apps')
+
+function selftest()
+   local c = config.new()
+   config.cpu(c, 'proc1')
+   config.cpu(c, 'proc2')
+   config.app(c, 'source', basic_apps.Source, {size=120, cpu='proc1'})
+   config.app(c, 'sink', basic_apps.Sink, {cpu='proc2'})
+   config.link(c, 'source.output -> sink.input')
+   engine.configure(c)
+
+   engine.main{duration=10, report={showlinks=true}}
+end
