e2570733dd80aa77701d56ea90b837a92bc5e604
Katerina Barone-Adesi <kbarone@igalia.com>
2015-07-29 13:35:33 +0100

    Investigating ICMPv4 error responses

--- tests/apps/lwaftr/end-to-end/end-to-end.sh
+++ tests/apps/lwaftr/end-to-end/end-to-end.sh
@@ -15,26 +15,38 @@ function quit_with_msg {
 
 function snabb_run_and_cmp {
    rm -f ${TEST_OUT}/endout.pcap
+   echo "${SNABB_BASE}/src/snabb snsh ${SNABB_BASE}/src/apps/lwaftr/main.lua \
+    $1 $2 ${TEST_OUT}/endout.pcap"
    ${SNABB_BASE}/src/snabb snsh ${SNABB_BASE}/src/apps/lwaftr/main.lua \
-    $1 $2 ${TEST_OUT}/endout.pcap  || quit_with_msg "Snabb failed"
+    $1 $2 ${TEST_OUT}/endout.pcap >/dev/null || quit_with_msg "Snabb failed"
    echo "cmp $3"; ls -lh $3 ${TEST_OUT}/endout.pcap
    cmp $3 ${TEST_OUT}/endout.pcap || \
-      quit_with_err "snabb snsh apps/lwaftr/main.lua $1 $2 $3"
+      quit_with_msg "snabb snsh apps/lwaftr/main.lua $1 $2 $3"
 }
 
-echo "Testing with a from-internet IPv4 packet found in the binding table."
+echo "Testing: from-internet IPv4 packet found in the binding table."
 snabb_run_and_cmp ${TEST_BASE}/icmp_on_fail.conf \
    ${TEST_BASE}/tcp-frominet-bound.pcap ${TEST_BASE}/tcp-afteraftr-ipv6.pcap
 
 
-echo "Testing with a from-internet IPv4 packet NOT found in the binding table."
+echo "Testing: from-internet IPv4 packet NOT found in the binding table."
 snabb_run_and_cmp ${TEST_BASE}/no_icmp.conf \
    ${TEST_BASE}/tcp-frominet-unbound.pcap ${TEST_BASE}/empty.pcap
 
+echo "Testing: from-internet IPv4 packet NOT found in the binding table (ICMP-on-fail)."
+snabb_run_and_cmp ${TEST_BASE}/icmp_on_fail.conf \
+   ${TEST_BASE}/tcp-frominet-unbound.pcap ${TEST_BASE}/empty.pcap
 # TODO: change the conf and test the ICMP reply to the above
 
-echo "Testing with a from-b4 IPv6 packet NOT found in the binding table."
+echo "Testing: from-b4 IPv6 packet NOT found in the binding table."
 snabb_run_and_cmp ${TEST_BASE}/no_icmp.conf \
    ${TEST_BASE}/tcp-afteraftr-ipv6.pcap ${TEST_BASE}/empty.pcap
+
+# echo "Testing: from-bp IPv6 packet found in the binding table." -> ipv4
+# echo "Testing: from-bp IPv6 packet hairpinning" -> ipv6
+# echo "Testing: from-bp IPv6 packet to other-b4 host, no hairpinning."->ipv4
+# echo "Testing: from-b4 IPv6 packet NOT found in the binding table (ICMP-on-fail)."
+
+# Test ICMP inputs (with and without drop policy)
 
 echo "All end-to-end lwAFTR tests passed."
