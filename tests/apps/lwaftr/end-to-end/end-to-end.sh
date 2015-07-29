dc501e6d5d08f0cb9fc35c2813f804b94d90fd6a
Katerina Barone-Adesi <kbarone@igalia.com>
2015-07-29 23:15:01 +0100

    All non-icmp hairpinning tests pass

--- tests/apps/lwaftr/end-to-end/end-to-end.sh
+++ tests/apps/lwaftr/end-to-end/end-to-end.sh
@@ -15,13 +15,15 @@ function quit_with_msg {
 
 function snabb_run_and_cmp {
    rm -f ${TEST_OUT}/endout.pcap
-   echo "${SNABB_BASE}/src/snabb snsh ${SNABB_BASE}/src/apps/lwaftr/main.lua \
-    $1 $2 ${TEST_OUT}/endout.pcap"
+   #echo "${SNABB_BASE}/src/snabb snsh ${SNABB_BASE}/src/apps/lwaftr/main.lua \
+   # $1 $2 ${TEST_OUT}/endout.pcap"
    ${SNABB_BASE}/src/snabb snsh ${SNABB_BASE}/src/apps/lwaftr/main.lua \
-    $1 $2 ${TEST_OUT}/endout.pcap > /dev/null || quit_with_msg "Snabb failed"
-   echo "cmp $3"; ls -lh $3 ${TEST_OUT}/endout.pcap
+      $1 $2 ${TEST_OUT}/endout.pcap $SNABB_OUT || quit_with_msg \
+        "Snabb failed: ${SNABB_BASE}/src/snabb snsh \
+         ${SNABB_BASE}/src/apps/lwaftr/main.lua  $1 $2 ${TEST_OUT}/endout.pcap"
+   #echo "cmp $3"; ls -lh $3 ${TEST_OUT}/endout.pcap
    cmp $3 ${TEST_OUT}/endout.pcap || \
-      quit_with_msg "snabb snsh apps/lwaftr/main.lua $1 $2 $3"
+      quit_with_msg "snabb snsh apps/lwaftr/main.lua $1 $2 $3 \ncmp $3"
 }
 
 echo "Testing: from-internet IPv4 packet found in the binding table."
@@ -41,12 +43,33 @@ echo "Testing: from-b4 IPv6 packet NOT found in the binding table."
 snabb_run_and_cmp ${TEST_BASE}/no_icmp.conf \
    ${TEST_BASE}/tcp-afteraftr-ipv6.pcap ${TEST_BASE}/empty.pcap
 
-echo "Testing: from-bp IPv6 packet found in the binding table." #-> ipv4
+echo "Testing: from-b4 IPv6 packet found in the binding table."
 snabb_run_and_cmp ${TEST_BASE}/no_icmp.conf \
    ${TEST_BASE}/tcp-fromb4-ipv6.pcap ${TEST_BASE}/decap-ipv4.pcap
 
-# echo "Testing: from-bp IPv6 packet hairpinning" -> ipv6
-# echo "Testing: from-bp IPv6 packet to other-b4 host, no hairpinning."->ipv4
+echo "Testing: from-b4 IPv6 packet, no hairpinning, B4-IPv6 non-B4-IPv4 dest"
+# The idea is that with hairpinning off, the packet goes out the inet interface
+# and something else routes it back for re-encapsulation. It's not clear why
+# this would be desired behaviour, but it's my reading of the RFC draft.
+# Note: this works regardless of the destination IPv4 address and whether
+# it is actually associated with the destination B4, which is even more
+# questionable.
+snabb_run_and_cmp ${TEST_BASE}/no_hairpin.conf \
+   ${TEST_BASE}/tcp-fromb4-partly-tob4-ipv6.pcap ${TEST_BASE}/decap-ipv4-nohair-iffy.pcap
+
+echo "Testing: from-b4 IPv6 packet, no hairpinning, B4-IPv6 and IPv4 dest"
+# The idea is that with hairpinning off, the packet goes out the inet interface
+# and something else routes it back for re-encapsulation. It's not clear why
+# this would be desired behaviour, but it's my reading of the RFC draft.
+snabb_run_and_cmp ${TEST_BASE}/no_hairpin.conf \
+   ${TEST_BASE}/tcp-fromb4-tob4-ipv6.pcap ${TEST_BASE}/decap-ipv4-nohair.pcap
+
+# TODO: verify that the partly-tob4 pcap errors with hairpinning, with-icmp.
+
+echo "Testing: from-b4 IPv6 packet, with hairpinning"
+snabb_run_and_cmp ${TEST_BASE}/no_icmp.conf \
+   ${TEST_BASE}/tcp-fromb4-tob4-ipv6.pcap ${TEST_BASE}/recap-ipv6.pcap
+
 # echo "Testing: from-b4 IPv6 packet NOT found in the binding table (ICMP-on-fail)."
 
 # Test ICMP inputs (with and without drop policy)
