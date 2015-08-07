befd3975d913a1e63ad7f4cb4732bd010ee9b4bc
Luke Gorrie <luke@snabb.co>
2015-08-07 18:38:35 +0200

    core.memory: Catch SIGSEGV to map DMA memory [PoC]
    This is a quick and dirty proof-of-concept hack done to support
    SnabbCo/snabbswitch#565 (multicore tests):
    
    Extend core.memory so that the virtual addresses of all packets are
    valid in all Snabb Switch processes. The memory allocated by
    memory.dma_alloc() is treated specially: if another process tries to
    access it then this triggers a SIGSEGV handler that will detect which
    memory is needed and automatically map it into the process.
    
    Two simple test scripts are provided: s1 that allocates memory and s2
    that modifies it without any explicit mapping.
    
    Run like this:
    
        $ sudo ./snabb snsh s1
        Installing SIGSEGV handler
        address cdata<char *>: 0x5009f7c00000
        current value = 0
        current value = 0
        current value = 0
        ...
    
    Then use the same address to change the value with s2 that simply
    converts it to a pointer and pokes it:
    
        $ make && sudo strace  ./snabb snsh s2 0x5009f7c00000 42
    
    and now you see the new value in s1:
    
        current value = 42
    
    This is really quick and dirty! I will clean it up if it works and we
    decide to keep it.

--- src/core/memory.c
+++ src/core/memory.c
@@ -13,6 +13,7 @@
 
 #include <assert.h>
 #include <fcntl.h>
+#include <signal.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -95,7 +96,18 @@ void *allocate_huge_page(int size)
   if (mlock(realptr, size) != 0) { goto fail; }
   memset(realptr, 0, size); // zero memory to avoid potential surprises
   shmdt(tmpptr);
-  shmctl(shmid, IPC_RMID, 0);
+  //shmctl(shmid, IPC_RMID, 0);
+  
+  // proof-of-concept hack: write shm id to a file.
+  {
+    char fname[256];
+    FILE *f;
+    snprintf(fname, sizeof(fname), "/tmp/%p", (void*)physical_address);
+    f = fopen(fname, "w");
+    fprintf(f, "%d\n", shmid);
+    fclose(f);
+  }
+  
   return realptr;
  fail:
   if (tmpptr  != MAP_FAILED) { shmdt(tmpptr); }
@@ -104,3 +116,33 @@ void *allocate_huge_page(int size)
   return NULL;
 }
 
+// SIGSEGV handler: attempts map packet memory on demand
+void allocate_on_sigsegv(int sig, siginfo_t *si, void *unused)
+{
+  uint64_t address = (uint64_t)si->si_addr;
+  printf("address = %p\n", si->si_addr);
+  if ((address & 0x500000000000ULL) != 0x500000000000ULL) {
+    // This is not DMA memory: die.
+    //exit(139);
+  } else {
+    char fname[256];
+    uint64_t physaddr = address  & ~0x500000000000ULL;
+    uint64_t physpage = physaddr & ~(2*1024*1024-1);
+    uint64_t virtpage = address  & ~(2*1024*1024-1);
+    snprintf(fname, sizeof(fname), "/tmp/%p", (void*)physpage);
+    FILE *f = fopen(fname, "r");
+    int id;
+    fscanf(f, "%d", &id);
+    fclose(f);
+    shmat(id, (void*)virtpage, 0);
+  }
+}
+
+void setup_signal()
+{
+  struct sigaction sa;
+  sa.sa_flags = SA_SIGINFO;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_sigaction = allocate_on_sigsegv;
+  assert(sigaction(SIGSEGV, &sa, NULL) != -1);
+}
