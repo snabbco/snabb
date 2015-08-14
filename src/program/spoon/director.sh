410c044166f1aade9666d99130cffce53153272d
Javier Guerra <javier@guerrag.com>
2015-08-14 02:29:12 -0500

    more presentable director.sh script

--- src/program/spoon/director.sh
+++ src/program/spoon/director.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 function cleanup() {
-   rm -f /var/run/snabb/link/spoon_link
-   
-   if [ -e /var/run/snabb/dma_map_ids ]; then
-      od -j4 -vi -w4 "$mapname" | while read offst id; do
+   rm -f /var/run/snabb/link/*
+
+   DMA_IDS="/var/run/snabb/dma_map_ids"
+   if [ -e "$DMA_IDS" ]; then
+      od -j4 -vi -w4 "$DMA_IDS" | while read offst id; do
          if [ -n "$id" -a "$id" != '0' ]; then
             ipcrm -m $id
          fi
       done
-      rm /var/run/snabb/dma_map_ids
+      rm "$DMA_IDS"
    fi
 }
 
 
 cleanup
 ./snabb snsh program/spoon/produce.lua &
-./snabb snsh program/spoon/consume.lua
+./snabb snsh program/spoon/consume.lua &
 
+wait -n
 for j in $(jobs -pr); do
    kill $j
 done
