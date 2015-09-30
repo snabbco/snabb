8226f4b7227b8a4604de94de239f3bed59136dc5
Andy Wingo <wingo@igalia.com>
2015-09-30 14:14:30 +0000

    Better verbosity control in lwaftr run
    Also fix -D option

--- src/program/snabb_lwaftr/run/run.lua
+++ src/program/snabb_lwaftr/run/run.lua
@@ -41,10 +41,9 @@ end
 function parse_args(args)
    if #args == 0 then show_usage(1) end
    local bt_file, conf_file, v4_pci, v6_pci
-   local opts = { verbose = true }
+   local opts = { verbosity = 0 }
    local handlers = {}
-   function handlers.v () opts.verbose = true  end
-   function handlers.u () opts.ultra_verbose = true opts.verbose = true end
+   function handlers.v () opts.verbosity = opts.verbosity + 1 end
    function handlers.D (arg)
       opts.duration = assert(tonumber(arg), "duration must be a number")
    end
@@ -85,9 +84,9 @@ function parse_args(args)
       end
    end
    function handlers.h() show_usage(0) end
-   lib.dogetopt(args, handlers, "b:c:n:m:vuDh",
+   lib.dogetopt(args, handlers, "b:c:n:m:vD:h",
       { bt = "b", conf = "c", ["v4-pci"] = "n", ["v6-pci"] = "m",
-         verbose = "v", ultraverbose = "u", duration = "D", help = "h" })
+         verbose = "v", duration = "D", help = "h" })
    return opts, bt_file, conf_file, v4_pci, v6_pci
 end
 
@@ -114,7 +113,7 @@ function run(args)
    config.link(c, 'lwaftr.v6 -> b4sideNic.rx')
    engine.configure(c)
 
-   if opts.ultra_verbose then
+   if opts.verbosity >= 2 then
       local function lnicui_info()
          app.report_apps()
       end
@@ -122,7 +121,7 @@ function run(args)
       timer.activate(t)
    end
 
-   if opts.verbose then
+   if opts.verbosity >= 1 then
       local csv = csv_stats.CSVStatsTimer.new()
       csv:add_app('inetNic', { 'tx', 'rx' }, { tx='IPv4 RX', rx='IPv4 TX' })
       csv:add_app('b4sideNic', { 'tx', 'rx' }, { tx='IPv6 RX', rx='IPv6 TX' })
