543a50b04d709aee6ea05e0bedccb160701c55cc
Max Rottenkolber <max@mr.gy>
2015-10-08 16:27:24 +0200

    scripts/dock.sh: Use `--rm' to delete container after run and inject DOCKERFLAGS into docker invocation.

--- src/scripts/dock.sh
+++ src/scripts/dock.sh
@@ -4,8 +4,7 @@ export SNABB_TEST_IMAGE=${SNABB_TEST_IMAGE:=eugeneia/snabb-nfv-test}
 
 # Snabb Switch Docker environment
 
-docker run --privileged -i --name "snabb_test-$USER" \
-    -v $(dirname $PWD):/snabbswitch \
+docker run --rm --privileged -i -v $(dirname $PWD):/snabbswitch $DOCKERFLAGS \
     -e SNABB_PCI0=$SNABB_PCI0 \
     -e SNABB_PCI1=$SNABB_PCI1 \
     -e SNABB_PCI_INTEL0=$SNABB_PCI_INTEL0 \
@@ -17,8 +16,3 @@ docker run --privileged -i --name "snabb_test-$USER" \
     -e SNABB_PCAP=$SNABB_PCAP \
     $SNABB_TEST_IMAGE \
     bash -c "mount -t hugetlbfs none /hugetlbfs && (cd snabbswitch/src; $*)"
-status=$?
-
-docker rm "snabb_test-$USER" >/dev/null
-
-exit $status
