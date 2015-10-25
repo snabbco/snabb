a7c1932df1099fc51ced83b1fd3b057263bccc31
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-10-25 12:55:35 +0200

    unix socket app

---
+++ src/apps/socket/unix.lua
@@ -0,0 +1,202 @@
+
+--unix socket app: transmit and receive packets through a named socket.
+--can be used in server (listening) or client (connecting) mode.
+
+module(...,package.seeall)
+
+local ffi    = require("ffi")
+local link   = require("core.link")
+local packet = require("core.packet")
+local S      = require("syscall")
+
+local UnixSocket = {}
+UnixSocket.__index = UnixSocket
+
+function UnixSocket:new (arg)
+
+   --process args
+
+   assert(arg, "filename or options expected")
+
+   local file, listen
+   if type(arg) == "string" then
+      file = arg
+   else
+      file = arg.filename
+      listen = arg.listen
+   end
+   assert(file, "filename expected")
+
+   --ljsyscall returns error as a a cdata instead of a string,
+   --and the standard assert() doesn't use tostring() on it.
+   local function assert(v, ...)
+      if not v then error(tostring((...)), 2) end
+      return v, ...
+   end
+
+   --open/close socket
+
+   local open, close
+
+   if listen then --server mode
+
+      local sock = assert(S.socket("unix", "stream, nonblock"))
+      S.unlink(file) --unlink to avoid EINVAL on bind()
+      local sa = S.t.sockaddr_un(file)
+      assert(sock:bind(sa))
+      assert(sock:listen())
+
+      function close()
+         S.close(sock)
+         S.unlink(file)
+      end
+
+      function open()
+         local sa = S.t.sockaddr_un()
+         local csock, err = sock:accept(sa)
+         if not csock then
+            if err.AGAIN then return end
+            assert(nil, err)
+         end
+         local close0 = close
+         function close()
+            S.close(csock)
+            close0()
+         end
+         return csock
+      end
+
+   else --client mode
+
+      local sock = assert(S.socket("unix", "stream, nonblock"))
+
+      function open()
+         local sa = S.t.sockaddr_un(file)
+         local ok, err = sock:connect(sa)
+         if not ok then
+            if err.CONNREFUSED or err.AGAIN then return end
+            assert(nil, err)
+         end
+         return sock
+      end
+
+      function close()
+         S.close(sock)
+      end
+
+   end
+
+   --send/receive packets
+
+   local sock
+   local function connect()
+      sock = sock or open()
+      return sock
+   end
+
+   local function can_receive()
+      if not connect() then return end
+      return assert(S.select({readfds = {sock}}, 0)).count == 1
+   end
+
+   local function can_send()
+      if not connect() then return end
+      return assert(S.select({writefds = {sock}}, 0)).count == 1
+   end
+
+   local function receive()
+      local p = packet.allocate()
+      local maxsz = ffi.sizeof(p.data)
+      local len, err = S.read(sock, p.data, maxsz)
+      if len == 0 then return end
+      if not len then
+         assert(nil, err)
+      end
+      p.length = len
+      return p
+   end
+
+   local function send(p)
+		assert(S.write(sock, p.data, p.length))
+   end
+
+   --app object
+
+   local self = setmetatable({}, self)
+
+   function self:pull()
+      local l = self.output.tx
+      if l == nil then return end
+      while not link.full(l) and can_receive() do
+         local p = receive()
+         if p then
+            link.transmit(l, p) --link owns p now so we mustn't free it
+         end
+      end
+   end
+
+   function self:push()
+      local l = self.input.rx
+      if l == nil then return end
+      while not link.empty(l) and can_send() do
+         local p = link.receive(l) --we own p now so we must free it
+			send(p)
+         packet.free(p)
+      end
+   end
+
+   function self:stop()
+      close()
+   end
+
+   return self
+end
+
+
+function selftest ()
+
+   local printapp = {}
+   function printapp:new (name)
+      return {
+         push = function(self)
+            local l = self.input.rx
+            if l == nil then return end
+            while not link.empty(l) do
+               local p = link.receive(l)
+               print(name..': ', p.length, ffi.string(p.data, p.length))
+               packet.free(p)
+            end
+         end
+      }
+   end
+
+   local echoapp = {}
+   function echoapp:new (text)
+      return {
+         pull = function(self)
+            local l = self.output.tx
+            if l == nil then return end
+            while not link.full(l) do
+               local p = packet.allocate()
+               ffi.copy(p.data, text)
+               p.length = #text
+               link.transmit(l, p)
+            end
+         end
+      }
+   end
+
+   local file = "/var/tmp/selftest.sock"
+   local c = config.new()
+   config.app(c,  "server", UnixSocket, {filename = file, listen = true})
+   config.app(c,  "client", UnixSocket, file)
+   config.app(c,  "print_client_tx", printapp, "client tx")
+   config.app(c,  "say_hello", echoapp, "hello ")
+   config.link(c, "client.tx -> print_client_tx.rx")
+   config.link(c, "say_hello.tx -> client.rx")
+   config.link(c, "server.tx -> server.rx")
+
+   engine.configure(c)
+   engine.main({duration=0.1, report = {showlinks=true}})
+end
+
