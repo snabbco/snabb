a0ba437de16166a134df9ccdbd16297f9a22d88d
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-11-15 20:19:16 +0200

    lisp: reconnect on disconnect and ignore SIGPIPE

--- src/program/lisper/dev-env/lisp.lua
+++ src/program/lisper/dev-env/lisp.lua
@@ -2,19 +2,28 @@
 
 --LISP controller mock-up program for testing.
 
+io.stdout:setvbuf'no'
+io.stderr:setvbuf'no'
+
+local function assert(v, ...)
+	if v then return v, ... end
+	error(tostring((...)), 2)
+end
+
 local ffi = require("ffi")
 local S   = require("syscall")
 
-CONTROL_SOCK = "/var/tmp/ctrl.socket"
-CONTROL_DATA = [[
+local CONTROL_SOCK = "/var/tmp/ctrl.socket"
+local CONTROL_DATA = [[
 [17185] 00:00:00:00:aa:01 fd80:2::2
 [17185] 00:00:00:00:aa:02 fd80:1::2
 ]]
 
-local sock = assert(S.socket("unix", "stream, nonblock"))
-local sa = S.t.sockaddr_un(CONTROL_SOCK)
+S.signal('pipe', 'ign') --I 💔 Linux
 
 ::retry::
+sock = sock or assert(S.socket("unix", "stream, nonblock"))
+local sa = S.t.sockaddr_un(CONTROL_SOCK)
 local ok, err = sock:connect(sa)
 if not ok then
 	if err.CONNREFUSED or err.AGAIN then
@@ -26,9 +35,15 @@ if not ok then
 end
 print'connected'
 
-while assert(S.select({writefds = {sock}}, 0)).count == 1 do
-	print'sending...'
-	assert(S.write(sock, CONTROL_DATA, #CONTROL_DATA))
+while true do
+	if assert(S.select({writefds = {sock}}, 0)).count == 1 then
+		print'sending...'
+		if not S.write(sock, CONTROL_DATA, #CONTROL_DATA) then
+			print'write error'
+			sock:close()
+			sock = nil
+			goto retry
+		end
+	end
 	S.sleep(1)
 end
-
