c2e1795590d08c6435090bae097aaa8f1f22c6fc
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-11-16 11:40:44 +0200

    lisp.lua now sends the contents of lisp.fib file

--- src/program/lisper/dev-env/lisp.lua
+++ src/program/lisper/dev-env/lisp.lua
@@ -1,25 +1,18 @@
 #!/usr/bin/env luajit
-
---LISP controller mock-up program for testing.
-
 io.stdout:setvbuf'no'
 io.stderr:setvbuf'no'
 
-local function assert(v, ...)
-	if v then return v, ... end
-	error(tostring((...)), 2)
-end
+--LISP controller mock-up program for testing.
 
-local function print() end --comment this to debug
+local function assert(v, ...)
+   if v then return v, ... end
+   error(tostring((...)), 2)
+end
 
 local ffi = require("ffi")
 local S   = require("syscall")
 
 local CONTROL_SOCK = "/var/tmp/ctrl.socket"
-local CONTROL_DATA = [[
-[17185] 00:00:00:00:aa:01 fd80:2::2
-[4660]  00:00:00:00:aa:02 fd80:1::2
-]]
 
 S.signal('pipe', 'ign') --I 💔 Linux
 
@@ -28,24 +21,29 @@ sock = sock or assert(S.socket("unix", "stream, nonblock"))
 local sa = S.t.sockaddr_un(CONTROL_SOCK)
 local ok, err = sock:connect(sa)
 if not ok then
-	if err.CONNREFUSED or err.AGAIN then
-		S.sleep(1)
-		print'retrying...'
-		goto retry
-	end
-	assert(nil, err)
+   if err.CONNREFUSED or err.AGAIN then
+      S.sleep(1)
+      print'retrying...'
+      goto retry
+   end
+   assert(nil, err)
 end
 print'connected'
 
 while true do
-	if assert(S.select({writefds = {sock}}, 0)).count == 1 then
-		print'sending...'
-		if not S.write(sock, CONTROL_DATA, #CONTROL_DATA) then
-			print'write error'
-			sock:close()
-			sock = nil
-			goto retry
-		end
-	end
-	S.sleep(1)
+   if assert(S.select({writefds = {sock}}, 0)).count == 1 then
+
+      local f = assert(io.open'lisp.fib')
+      local data = assert(f:read'*a')
+      f:close()
+
+      print'sending...'
+      if not S.write(sock, data, #data) then
+         print'write error'
+         sock:close()
+         sock = nil
+         goto retry
+      end
+   end
+   S.sleep(1)
 end
