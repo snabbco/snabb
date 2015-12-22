cfa71691e345d170e579d65fa7387827aad89928
Nikolay Nikolaev <n.nikolaev@virtualopensystems.com>
2015-12-22 17:20:10 +0200

    VirtioNet: introduce the user-space virtio-net driver
    An implementation of the virtio-net driver specification, intended to run
    inside a guest virtual machine.
    
    Using indirect descriptors allow for more effective usage of the descriptor
    table. Each statically allocated to a indirect descriptor table, where a
    preallocated packet header is placed. Adding a buffer is simply finding the
    index of the descriptor and putting the buffer physical address into its addr field.

---
+++ src/apps/virtio_net/virtio_net.lua
@@ -0,0 +1,75 @@
+-- Application to connect to a virtio-net driver implementation
+--
+-- Licensed under the Apache 2.0 license
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Copyright (c) 2015 Virtual Open Systems
+--
+
+module(..., package.seeall)
+
+local basic_apps = require("apps.basic.basic_apps")
+local pcap       = require("apps.pcap.pcap")
+local net_dirver = require("lib.virtio.net_driver")
+local main       = require("core.main")
+
+VirtioNet = {}
+VirtioNet.__index = VirtioNet
+
+local receive, transmit, full, nreadable, nwritable = link.receive, link.transmit, link.full, link.nreadable, link.nwritable
+
+function VirtioNet:new(args)
+   return setmetatable({
+      device = assert(net_dirver.VirtioNetDriver:new(args)),
+   }, self)
+end
+
+function VirtioNet:stop()
+   self.device:close()
+end
+
+function VirtioNet:push()
+   local dev = self.device
+   local l = self.input.rx
+
+   dev:recycle_transmit_buffers()
+
+   local to_transmit = math.min(nreadable(l), dev:can_transmit())
+
+   for i=0, to_transmit - 1 do
+      dev:transmit(receive(l))
+   end
+   dev:sync_transmit()
+   dev:notify_transmit()
+end
+
+function VirtioNet:pull()
+   local dev = self.device
+   local l = self.output.tx
+   local to_receive = math.min(nwritable(l), dev:can_receive())
+   
+   for i=0, to_receive - 1 do
+      transmit(l, dev:receive())
+   end
+   dev:add_receive_buffers()
+   dev:sync_receive()
+end
+
+function selftest()
+   local pcidev = os.getenv("SNABB_TEST_VIRTIO_PCIDEV")
+   if not pcidev then
+      print("SNABB_TEST_VIRTIO_PCIDEV was not set\nTest skipped")
+      os.exit(engine.test_skipped_code)
+   end
+   local input_file = "apps/keyed_ipv6_tunnel/selftest.cap.input"
+
+   engine.configure(config.new())
+   local c = config.new()
+   config.app(c, 'source', pcap.PcapReader, input_file)
+   config.app(c, 'VirtioNet', VirtioNet, {pciaddr=pcidev})
+   config.app(c, 'sink', basic_apps.Sink)
+   config.link(c, 'source.output -> VirtioNet.rx')
+   config.link(c, 'VirtioNet.tx -> sink.input')
+   engine.configure(c)
+   engine.main({duration = 1, report={showlinks=true, showapps=true}})
+end
