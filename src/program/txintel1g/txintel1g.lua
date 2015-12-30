51a514d9125ba5fd00c8c66740814fdd2a9e6ed1
hb9cwp <rolf.sommerhalder@alumni.ethz.ch>
2015-12-30 15:03:03 +0100

    Intel1g Tx to test MAC/PHY

--- src/program/txintel1g/txintel1g.lua
+++ src/program/txintel1g/txintel1g.lua
@@ -1,18 +1,28 @@
--- skeletton for program see
+-- skelettons for program see
 --  https://github.com/SnabbCo/snabbswitch/blob/master/src/doc/getting-started.md
+--  src/program/top/
 
-module(...,package.seeall)
+module(..., package.seeall)
 
 local basic= require("apps.basic.basic_apps")
 local intel1g= require("apps.intel.intel1g")
---local lib= require("core.lib")
+local lib= require("core.lib")
+local usage= require("program.txintel1g.Usage_inc")
 
+local long_opts= {
+ help = "h"
+}
 
-function run()
- print("selftest: txIntel1g")
- local pciaddr= os.getenv("SNABB_INTEL1G_1")
+function run(args)
+ print("selftest: txintel1g")
+ --local pciaddr= os.getenv("SNABB_INTEL1G_1")
+ local opt= {}
+ function opt.h(arg) print(usage) main.exit(1) end
+ args= lib.dogetopt(args, opt, "h", long_opts)
+ if #args >1 then print(usage) main.exit(1) end
+ local pciaddr= args[1]
  if not pciaddr then
-  print("SNABB_INTEL1G_1 not set")
+  print("Usage: txintel1g <PCI addr of interface>")
   main.exit(1)
  end
 
@@ -27,7 +37,7 @@ function run()
  engine.app_table.nic.stop()
  --local li = engine.app_table.nic.input[1]
  local li = engine.app_table.nic.input["rx"]          -- same-same as [1]
- assert(li, "intel1g: no input link")
+ assert(li, "txintel1g: no input link")
  local s= link.stats(li)
  print("input link:  txpackets= ", s.txpackets, "  rxpackets= ", s.rxpackets, "  txdrop= ", s.txdrop)
 end
