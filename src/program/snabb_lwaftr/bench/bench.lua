6b4d7890d2d4e43a26ce5136be1dd762a330041d
Andy Wingo <wingo@igalia.com>
2016-01-06 16:46:13 +0100

    Use CSV stats in benchmark instead of statistics apps
    It seems there was a performance overhead of using the statistics apps,
    which makes sense.  Switching to the timer and the counters brings us
    from 3.67MPPS to 3.89MPPS.

--- src/program/snabb_lwaftr/bench/bench.lua
+++ src/program/snabb_lwaftr/bench/bench.lua
@@ -4,6 +4,7 @@ local app = require("core.app")
 local basic_apps = require("apps.basic.basic_apps")
 local config = require("core.config")
 local lib = require("core.lib")
+local csv_stats  = require("lib.csv_stats")
 local pcap = require("apps.pcap.pcap")
 local lwaftr = require("apps.lwaftr.lwaftr")
 
@@ -34,21 +35,23 @@ function run(args)
    config.app(c, "repeaterv4", basic_apps.Repeater)
    config.app(c, "repeaterv6", basic_apps.Repeater)
    config.app(c, "lwaftr", lwaftr.LwAftr, conf_file)
-   config.app(c, "statisticsv4", basic_apps.Statistics)
-   config.app(c, "statisticsv6", basic_apps.Statistics)
    config.app(c, "sinkv4", basic_apps.Sink)
    config.app(c, "sinkv6", basic_apps.Sink)
 
    config.link(c, "capturev4.output -> repeaterv4.input")
    config.link(c, "repeaterv4.output -> lwaftr.v4")
-   config.link(c, "lwaftr.v4 -> statisticsv4.input")
-   config.link(c, "statisticsv4.output -> sinkv4.input")
+   config.link(c, "lwaftr.v4 -> sinkv4.input")
 
    config.link(c, "capturev6.output -> repeaterv6.input")
    config.link(c, "repeaterv6.output -> lwaftr.v6")
-   config.link(c, "lwaftr.v6 -> statisticsv6.input")
-   config.link(c, "statisticsv6.output -> sinkv6.input")
+   config.link(c, "lwaftr.v6 -> sinkv6.input")
 
    app.configure(c)
+
+   local csv = csv_stats.CSVStatsTimer.new()
+   csv:add_app('sinkv4', { 'input' }, { input='Encapsulation' })
+   csv:add_app('sinkv6', { 'input' }, { input='Decapsulation' })
+   csv:activate()
+
    app.main({duration=opts.duration})
 end
