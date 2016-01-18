848005ddffbba48d7088f76f75647c053dd52de0
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2016-01-18 20:45:46 +0200

    loading dasm linker from within the snabb exe

--- src/dasm.lua
+++ src/dasm.lua
@@ -6,7 +6,7 @@ local ffi = require'ffi'
 local bit = require'bit'
 local arch = ffi.arch
 if arch == 'x64' then arch = 'x86' end --same linker for x64
-local C = ffi.load('dasm_'..arch)
+local C = ffi.C
 local M = {C = C}
 
 M._VERSION = 10400
