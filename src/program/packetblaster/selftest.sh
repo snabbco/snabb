3bca4272d5d58b3f384d7eb034adfbbd7660ccca
aequabit <contact@aequabit.de>
2016-01-25 13:01:32 +0100

    packetblaster: Modified selftest to test both replay and synth mode.

--- src/program/packetblaster/selftest.sh
+++ src/program/packetblaster/selftest.sh
@@ -10,7 +10,14 @@ fi
 
 # Simple test: Just make sure packetblaster runs for a period of time
 # (doesn't crash on startup).
-timeout 5 ./snabb packetblaster replay program/snabbnfv/test_fixtures/pcap/64.pcap ${SNABB_PCI0}
+timeout 5 ./snabb packetblaster replay --duration 1 replay program/snabbnfv/test_fixtures/pcap/64.pcap ${SNABB_PCI0}
+status=$?
+if [ $status != 124 ]; then
+    echo "Error: expected timeout (124) but got ${status}"
+    exit 1
+fi
+
+timeout 5 ./snabb packetblaster synth --src 11:11:11:11:11:11 --dst 22:22:22:22:22:22 --sizes 64,128,256 --duration 1 ${SNABB_PCI0}
 status=$?
 if [ $status != 124 ]; then
     echo "Error: expected timeout (124) but got ${status}"
