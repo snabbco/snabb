329315e10b2674832e195ad07a59e29335e6eb45
Luke Gorrie <luke@snabb.co>
2016-02-23 21:01:02 +0000

    core/snabbswitch.c: Require 64-bit word size at compile time
    Using the method suggested by Matthew Emerson on Github #752.

--- src/core/snabbswitch.c
+++ src/core/snabbswitch.c
@@ -4,9 +4,11 @@
 #include "lualib.h"
 #include "lauxlib.h"
 
-#if sizeof(uint64_t) != sizeof(uintptr_t)
-#error "Snabb only supports 64-bit platforms. See https://github.com/SnabbCo/snabbswitch/blob/master/src/doc/porting.md"
-#end
+#include <stdint.h>
+
+#if UINTPTR_MAX != UINT64_MAX
+#error "64-bit word size required. See doc/porting.md."
+#endif
 
 int argc;
 char** argv;
