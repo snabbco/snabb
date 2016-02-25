576347692f77534920081b3c69da86a86d7174d4
Adrián Pérez de Castro <aperez@igalia.com>
2016-02-25 22:29:35 +0200

    Implement the L7Spy application and "wall spy" command
    The L7Spy application is implemented in terms of a Scanner object, which
    abstracts the details of the mechanism used to analyze packets. This patch
    includes apps.wall.scanner.NdpiScanner, which uses ljndpi.

---
+++ src/apps/wall/scanner.lua
@@ -0,0 +1,219 @@
+module(..., package.seeall)
+
+local util = require("apps.wall.util")
+local lib  = require("core.lib")
+local bit  = require("bit")
+local ffi  = require("ffi")
+
+local rd16, rd32 = util.rd16, util.rd32
+local tobit, lshift, rshift = bit.tobit, bit.lshift, bit.rshift
+local band, bxor, bnot = bit.band, bit.bxor, bit.bnot
+
+-- Constants: Ethernet
+ETH_TYPE_IPv4        = lib.htons(0x0800)
+ETH_TYPE_IPv6        = lib.htons(0x86DD)
+ETH_TYPE_VLAN        = lib.htons(0x8100)
+ETH_TYPE_OFFSET      = 12
+ETH_HEADER_SIZE      = 14
+
+-- Constants: IPv4
+IPv4_VER_IHL_OFFSET  = 0
+IPv4_DSCP_ECN_OFFSET = 1
+IPv4_LEN_OFFSET      = 2
+IPv4_FRAG_ID_OFFSET  = 4
+IPv4_FLAGS_OFFSET    = 6
+IPv4_TTL_OFFSET      = 8
+IPv4_PROTO_OFFSET    = 9
+IPv4_CHECKSUM_OFFSET = 10
+IPv4_SRC_ADDR_OFFSET = 12
+IPv4_DST_ADDR_OFFSET = 16
+
+IPv4_PROTO_TCP       = 6   -- uint8_t
+IPv4_PROTO_UDP       = 17  -- uint8_t
+
+-- Constants: IPv6
+
+-- Constants: TCP
+TCP_HEADER_SIZE      = 20
+TCP_SRC_PORT_OFFSET  = 0
+TCP_DST_PORT_OFFSET  = 2
+
+-- Constants: UDP
+UDP_HEADER_SIZE      = 8
+UDP_SRC_PORT_OFFSET  = 0
+UDP_DST_PORT_OFFSET  = 2
+
+
+ffi.cdef [[
+   struct swall_flow_key_ipv4 {
+      uint16_t vlan_id;
+      uint8_t  __pad;
+      uint8_t  ip_proto;
+      uint32_t lo_addr;
+      uint32_t hi_addr;
+      uint16_t lo_port;
+      uint16_t hi_port;
+   } __attribute__((packed));
+
+   struct swall_flow_key_ipv6 {
+      uint16_t vlan_id;
+      uint8_t  __pad;
+      uint8_t  ip_proto;
+      uint64_t lo_addr;
+      uint64_t hi_addr;
+      uint16_t lo_port;
+      uint16_t hi_port;
+   } __attribute__((packed));
+]]
+
+local INT32_MIN = -0x80000000
+local function hash32(i32)
+   i32 = tobit(i32)
+   i32 = i32 + bnot(lshift(i32, 15))
+   i32 = bxor(i32, (rshift(i32, 10)))
+   i32 = i32 + lshift(i32, 3)
+   i32 = bxor(i32, rshift(i32, 6))
+   i32 = i32 + bnot(lshift(i32, 11))
+   i32 = bxor(i32, rshift(i32, 16))
+
+   -- Unset the low bit, to distinguish valid hashes from HASH_MAX.
+   i32 = lshift(i32, 1)
+   -- Project result to u32 range.
+   return i32 - INT32_MIN
+end
+
+local uint32_ptr = ffi.typeof("uint32_t*")
+local function make_cdata_hash_function(sizeof)
+   assert(sizeof >= 4)
+   assert(sizeof % 4 == 0)
+
+   local rounds = (sizeof / 4) - 1
+   return function (cdata)
+      cdata = ffi.cast(uint32_ptr, cdata)
+      local h = hash32(cdata[0])
+      for i = 1, rounds do
+         h = hash32(cdata[i])
+      end
+      return h
+   end
+end
+
+local uint8_ptr = ffi.typeof("uint8_t*")
+
+local flow_key_ipv4_size = ffi.sizeof("struct swall_flow_key_ipv4")
+assert(flow_key_ipv4_size % 4 == 0)
+
+local flow_key_ipv4_lo_addr_offset =
+   ffi.offsetof("struct swall_flow_key_ipv4", "lo_addr")
+local flow_key_ipv4_hi_addr_offset =
+   ffi.offsetof("struct swall_flow_key_ipv4", "hi_addr")
+
+local flow_key_ipv4 = ffi.metatype("struct swall_flow_key_ipv4", {
+   __index = {
+      hash = make_cdata_hash_function(flow_key_ipv4_size);
+      eth_type = function (self) return ETH_TYPE_IPv4 end;
+      lo_addr_ptr = function (self)
+         return ffi.cast(uint8_ptr, self) + flow_key_ipv4_lo_addr_offset
+      end;
+      hi_addr_ptr = function (self)
+         return ffi.cast(uint8_ptr, self) + flow_key_ipv4_hi_addr_offset
+      end;
+   }
+})
+
+local flow_key_ipv6_size = ffi.sizeof("struct swall_flow_key_ipv6")
+assert(flow_key_ipv6_size % 4 == 0)
+
+local flow_key_ipv6_lo_addr_offset =
+   ffi.offsetof("struct swall_flow_key_ipv6", "lo_addr")
+local flow_key_ipv6_hi_addr_offset =
+   ffi.offsetof("struct swall_flow_key_ipv6", "hi_addr")
+
+local flow_key_ipv6 = ffi.metatype("struct swall_flow_key_ipv6", {
+   __index = {
+      hash = make_cdata_hash_function(flow_key_ipv6_size);
+      eth_type = function (self) return ETH_TYPE_IPv6 end;
+      lo_addr_ptr = function (self)
+         return ffi.cast(uint8_ptr, self) + flow_key_ipv6_lo_addr_offset
+      end;
+      hi_addr_ptr = function (self)
+         return ffi.cast(uint8_ptr, self) + flow_key_ipv6_hi_addr_offset
+      end;
+   }
+})
+
+-- Helper functions
+
+local function ihl(p, offset)
+   local ver_and_ihl = p.data[offset]
+   return band(ver_and_ihl, 0x0F) * 4
+end
+
+
+Scanner = subClass()
+Scanner._name = "SnabbWall base packet Scanner"
+
+function Scanner:extract_packet_info(p)
+   local eth_type  = rd16(p.data + ETH_TYPE_OFFSET)
+   local ip_offset = ETH_HEADER_SIZE
+   local vlan_id   = 0
+
+   while eth_type == ETH_TYPE_VLAN do
+      vlan_id   = rd16(p.data + ip_offset)
+      eth_type  = rd16(p.data + ip_offset + 2)
+      ip_offset = ip_offset + 4
+   end
+
+   local key, src_addr, src_port, dst_addr, dst_port, ip_proto
+   if eth_type == ETH_TYPE_IPv4 then
+      key = flow_key_ipv4()
+      src_addr = rd32(p.data + ip_offset + IPv4_SRC_ADDR_OFFSET)
+      dst_addr = rd32(p.data + ip_offset + IPv4_DST_ADDR_OFFSET)
+      ip_proto = p.data[ip_offset + IPv4_PROTO_OFFSET]
+
+      local ip_payload_offset = ip_offset + ihl(p, ip_offset)
+      if ip_proto == IPv4_PROTO_TCP then
+         src_port = rd16(p.data + ip_payload_offset + TCP_SRC_PORT_OFFSET)
+         dst_port = rd16(p.data + ip_payload_offset + TCP_DST_PORT_OFFSET)
+      elseif ip_proto == IPv4_PROTO_UDP then
+         src_port = rd16(p.data + ip_payload_offset + UDP_SRC_PORT_OFFSET)
+         dst_port = rd16(p.data + ip_payload_offset + UDP_DST_PORT_OFFSET)
+      end
+   elseif eth_type == ETH_TYPE_IPv6 then
+      key = flow_key_ipv6()
+      assert(false, "unimplemented")  -- TODO
+   else
+      return nil
+   end
+
+   key.vlan_id = vlan_id
+   key.ip_proto = ip_proto
+
+   if src_addr < dst_addr then
+      key.lo_addr, key.hi_addr = src_addr, dst_addr
+   else
+      key.lo_addr, key.hi_addr = dst_addr, src_addr
+   end
+
+   if src_port and dst_port then
+      if src_port < dst_port then
+         key.lo_port, key.hi_port = src_port, dst_port
+      else
+         key.lo_port, key.hi_port = dst_port, src_port
+      end
+   end
+
+   return key, ip_offset, src_addr, src_port, dst_addr, dst_port
+end
+
+function Scanner:get_flow(p)
+   error("method must be overriden in a subclass")
+end
+
+function Scanner:scan_packet(p, time)
+   error("method must be overriden in a subclass")
+end
+
+function Scanner:protocol_name(protocol)
+   return tostring(protocol)
+end
---
+++ src/program/wall/spy/spy.lua
@@ -0,0 +1,147 @@
+module(..., package.seeall)
+
+local lib  = require("core.lib")
+local ipv4 = require("lib.protocol.ipv4")
+local ipv6 = require("lib.protocol.ipv6")
+local util = require("apps.wall.util")
+local scan = require("apps.wall.scanner")
+
+local long_opts = {
+   help    = "h";
+   verbose = "v";
+}
+
+local function printf(fmt, ...)
+   io.write(fmt:format(...))
+end
+
+local function report_flow(scanner, flow)
+   local lo_addr, hi_addr = "<unknown>", "<unknown>"
+   local eth_type = flow.key:eth_type()
+   if eth_type == scan.ETH_TYPE_IPv4 then
+      lo_addr = ipv4:ntop(flow.key:lo_addr_ptr())
+      hi_addr = ipv4:ntop(flow.key:hi_addr_ptr())
+   elseif eth_type == scan.ETH_TYPE_IPv6 then
+      lo_addr = ipv6:ntop(flow.key:lo_addr_ptr())
+      hi_addr = ipv6:ntop(flow.key:hi_addr_ptr())
+   end
+
+   printf("%#010x %4dp %15s:%-5d - %15s:%-5d  %s:%s\n",
+      flow.key:hash(), flow.packets,
+      lo_addr, flow.key.lo_port,
+      hi_addr, flow.key.hi_port,
+      scanner:protocol_name(flow.protocol),
+      scanner:protocol_name(flow.proto_master))
+end
+
+local Report = setmetatable({}, util.SouthAndNorth)
+Report.__index = Report
+
+function Report:new (scanner)
+   return setmetatable({
+      scanner = scanner;
+      packets = 0;
+   }, self)
+end
+
+function Report:on_northbound_packet (p)
+   self.packets = self.packets + 1
+   local flow = self.scanner:get_flow(p)
+   if flow and not flow.reported then
+      local proto = self.scanner:protocol_name(flow.protocol)
+      if proto:lower() ~= "unknown" then
+         report_flow(self.scanner, flow)
+         flow.reported = true
+      end
+   end
+end
+Report.on_southbound_packet = Report.on_northbound_packet
+
+
+local inputs = {}
+
+function inputs.pcap (kind, path)
+   return "output", { require("apps.pcap.pcap").PcapReader, path }
+end
+
+function inputs.raw (kind, device)
+   return "output", { require("apps.socket.raw").RawSocket, device }
+end
+
+function inputs.tap (kind, device)
+   return "output", { require("apps.tap.tap").Tap, device }
+end
+
+function inputs.intel10g (kind, device)
+   local conf = { pciaddr = device }
+   return "rx", { require("apps.intel.intel_app").Intel10G, conf }
+end
+
+function inputs.intel1g (kind, device)
+   local conf = { pciaddr = device }
+   return "rx", { require("apps.intel.intel1g").Intel1G, conf }
+end
+
+
+local function setup_input(c, input_spec)
+   local kind, arg = input_spec_pattern:match(input_spec)
+   if not kind then
+      kind, arg = "pcap", input_spec
+   end
+   if not inputs[kind] then
+      return nil, "No such input kind: " .. kind
+   end
+   return inputs[kind](kind, arg)
+end
+
+
+function run (args)
+   local verbosity = 0
+   local opt = {
+      v = function (arg)
+         verbosity = verbosity + 1
+      end;
+      h = function (arg)
+         print(require("program.wall.spy.README_inc"))
+         main.exit(0)
+      end;
+   }
+
+   args = lib.dogetopt(args, opt, "hv", long_opts)
+   if #args ~= 2 then
+      print(require("program.wall.spy.README_inc"))
+      main.exit(1)
+   end
+
+   if not inputs[args[1]] then
+      io.stderr:write("No such input available: ", args[1], "\n")
+      main.exit(1)
+   end
+
+   local source_link_name, app = inputs[args[1]](args[1], args[2])
+   if not source_link_name then
+      io.stderr:write(app, "\n")
+      main.exit(1)
+   end
+
+   -- FIXME: When multiple scanners are available, allow selecting others.
+   local s = require("apps.wall.scanner.ndpi"):new()
+
+   local c = config.new()
+   config.app(c, "source", unpack(app))
+   config.app(c, "l7spy", require("apps.wall.l7spy").L7Spy, { scanner = s })
+   config.link(c, "source." .. source_link_name .. " -> l7spy.south")
+
+   if verbosity > 0 then
+      config.app(c, "report", Report, s)
+      config.link(c, "l7spy.north -> report.south")
+   end
+
+   engine.configure(c)
+   engine.busywait = true
+   engine.main {
+      done = function ()
+         return engine.app_table.source.done
+      end
+   }
+end
