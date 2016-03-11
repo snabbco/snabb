91b3b10692eeb230c6c82a1621697d46dc07e0a2
R. Matthew Emerson <rme@acm.org>
2016-03-11 15:23:58 -0500

    Use private struct link variants for ff mode and for new mc mode.

--- src/program/snabbmark/mp.lua
+++ src/program/snabbmark/mp.lua
@@ -9,11 +9,204 @@ local S = require("syscall")
 local pmu = require("lib.pmu")
 local lib = require("core.lib")
 
+local shm = require("core.shm")
+local band = require("bit").band
+
 -- Ring benchmark:
 -- See how quickly packets are cycled through a ring of processes.
 --
 -- Each process copies packets from its input to its output link. Each
 -- link is populated with an initial "burst" of packets.
+
+local cache_line_size = 64
+
+-- ff (FastForward) link
+
+-- John Giacomoni, Tipp Moseley, and Manish Vachharajani. 2008.
+-- FastForward for efficient pipeline parallelism:
+-- a cache-optimized concurrent lock-free queue. In Proceedings of the
+-- 13th ACM SIGPLAN Symposium on Principles and practice of parallel
+-- programming (PPoPP '08). ACM, New York, NY, USA, 43-52.
+-- DOI=http://dx.doi.org/10.1145/1345206.1345215
+
+local ff_size = 256
+
+ffi.cdef([[
+/*
+ * Put read and write members on private cache lines,
+ * which are assumed to be 64 bytes long.
+ */
+struct ff_link
+{
+    struct packet *packets[$];
+    uint32_t read  __attribute__((aligned(64)));
+    uint32_t write __attribute__((aligned(64)));
+};
+]], ff_size)
+
+function ff_new (name)
+   local r = shm.map("links/"..name, "struct ff_link")
+   -- try to verify expected alignment
+   local p = ffi.new("struct ff_link *[1]")
+   p[0] = r
+   local n = ffi.cast("uintptr_t", p[0])
+   local mask = cache_line_size - 1
+   assert(band(n, mask) == 0, "struct ff_link not 64-bit aligned")
+   assert(band(ffi.offsetof("struct ff_link", "read"), mask) == 0,
+	  "read field not 64-bit aligned within struct ff_link")
+   assert(band(ffi.offsetof("struct ff_link", "write"), mask) == 0,
+	  "write field not 64-bit aligned within struct ff_link")
+   return r
+end
+
+function ff_empty (r)
+   return r.packets[r.read] == nil
+end
+
+function ff_available (r)
+   return r.packets[r.write] == nil
+end
+
+function ff_full (r)
+   return r.packets[r.write] ~= nil
+end
+
+function ff_maybe_rx (r)
+   local p = r.packets[r.read];
+   if p ~= nil then
+      r.packets[r.read] = nil;
+      r.read = band(r.read + 1, ff_size - 1)
+   end
+   return p
+end
+
+function ff_maybe_tx (r, p)
+   if r.packets[r.write] ~= nil then
+      return nil
+   else
+      r.packets[r.write] = p
+      r.write = band(r.write + 1, ff_size - 1)
+      return p
+   end
+end
+
+
+-- mc (MCRingBuffer) link
+
+-- Patrick P. C. Lee, Tian Bu, and Girish Chandranmenon. 2009.
+-- A lock-free, cache-efficient shared ring buffer for multi-core architectures.
+-- In Proceedings of the 5th ACM/IEEE Symposium on Architectures for Networking
+-- and Communications Systems (ANCS '09). ACM, New York, NY, USA, 78-79.
+-- DOI=http://dx.doi.org/10.1145/1882486.1882508
+
+local mc_size = 256
+local mc_batch_size = 10
+
+ffi.cdef([[
+struct mc_link
+{
+    struct packet *packets[$];
+    uint32_t read __attribute__((aligned(64)));
+    uint32_t write;
+    uint32_t local_write __attribute__((aligned(64)));
+    uint32_t next_read;
+    uint32_t read_batch;
+    uint32_t local_read __attribute__((aligned(64)));
+    uint32_t next_write;
+    uint32_t write_batch;
+};
+]], mc_size)
+
+function mc_new (name)
+   local r = shm.map("links/"..name, "struct mc_link")
+   local p = ffi.new("struct mc_link *[1]")
+   p[0] = r
+   local n = ffi.cast("uintptr_t", p[0])
+   local mask = cache_line_size - 1
+   assert(band(n, mask) == 0, "struct mc_link not 64-bit aligned")
+   assert(band(ffi.offsetof("struct mc_link", "read"), mask) == 0,
+	  "read field not 64-byte aligned within struct mc_link")
+   assert(band(ffi.offsetof("struct mc_link", "local_write"), mask) == 0,
+	  "local_write field not 64-byte aligned within struct mc_link")
+   assert(band(ffi.offsetof("struct mc_link", "local_read"), mask) == 0,
+	  "local_read field not 64-byte aligned within struct mc_link")
+
+   return r
+end
+
+function mc_maybe_rx (r)
+   if r.next_read == r.local_write then
+      if r.next_read == r.write then
+	 return nil
+      end
+      r.local_write = r.write
+   end
+   local p = r.packets[r.next_read]
+   r.next_read = band(r.next_read + 1, mc_size -1)
+   r.read_batch = r.read_batch + 1
+   if r.read_batch >= mc_batch_size then
+      r.read = r.next_read
+      r.read_batch = 0
+   end
+   return p
+end
+
+function mc_maybe_tx (r, p)
+   local after_next = band(r.next_write + 1, mc_size - 1)
+   if after_next == r.local_read then
+      if after_next == r.read then
+	 return nil
+      end
+      r.local_read = r.read
+   end
+   r.packets[r.next_write] = p
+   r.next_write = after_next
+   r.write_batch = r.write_batch + 1
+   if r.write_batch >= mc_batch_size then
+      r.write = r.next_write
+      r.write_batch = 0
+   end
+   return p
+end
+
+
+function make_links(mode, nlinks, npackets)
+   local links = {}
+
+   if mode == "basic" then
+      for i = 0, nlinks - 1 do
+	 links[i] = link.new(tostring(i))
+	 for j = 1, npackets do
+	    link.transmit(links[i], packet.allocate())
+	 end
+      end
+   elseif mode == "ff" then
+      for i = 0, nlinks - 1 do
+	 links[i] = ff_new(tostring(i))
+	 for j = 1, npackets do
+	    local p = ff_maybe_tx(links[i], packet.allocate())
+	    if p == nil then
+	       error("can't transmit packet while priming ff links")
+	    end
+	 end
+      end
+   elseif mode == "mc" then
+      for i = 0, nlinks - 1 do
+	 links[i] = mc_new(tostring(i))
+	 for j = 1, npackets do
+	    local p = mc_maybe_tx(links[i], packet.allocate())
+	    if p == nil then
+	       error("can't transmit packet while priming mc links")
+	    end
+	 end
+      end
+   else
+      error("unknown mode " .. mode)
+   end
+
+   return links
+end
+
 function mp_ring (args)
    local function usage ()
       print(require("program.snabbmark.README_mp_inc"))
@@ -54,14 +247,16 @@ function mp_ring (args)
    for k, v in pairs(c) do
       print(("%12s: %s"):format(k,v))
    end
-   links = {}
+
+   links = make_links(c.mode, c.processes, c.burst)
+   
    -- Create links to connect the processes in a loop
-   for i = 0, c.processes-1 do
-      links[i] = link.new(tostring(i))
-      for j = 1, c.burst do
-         link.transmit(links[i], packet.allocate())
-      end
-   end
+   -- for i = 0, c.processes-1 do
+   --    links[i] = link.new(tostring(i))
+   --    for j = 1, c.burst do
+   --       link.transmit(links[i], packet.allocate())
+   --    end
+   -- end
    -- Create per-process counters
    local counters = ffi.cast("uint64_t *",
                              memory.dma_alloc(c.processes*ffi.sizeof("uint64_t")))
@@ -106,7 +301,7 @@ function mp_ring (args)
 	 elseif c.mode == "ff" then
             local acc = ffi.new("uint8_t[1]")
 	    while counters[i] < c.packets do
-	       local p = link.maybe_rx(input)
+	       local p = ff_maybe_rx(input)
 	       if p ~= nil then
                   -- Read some packet data
                   for j = 0, c.readbytes do
@@ -119,7 +314,29 @@ function mp_ring (args)
 
 		  local sent = nil
 		  repeat
-		     sent = link.maybe_tx(output, p)
+		     sent = ff_maybe_tx(output, p)
+		  until sent ~= nil
+		  counters[i] = counters[i] + 1
+	       end
+	       core.lib.compiler_barrier()
+	    end
+	 elseif c.mode == "mc" then
+            local acc = ffi.new("uint8_t[1]")
+	    while counters[i] < c.packets do
+	       local p = mc_maybe_rx(input)
+	       if p ~= nil then
+                  -- Read some packet data
+                  for j = 0, c.readbytes do
+                     acc[0] = acc[0] + p.data[j]
+                  end
+                  -- Write some packet data
+                  for j = 0, c.writebytes do
+                     p.data[j] = i
+                  end
+
+		  local sent = nil
+		  repeat
+		     sent = mc_maybe_tx(output, p)
 		  until sent ~= nil
 		  counters[i] = counters[i] + 1
 	       end
