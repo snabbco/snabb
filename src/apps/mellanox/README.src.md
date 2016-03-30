45d0696b6229629c0d68ea7f21789b9568424b1f
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2016-03-30 20:29:40 +0000

    stub module and readme

---
+++ src/apps/mellanox/README.src.md
@@ -0,0 +1,4 @@
+# Mellanox ConnectX-4 Ethernet Controller App
+
+## MCX4 (apps.mellanox.connectx4)
+
---
+++ src/apps/mellanox/connectx4.lua
@@ -0,0 +1,18 @@
+
+--- Device driver for the Mellanox ConnectX-4 series Ethernet controller.
+
+module(...,package.seeall)
+
+local ffi      = require "ffi"
+local C        = ffi.C
+local lib      = require("core.lib")
+local pci      = require("lib.hardware.pci")
+local register = require("lib.hardware.register")
+local index_set = require("lib.index_set")
+local macaddress = require("lib.macaddress")
+local mib = require("lib.ipc.shmem.mib")
+local timer = require("core.timer")
+
+function selftest()
+    print'hello'
+end
