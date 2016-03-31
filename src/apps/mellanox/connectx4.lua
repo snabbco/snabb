dfbd705c03a13fb830d4be546f340a2177b64734
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2016-03-31 18:35:02 +0300

    firmware revision register

--- src/apps/mellanox/connectx4.lua
+++ src/apps/mellanox/connectx4.lua
@@ -12,19 +12,36 @@ local index_set = require("lib.index_set")
 local macaddress = require("lib.macaddress")
 local mib = require("lib.ipc.shmem.mib")
 local timer = require("core.timer")
+local bits, bitset = lib.bits, lib.bitset
+local band, bor, lshift = bit.band, bit.bor, bit.lshift
 
 ConnectX4 = {}
 ConnectX4.__index = ConnectX4
 
-function ConnectX4:new(arg)
-   local conf = config.parse_app_arg(arg)
-   local self = setmetatable({}, self)
+local init_segment_desc = [[
+fw_rev 0x0000 - RO Firmware Revision
+]]
 
-   local pciaddress = conf.pciaddress
+function ConnectX4:new(arg)
+   local self = setmetatable({}, self)
+   local conf = config.parse_app_arg(arg)
+   local pciaddress = pci.qualified(conf.pciaddress)
 
    pci.unbind_device_from_linux(pciaddress)
    pci.set_bus_master(pciaddress, true)
    local base, fd = pci.map_pci_memory(pciaddress, 0)
+   local r = {} --config registers
+
+   register.define(init_segment_desc, r, base)
+
+   print(r.fw_rev())
+
+   function self:stop()
+      if not base then return end
+      pci.set_bus_master(pciaddress, false)
+      pci.close_pci_resource(fd, base)
+      base, fd = nil
+   end
 
    --[[
    register.define(config_registers_desc, self.r, self.base)
@@ -41,7 +58,6 @@ function ConnectX4:new(arg)
    return self
 end
 
-
 function selftest()
    local pcidev1 = lib.getenv("SNABB_PCI_CONNECTX40") or lib.getenv("SNABB_PCI0")
    local pcidev2 = lib.getenv("SNABB_PCI_CONNECTX41") or lib.getenv("SNABB_PCI1")
@@ -57,10 +73,11 @@ function selftest()
    local device_info_1 = pci.device_info(pcidev1)
    local device_info_2 = pci.device_info(pcidev2)
 
-   print(device_info_1.model)
-
    local app1 = ConnectX4:new{pciaddress = pcidev1}
    local app2 = ConnectX4:new{pciaddress = pcidev2}
 
+   engine.main({duration = 1, report={showlinks=true, showapps=false}})
 
+   app1:stop()
+   app2:stop()
 end
