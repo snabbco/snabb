2715120de679b2dbafcd2e1709c3b9dae3a9fe0e
Max Rottenkolber <max@mr.gy>
2016-04-06 18:48:24 +0200

    apps.test.match: Rename testsink to Match, remove harness function.

--- src/apps/test/README.md
+++ src/apps/test/README.md
@@ -1,38 +1,30 @@
-# (apps.testsink.testsink)
+# Test Apps
 
-## The TestSink app
-The `TestSink` app is used to aid unit testing. It takes 2 inputs `rx` and `comparator`.
-Each packet from `rx` is compared with each packet from `comparator`, when packets differ it's 
-logged to `app.errs`. Each packet from `comparator` must have a corresponding packet from `rx`.
-`fuzzy` controls whether each packet from `rx` must have a corresponding pcaket from `comparator`.
-When `fuzzy = false` it **MUST**, when `fuzzy = true` it **MAY** not. Fuzzy matching is useful when 
-'uncontrolled' traffic may be seen, for example IPv6 ND packets when testing pcap eplays on a 
-linux Tap device.
+## Match (apps.test.match)
 
+The `Match` app compares packets received on its input port `rx` with those
+received on the reference input port `comparator`, and reports mismatches as
+well as packets from `comparator` that were not matched.
 
-```
-config.app(c, "tsink", testsink.TestSink, { fuzzy = boolean })
--- tsink.rx and tsink.comparator are the associated links
--- engine.app_table.tsink.errs is a table containing errors
-```
+    DIAGRAM: Match
+                  +----------+
+                  |          |
+           rx ----*          |
+                  |   Match  |
+    comparator ---*          |
+                  |          |
+                  +----------+
 
-## A simple test harness
-`ok, errs = TestSink.harness(app, inputpcaps, comparatorpcaps)`
-* `app` is an app such as `basic_apps.Tee` or `testsink.TestSink`
-* `inputpcaps` is a table of app input names as keys and pcaps as values
-* `comparatorpcaps` is a table of app output names as keys and pcaps as values
-* A PcapReader is created to feed each input of `app` from `inputpcaps`
-* A TestSink is created to compare each app output with the associated pcap from `comparatorpcaps`
+— Method **Match:errors**
 
-The following tests the Tee app
-```
-   local tee = require("apps.basic.basic_apps").Tee
-   local ok, res = harness(tee, { input = "apps/testsink/selftest1.pcap" }, 
-      { 
-         out = "apps/testsink/selftest1.pcap", 
-         out2 = "apps/testsink/selftest1.pcap" 
-      })
-   assert(ok)
-   assert(#res.out == 0)
-   assert(#res.out2 == 0)
-```
+Returns the recorded errors as an array of strings.
+
+### Configuration
+
+The `Match` app accepts a table as its configuration argument. The following
+keys are defined:
+
+— Key **fuzzy**
+
+*Optional.* If this key is `true` packets from `rx` that do not match the next
+packet from `comparator` are ignored. The default is `false`.
--- src/apps/test/match.lua
+++ src/apps/test/match.lua
@@ -1,27 +1,30 @@
 module(...,package.seeall)
 
-local packet = require("core.packet")
-local link = require("core.link")
 local ffi = require("ffi")
 local C = ffi.C
 local lib = require("core.lib")
 
---- ### `TestSink` app: Receive and discard packets matching against comparator
-TestSink = {} 
-
-function TestSink:new(opts)
-   return setmetatable({ opts = opts, seen = 0, errs = { } }, { __index=TestSink })
+local function dump (p)
+   return lib.hexdump(ffi.string(packet.data(p), packet.length(p)))
 end
 
-function TestSink:push()
+Match = {}
+
+function Match:new (arg)
+   local conf = arg and config.parse_app_arg(arg) or {}
+   return setmetatable({ fuzzy = conf.fuzzy, seen = 0, errs = { } },
+                       { __index=Match })
+end
+
+function Match:push ()
    while not link.empty(self.input.rx) do
       local p = link.receive(self.input.rx)
       local cmp = link.front(self.input.comparator)
       if not cmp then
-      elseif packet.length(cmp) ~= packet.length(p) 
+      elseif packet.length(cmp) ~= packet.length(p)
          or C.memcmp(packet.data(cmp), packet.data(p), packet.length(cmp)) ~= 0 then
-         if not self.opts.fuzzy then
-            self:log(cmp,p)
+         if not self.fuzzy then
+            table.insert(self.errs, "Mismatch:\n"..dump(cmp).."\n"..dump(p))
          end
       else
          self.seen = self.seen + 1
@@ -31,87 +34,49 @@ function TestSink:push()
    end
 end
 
-function TestSink:log(a,b)
-   local str = "Packets differ\n" .. lib.hexdump(ffi.string(packet.data(a), packet.length(a))) 
-      .. "\n" .. lib.hexdump(ffi.string(packet.data(b), packet.length(b)))
-      table.insert(self.errs, str)
-end
-
-function TestSink:stop()
-   if not link.empty(self.input.comparator) then
-      table.insert(self.errs, "Packets after " .. tostring(self.seen) .. " were never matched")
+function Match:report ()
+   for _, error in ipairs(self:errors()) do
+      print(error)
    end
 end
 
-function harness(app, inputs, comps, opts)
-   local pcap = require("apps.pcap.pcap")
-   local c = config.new()
-   -- reset the engine
-   engine.configure(c)
-   c = config.new()
-   config.app(c, "app", app)
-   for i,v in pairs(inputs) do
-      config.app(c, ("src%s"):format(i), pcap.PcapReader, v)
-      config.link(c, ("src%s.output -> app.%s"):format(i,i))
+function Match:errors ()
+   while not link.empty(self.input.comparator) do
+      local p = link.receive(self.input.comparator)
+      table.insert(self.errs, "Not matched:\n"..dump(p))
+      packet.free(p)
    end
-   for i, v in pairs(comps) do
-      config.app(c, ("cmp_%s"):format(i), pcap.PcapReader, v)
-      config.app(c, ("sink_%s"):format(i), TestSink)
-      config.link(c, ("app.%s -> sink_%s.rx"):format(i,i))
-      config.link(c, ("cmp_%s.output -> sink_%s.comparator"):format(i,i))
-   end
-   engine.configure(c)
-   engine.main({ duration = 1, no_report = true })
-   results = { }
-   count = 0
-   for i, _ in pairs(comps) do
-      results[i] = engine.app_table[("sink_%s"):format(i)].errs
-      count = count + #results[i]
-   end
-   return count == 0, results
+   return self.errs
 end
 
 function selftest()
+   local basic_apps = require("apps.basic.basic_apps")
    local c = config.new()
-   local pcap = require("apps.pcap.pcap")
-   
-   config.app(c, "sink", TestSink)
-   config.app(c, "src", pcap.PcapReader, "apps/testsink/selftest1.pcap")
-   config.app(c, "comparator", pcap.PcapReader, "apps/testsink/selftest1.pcap")
+
+   config.app(c, "sink", Match)
+   config.app(c, "src", basic_apps.Source, 8)
+   config.app(c, "comparator", basic_apps.Source, 8)
    config.link(c, "src.output -> sink.rx")
    config.link(c, "comparator.output -> sink.comparator")
    engine.configure(c)
-   engine.main({duration=1, no_report = true })
-   assert(#engine.app_table.sink.errs == 0)
-   
-   engine.configure(config.new())
-   config.app(c, "comparator", pcap.PcapReader, "apps/testsink/selftest2.pcap")
-   engine.configure(c)
-   engine.main({duration=1, no_report = true })
-   assert(#engine.app_table.sink.errs == 32)
+   engine.main({duration=0.0001})
+   assert(#engine.app_table.sink:errors() == 0)
 
    engine.configure(config.new())
-   config.app(c, "sink", TestSink, { fuzzy = true })
+   config.app(c, "comparator", basic_apps.Source, 12)
    engine.configure(c)
-   engine.main({duration=1, no_report = 1})
-   assert(#engine.app_table.sink.errs == 0)
+   engine.main({duration=0.0001})
+   assert(#engine.app_table.sink:errors() > 0)
 
-   local tee = require("apps.basic.basic_apps").Tee
-   local ok, res = harness(tee, { input = "apps/testsink/selftest1.pcap" }, 
-      { 
-         out = "apps/testsink/selftest1.pcap", 
-         out2 = "apps/testsink/selftest1.pcap" 
-      })
-   assert(ok)
-   assert(#res.out == 0)
-   assert(#res.out2 == 0)
-
-   ok, res = harness(tee, { input = "apps/testsink/selftest1.pcap" }, 
-      { 
-         out = "apps/testsink/selftest1.pcap", 
-         out2 = "apps/testsink/selftest2.pcap" 
-      })
-   assert(not ok)
-   assert(#res.out == 0)
-   assert(#res.out2 == 32)
+   engine.configure(config.new())
+   config.app(c, "sink", Match, {fuzzy=true})
+   config.app(c, "comparator", basic_apps.Source, 8)
+   config.app(c, "garbage", basic_apps.Source, 12)
+   config.app(c, "join", basic_apps.Join)
+   config.link(c, "src.output -> join.src")
+   config.link(c, "garbage.output -> join.garbage")
+   config.link(c, "join.out -> sink.rx")
+   engine.configure(c)
+   engine.main({duration=0.0001})
+   assert(#engine.app_table.sink:errors() == 0)
 end
