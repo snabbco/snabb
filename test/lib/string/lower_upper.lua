f40304dded5d07a0dd755751c811cd8ccaf9565b
Peter Cawley <corsix@corsix.org>
2016-04-09 15:29:53 +0100

    Modernise string_op test.

--- test/lib/string/lower_upper.lua
+++ test/lib/string/lower_upper.lua
@@ -14,3 +14,38 @@ do --- repeated
     assert(s == l)
   end
 end
+
+do --- repeated with growing string
+  local y, z
+  local x = "aBcDe"
+  for i=1,100 do
+    y = string.upper(x)
+    z = y.."fgh"
+  end
+  assert(y == "ABCDE")
+  assert(z == "ABCDEfgh")
+end
+
+do --- misc upper
+  local y
+  for i=1,100 do y = string.upper("aBc9") end
+  assert(y == "ABC9")
+  local x = ":abCd+"
+  for i=1,100 do y = string.upper(x) end
+  assert(y == ":ABCD+")
+  x = 1234
+  for i=1,100 do y = string.upper(x) end
+  assert(y == "1234")
+end
+
+do --- misc lower
+  local y
+  for i=1,100 do y = string.lower("aBc9") end
+  assert(y == "abc9")
+  local x = ":abcd+"
+  for i=1,100 do y = string.lower(x) end
+  assert(y == ":abcd+")
+  x = 1234
+  for i=1,100 do y = string.lower(x) end
+  assert(y == "1234")
+end
--- test/lib/string/rep.lua
+++ test/lib/string/rep.lua
@@ -1,3 +1,5 @@
+local rep = string.rep
+
 do --- smoke
   assert(("p"):rep(0) == "")
   assert(("a"):rep(3) == "aaa")
@@ -10,4 +12,49 @@ do --- versus concat
     s = s .. "{}"
     assert(s == ("{}"):rep(i))
   end
+end
+
+do --- misc
+  local y
+  for i=1,100 do y = rep("a", 10) end
+  assert(y == "aaaaaaaaaa")
+  for i=1,100 do y = rep("ab", 10) end
+  assert(y == "abababababababababab")
+  for i=1,100 do y = rep("ab", 10, "c") end
+  assert(y == "abcabcabcabcabcabcabcabcabcab")
+  local x = "a"
+  for i=1,100 do y = rep(x, 10) end
+  assert(y == "aaaaaaaaaa")
+  local n = 10
+  for i=1,100 do y = rep(x, n) end
+  assert(y == "aaaaaaaaaa")
+  x = "ab"
+  for i=1,100 do y = rep(x, n) end
+  assert(y == "abababababababababab")
+  x = 12
+  n = "10"
+  for i=1,100 do y = rep(x, n) end
+  assert(y == "12121212121212121212")
+end
+
+do --- iterate to table
+  local t = {}
+  for i=1,100 do t[i] = rep("ab", i-85) end
+  assert(t[100] == "ababababababababababababababab")
+  for i=1,100 do t[i] = rep("ab", i-85, "c") end
+  assert(t[85] == "")
+  assert(t[86] == "ab")
+  assert(t[87] == "abcab")
+  assert(t[100] == "abcabcabcabcabcabcabcabcabcabcabcabcabcabcab")
+end
+
+do --- iterate and concat
+  local y, z
+  local x = "ab"
+  for i=1,100 do
+    y = rep(x, i-90)
+    z = y.."fgh"
+  end
+  assert(y == "abababababababababab")
+  assert(z == "ababababababababababfgh")
 end
