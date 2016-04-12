6baa1f34aa523a7682bdd483bfb3aef6958f53c7
Pete Bristow <pete.bristow@gmail.com>
2016-04-12 16:44:35 +0100

    Feedback in PR869: do not block the whole process

--- src/apps/test/delayed_start.lua
+++ src/apps/test/delayed_start.lua
@@ -10,14 +10,12 @@ Useful when feeding pcaps into a physical nic. The delay lets the peer
 NIC completely initialize before sending packets so none are dropped.
 ]]
 function Delayed_start:new(delay)
-   return setmetatable({ delay = delay }, { __index = Delayed_start})
+   return setmetatable({ start = engine.now() + delay },
+					  { __index = Delayed_start })
 end
 
 function Delayed_start:push()
-   if self.delay then
-      C.usleep(self.delay)
-      self.delay = nil
-   end
+   if engine.now() < self.start then return end
    for _ = 1, link.nreadable(self.input.input) do
       link.transmit(self.output.output, link.receive(self.input.input))
    end
