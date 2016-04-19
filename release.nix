8982625b6eb1428b0c8044506e6c3027c28ed0a3
Domen Kožar <domen@dev.si>
2016-04-19 11:02:35 +0100

    release.nix: respect bash env, provide fallback values

--- release.nix
+++ release.nix
@@ -13,6 +13,9 @@ let
     url = "http://lab1.snabb.co:2008/~max/assets/vm-ubuntu-trusty-14.04-dpdk-snabb.tar.gz";
     sha256 = "0323591i925jhd6wv8h268wc3ildjpa6j57n4p9yg9d6ikwkw06j";
   };
+  optionalGetEnv = first: default: let
+      maybeEnv = builtins.getEnv first;
+    in if (maybeEnv != "") then maybeEnv else default;
 in rec {
   manual = import ./src/doc {};
   snabb = import ./default.nix {};
@@ -42,9 +45,9 @@ in rec {
 
     doCheck = true;
     checkPhase = ''
-      export SNABB_PCI0="0000:01:00.0"
-      export SNABB_PCI_INTEL1="0000:01:00.1"
-      export SNABB_PCI_INTEL0="0000:01:00.0"
+      export SNABB_PCI0=${ optionalGetEnv "SNABB_PCI0" "0000:01:00.0"}
+      export SNABB_PCI_INTEL0=${ optionalGetEnv "SNABB_PCI_INTEL0" "0000:01:00.0"}
+      export SNABB_PCI_INTEL1=${ optionalGetEnv "SNABB_PCI_INTEL1" "0000:01:00.1"}
       export FAIL_ON_FIRST=true
 
       # run tests
