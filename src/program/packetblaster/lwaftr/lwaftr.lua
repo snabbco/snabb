1c023ec29ebd7ba5182ddc91701723981c6270ea
Marcel Wiget <mwiget@juniper.net>
2016-04-19 17:27:14 -0700

    fix indentation

--- src/program/packetblaster/lwaftr/lwaftr.lua
+++ src/program/packetblaster/lwaftr/lwaftr.lua
@@ -70,17 +70,17 @@ function run (args)
    local sizes = { 64, 64, 64, 64, 64, 64, 64, 594, 594, 594, 1500 }
    local sizes_ipv6 = { 104, 104, 104, 104, 104, 104, 104, 634, 634, 634, 1540 }
    function opt.S (arg)
-     sizes = {}
-     sizes_ipv6 = {}
-     for size in string.gmatch(arg, "%d+") do
-       local s = tonumber(size)
-       if s < 28 then
-         s = 28
-         print("Warning: Increasing IPv4 packet size to 28")
-       end
-       sizes[#sizes+1] = s
-       sizes_ipv6[#sizes_ipv6+1] = s + 40
-     end
+      sizes = {}
+      sizes_ipv6 = {}
+      for size in string.gmatch(arg, "%d+") do
+         local s = tonumber(size)
+         if s < 28 then
+            s = 28
+            print("Warning: Increasing IPv4 packet size to 28")
+         end
+         sizes[#sizes+1] = s
+         sizes_ipv6[#sizes_ipv6+1] = s + 40
+      end
    end
 
    local src_mac = "00:00:00:00:00:00"
@@ -91,15 +91,15 @@ function run (args)
 
    local b4_ipv6, b4_ipv4, b4_port = "2001:db8::", "10.0.0.0", 1024
    function opt.b (arg) 
-     for s in string.gmatch(arg, "[%w.:]+") do
-        if string.find(s, ":") then
-          b4_ipv6 = s
-        elseif string.find(s, '.',1,true) then
-          b4_ipv4 = s
-        else
-          b4_port = assert(tonumber(s), string.format("UDP port %s is not a number!", s))
-        end
-     end
+      for s in string.gmatch(arg, "[%w.:]+") do
+         if string.find(s, ":") then
+            b4_ipv6 = s
+         elseif string.find(s, '.',1,true) then
+            b4_ipv4 = s
+         else
+            b4_port = assert(tonumber(s), string.format("UDP port %s is not a number!", s))
+         end
+      end
    end
 
    local public_ipv4 = "8.8.8.8"
@@ -110,37 +110,37 @@ function run (args)
 
    local count = 1
    function opt.c (arg) 
-     count = assert(tonumber(arg), "count is not a number!")
+      count = assert(tonumber(arg), "count is not a number!")
    end
 
    local rate = 1
    function opt.r (arg) 
-     rate = assert(tonumber(arg), "rate is not a number!")
+      rate = assert(tonumber(arg), "rate is not a number!")
    end
 
    local target 
    local pciaddr
    function opt.p (arg) 
-     pciaddr = arg
-     target = pciaddr
+      pciaddr = arg
+      target = pciaddr
    end
 
    local tap_interface
    function opt.t (arg) 
-     tap_interface = arg
-     target = tap_interface
+      tap_interface = arg
+      target = tap_interface
    end
 
    local int_interface
    function opt.i (arg) 
-     int_interface = arg
-     target = int_interface
+      int_interface = arg
+      target = int_interface
    end
 
    local sock_interface
    function opt.k (arg) 
-     sock_interface = arg
-     target = sock_interface
+      sock_interface = arg
+      target = sock_interface
    end
 
    local ipv4_only = false
@@ -153,82 +153,82 @@ function run (args)
 
    local vlan = nil
    function opt.v (arg) 
-     vlan = assert(tonumber(arg), "duration is not a number!")
+      vlan = assert(tonumber(arg), "duration is not a number!")
    end
 
    local pcap_file, single_pass
    function opt.o (arg) 
-     pcap_file = arg
-     target = pcap_file
-     single_pass = true
+      pcap_file = arg
+      target = pcap_file
+      single_pass = true
    end
 
    args = lib.dogetopt(args, opt, "VD:hS:s:a:d:b:iI:c:r:46p:v:o:t:i:k:", long_opts)
 
    if not target then
-     print("either --pci, --tap, --sock, --int or --pcap are required parameters")
-     main.exit(1)
+      print("either --pci, --tap, --sock, --int or --pcap are required parameters")
+      main.exit(1)
    end
 
    print(string.format("packetblaster lwaftr: Sending %d clients at %.3f MPPS to %s", count, rate, target))
    print()
 
    if not ipv4_only then
-    print(string.format("IPv6: %s > %s: %s:%d > %s:12345", b4_ipv6, aftr_ipv6, b4_ipv4, b4_port, public_ipv4))
-    print("      source IPv6 and source IPv4/Port adjusted per client")
-    print("IPv6 packet sizes: " .. table.concat(sizes_ipv6,","))
+      print(string.format("IPv6: %s > %s: %s:%d > %s:12345", b4_ipv6, aftr_ipv6, b4_ipv4, b4_port, public_ipv4))
+      print("      source IPv6 and source IPv4/Port adjusted per client")
+      print("IPv6 packet sizes: " .. table.concat(sizes_ipv6,","))
    end
 
    if not ipv6_only then
-    print()
-    print(string.format("IPv4: %s:12345 > %s:%d", public_ipv4, b4_ipv4, b4_port))
-    print("      destination IPv4 and Port adjusted per client")
-    print("IPv4 packet sizes: " .. table.concat(sizes,","))
+      print()
+      print(string.format("IPv4: %s:12345 > %s:%d", public_ipv4, b4_ipv4, b4_port))
+      print("      destination IPv4 and Port adjusted per client")
+      print("IPv4 packet sizes: " .. table.concat(sizes,","))
    end
 
    if ipv4_only and ipv6_only then
-     print("Remove options v4only and v6only to generate both")
-     main.exit(1)
+      print("Remove options v4only and v6only to generate both")
+      main.exit(1)
    end
 
    config.app(c, "generator", Lwaftrgen, { 
-     sizes = sizes, count = count, aftr_ipv6 = aftr_ipv6, rate = rate,
-     src_mac = src_mac, dst_mac = dst_mac, vlan = vlan,
-     b4_ipv6 = b4_ipv6, b4_ipv4 = b4_ipv4, b4_port = b4_port,
-     public_ipv4 = public_ipv4, single_pass = single_pass,
-     ipv4_only = ipv4_only, ipv6_only = ipv6_only })
+      sizes = sizes, count = count, aftr_ipv6 = aftr_ipv6, rate = rate,
+      src_mac = src_mac, dst_mac = dst_mac, vlan = vlan,
+      b4_ipv6 = b4_ipv6, b4_ipv4 = b4_ipv4, b4_port = b4_port,
+      public_ipv4 = public_ipv4, single_pass = single_pass,
+      ipv4_only = ipv4_only, ipv6_only = ipv6_only })
 
    local input, output
 
    if tap_interface then
-     if dir_exists(("/sys/devices/virtual/net/%s"):format(tap_interface)) then
-       config.app(c, "tap", Tap, tap_interface)
-       input, output = "tap.input", "tap.output"
-     else
-       print(string.format("tap interface %s doesn't exist", tap_interface))
-       main.exit(1)
-     end
+      if dir_exists(("/sys/devices/virtual/net/%s"):format(tap_interface)) then
+         config.app(c, "tap", Tap, tap_interface)
+         input, output = "tap.input", "tap.output"
+      else
+         print(string.format("tap interface %s doesn't exist", tap_interface))
+         main.exit(1)
+      end
    elseif pciaddr then
-     local device_info = pci.device_info(pciaddr)
-     if vlan then
-       print(string.format("vlan set to %d", vlan))
-     end
-     if device_info then
-       config.app(c, "nic", require(device_info.driver).driver,
-       {pciaddr = pciaddr, vmdq = false, macaddr = src_mac, mtu = 9500})
-       input, output = "nic.rx", "nic.tx"
-     else
-       fatal(("Couldn't find device info for PCI or tap device %s"):format(pciaddr))
-     end
+      local device_info = pci.device_info(pciaddr)
+      if vlan then
+         print(string.format("vlan set to %d", vlan))
+      end
+      if device_info then
+         config.app(c, "nic", require(device_info.driver).driver,
+         {pciaddr = pciaddr, vmdq = false, macaddr = src_mac, mtu = 9500})
+         input, output = "nic.rx", "nic.tx"
+      else
+         fatal(("Couldn't find device info for PCI or tap device %s"):format(pciaddr))
+      end
    elseif int_interface then
-     config.app(c, "int", raw.RawSocket, int_interface)
-     input, output = "int.rx", "int.tx"
+      config.app(c, "int", raw.RawSocket, int_interface)
+      input, output = "int.rx", "int.tx"
    elseif sock_interface then
-     config.app(c, "virtio", VhostUser, { socket_path=sock_interface } )
-     input, output = "virtio.rx", "virtio.tx"
+      config.app(c, "virtio", VhostUser, { socket_path=sock_interface } )
+      input, output = "virtio.rx", "virtio.tx"
    else
-     config.app(c, "pcap", pcap.PcapWriter, pcap_file)
-     input, output = "pcap.input", "pcap.output"
+      config.app(c, "pcap", pcap.PcapWriter, pcap_file)
+      input, output = "pcap.input", "pcap.output"
    end
 
    config.link(c, output .. " -> generator.input")
@@ -238,13 +238,13 @@ function run (args)
    engine.configure(c)
 
    if verbose then
-     print ("enabling verbose")
-     local fn = function ()
-       print("Transmissions (last 1 sec):")
-       engine.report_apps()
-     end
-     local t = timer.new("report", fn, 1e9, 'repeating')
-     timer.activate(t)
+      print ("enabling verbose")
+      local fn = function ()
+         print("Transmissions (last 1 sec):")
+         engine.report_apps()
+      end
+      local t = timer.new("report", fn, 1e9, 'repeating')
+      timer.activate(t)
    end
 
    if duration then engine.main({duration=duration})
