snabbco/snabb

commit 339a80a787cbb0b71209bb29044e52e1cab76a79

New implementation of the learning bridge app
apps/bridge has been re-written to use a custom-built hash table to
store MAC addresses instead of a bloom filter for performance and
management reasons.  Performance considerations have made it necessary
to implement part of the mac_table module in C.

src/apps/bridge/base.lua
@@ -16,23 +16,39 @@
 --              ...},
 --            config = { <bridge-specific-config> } }
 --
+-- Port names have to be unique by themselves, irrespective of whether
+-- they are free ports or belong to a split-horizon group.
+--
 -- The "config" table contains configuration options specific to a
--- derived class.  It is ignored by the base class.
+-- derived class.  It is ignored by the base class.  A derived class
+-- can access the configuration via self._conf.config.
 --
--- The base constructor checks the configuration and creates the
--- following arrays as private instance variables for efficient access
--- in the push() method (which must be provided by any derived class).
+-- To make processing in the fast path easier, each port and group is
+-- assigned a unique integer greater than zero to serve as a "handle".
+-- The group handle 0 is assigned to all free ports.
 --
---  self._src_ports
+-- The base constructor creates the following arrays as private
+-- instance variables for efficient access in the push() method (which
+-- must be provided by any derived class).
 --
---     This array contains the names of all ports connected to the
---     bridge.
+--  self._ports
+--
+--     Each port is assigned a table containing the following information
+--
+--         { name = <port-name>,
+--           group = <group-handle>,
+--           handle = <handle> }
+--
+--     The tables of all ports is stored in the self._ports table,
+--     which can be indexed by both, the port name as well as the port
+--     handle to access the information for a particular port.
 --
 --  self._dst_ports
 --
---     This table is keyed by the name of an input port and associates
---     it with an array of output ports according to the split-horizon
---     topology.
+--     This is an array which stores an array of egress port handles
+--     for every ingress port handle.  According to the split-horizon
+--     semantics, this includes all port handles except the ingress
+--     handle and all handles that belong to the same group.
 --
 -- The push() method of a derived class should iterate over all source
 -- ports and forward the incoming packets to the associated output
@@ -57,15 +73,37 @@ function bridge:new (arg)
 
    -- Create a list of forwarding ports for all ports connected to the
    -- bridge, taking split horizon groups into account
-   local ports = {}
+   local ports, groups = {}, {}
    local function add_port(port, group)
       assert(not ports[port],
              self:name()..": duplicate definition of port "..port)
-      ports[port] = group
+      local group_handle = 0
+      if group then
+         local desc = groups[group]
+         if not desc then
+            desc = { name = group, ports = {} }
+            groups[group] = desc
+            table.insert(groups, desc)
+            desc.handle = #groups
+         end
+         group_handle = desc.handle
+      end
+      local desc = { name = port,
+                     group = group_handle }
+      ports[port] = desc
+      table.insert(ports, desc)
+      desc.handle = #ports
+      if group_handle ~= 0 then
+         table.insert(groups[group_handle].ports, desc.handle)
+      end
    end
+
+   -- Add free ports
    for _, port in ipairs(conf.ports) do
-      add_port(port, '')
+      add_port(port)
    end
+
+   -- Add split horizon groups
    if conf.split_horizon_groups then
       for group, ports in pairs(conf.split_horizon_groups) do
          for _, port in ipairs(ports) do
@@ -73,17 +111,67 @@ function bridge:new (arg)
          end
       end
    end
-   local src_ports, dst_ports = {}, {}
-   for sport, sgroup in pairs(ports) do
-      table.insert(src_ports, sport)
+
+   -- Create list of egress ports for each ingress port, containing
+   -- all free ports as well as all ports from different split-horizon
+   -- groups
+   local dst_ports = {}
+   for sport, sdesc in ipairs(ports) do
       dst_ports[sport] = {}
-      for dport, dgroup in pairs(ports) do
-         if not (sport == dport or (sgroup ~= '' and sgroup == dgroup)) then
+      for dport, ddesc in ipairs(ports) do
+         if not (sport == dport or (sdesc.group ~= 0 and
+                                    sdesc.group == ddesc.group)) then
             table.insert(dst_ports[sport], dport)
          end
       end
    end
-   o._src_ports = src_ports
+   o._groups = groups
+   o._ports = ports
    o._dst_ports = dst_ports
    return o
+end
+
+-- API
+--
+-- Add the ingress and egress links to the port descriptor tables,
+-- accessible via the keys l_in and l_out, respectively.  This helps
+-- to speed up packet forwarding by eliminating a lookup in the input
+-- and output tables.
+function bridge:post_config ()
+   assert(self.input and self.output)
+   for _, port in ipairs(self._ports) do
+      port.l_in = self.input[port.name]
+      port.l_out = self.output[port.name]
+   end
+end
+
+-- API
+--
+-- Print the port configuration and forwarding tables of the bridge.
+-- This is primarily intended for debugging.
+function bridge:info ()
+   local ports, groups = self._ports, self._groups
+   local function nh (n, h)
+      return n.."("..h..")"
+   end
+   print("Free ports:")
+   for p, desc in ipairs(ports) do
+      if desc.group == 0 then
+         print("\t"..nh(desc.name, p))
+      end
+   end
+   print("Split-horizon groups:")
+   for g, desc in ipairs(groups) do
+      print("\t"..nh(desc.name, g)..", members:")
+      for _, p in ipairs(desc.ports) do
+         print("\t\t"..nh(ports[p].name, p))
+      end
+   end
+   print("Forwarding tables:")
+   for p, dst in ipairs(self._dst_ports) do
+      print("\t"..nh(ports[p].name, p))
+      for _, d in ipairs(dst) do
+         print("\t\t"..nh(ports[d].name, d))
+      end
+   end
 end

src/apps/bridge/README.md.src
@@ -54,38 +54,45 @@ The flooding `bridge` app ignores the *config* key of its configuration.
 
 # Learning bridge (apps.bridge.learning)
 
-The learning `bridge` app implements a *learning bridge* using a [Bloom
-filter](https://en.wikipedia.org/wiki/Bloom_filter) to store the set of
-MAC source addresses of packets arriving on each input port. When a
-packet is received it is forwarded to all output ports whose
-corresponding input ports match the packet's destination MAC address.
-When no input port matches, the packet is flooded to all output ports.
-Multicast MAC addresses are always flooded to all output ports associated
-with the input port. The scoping rules according to the split-horizon
-topology apply unchanged.
+The learning `bridge` app implements a *learning bridge* using a
+custom hash table to store the set of MAC source addresses of packets
+arriving on each input port. When a packet is received it is forwarded
+to all output ports whose corresponding input ports match the packet's
+destination MAC address.  When no input port matches, the packet is
+flooded to all output ports.  Multicast MAC addresses are always
+flooded to all output ports associated with the input port. The
+scoping rules according to the split-horizon topology apply unchanged.
 
 ## Configuration
 
 The learning `bridge` app accepts a table as the value of the *config*
 key of its configuration. The following keys are defined:
 
-— Key **mac_table_size**
+— Key **mac_table**
 
-**Optional**. Expected maximum number of MAC addresses to store in each
-per-port Bloom filter. Default is 1000.
+*Optional*. This is a table that defines the characteristics of the
+  MAC table.  The following keys are defined
 
-— Key **fp_rate**
+— Key **size**
 
-**Optional**. Maximum rate of false-positives for look-ups in the Bloom
-filters, provided the number of distinct objects stored in the filter
-does not exceed *mac_table_size*. Default is 0.001.
+*Optional*. The number of MAC addresses to be stored in the
+ table. Default is 256. The size of the table is increased
+ automatically if this limit is reached or if an overflow in one of
+ the hash buckets occurs.  This value is capped by **resize_max**.
 
 — Key **timeout**
 
-**Optional**. Timeout for learned MAC addresses in seconds. Default is
+*Optional*. Timeout for learned MAC addresses in seconds. Default is
 60.
 
 — Key **verbose**
 
-**Optional**. A boolean value. If true, enables the printing of debugging
-output. Default is `false`.
+*Optional*. A boolean value. If true, statistics about table usage is logged during each timeout interval.  Default is `false`.
+
+— Key **copy_on_resize**
+
+*Optional*. A boolean value. If true, the contents of the table is copied to the newly allocated table after a resize operation.  Default is `true`.
+
+— Key **resize_max**
+
+*Optional*. An upper bound for the size of the table. Default is 65536.

src/apps/bridge/learning.h
@@ -1,0 +1,68 @@
+/* Type for port and split-horizon group handles */
+typedef uint16_t handle_t;
+
+/* List of egress port handles.  This fixed-sized declaration is used
+   in mac_table.c.  The actual port lists are of variable size and
+   allocated by learning.lua. */
+typedef struct {
+  uint16_t length;
+  handle_t ports[1];
+} port_list_t;
+
+/* Packets are queued in various packet forwarding tables during
+   processing in learning:push().  Each entry points to a list of port
+   handles to which the packet will be sent. */
+typedef struct {
+  struct packet *p;
+  port_list_t *plist;
+} pft_entry_t;
+
+typedef struct pft {
+  uint16_t length;
+  pft_entry_t entries[1];
+} pft_t;
+
+/* Mapping of a destination MAC address to an egress port handle.  The
+   split-horizon group to which it belongs is stored as well in order
+   to detect group collision during lookup.  The address is stored as
+   a 64-bit integer in host-byte order to allow efficient access and
+   comparison.  A bucket in the hash table is made up of an array of
+   BUCKET_SIZE entries of this type. */
+typedef struct {
+  uint64_t mac;
+  handle_t port;
+  handle_t group;
+} mac_entry_t;
+
+/* The MAC addresses are stored in simple hash tables with fixed-sized
+   buckets. */
+typedef struct {
+  uint32_t ubuckets; /* Number of buckets with at least one used slot */
+  uint32_t entries;  /* Number of stored objects */
+  uint8_t  overflow; /* Flag to indicate overflow in at least one bucket */
+} hash_table_header_t;
+
+/* This fixed-sized declaration is used in mac_table.c.  The actual
+   tables are of variable size and allocated by mac_table.lua.  The
+   size of a bucket must be known to access a row in the buckets
+   matrix.  There should be no need to change BUCKET_SIZE for
+   perfomance reasons, see the comments in mac_table.lua. This
+   declaration must match the ctype for hash_table_t in
+   mac_table.lua. */
+enum { BUCKET_SIZE = 6 };
+typedef struct {
+  hash_table_header_t h;
+  mac_entry_t buckets[1][BUCKET_SIZE];
+} hash_table_t;
+
+typedef struct {
+  handle_t port;
+  handle_t group;
+} lookup_result_t;
+
+void mac_table_insert(uint64_t mac, handle_t port, handle_t group,
+                      hash_table_t **tables, uint32_t index);
+lookup_result_t *mac_table_lookup(uint64_t mac, mac_entry_t *bucket);
+void mac_table_lookup_pft(uint64_t mac, mac_entry_t *bucket,
+                          handle_t port, handle_t group, struct packet *p,
+                          pft_t **pfts, port_list_t *flood_pl);

src/apps/bridge/learning.lua
@@ -1,62 +1,84 @@
 -- This class derives from lib.bridge.base and implements a "learning
--- bridge" using a Bloom filter (provided by lib.bloom_filter) to
--- store the set of MAC source addresses of packets arriving on each
--- port.
+-- bridge" using a MAC address table provided by apps.bridge.mac_table
+-- to store the set of source addresses of packets arriving on all
+-- ports.
+
+-- When a packet is received on a port, the MAC source address is
+-- stored in the table along with the handle of the port and the
+-- handle of the split-horizon group to which the port belongs.
 --
--- Two Bloom storage cells called mac_table and mac_shadow are
--- allocated for each port connected to the bridge.  For each packet
--- arriving on a port, the MAC source address is stored in both cells.
--- The mac_table cell is used during packet forwarding while
--- mac_shadow is used to time out the learned addresses.
+-- The decision of where to forward a packet is made by looking up the
+-- destination MAC address in the table.  If there is a match, there
+-- are two possible outcomes:
 --
--- When a packet is received on a port, its MAC destination address is
--- looked up in the mac_table cells of all associated output
--- ports. The packet is sent on all ports for which the lookup results
--- in a match, replicating the packet if necessary.  A destination can
--- be associated with multiple output ports, either because the
--- address has actually been learned on multiple ports or due to false
--- positives in the lookup operation, which are inevitable for Bloom
--- filters.
+--   1. If the ingress and egress ports belong to the same
+--      split-horizon group or the egress port is the same as the
+--      ingress port, the packet is discarded.
 --
--- Multicast MAC addresses are always flooded to all output ports
--- associated with the input port.
+--   2. Otherwise, the packet is forwarded to the egress port.  This
+--      is referred to as "unicast forwarding".
 --
--- The timing out of learned addresses is implemented by periodically
--- copying mac_shadow to mac_table and clearing mac_shadow for every
--- port.  I.e., mac_table contains only the addresses learned during
--- the past timeout interval.
+-- If there is no match, the packet is flooded to all egress ports
+-- that are not part of the same split-horizon group, except the
+-- ingress port.  This is referred to as "flooding forwarding".
+--
+-- Multicast packets don't receive special treatment to avoid an
+-- additional branch in the forwarding loop.  Such packets are
+-- implicitely flooded because the lookup in the MAC address table
+-- will always fail.
+--
+-- Expiration of learned MAC addresses is performed by the mac_table
+-- module.
 --
 -- Configuration variables (via the "config" table in the generic
--- configuration of the base class)
+-- configuration of the base class):
 --
---   mac_table_size (default 1000)
+--   mac_table
 --
---     Expected maximum number of MAC addresses to store in each
---     per-port Bloom filter.
+--     If specified, must be a table which is passed on unchanged to
+--     the constructor of the mac_table class.  It can be used to
+--     override the default settings of the MAC table used by the
+--     bridge.
 --
---   fp_rate (default 0.001)
+-- Notes on performance and implementation choices:
 --
---     Maximum rate of false-positives for lookups in the Bloom
---     filters, provided the number of distinct objects stored in the
---     filter does not exceed mac_table_size.
+-- The different forwarding decisions depending on the results of the
+-- MAC table lookups as described above creates multiple code paths
+-- that need to be compiled efficiently.  If all of them were
+-- implemented in a single loop, the compiler would only be able to
+-- optimize (at most) one of them and generate much less optimized
+-- code for all other cases.  The push() loop of the bridge uses two
+-- techniques to overcome this problem.
 --
---   timeout (default 60 seconds)
+-- The first technique is to "factorize" the code pertaining to each
+-- of the use cases into separate loops, which are branch-free by
+-- themselves.  This is achieved by classifying the incoming packets
+-- into one of the forwarding categories (unicast, flooding, discard)
+-- in the loop that reads the packets from the ingress ports.
 --
---     Timeout for learned MAC addresses in seconds.
+-- After this loop has terminated, separate loops over each forwarding
+-- table apply a single forwarding paradigm to all packets in the same
+-- category.  Each loop is branch-free and can be compiled
+-- efficiently.
 --
---   verbose (default false)
---
---     If true, a diagnostic message containing the storage cell usage
---     of each mac_table is printed to stdout
---
+-- It is obvious that the first loop, which categorizes the packets,
+-- cannot be written in a branch-free manner (categorization implies
+-- conditions and, hence, branches of some kind).  As a consequence,
+-- this loop is very hard to get compiled efficiently for all cases.
+-- This is where the second technique comes in.  It basically hides
+-- the branchy code from the compiler by moving it into a C function.
+-- This is all done inside the mac_table module, which also provides
+-- more documentation on this issue.
 
 module(..., package.seeall)
 
 local ffi = require("ffi")
-local bridge_base = require("apps.bridge.base").bridge
+local lib = require("core.lib")
 local packet = require("core.packet")
 local link = require("core.link")
-local bloom = require("lib.bloom_filter")
+local bridge_base = require("apps.bridge.base").bridge
+local mac_table = require("apps.bridge.mac_table")
+require("apps.bridge.learning_h")
 local ethernet = require("lib.protocol.ethernet")
 
 local empty, receive, transmit = link.empty, link.receive, link.transmit
@@ -65,123 +87,170 @@ local clone = packet.clone
 bridge = subClass(bridge_base)
 bridge._name = "learning bridge"
 
-local default_config = { mac_table_size = 1000, fp_rate = 0.001,
-                         timeout = 60, verbose = false }
+-- ctype for a packet forwarding table
+local pft_t = ffi.typeof([[
+  struct {
+    uint16_t length;
+    pft_entry_t entries[?];
+  }]])
+-- ctype for a list of port handles
+local port_list_t = ffi.typeof([[
+  struct {
+    uint16_t length;
+    handle_t ports[?];
+  }]])
+
+
+-- Allocate packet forwarding tables (pft):
+--
+--   ucast Used for forwarding to a known destination MAC address.
+--         A port list of length 1 is allocated for every slot in
+--         this pft.
+--
+--   flood Used for flooding when the destination MAC address is
+--         unkown.  No port lists are allocated for this pft.  The
+--         mac_table:lookup_pft() method will insert a pointer to
+--         the pre-allocated flooding port list associated with the
+--         ingress port of the packet.
+--
+--   discard Used to discard packets when a destination MAC address
+--           is kown but belongs to the same split-horizon group as
+--           the ingress port of the packet or the egress port
+--           coincides with the ingress port.
+--
+-- The initial size of each table is 256.  If the total number of
+-- inbound packets processed by a call to push() exceeds this number,
+-- all tables are re-allocated by adding the initial size to its
+-- current size (linear growth).
+local initial_pft_size = 256
+local pft_spec = { ucast = true, flood = false, discard = false }
+local function alloc_pft (self, size)
+   local size = size or initial_pft_size
+   local pft = { anchor = {} }
+   local i = 0
+   for type, alloc_pl in pairs(pft_spec) do
+      pft[type] = ffi.new(pft_t, size)
+      if alloc_pl then
+         for i = 0, size-1 do
+            -- Allocate a "unicast" port list and anchor it to prevent
+            -- it from being garbage-collected.
+            table.insert(pft.anchor, port_list_t(1))
+            local pl = pft.anchor[#pft.anchor]
+            -- C.lookup_pft() depends on this
+            pl.length = 1
+            pft[type].entries[i].plist = ffi.cast("port_list_t*", pl)
+         end
+      end
+      self._pft_C[i] = ffi.cast("pft_t*", pft[type])
+      i = i+1
+   end
+   self._pft = pft
+   self._pft_size = size
+end
 
 function bridge:new (arg)
    local o = bridge:superClass().new(self, arg)
-   local conf = o._conf
-   for k, v in pairs(default_config) do
-      if not conf[k] then
-         conf[k] = v
+   o._mac_table = mac_table:new(o._conf.config.mac_table)
+
+   -- Note: the indices of arrays accessed via port handles start at
+   -- 1.  All other arrays start at 0.
+
+   -- cdata version of the port-to-group mapping.
+   o._p2group = ffi.new("handle_t[?]", #o._ports+1)
+   for i = 1, #o._ports do
+      o._p2group[i] = o._ports[i].group
+   end
+
+   -- cdata version of egress port lists used for flooding.
+   local flood_pl = { anchor = {} }
+   for sp, dst in ipairs(o._dst_ports) do
+      local pl = port_list_t(#dst)
+      pl.length = #dst
+      for i = 1, #dst do
+         pl.ports[i-1] = dst[i]
       end
+      table.insert(flood_pl.anchor, pl)
+      flood_pl[sp] = ffi.cast("port_list_t*", pl)
    end
-   local bf = bloom:new(conf.mac_table_size, conf.fp_rate)
-   o._bf = bf
-   o._nsrc_ports = #o._src_ports
-   o._port_index = 1
-   -- Per-port Bloom filters
-   o._filters = {}
-   for _, port in ipairs(o._src_ports) do
-      o._filters[port] = { mac_table = bf:cell_new(),
-                           mac_shadow = bf:cell_new(),
-                           mac_address = bf:item_new()
-                        }
-
-   end
-   o._eth_dst = bf:item_new()
-
-   timer.activate(timer.new("mac_learn_timeout",
-                               function (t)
-                               if conf.verbose then
-                                  print("MAC learning timeout")
-                                  print("Table usage per port:")
-                               end
-                                  for port, filter in pairs(o._filters) do
-                                     bf:cell_copy(filter.mac_shadow, filter.mac_table)
-                                     bf:cell_clear(filter.mac_shadow)
-                                  if conf.verbose then
-                                     print(string.format("\t%s: %02.2f%%", port,
-                                                         100*bf:cell_usage(filter.mac_table)))
-                                  end
-                                  end
-                               end,
-                            conf.timeout *1e9, 'repeating')
-                  )
-
-   -- Caches for various cdata pointer objects to avoid boxing in the
-   -- push() loop
-   o._cache = {
-      p = ffi.new("struct packet *[1]"),
-      mem = ffi.new("uint8_t *[1]")
-   }
+   o._flood_pl = flood_pl
+   o._pft_C = ffi.new("pft_t *[3]")
+   alloc_pft(o)
+   -- Box to store a pointer to a MAC address in memory
+   o._mac = ffi.new("uint8_t *[1]")
+   o._logger = lib.logger_new({ module = "bridge" })
    return o
 end
 
--- We only process a single input port for each call of the push()
--- method to reduce the number of nested loops.  A better
--- understanding of the JIT compiler is needed to decide whether this
--- is actually a good thing or not.  Empirical data suggests it is :)
 function bridge:push()
-   local src_port = self._src_ports[self._port_index]
-   local l_in = self.input[src_port]
-   while not empty(l_in) do
-      local cache = self._cache
-      local dst_ports = self._dst_ports
-      local p = cache.p
-      local mem = cache.mem
-      local filters = self._filters
-      local eth_dst = self._eth_dst
-      local bf = self._bf
-      p[0] = receive(l_in)
-
-      -- Create a storage item from the destination MAC address
-      -- for matching with the source addresses learned on the
-      -- outbound ports, unless it is a multicast address.
-      mem[0] = packet.data(p[0])
-      local is_mcast = ethernet:is_mcast(mem[0])
-      if not is_mcast then
-         bf:store_value(mem, 6, eth_dst)
-      end
-
-      -- Store the source MAC address in the active and shadow
-      -- Bloom filters.
-      local filter = filters[src_port]
-      local mac_address = filter.mac_address
-      mem[0] = mem[0] + 6
-      bf:store_value(mem, 6, mac_address, filter.mac_table)
-      bf:store_item(mac_address, filter.mac_shadow)
-
-      local ports = dst_ports[src_port]
-      local copy = false
-      local j = 1
-      while ports[j] do
-         local dst_port = ports[j]
-         if is_mcast or bf:check_item(eth_dst, filters[dst_port].mac_table) then
-            if not copy then
-               transmit(self.output[dst_port], p[0])
-               copy = true
-            else
-               transmit(self.output[dst_port], clone(p[0]))
+   local ports = self._ports
+   local mac_table = self._mac_table
+   local pft = self._pft
+   local pft_size = self._pft_size
+   pft.ucast.length = 0
+   pft.flood.length = 0
+   pft.discard.length = 0
+   local ip = 1   -- ingress port
+   local packets = 0
+   while ports[ip] do
+      local ig = self._p2group[ip] -- ingress split-horizon group
+      local l_in = ports[ip].l_in
+      while not empty(l_in) do
+         local p = receive(l_in)
+         packets = packets + 1
+         -- Lookup the destination MAC address and associate the
+         -- packet with one of the packet forwarding tables according
+         -- to the result.
+         local mac = self._mac
+         mac[0] = packet.data(p)
+         mac_table:lookup_pft(mac, ip, ig, p, self._pft_C, self._flood_pl[ip])
+         -- Associate the source MAC address with the ingress port and
+         -- group.  Multicast addresses are forbidden to occur as
+         -- source address on the wire, but we check anyway to be sure
+         -- that we don't store them by accident.
+         mac[0] = mac[0] + 6
+         if not ethernet:is_mcast(mac[0]) then
+            mac_table:insert(mac, ip, ig)
+         end
+         if packets >= pft_size then
+            -- Dynamically increase the size of all forwarding tables
+            -- in a linear fashion.
+            local new_size = self._pft_size + initial_pft_size
+            if self._logger:can_log() then
+               self._logger:log("packet forwarding table overflow, "
+                                   .."increasing size from "
+                                   ..self._pft_size.." to "
+                                   ..new_size.." slots")
             end
-         end
-         j = j + 1
-      end
-      if not copy then
-         -- The source MAC address is unknown, flood the packet to
-         -- all ports
-         local output = self.output
-         transmit(output[ports[1]], p[0])
-         local j = 2
-         while ports[j] do
-            transmit(output[ports[j]], clone(p[0]))
-            j = j + 1
+            alloc_pft(self, new_size)
+            goto BREAK
          end
       end
-   end -- of while not empty(l_in)
-   if self._port_index == self._nsrc_ports then
-      self._port_index = 1
-   else
-      self._port_index = self._port_index + 1
+      ip = ip + 1
+   end
+   ::BREAK::
+
+   -- Unicast forwarding.
+   for i = 0, pft.ucast.length-1 do
+      local pfe = pft.ucast.entries[i]
+      local pl = pfe.plist
+      transmit(ports[pl.ports[0]].l_out, pfe.p)
+   end
+
+   -- Multicast/Flooding forwarding.  The first packet is transmitted
+   -- regularly.  All other packets need to be cloned from the first
+   -- one.
+   for i = 0, pft.flood.length-1 do
+      local pfe = pft.flood.entries[i]
+      local pl = pfe.plist
+      transmit(ports[pl.ports[0]].l_out, pfe.p)
+      for j = 1, pl.length-1 do
+         transmit(ports[pl.ports[j]].l_out, clone(pfe.p))
+      end
+   end
+
+   -- Discard packets that failed the split-horizon check.
+   for i = 0, pft.discard.length-1 do
+      local pfe = pft.discard.entries[i]
+      packet.free(pfe.p)
    end
 end

commit 6c0c04d05fafbb1db31dfa120731bd3863a000ee

quick fix of broken (flooding) bridge: probably broken since 339a80a787cbb0b71209bb29044e52e1cab76a79 nil value flooding.lua l24: local src_ports = self._src_ports -- _src_ports was never set

src/apps/bridge/base.lua
@@ -123,12 +123,13 @@ function bridge:new (arg)
          end
       end
    end
-
+   
    -- Create list of egress ports for each ingress port, containing
    -- all free ports as well as all ports from different split-horizon
    -- groups
-   local dst_ports = {}
+   local dst_ports, src_ports = {},{}
    for sport, sdesc in ipairs(ports) do
+      table.insert(src_ports, sport)
       dst_ports[sport] = {}
       for dport, ddesc in ipairs(ports) do
          if not (sport == dport or (sdesc.group ~= 0 and
@@ -137,6 +138,7 @@ function bridge:new (arg)
          end
       end
    end
+   o._src_ports = src_ports
    o._groups = groups
    o._ports = ports
    o._dst_ports = dst_ports