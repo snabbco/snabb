0cb8bd86955abb9e6a35617f338d385f45257364
Andy Wingo <wingo@igalia.com>
2016-05-25 15:52:52 +0200

    Add more NUMA methods

--- src/lib/numa.lua
+++ src/lib/numa.lua
@@ -2,6 +2,9 @@ module(..., package.seeall)
 
 local S = require("syscall")
 
+local bound_cpu
+local bound_numa_node
+
 function cpu_get_numa_node (cpu)
    local node = 0
    while true do
@@ -15,23 +18,67 @@ function cpu_get_numa_node (cpu)
    end
 end
 
-function set_cpu (cpu)
+function pci_get_numa_node (addr)
+   local file = assert(io.open('/sys/bus/pci/devices/'..addr..'/numa_node'))
+   local node = tonumber(file)
+   -- node can be -1.
+   if node >= 0 then return node end
+end
+
+function unbind_cpu ()
    local cpu_set = S.sched_getaffinity()
    cpu_set:zero()
-   cpu_set:set(cpu)
-   S.sched_setaffinity(0, cpu_set)
+   for i = 0, 1023 do cpu_set:set(i) end
+   assert(S.sched_setaffinity(0, cpu_set))
+   bound_cpu = nil
+end
 
-   local policy = S.get_mempolicy()
-   mask:zero()
-   mask:set(cpu) -- fixme should be numa node
-   S.set_mempolicy(policy.mode, policy.mask)
-   if not S.sched_setscheduler(0, "fifo", 1) then
+function bind_to_cpu (cpu)
+   if cpu == bound_cpu then return end
+   if not cpu then return unbind_cpu() end
+   assert(not bound_cpu, "already bound")
+
+   assert(S.sched_setaffinity(0, cpu))
+   local cpu_and_node = S.getcpu()
+   assert(cpu_and_node.cpu == cpu)
+   bound_cpu = cpu
+
+   bind_to_numa_node (cpu_and_node.node)
+end
+
+function unbind_numa_node ()
+   assert(S.set_mempolicy('default'))
+   bound_numa_node = nil
+end
+
+function bind_to_numa_node (node)
+   if node == bound_numa_node then return end
+   if not node then return unbind_numa_node() end
+   assert(not bound_numa_node, "already bound")
+
+   assert(S.set_mempolicy('bind', node))
+   bound_numa_node = node
+end
+
+function prevent_preemption(priority)
+   if not S.sched_setscheduler(0, "fifo", priority or 1) then
       fatal('Failed to enable real-time scheduling.  Try running as root.')
    end
 end
 
 function selftest ()
-   print(cpu_get_numa_node(0))
-   print(S.sched_getaffinity())
-   print(S.get_mempolicy().mask)
+   print('selftest: numa')
+   bind_to_cpu(0)
+   assert(bound_cpu == 0)
+   assert(bound_numa_node == 0)
+   assert(S.getcpu().cpu == 0)
+   assert(S.getcpu().node == 0)
+   bind_to_cpu(nil)
+   assert(bound_cpu == nil)
+   assert(bound_numa_node == 0)
+   assert(S.getcpu().node == 0)
+   bind_to_numa_node(nil)
+   assert(bound_cpu == nil)
+   assert(bound_numa_node == nil)
+   print('selftest: numa: ok')
 end
