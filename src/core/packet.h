e25a6bb8e80690c34ac9f9f4d5fe8d2bfa452d3f
Andy Wingo <wingo@igalia.com>
2016-06-07 13:27:04 +0000

    Add some headroom before a packet's data.

--- src/core/packet.h
+++ src/core/packet.h
@@ -1,10 +1,16 @@
 /* Use of this source code is governed by the Apache 2.0 license; see COPYING. */
 
-// The maximum amount of payload in any given packet.
-enum { PACKET_PAYLOAD_SIZE = 10*1024 };
+enum {
+    // A few bytes of headroom if needed by a low-level transport like
+    // virtio.
+    PACKET_HEADROOM_SIZE = 32,
+    // The maximum amount of payload in any given packet.
+    PACKET_PAYLOAD_SIZE = 10*1024
+};
 
 // Packet of network data, with associated metadata.
 struct packet {
+    unsigned char headroom[PACKET_HEADROOM_SIZE];
     unsigned char data[PACKET_PAYLOAD_SIZE];
     uint16_t length;           // data payload length
 };
