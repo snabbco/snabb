fbec950fa34ac903d74a524fd5733b5471175e86
Andy Wingo <wingo@igalia.com>
2016-06-09 15:39:41 +0200

    Bring length back to beginning of struct packet

--- src/core/packet.h
+++ src/core/packet.h
@@ -3,7 +3,7 @@
 enum {
     // A few bytes of headroom if needed by a low-level transport like
     // virtio.
-    PACKET_HEADROOM_SIZE = 56,
+    PACKET_HEADROOM_SIZE = 54,
     // The maximum amount of payload in any given packet.
     PACKET_PAYLOAD_SIZE = 10*1024
 };
@@ -11,8 +11,8 @@ enum {
 // Packet of network data, with associated metadata.
 struct packet {
     unsigned char *data;
+    uint16_t length;           // data payload length
     unsigned char headroom[PACKET_HEADROOM_SIZE];
     unsigned char data_[PACKET_PAYLOAD_SIZE];
-    uint16_t length;           // data payload length
 };
 
