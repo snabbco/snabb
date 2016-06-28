eaad21d2a6ad327bb6eea2af6e27f60ffeed7578
Alexander Gall <alexander.gall@gmail.com>
2016-06-28 08:58:31 +0200

    Fix regression introduced by 11a19ce1

--- src/apps/intel/intel1g.lua
+++ src/apps/intel/intel1g.lua
@@ -107,7 +107,7 @@ function Intel1g:new(conf)
    -- Setup device access
    pci.unbind_device_from_linux(pciaddress)
    pci.set_bus_master(pciaddress, true)
-   local regs, mmiofd = pci.map_pci_memory(pciaddress, 0)
+   local regs, mmiofd = pci.map_pci_memory(pciaddress, 0, true)
 
    -- Common utilities, see snabb/src/lib/hardware/register.lua
    local function bitvalue (value)
