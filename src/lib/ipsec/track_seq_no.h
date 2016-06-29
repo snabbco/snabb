4c311c928b72f618bb6fb12fbd5aad73462f6ce8
Timo Buhrmester <penenen@googlemail.com>
2016-06-29 14:54:59 +0000

    lib.ipsec.esp: Anti-replay (RFC 4303 App. A)

--- src/lib/ipsec/track_seq_no.h
+++ src/lib/ipsec/track_seq_no.h
@@ -1,1 +1,9 @@
-uint32_t track_seq_no (uint32_t, uint32_t, uint32_t, uint32_t);
+struct arepl_state;
+
+// done using ffi.C.new now
+//struct arepl_state *arepl_alloc_state(uint32_t winsz);
+//void arepl_free_state(struct arepl_state *st);
+
+bool arepl_pass(uint32_t seq_hi, uint32_t seq_lo, struct arepl_state *st);
+void arepl_accept(uint32_t seq_hi, uint32_t seq_lo, struct arepl_state *st);
+uint32_t arepl_infer_seq_hi(uint32_t seq_lo, struct arepl_state *st);
