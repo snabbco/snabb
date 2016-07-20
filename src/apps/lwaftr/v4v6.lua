2b35a950cf8733e21fe381db5020ff2ffa5286af
Diego Pino Garcia <dpino@igalia.com>
2016-07-20 14:29:53 +0000

    Add V4V6 selftest

--- src/apps/lwaftr/v4v6.lua
+++ src/apps/lwaftr/v4v6.lua
@@ -72,34 +72,114 @@ function v4v6:push()
    end
 
    -- Split input to IPv4 and IPv6 traffic.
-   while not link.empty(input) do
-      local pkt = receive(input)
-      if is_ipv4(pkt) then
-         if mirror then
-            mirror_ipv4(pkt, mirror, ipv4_num)
+   if input then
+      while not link.empty(input) do
+         local pkt = receive(input)
+         if is_ipv4(pkt) then
+            if mirror then
+               mirror_ipv4(pkt, mirror, ipv4_num)
+            end
+            transmit(v4_tx, pkt)
+         else
+            if mirror then
+               mirror_ipv6(pkt, mirror, ipv4_num)
+            end
+            transmit(v6_tx, pkt)
          end
-         transmit(v4_tx, pkt)
-      else
-         if mirror then
-            mirror_ipv6(pkt, mirror, ipv4_num)
-         end
-         transmit(v6_tx, pkt)
       end
    end
 
    -- Join IPv4 and IPv6 traffic to output.
-   while not link.empty(v4_rx) do
-      local pkt = receive(v4_rx)
-      if mirror and not link.full(mirror) then
-         mirror_ipv4(pkt, mirror, ipv4_num)
+   if output then
+      while not link.empty(v4_rx) do
+         local pkt = receive(v4_rx)
+         if mirror and not link.full(mirror) then
+            mirror_ipv4(pkt, mirror, ipv4_num)
+         end
+         transmit(output, pkt)
       end
-      transmit(output, pkt)
-   end
-   while not link.empty(v6_rx) do
-      local pkt = receive(v6_rx)
-      if mirror then
-         mirror_ipv6(pkt, mirror, ipv4_num)
+      while not link.empty(v6_rx) do
+         local pkt = receive(v6_rx)
+         if mirror then
+            mirror_ipv6(pkt, mirror, ipv4_num)
+         end
+         transmit(output, pkt)
       end
-      transmit(output, pkt)
    end
+end
+
+-- Tests.
+
+local function ipv4_pkt ()
+   local lib = require("core.lib")
+   return packet.from_string(lib.hexundump([[
+      02 aa aa aa aa aa 02 99 99 99 99 99 08 00 45 00
+      02 18 00 00 00 00 0f 11 d3 61 0a 0a 0a 01 c1 05
+      01 64 30 39 14 00 00 20 00 00 00 00 00 00 00 00
+      00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+      00 00
+   ]], 66))
+end
+
+local function ipv6_pkt ()
+   local lib = require("core.lib")
+   return packet.from_string(lib.hexundump([[
+      02 aa aa aa aa aa 02 99 99 99 99 99 86 dd 60 00
+      01 f0 01 f0 04 ff fc 00 00 01 00 02 00 03 00 04
+      00 05 00 00 44 2d fc 00 00 00 00 00 00 00 00 00
+      00 00 00 00 01 00 45 00 01 f0 00 00 00 00 0f 11
+      d2 76 c1 05 02 77 0a 0a 0a 01 0c 00 30 39 00 20
+      00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+      00 00 00 00 00 00 00 00 00 00
+   ]], 106))
+end
+
+local function test_split ()
+   local basic_apps = require("apps.basic.basic_apps")
+   engine.configure(config.new()) -- Clean up engine.
+
+   local c = config.new()
+   config.app(c, 'source', basic_apps.Join)
+   config.app(c, 'v4v6', v4v6)
+   config.app(c, 'sink', basic_apps.Sink)
+
+   config.link(c, 'source.output -> v4v6.input')
+   config.link(c, 'v4v6.v4_tx -> sink.in1')
+   config.link(c, 'v4v6.v6_tx -> sink.in2')
+
+   engine.configure(c)
+   link.transmit(engine.app_table.source.output.output, ipv4_pkt())
+   link.transmit(engine.app_table.source.output.output, ipv6_pkt())
+   engine.main({duration = 0.1, noreport = true})
+
+   assert(link.stats(engine.app_table.sink.input.in1).rxpackets == 1)
+   assert(link.stats(engine.app_table.sink.input.in2).rxpackets == 1)
+end
+
+local function test_join ()
+   local basic_apps = require("apps.basic.basic_apps")
+   engine.configure(config.new()) -- Clean up engine.
+
+   local c = config.new()
+   config.app(c, 'source', basic_apps.Join)
+   config.app(c, 'v4v6', v4v6)
+   config.app(c, 'sink', basic_apps.Sink)
+
+   config.link(c, 'source.output -> v4v6.v4_rx')
+   config.link(c, 'source.output -> v4v6.v6_rx')
+   config.link(c, 'v4v6.output -> sink.input')
+
+   engine.configure(c)
+   link.transmit(engine.app_table.source.output.output, ipv4_pkt())
+   link.transmit(engine.app_table.source.output.output, ipv6_pkt())
+   engine.main({duration = 0.1, noreport = true})
+
+   assert(link.stats(engine.app_table.sink.input.input).rxpackets == 2)
+end
+
+function selftest ()
+   print("v4v6: selftest")
+   test_split()
+   test_join()
+   print("OK")
 end
