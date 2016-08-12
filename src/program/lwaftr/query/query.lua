1119dfb9907e8e1f49af0b2c36aa36080088fa5d
Diego Pino Garcia <dpino@igalia.com>
2016-08-12 17:54:26 +0000

    Import 'lwaftr query' program to lwaftr branch

--- src/program/lwaftr/query/query.lua
+++ src/program/lwaftr/query/query.lua
@@ -1,12 +1,16 @@
 module(..., package.seeall)
 
+local S = require("syscall")
 local counter = require("core.counter")
 local lib = require("core.lib")
+local lwaftr = require("apps.lwaftr.lwaftr")
 local shm = require("core.shm")
-local S = require("syscall")
+local top = require("program.top.top")
+
+local select_snabb_instance = top.select_snabb_instance
 
 -- Get the counter dir from the code.
-local counters_rel_dir = require("apps.lwaftr.lwaftr").counters_dir
+local counters_rel_dir = lwaftr.counters_dir
 
 function show_usage (code)
    print(require("program.lwaftr.query.README_inc"))
@@ -21,35 +25,17 @@ function parse_args (raw_args)
    return args[1]
 end
 
--- TODO: taken from program/top/top.lua, unify somewhere.
-function select_snabb_instance (pid)
-   local instances = shm.children("//")
-   if pid then
-      -- Try to use the given pid.
-      for _, instance in ipairs(instances) do
-         if instance == pid then return pid end
-      end
-      print("No such Snabb instance: "..pid)
-   elseif #instances == 2 then
-      -- Two means one is us, so we pick the other.
-      local own_pid = tostring(S.getpid())
-      if instances[1] == own_pid then return instances[2]
-      else                            return instances[1] end
-   elseif #instances == 1 then print("No Snabb instance found.")
-   else print("Multiple Snabb instances found. Select one.") end
-   os.exit(1)
-end
-
 function print_counters (tree)
    local cnt, cnt_path, value
    print("lwAFTR operational counters (non-zero)")
    -- Open, read and print whatever counters are in that directory.
-   local counters_path = tree .. "/" .. counters_rel_dir
+   local counters_path = "/" .. tree .. "/" .. counters_rel_dir
    for _, name in ipairs(shm.children(counters_path)) do
       cnt_path = counters_path .. name
       cnt = counter.open(cnt_path, 'readonly')
       value = tonumber(counter.read(cnt))
       if value ~= 0 then
+         name = name:gsub(".counter$", "")
          print(name..": "..value)
       end
    end
@@ -57,6 +43,6 @@ end
 
 function run (raw_args)
    local target_pid = parse_args(raw_args)
-   local instance_tree = "//"..(select_snabb_instance(target_pid))
+   local instance_tree = select_snabb_instance(target_pid)
    print_counters(instance_tree)
 end
