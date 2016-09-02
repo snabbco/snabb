fb32e41b74d91e01c611332b4f24da5a2aaa3eaf
Diego Pino Garcia <dpino@igalia.com>
2016-09-02 12:10:31 +0000

    Refactor 'packetblaster synth'

--- src/program/packetblaster/synth/synth.lua
+++ src/program/packetblaster/synth/synth.lua
@@ -8,7 +8,6 @@ local Synth     = require("apps.test.synth").Synth
 local lib       = require("core.lib")
 
 local packetblaster = require("program.packetblaster.packetblaster")
-local usage = require("program.packetblaster.synth.README_inc")
 
 local long_opts = {
    duration     = "D",
@@ -18,16 +17,20 @@ local long_opts = {
    sizes        = "S"
 }
 
+local function show_usage (code)
+   print(require("program.packetblaster.synth.README_inc"))
+   main.exit(code)
+end
+
 function run (args)
    local opt = {}
    local duration
    local c = config.new()
-   function opt.D (arg) 
-      duration = assert(tonumber(arg), "duration is not a number!")  
+   function opt.D (arg)
+      duration = assert(tonumber(arg), "duration is not a number!")
    end
-   function opt.h (arg)
-      print(usage)
-      main.exit(1)
+   function opt.h ()
+      show_usage(0)
    end
 
    local source
@@ -36,14 +39,15 @@ function run (args)
    function opt.s (arg) source = arg end
    function opt.d (arg) destination = arg end
    function opt.S (arg)
-     sizes = {}
-     for size in string.gmatch(arg, "%d+") do
-       sizes[#sizes+1] = tonumber(size)
-     end
+      sizes = {}
+      for size in string.gmatch(arg, "%d+") do
+         sizes[#sizes+1] = tonumber(size)
+      end
    end
 
    args = lib.dogetopt(args, opt, "hD:s:d:S:", long_opts)
+   if not (sizes or source or destination) then show_usage(1) end
    config.app(c, "source", Synth, { sizes = sizes,
-     src = source, dst = destination })
+      src = source, dst = destination })
    packetblaster.run_loadgen(c, args, duration)
 end
