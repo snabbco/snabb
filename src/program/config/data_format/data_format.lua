66d1cddb40f73b4c29e349f2660067502cecf796
Jessica Tallon <tsyesika@tsyesika.se>
2016-11-07 15:23:00 +0100

    Convert node displaying into data driven code
    This converts a if-elseif-else block which was being used
    as a mechanism for selecting between different node types
    and instead converting it to a more function driven method.
    
    Now there is a table of methods built up that handle the different
    types and those are selected based of the node type to handle the
    display of the data.

--- src/program/config/data_format/data_format.lua
+++ src/program/config/data_format/data_format.lua
@@ -38,10 +38,10 @@ function run(args)
       end
       return rtn
    end
-   function collate_options(name, node, keys)
+   function collate_options(name, node)
       local options = {}
-      if keys ~= nil then
-	 for _, k in pairs(keys) do
+      if node.keys ~= nil then
+	 for _, k in pairs(node.keys) do
 	    if name == k then options.key = true end
 	 end
       end
@@ -56,69 +56,74 @@ function run(args)
       end
       return options
    end
-   function display_node(name, node, options, level, parent)
-      function comment()
-	 local comments = {}
-	 if options.mandatory == true then
-	    comments[#comments + 1] = "mandatory"
-	 end
-	 if options.key then
-	    comments[#comments + 1] = "key"
-	 end
-	 if options.range then
-	    comments[#comments + 1] = "between " .. options.range
-	 end
-	 local rtn = nil
-	 for n, c in pairs(comments) do
-	    if n == 1 then
-	       rtn = "// " .. c
-	    else
-	       rtn = rtn  .. " " .. c
-	    end
-	 end
-	 return rtn
+   function comment(options)
+      local comments = {}
+      if options.mandatory == true then
+	 comments[#comments + 1] = "mandatory"
       end
-      function display_leaf(keyword, argument)
-	 if argument == "union" then argument = union_type(node) end
-	 local comments = comment()
-	 local str = keyword .. " " .. argument .. ";"
-	 if comments then
-	    print_level(level, str .. " " .. comments)
+      if options.key then
+	 comments[#comments + 1] = "key"
+      end
+      if options.range then
+	 comments[#comments + 1] = "between " .. options.range
+      end
+      local rtn = nil
+      for n, c in pairs(comments) do
+	 if n == 1 then
+	    rtn = "// " .. c
 	 else
-	    print_level(level, str)
+	    rtn = rtn  .. " " .. c
 	 end
       end
+      return rtn
+   end
+
+   function display_leaf(level, keyword, argument, options)
+      if argument == "union" then argument = union_type(node) end
+      local comments = comment(options)
+      local str = keyword .. " ".. argument .. ";"
+      if comments then
+	 print_level(level, str .. " " .. comments)
+      else
+	 print_level(level, str)
+      end
+   end
+
+   local describers = {}
+   local function describe(level, name, node, options)
+      local err = "Unknown node type: "..node.type
+      local options = collate_options(name, node)
+      assert(describers[node.type], err)(level, name, node, options)
+   end
+   function describe_members(node, level)
       if level == nil then level = 0 end
-      if node.type == "table" or node.type == "struct" then
-	 local keys = {}
-	 if node.keys then
-	    print_level(
-	       level,
-	       "// List: this nested structure is repeated with (a) unique key(s)"
-	    )
-	 end
-	 print_level(level, name.." {")
-         display_data_format(node, level + 1)
-         print_level(level, "}")
-      elseif node.type == "scalar" then
-         display_leaf(name, node.argument_type.argument_string)
-      elseif node.type == "array" then
+      for name, n in pairs(node.members) do
+	 local options = collate_options(name, node)
+	 describe(level, name, n, options)
+      end
+   end
+   function describers.scalar(level, name, node, options)
+      display_leaf(level, name, node.argument_type.argument_string, options)
+   end
+   function describers.table(level, name, node, options)
+      if node.keys then
 	 print_level(
 	    level,
-	    "// Array: made by repeating the keyword followed by each element"
+	    "// List: this nested structure is repeated with (a) unique key(s)"
 	 )
-	 display_leaf(name, node.element_type.argument_string)
-      else
       end
+      print_level(level, name.." {")
+      describe_members(node, level + 1)
+      print_level(level, "}")
    end
-
-   function display_data_format(grammar, level)
-      for k,v in pairs(grammar.members) do
-	 local options = collate_options(k, v, grammar.keys)
-         display_node(k, v, options, level)
-      end
+   describers.struct = describers.table
+   function describers.array(level, name, node, options)
+      print_level(
+	 level,
+	 "// Array: made by repeating the keyword followed by each element"
+      )
+      display_leaf(level, name, node.element_type.argument_string, options)
    end
-
 
    local yang_module = parse_args(args)
 
@@ -126,5 +131,5 @@ function run(args)
    local s = schema.parse_schema_file(yang_module)
    local grammar = yang_data.data_grammar_from_schema(s)
 
-   display_data_format(grammar, 0)
+   describe_members(grammar)
 end
