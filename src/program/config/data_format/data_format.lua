24484afc50ef3d7f8dd977b70eef6a1a95b18382
Jessica Tallon <tsyesika@tsyesika.se>
2016-11-07 17:04:07 +0100

    Refactor options and data_format config file
    This whilst refactoring most of data_format is largely to add refactor
    options so that they are only calculated when the option can actually
    be set. This also makes some small changes like removing an unused
    require statement and changing the tab width from 4 to 2

--- src/program/config/data_format/data_format.lua
+++ src/program/config/data_format/data_format.lua
@@ -4,14 +4,136 @@ module(..., package.seeall)
 local lib = require("core.lib")
 local schema = require("lib.yang.schema")
 local yang_data = require("lib.yang.data")
-local usage = require("program.config.data_format.README_inc")
 
 -- Number of spaces a tab should consist of when indenting config.
-local tab_spaces = 4
+local tab_spaces = 2
+
+local function print_level(level, ...)
+   io.write(string.rep(" ", level * tab_spaces))
+   print(...)
+end
+
+local function union_type(union)
+   local rtn
+   for _, t in pairs(union.argument_type.union) do
+      if rtn then
+	 rtn = rtn .. " | " .. t.argument_string
+      else
+	 rtn = t.argument_string
+      end
+   end
+   return rtn
+end
+
+local function comment(opts)
+   local comments = {}
+   if opts.mandatory == true then
+      comments[#comments + 1] = "mandatory"
+   end
+   if opts.key then
+      comments[#comments + 1] = "key"
+   end
+   if opts.range then
+      comments[#comments + 1] = "between " .. opts.range
+   end
+   local rtn = nil
+   for n, c in pairs(comments) do
+      if n == 1 then
+	 rtn = "// " .. c
+      else
+	 rtn = rtn  .. " " .. c
+      end
+   end
+   return rtn
+end
+
+local function display_leaf(level, keyword, argument, opts)
+   if argument == "union" then argument = union_type(node) end
+   local comments
+   if opts then comments = comment(opts) end
+   local str = keyword .. " ".. argument .. ";"
+   if comments then
+      print_level(level, str .. " " .. comments)
+   else
+      print_level(level, str)
+   end
+end
 
 local function show_usage(status)
    print(require("program.config.data_format.README_inc"))
    main.exit(status)
+end
+
+-- Contains verious option handling code.
+local options = {}
+
+function options.key(keys)
+   return function (name)
+      for _, k in pairs(keys) do
+	 if name == k then return true end
+      end
+      return false
+   end
+end
+
+function options.mandatory(name, node)
+   if node.mandatory then return node.mandatory end
+end
+
+function options.range(name, node)
+   if node.argument_type.range then
+      return node.argument_type.range.argument_string
+   end
+   return nil
+end
+
+-- Contains the handlers which know how to describe certain data node types.
+local describers = {}
+
+local function describe(level, name, node, ...)
+   local err = "Unknown node type: "..node.type
+   assert(describers[node.type], err)(level, name, node, ...)
+end
+
+local function describe_members(node, level, ...)
+   if level == nil then level = 0 end
+   for name, n in pairs(node.members) do
+      describe(level, name, n, ...)
+   end
+end
+
+function describers.scalar(level, name, node, is_key)
+   local opts = {}
+   if is_key then opts.key = is_key(name, node) end
+   opts.mandatory = options.mandatory(name, node)
+   opts.range = options.range(name, node)
+   display_leaf(level, name, node.argument_type.argument_string, opts)
+end
+
+function describers.table(level, name, node)
+   if node.keys then
+      print_level(
+	 level,
+	 "// List: this nested structure is repeated with (a) unique key(s)"
+      )
+   end
+   print_level(level, name.." {")
+   describe_members(node, level + 1, options.key(node.keys))
+   print_level(level, "}")
+end
+
+function describers.struct(level, name, node)
+   print_level(level, name.." {")
+   describe_members(node, level + 1)
+   print_level(level, "}")
+end
+
+function describers.array(level, name, node)
+   print_level(
+      level,
+      "// Array: made by repeating the keyword followed by each element"
+   )
+   display_leaf(level, name, node.element_type.argument_string)
 end
 
 local function parse_args(args)
@@ -23,108 +145,6 @@ local function parse_args(args)
 end
 
 function run(args)
-   function print_level(level, ...)
-      io.write(string.rep(" ", level * tab_spaces))
-      print(...)
-   end
-   function union_type(union)
-      local rtn
-      for _, t in pairs(union.argument_type.union) do
-         if rtn then
-            rtn = rtn .. " | " .. t.argument_string
-         else
-            rtn = t.argument_string
-         end
-      end
-      return rtn
-   end
-   function collate_options(name, node)
-      local options = {}
-      if node.keys ~= nil then
-	 for _, k in pairs(node.keys) do
-	    if name == k then options.key = true end
-	 end
-      end
-      if node.argument_type then
-	 local at = node.argument_type
-	 if at.range then
-	    options.range = at.range.argument_string
-	 end
-      end
-      if node.mandatory then
-	 options.mandatory = true
-      end
-      return options
-   end
-   function comment(options)
-      local comments = {}
-      if options.mandatory == true then
-	 comments[#comments + 1] = "mandatory"
-      end
-      if options.key then
-	 comments[#comments + 1] = "key"
-      end
-      if options.range then
-	 comments[#comments + 1] = "between " .. options.range
-      end
-      local rtn = nil
-      for n, c in pairs(comments) do
-	 if n == 1 then
-	    rtn = "// " .. c
-	 else
-	    rtn = rtn  .. " " .. c
-	 end
-      end
-      return rtn
-   end
-
-   function display_leaf(level, keyword, argument, options)
-      if argument == "union" then argument = union_type(node) end
-      local comments = comment(options)
-      local str = keyword .. " ".. argument .. ";"
-      if comments then
-	 print_level(level, str .. " " .. comments)
-      else
-	 print_level(level, str)
-      end
-   end
-
-   local describers = {}
-   local function describe(level, name, node, options)
-      local err = "Unknown node type: "..node.type
-      local options = collate_options(name, node)
-      assert(describers[node.type], err)(level, name, node, options)
-   end
-   function describe_members(node, level)
-      if level == nil then level = 0 end
-      for name, n in pairs(node.members) do
-	 local options = collate_options(name, node)
-	 describe(level, name, n, options)
-      end
-   end
-   function describers.scalar(level, name, node, options)
-      display_leaf(level, name, node.argument_type.argument_string, options)
-   end
-   function describers.table(level, name, node, options)
-      if node.keys then
-	 print_level(
-	    level,
-	    "// List: this nested structure is repeated with (a) unique key(s)"
-	 )
-      end
-      print_level(level, name.." {")
-      describe_members(node, level + 1)
-      print_level(level, "}")
-   end
-   describers.struct = describers.table
-   function describers.array(level, name, node, options)
-      print_level(
-	 level,
-	 "// Array: made by repeating the keyword followed by each element"
-      )
-      display_leaf(level, name, node.element_type.argument_string, options)
-   end
-
    local yang_module = parse_args(args)
 
    -- Fetch and parse the schema module.
