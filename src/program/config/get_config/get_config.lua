48bb8f213d5b9c1c2f5e801fdeeeb306f096710b
Andy Wingo <wingo@igalia.com>
2016-11-12 14:09:11 +0100

    snabb config get-config uses RPC mechanism
    We serialize the call and parse the response using the RPC yang
    facilities.

--- src/program/config/get_config/get_config.lua
+++ src/program/config/get_config/get_config.lua
@@ -5,7 +5,8 @@ local S = require("syscall")
 local ffi = require("ffi")
 local lib = require("core.lib")
 local schema = require("lib.yang.schema")
-local yang_data = require("lib.yang.data")
+local yang = require("lib.yang.yang")
+local data = require("lib.yang.data")
 
 -- Number of spaces a tab should consist of when indenting config.
 local tab_spaces = 2
@@ -16,11 +17,18 @@ local function show_usage(status)
 end
 
 local function parse_args(args)
+   local schema_name, revision_date
    local handlers = {}
-   handlers.h = function() show_usage(0) end
-   args = lib.dogetopt(args, handlers, "h", {help="h"})
-   if #args ~= 2 then show_usage(1) end
-   return unpack(args)
+   function handlers.h() show_usage(0) end
+   function handlers.s(arg) schema_name = arg end
+   function handlers.r(arg) revision_date = arg end
+   args = lib.dogetopt(args, handlers, "h:s:r",
+                       {help="h", ['schema-name']="s", schema="s",
+                        ['revision-date']="r", revision="r"})
+   if not schema_name then show_usage(1) end
+   if #args < 2 or #args > 2 then show_usage(1) end
+   local instance_id, path = unpack(args)
+   return schema_name, revision_date, instance_id, path
 end
 
 local function read_length(socket)
@@ -45,20 +53,40 @@ local function read_msg(socket, len)
    return ffi.string(buf, len)
 end
 
+function prepare_rpc_list(rpcs)
+   local schema = yang.load_schema_by_name('snabb-config-leader-v1')
+   local print_input = data.rpc_input_printer_from_schema(schema)
+   local parse_output = data.rpc_output_parser_from_schema(schema)
+   return print_input(rpcs), parse_output
+end
+
+function prepare_rpc(id, data)
+   local str, parse_responses = prepare_rpc_list({{id=id, data=data}})
+   local function parse_response(str)
+      local responses = parse_responses(str)
+      assert(#responses == 1)
+      assert(responses[1].id == id)
+      return responses[1].data
+   end
+   return str, parse_response
+end
+
 function run(args)
-   local socket_file_name, msg = parse_args(args)
+   local schema_name, revision_date, instance_id, path = parse_args(args)
+   local data = { schema = schema_name, revision = revision_date, path = path }
+   local message, parse_response = prepare_rpc('get-config', data)
+
+   print(message)
 
    local socket = assert(S.socket("unix", "stream"))
-
-   local sa = S.t.sockaddr_un(socket_file_name)
+   local sa = S.t.sockaddr_un(instance_id)
    assert(socket:connect(sa))
-
-   socket:write(tostring(#msg)..'\n'..msg)
-
+   socket:write(tostring(#message)..'\n'..message)
    local len = read_length(socket)
    local msg = read_msg(socket, len)
-   print(msg)
-
    socket:close()
+
+   print(parse_response(msg))
+
    main.exit(0)
 end
