87c887cf868ba4bce3b7c9c016058262710deb03
Andy Wingo <wingo@igalia.com>
2016-11-17 11:40:06 +0100

    Refactor "snabb config"
    More functionality moves to a new "common.lua" file.

---
+++ src/program/config/common.lua
@@ -0,0 +1,106 @@
+-- Use of this source code is governed by the Apache 2.0 license; see COPYING.
+module(..., package.seeall)
+
+local S = require("syscall")
+local ffi = require("ffi")
+local lib = require("core.lib")
+local shm = require("core.shm")
+local rpc = require("lib.yang.rpc")
+
+function show_usage(command, status, err_msg)
+   if err_msg then print('error: '..err_msg) end
+   print(require("program.config."..command:gsub('-','_')..".README_inc"))
+   main.exit(status)
+end
+
+local parse_command_line_opts = {
+   command = { required=true },
+   with_config_file = { default=false },
+   with_path = { default=false },
+   with_value = { default=false }
+}
+
+function parse_command_line(args, opts)
+   opts = lib.parse(opts, parse_command_line_opts)
+   local function err(msg) show_usage(opts.command, 1, msg) end
+   local schema_name, revision_date
+   local handlers = {}
+   function handlers.h() show_usage(opts.command, 0) end
+   function handlers.s(arg) schema_name = arg end
+   function handlers.r(arg) revision_date = arg end
+   args = lib.dogetopt(args, handlers, "hs:r:",
+                       {help="h", ['schema-name']="s", schema="s",
+                        ['revision-date']="r", revision="r"})
+   if not schema_name then err("missing --schema arg") end
+   if #args == 0 then err() end
+   local instance_id = table.remove(args, 1)
+   local ret = { schema_name, revision_date, instance_id }
+   if opts.with_config_file then
+      if #args == 0 then err("missing config file argument") end
+      local file = table.remove(args, 1)
+      table.insert(ret, yang.load_configuration(file, schema_name))
+   end
+   if opts.with_path then
+      if #args == 0 then err("missing path argument") end
+      local path = table.remove(args, 1)
+      -- Waiting on our XPath parsing library :)
+      if path ~= '/' then err("paths other than / currently unimplemented") end
+      table.insert(ret, path)
+   end
+   if opts.with_value then
+      local parser = data_parser(schema_name, path)
+      if #args == 0 then
+         table.insert(ret, parser(io.stdin:read('*a')))
+      else
+         table.insert(ret, parser(table.remove(args, 1)))
+      end
+   end
+   if #args ~= 0 then err("too many arguments") end
+   return unpack(ret)
+end
+
+function open_socket_or_die(instance_id)
+   S.signal('pipe', 'ign')
+   local socket = assert(S.socket("unix", "stream"))
+   local tail = instance_id..'/config-leader-socket'
+   local by_name = S.t.sockaddr_un(shm.root..'/by-name/'..tail)
+   local by_pid = S.t.sockaddr_un(shm.root..'/'..tail)
+   if not socket:connect(by_name) and not socket:connect(by_pid) then
+      io.stderr:write(
+         "Could not connect to config leader socket on Snabb instance '"..
+            instance_id.."'.\n")
+      main.exit(1)
+   end
+   return socket
+end
+
+function send_message(socket, msg_str)
+   socket:write(tostring(#msg_str)..'\n'..msg_str)
+end
+
+local function read_length(socket)
+   local len = 0
+   while true do
+      local ch = assert(socket:read(nil, 1))
+      assert(ch ~= '', 'short read')
+      if ch == '\n' then return len end
+      assert(tonumber(ch), 'not a number: '..ch)
+      len = len * 10 + tonumber(ch)
+      assert(len < 1e8, 'length too long: '..len)
+   end
+end
+
+local function read_msg(socket, len)
+   local buf = ffi.new('uint8_t[?]', len)
+   local pos = 0
+   while pos < len do
+      local count = assert(socket:read(buf+pos, len-pos))
+      if count == 0 then error('short read') end
+      pos = pos + count
+   end
+   return ffi.string(buf, len)
+end
+
+function recv_message(socket)
+   return read_msg(socket, read_length(socket))
+end
--- src/program/config/get/get.lua
+++ src/program/config/get/get.lua
@@ -3,75 +3,20 @@ module(..., package.seeall)
 
 local S = require("syscall")
 local ffi = require("ffi")
-local lib = require("core.lib")
 local shm = require("core.shm")
 local rpc = require("lib.yang.rpc")
-
--- Number of spaces a tab should consist of when indenting config.
-local tab_spaces = 2
-
-local function show_usage(status)
-   print(require("program.config.get.README_inc"))
-   main.exit(status)
-end
-
-local function parse_args(args)
-   local schema_name, revision_date
-   local handlers = {}
-   function handlers.h() show_usage(0) end
-   function handlers.s(arg) schema_name = arg end
-   function handlers.r(arg) revision_date = arg end
-   args = lib.dogetopt(args, handlers, "hs:r:",
-                       {help="h", ['schema-name']="s", schema="s",
-                        ['revision-date']="r", revision="r"})
-   if not schema_name then show_usage(1) end
-   if #args < 2 or #args > 2 then show_usage(1) end
-   local instance_id, path = unpack(args)
-   return schema_name, revision_date, instance_id, path
-end
-
-local function read_length(socket)
-   local len = 0
-   while true do
-      local ch = assert(socket:read(nil, 1))
-      assert(ch ~= '', 'short read')
-      if ch == '\n' then return len end
-      assert(tonumber(ch), 'not a number: '..ch)
-      len = len * 10 + tonumber(ch)
-      assert(len < 1e8, 'length too long: '..len)
-   end
-end
-
-local function read_msg(socket, len)
-   local buf = ffi.new('uint8_t[?]', len)
-   local pos = 0
-   while pos < len do
-      local count = assert(socket:read(buf+pos, len-pos))
-      if count == 0 then error('short read') end
-      pos = pos + count
-   end
-   return ffi.string(buf, len)
-end
+local common = require("program.config.common")
 
 function run(args)
-   local schema_name, revision_date, instance_id, path = parse_args(args)
+   local schema_name, revision_date, instance_id, path =
+      common.parse_command_line(args, { command='get', with_path=true })
    local caller = rpc.prepare_caller('snabb-config-leader-v1')
    local data = { schema = schema_name, revision = revision_date, path = path }
    local msg, parse = rpc.prepare_call(caller, 'get-config', data)
-   local socket = assert(S.socket("unix", "stream"))
-   local tail = instance_id..'/config-leader-socket'
-   local by_name = S.t.sockaddr_un(shm.root..'/by-name/'..tail)
-   local by_pid = S.t.sockaddr_un(shm.root..'/'..tail)
-   if not socket:connect(by_name) and not socket:connect(by_pid) then
-      io.stderr:write(
-         "Could not connect to config leader socket on Snabb instance '"..
-            instance_id.."'.\n")
-      main.exit(1)
-   end
-   socket:write(tostring(#msg)..'\n'..msg)
-   local len = read_length(socket)
-   msg = read_msg(socket, len)
+   local socket = common.open_socket_or_die(instance_id)
+   common.send_message(socket, msg)
+   local reply = common.recv_message(socket)
    socket:close()
-   print(parse(msg).config)
+   print(parse(reply).config)
    main.exit(0)
 end
