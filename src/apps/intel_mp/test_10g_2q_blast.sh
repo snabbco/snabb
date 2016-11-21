89754f20a8d9d1105d4f1ba8b8eb11151c364c54
Pete Bristow <pete.bristow@gmail.com>
2016-11-21 18:04:43 +0000

    remove taskset pinning

--- src/apps/intel_mp/test_10g_2q_blast.sh
+++ src/apps/intel_mp/test_10g_2q_blast.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-SNABB_SEND_BLAST=true taskset -c 1 ./testsend.snabb Intel82599 $SNABB_PCI_INTEL1 0 source.pcap &
+SNABB_SEND_BLAST=true ./testsend.snabb Intel82599 $SNABB_PCI_INTEL1 0 source.pcap &
 BLAST=$!
 
-SNABB_RECV_SPINUP=2 SNABB_RECV_DURATION=5 taskset -c 2 ./testrecv.snabb Intel82599 $SNABB_PCI_INTEL0 0 > results.0 &
-SNABB_RECV_SPINUP=2 SNABB_RECV_DURATION=5 taskset -c 3 ./testrecv.snabb Intel82599 $SNABB_PCI_INTEL0 1 > results.1
+SNABB_RECV_SPINUP=2 SNABB_RECV_DURATION=5 ./testrecv.snabb Intel82599 $SNABB_PCI_INTEL0 0 > results.0 &
+SNABB_RECV_SPINUP=2 SNABB_RECV_DURATION=5 ./testrecv.snabb Intel82599 $SNABB_PCI_INTEL0 1 > results.1
 
 sleep 1
 kill -9 $BLAST
