c545c5f522e05f2b3d6e4572d87f4ccea30ec3e0
Jessica Tallon <tsyesika@igalia.com>
2016-11-28 12:10:25 +0100

    Change use of shallow copy to make more robust

--- src/lib/yang/schema.lua
+++ src/lib/yang/schema.lua
@@ -576,13 +576,12 @@ local function inherit_config(schema, config)
       assert(not config or schema.config == false)
       config = schema.config
    elseif config ~= nil then
-      schema = shallow_copy(schema)
       schema.config = config
    end
 
    if schema.body then
       for name, node in pairs(schema.body) do
-         schema.body[name] = inherit_config(node, config)
+         schema.body[name] = inherit_config(shallow_copy(node), config)
       end
    end
 
