4ad0c2ef305f94635dd82f5ab4fa7972709acf3a
Asumu Takikawa <asumu@igalia.com>
2016-12-07 15:58:14 -0800

    Minor fix to selftest.sh

--- src/program/lwaftr/tests/propbased/selftest.sh
+++ src/program/lwaftr/tests/propbased/selftest.sh
@@ -4,6 +4,6 @@ set -e
 
 SKIPPED_CODE=43
 
-if [ -z $SNABB_PCI0 0 ]; then exit SKIPPED_CODE; fi
+if [ -z $SNABB_PCI0 ]; then exit SKIPPED_CODE; fi
 
 ./snabb quickcheck program.lwaftr.tests.propbased.prop_nocrash $SNABB_PCI0
