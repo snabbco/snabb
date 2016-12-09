84a0ac1473832ebd5df111b7caffb1c90031c44a
Jessica Tallon <tsyesika@igalia.com>
2016-12-09 10:28:36 +0100

    Add forgotten selftest.sh

---
+++ src/program/lwaftr/tests/config/selftest.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+./program/lwaftr/tests/config/test-config-get.sh
+./program/lwaftr/tests/config/test-config-set.sh
+./program/lwaftr/tests/config/test-config-add.sh
+./program/lwaftr/tests/config/test-config-remove.sh
+./program/lwaftr/tests/config/test-config-get-state.sh
