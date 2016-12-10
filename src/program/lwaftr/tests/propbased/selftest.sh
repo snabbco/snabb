a0c779e02b3b3a0fdcf511ac226980b2153a264e
Asumu Takikawa <asumu@igalia.com>
2016-12-09 17:13:26 -0800

    Add test ensuring two config runs yield the same result

--- src/program/lwaftr/tests/propbased/selftest.sh
+++ src/program/lwaftr/tests/propbased/selftest.sh
@@ -7,3 +7,4 @@ SKIPPED_CODE=43
 if [ -z $SNABB_PCI0 ]; then exit $SKIPPED_CODE; fi
 
 ./snabb quickcheck program.lwaftr.tests.propbased.prop_nocrash $SNABB_PCI0
+./snabb quickcheck program.lwaftr.tests.propbased.prop_sameval $SNABB_PCI0
