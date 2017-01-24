6ac8f23c1e25a57eaa16e4928e54fead106ffe93
Asumu Takikawa <asumu@igalia.com>
2017-01-24 12:48:17 -0800

    Make snabb wall filter more complete
    Adds multiple input sources, flags to set the output
    path and whether to show the link report, and reads
    the firewall rules from a file.

--- src/program/wall/filter/filter.lua
+++ src/program/wall/filter/filter.lua
@@ -1,33 +1,77 @@
 module(..., package.seeall)
 
-local fw   = require("apps.wall.l7fw")
-local pcap = require("apps.pcap.pcap")
+local fw     = require("apps.wall.l7fw")
+local pcap   = require("apps.pcap.pcap")
+local lib    = require("core.lib")
+local common = require("program.wall.common")
+
+local long_opts = {
+   help = "h",
+   links = "l",
+   output = "o",
+}
 
 function run (args)
-   if #args ~= 2 then
+   local output_file, showlinks = nil, false
+   local opt = {
+      o = function (arg)
+         output_file = arg
+      end,
+      h = function (arg)
+         print(require("program.wall.filter.README_inc"))
+         main.exit(0)
+      end,
+      l = function (arg)
+         showlinks = true
+      end
+   }
+
+   args = lib.dogetopt(args, opt, "hlo:", long_opts)
+   if #args ~= 3 then
       print("TODO instructions")
       main.exit(1)
    end
 
-   -- TODO: baking in pcap file for now in for now, allow
-   --       other sources later?
-   local in_file  = args[1]
-   local out_file = args[2]
-   local scanner  = require("apps.wall.scanner.ndpi"):new()
+   local rule_str = io.open(args[1]):read("*a")
+   local rules = assert(load("return " .. rule_str))()
+
+   if type(rules) ~= "table" then
+      io.stderr:write("Rules file doesn't define a table\n")
+      main.exit(1)
+   end
+
+   if not common.inputs[args[2]] then
+      io.stderr:write("No such input available: ", args[1], "\n")
+      main.exit(1)
+   end
+
+   local source_link_name, app = common.inputs[args[2]](args[2], args[3])
+   if not source_link_name then
+      io.stderr:write(app, "\n")
+      main.exit(1)
+   end
+
+   local scanner = require("apps.wall.scanner.ndpi"):new()
 
    local c = config.new()
-   config.app(c, "source", pcap.PcapReader, in_file)
+   config.app(c, "source", unpack(app))
    config.app(c, "l7spy", require("apps.wall.l7spy").L7Spy, { scanner = scanner })
-   config.app(c, "sink", pcap.PcapWriter, out_file)
-   -- TODO: hardcoded some rules here for this experiment
-   local rules = { RTP = [[match { udp => accept; otherwise => drop }]],
-                   DNS = [[match { flow_count > 2 => accept; otherwise => drop }]],
-                   default = "drop" }
+   if not output_file then
+      config.app(c, "sink", require("apps.basic.basic_apps").Sink)
+   else
+      config.app(c, "sink", pcap.PcapWriter, output_file)
+   end
    config.app(c, "l7fw", require("apps.wall.l7fw").L7Fw, { scanner = scanner, rules = rules })
-   config.link(c, "source.output -> l7spy.south")
+   config.link(c, "source." .. source_link_name .. " -> l7spy.south")
    config.link(c, "l7spy.north -> l7fw.input")
    config.link(c, "l7fw.output -> sink.input")
 
    engine.configure(c)
-   engine.main({duration = 0.1, report = {showlinks = true}})
+   engine.busywait = true
+   engine.main({
+      report = { showlinks = showlinks },
+      done = function ()
+         return engine.app_table.source.done
+      end
+   })
 end
