698a190a1f656096e1a8b749e8ffccec53a4fe81
Asumu Takikawa <asumu@igalia.com>
2017-01-24 16:38:45 -0800

    First implementation of reject l7fw policy

--- src/apps/wall/l7fw.lua
+++ src/apps/wall/l7fw.lua
@@ -7,9 +7,14 @@ module(..., package.seeall)
 --   * a simple action ("drop", "reject", "accept")
 --   * a pfmatch expression
 
-local link   = require("core.link")
-local packet = require("core.packet")
-local match  = require("pf.match")
+local ffi      = require("ffi")
+local link     = require("core.link")
+local packet   = require("core.packet")
+local datagram = require("lib.protocol.datagram")
+local ether    = require("lib.protocol.ethernet")
+local icmp     = require("lib.protocol.icmp.header")
+local ipv4     = require("lib.protocol.ipv4")
+local match    = require("pf.match")
 
 L7Fw = {}
 L7Fw.__index = L7Fw
@@ -17,7 +22,9 @@ L7Fw.__index = L7Fw
 -- create a new firewall app object given an instance of Scanner
 -- and firewall rules
 function L7Fw:new(config)
-   local obj = { scanner = config.scanner,
+   local obj = { local_ip = config.local_ip,
+                 local_macaddr = config.local_macaddr,
+                 scanner = config.scanner,
                  rules = config.rules,
                  -- this map tracks flows to compiled pfmatch functions
                  -- so that we only compile them once per flow
@@ -33,9 +40,8 @@ end
 
 -- called by pfmatch handler, handle rejection response
 function L7Fw:reject(pkt, len)
-   -- TODO: implement ICMP/TCP RST response
+   link.transmit(self.output.output, self:make_icmpv4_packet(self.current_packet))
    packet.free(self.current_packet)
-   return
 end
 
 -- called by pfmatch handler, forward packet
@@ -64,6 +70,8 @@ function L7Fw:push()
             self:accept(pkt.data, pkt.length)
          elseif policy == "drop" then
             self:drop(pkt.data, pkt.length)
+         elseif policy == "reject" then
+            self:reject(pkt.data, pkt.length)
          -- handle a pfmatch string case
          elseif type(policy) == "string" then
             if self.handler_map[policy] then
@@ -85,4 +93,35 @@ function L7Fw:push()
          link.transmit(o, pkt)
       end
    end
+end
+
+-- create an ICMPv4 port unreachable packet
+function L7Fw:make_icmpv4_packet(pkt)
+   local ether_orig = ether:new_from_mem(pkt.data, pkt.length)
+   local ipv4_orig  = ipv4:new_from_mem(pkt.data + ether_orig:sizeof(),
+                                        pkt.length - ether_orig:sizeof())
+
+   local dgram   = datagram:new()
+   local ether_h = ether:new({ dst = ether_orig:src(),
+                               src = self.local_macaddr,
+                               type = 0x0800 })
+   local ipv4_h  = ipv4:new({ dst = ipv4_orig:src(),
+                              src = ipv4:pton(self.local_ip),
+                              protocol = 1,
+                              ttl = 64 })
+   local icmp_h  = icmp:new(3, 3)
+
+   dgram:payload(ffi.new("uint8_t [4]"), 4)
+   dgram:payload(ipv4_orig:header(), ipv4_orig:sizeof())
+   dgram:payload(pkt.data + ether_orig:sizeof() + ipv4_orig:sizeof(), 8)
+
+   icmp_h:checksum(dgram:payload())
+   dgram:push(icmp_h)
+   ipv4_h:total_length(ipv4_h:sizeof() + icmp_h:sizeof() +
+                       4 + -- extra zero bytes
+                       ipv4_orig:sizeof() + 8)
+   dgram:push(ipv4_h)
+   dgram:push(ether_h)
+
+   return dgram:packet()
 end
