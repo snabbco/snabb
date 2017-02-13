fd9d038ab2d6b432170946b6715fdf5ba2374a99
Nicola Larosa <github@teknico.net>
2017-02-13 16:40:07 +0100

    Add a selftest for the lwaftr bench command (#739)

--- src/program/lwaftr/selftest.sh
+++ src/program/lwaftr/selftest.sh
@@ -10,23 +10,7 @@ if [[ $EUID != 0 ]]; then
     exit 1
 fi
 
-# These are tests for lwaftr front ends.
-
-echo "Testing snabb lwaftr bench"
-./snabb lwaftr bench -D 0.1 ${TDIR}/icmp_on_fail.conf \
-    ${TDIR}/tcp-frominet-bound.pcap ${TDIR}/tcp-fromb4-ipv6.pcap
-
-echo "Testing snabb lwaftr bench --reconfigurable"
-./snabb lwaftr bench --reconfigurable -D 1 ${TDIR}/icmp_on_fail.conf \
-    ${TDIR}/tcp-frominet-bound.pcap ${TDIR}/tcp-fromb4-ipv6.pcap &
-
-LEADER_PID=$!
-
-sleep 0.1
-./snabb config get $LEADER_PID /
-
-
-# The rest of the tests require real hardware
+# The tests require real hardware
 
 if [ -z "$SNABB_PCI0" ]; then
    echo "Skipping tests which require real hardware, SNABB_PCI0 not set"
