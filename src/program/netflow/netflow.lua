b07d1f0e3277e91089b1a3bdc56597aeb3c0a84c
Asumu Takikawa <asumu@igalia.com>
2017-03-10 11:43:23 -0800

    Start to implement V9/IPFIX communication code

--- src/program/netflow/netflow.lua
+++ src/program/netflow/netflow.lua
@@ -10,13 +10,16 @@ local netflow = require("apps.netflow.netflow")
 function run (args)
    local c = config.new()
 
-   --config.app(c, "source", raw.RawSocket, args[1])
-   config.app(c, "source", pcap.PcapReader, args[1])
+   config.app(c, "source", raw.RawSocket, args[1])
+   --config.app(c, "source", pcap.PcapReader, args[1])
+   config.app(c, "sink", pcap.PcapWriter, args[2])
    config.app(c, "exporter", netflow.NetflowExporter, {})
 
-   config.link(c, "source.output -> exporter.input")
+   --config.link(c, "source.output -> exporter.input")
+   config.link(c, "source.tx -> exporter.input")
+   config.link(c, "exporter.output -> sink.input")
 
    engine.configure(c)
 
-   engine.main({ duration=5, report = { showapps=true, showlinks=true } })
+   engine.main({ duration=120, report = { showapps=true, showlinks=true } })
 end
