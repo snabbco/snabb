c7eb0bd896ee4916e692a8e26f36febafed61804
Diego Pino Garcia <dpino@igalia.com>
2017-03-22 14:48:50 +0100

    Add option 'duration' in run_nohw
    * Link statistics are only shown up if the program finishes, thus a
    duration is needed.
    * Refactor options in run_nohw.

--- src/program/lwaftr/run_nohw/run_nohw.lua
+++ src/program/lwaftr/run_nohw/run_nohw.lua
@@ -17,12 +17,13 @@ local function check(flag, fmt, ...)
 end
 
 local function parse_args(args)
-   local verbosity = 0
+   local opts = {
+      verbosity = 0,
+   }
    local conf_file, b4_if, inet_if
-   local bench_file
    local handlers = {
       v = function ()
-         verbosity = verbosity + 1
+         opts.verbosity = opts.verbosity + 1
       end,
       c = function (arg)
          check(file_exists(arg), "no such file '%s'", arg)
@@ -35,26 +36,29 @@ local function parse_args(args)
          inet_if = arg
       end,
       b = function (arg)
-         bench_file = arg
+         opts.bench_file = arg
       end,
       h = function (arg)
          print(require("program.lwaftr.run_nohw.README_inc"))
          main.exit(0)
       end,
+      D = function (arg)
+         opts.duration = assert(tonumber(arg), "duration must be a number")
+      end,
    }
-   lib.dogetopt(args, handlers, "b:c:B:I:vh", {
+   lib.dogetopt(args, handlers, "b:c:B:I:vhD:", {
       help = "h", conf = "c", verbose = "v",
       ["b4-if"] = "B", ["inet-if"] = "I",
-      ["bench-file"] = "b",
+      ["bench-file"] = "b", duration = "D",
    })
    check(conf_file, "no configuration specified (--conf/-c)")
    check(b4_if, "no B4-side interface specified (--b4-if/-B)")
    check(inet_if, "no Internet-side interface specified (--inet-if/-I)")
-   return verbosity, conf_file, b4_if, inet_if, bench_file
+   return conf_file, b4_if, inet_if, opts
 end
 
 function run(parameters)
-   local verbosity, conf_file, b4_if, inet_if, bench_file = parse_args(parameters)
+   local conf_file, b4_if, inet_if, opts = parse_args(parameters)
    local conf = require('apps.lwaftr.conf').load_lwaftr_config(conf_file)
    local c = config.new()
 
@@ -76,12 +80,16 @@ function run(parameters)
 
    engine.configure(c)
 
-   if verbosity >= 1 then
-      local csv = CSVStatsTimer:new(bench_file)
+   if opts.verbosity >= 1 then
+      local csv = CSVStatsTimer:new(opts.bench_file)
       csv:add_app("inet", {"tx", "rx"}, { tx = "IPv4 TX", rx = "IPv4 RX" })
       csv:add_app("b4if", {"tx", "rx"}, { tx = "IPv6 TX", rx = "IPv6 RX" })
       csv:activate()
    end
 
-   engine.main({report = { showlinks = true }})
+   if opts.duration then
+      engine.main({duration = opts.duration, report = { showlinks = true }})
+   else
+      engine.main()
+   end
 end
