248ec2691eed49911fb12409199bb3a874fa0a9b
Diego Pino Garcia <dpino@igalia.com>
2017-03-23 12:20:21 +0000

    Rework unit test

--- src/program/lwaftr/tests/subcommands/run_nohw_test.py
+++ src/program/lwaftr/tests/subcommands/run_nohw_test.py
@@ -1,5 +1,5 @@
 """
-Test the "snabb lwaftr run_nohw" subcommand.
+Test the "snabb lwaftr run-nohw" subcommand.
 """
 
 import unittest
@@ -10,10 +10,10 @@ from test_env import DATA_DIR, SNABB_CMD, BaseTestCase
 
 class TestRun(BaseTestCase):
 
-    program = [
-        str(SNABB_CMD), 'lwaftr', 'run_nohw',
+    cmd_args = [
+        str(SNABB_CMD), 'lwaftr', 'run-nohw',
     ]
-    cmd_args = {
+    cmd_options = {
         '--duration': '1',
         '--bench-file': '/dev/null',
         '--conf': str(DATA_DIR / 'icmp_on_fail.conf'),
@@ -24,21 +24,14 @@ class TestRun(BaseTestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.create_veth_pair()
-
-    @classmethod
-    def create_veth_pair(cls):
         veth0 = cls.random_veth_name()
         veth1 = cls.random_veth_name()
-
         # Create veth pair.
         check_call(('ip', 'link', 'add', veth0, 'type', 'veth', 'peer', \
             'name', veth1))
-
         # Set interfaces up.
         check_call(('ip', 'link', 'set', veth0, 'up'))
         check_call(('ip', 'link', 'set', veth1, 'up'))
-
         # Add interface names to class.
         cls.veths.append(veth0)
         cls.veths.append(veth1)
@@ -48,30 +41,21 @@ class TestRun(BaseTestCase):
         return 'veth%s' % randint(10000, 999999)
 
     def test_run_nohw(self):
-        self.execute_run_test(self.cmd_args)
-
-    def execute_run_test(self, cmd_args):
-        self.cmd_args['--inet-if'] = self.veths[0]
-        self.cmd_args['--b4-if'] = self.veths[1]
+        self.cmd_options['--inet-if'] = self.veths[0]
+        self.cmd_options['--b4-if'] = self.veths[1]
         output = self.run_cmd(self.build_cmd())
         self.assertIn(b'link report', output,
             b'\n'.join((b'OUTPUT', output)))
 
     def build_cmd(self):
-        result = self.program
-        for item in self.cmd_args.items():
-            for each in item:
-                result.append(each)
+        result = self.cmd_args
+        for key, value in self.cmd_options.items():
+            result.extend((key, value))
         return result
 
     @classmethod
     def tearDownClass(cls):
-        cls.remove_veths()
-
-    @classmethod
-    def remove_veths(cls):
-        for i in range(0, len(cls.veths), 2):
-            check_call(('ip', 'link', 'delete', cls.veths[i]))
+        check_call(('ip', 'link', 'delete', cls.veths[0]))
 
 if __name__ == '__main__':
     unittest.main()
