76afdf7419d4f80563031a1bffa6c52e8add9fa0
Diego Pino Garcia <dpino@igalia.com>
2017-03-28 12:04:38 +0200

    Refactor quickcheck code
    - Remove references to pflua.
    - Remove globals.
    - Use os.exit.
    - Parse arguments like other lwaftr programs.

--- src/program/lwaftr/quickcheck/quickcheck.lua
+++ src/program/lwaftr/quickcheck/quickcheck.lua
@@ -1,19 +1,46 @@
 module(...,package.seeall)
 
-local utils = require('program.lwaftr.quickcheck.utils')
+local lib = require("core.lib")
+local utils = require("program.lwaftr.quickcheck.utils")
 
-local program_name = 'snabb lwaftr quickcheck'
+local function show_usage(code)
+   print(require("program.lwaftr.quickcheck.README_inc"))
+   main.exit(code)
+end
 
-local seed, iterations, prop_name, prop_args, prop, prop_info
+local function parse_args (args)
+   local handlers = {}
+   local opts = {
+      iterations = 100,
+   }
+   function handlers.h() show_usage(0) end
+   handlers["seed"] = function (arg)
+      opts["seed"] = assert(tonumber(arg), "seed must be a number")
+   end
+   handlers["iterations"] = function (arg)
+		opts["iterations"] = assert(tonumber(arg), "iterations must be a number")
+   end
+   args = lib.dogetopt(args, handlers, "h",
+      { help="h", ["seed"] = 0, ["iterations"] = 0 })
+   if #args == 0 then show_usage(1) end
+   if not opts.seed then
+      local seed = math.floor(utils.gmtime() * 1e6) % 10^9
+      print("Using time as seed: "..seed)
+      opts.seed = seed
+   end
+   local prop_name = table.remove(args, 1)
+
+   return opts, prop_name, args
+end
 
 -- Due to limitations of Lua 5.1, finding if a command failed is convoluted.
 local function find_gitrev()
    local fd = io.popen('git rev-parse HEAD 2>/dev/null ; echo -n "$?"')
    local cmdout = fd:read("*all")
-   fd:close() -- Always true in 5.1, with Lua or LuaJIT
+   fd:close() -- Always true in 5.1, with Lua or LuaJIT.
    local _, _, git_ret = cmdout:find("(%d+)$")
    git_ret = tonumber(git_ret)
-   if git_ret ~= 0 then -- Probably not in a git repo
+   if git_ret ~= 0 then -- Probably not in a git repo.
       return nil
    else
       local _, _, sha1 = cmdout:find("(%x+)")
@@ -26,68 +53,26 @@ local function print_gitrev_if_available()
    if rev then print(("Git revision %s"):format(rev)) end
 end
 
-local function rerun_usage(i)
-   print(("Rerun as: %s --seed=%s --iterations=%s %s %s"):
-         format(program_name, seed, i + 1,
-                prop_name, table.concat(prop_args, " ")))
+local function initialize_property (name, args)
+   local prop = require(name)
+   if not prop.handle_prop_args then
+      assert(#args == 0, "Property does not take options "..name)
+   end
+   return prop, prop.handle_prop_args(args)
 end
 
-function initialize(options)
-   seed, iterations, prop_name, prop_args =
-      options.seed, options.iterations, options.prop_name, options.prop_args
-
-   if not seed then
-      seed = math.floor(utils.gmtime() * 1e6) % 10^9
-      print("Using time as seed: "..seed)
+function run (args)
+   local opts, prop_name, prop_args = parse_args(args)
+   local rerun_usage = function (i)
+      print(("Rerun as: snabb lwaftr quickcheck --seed=%s --iterations=%s %s %s"):
+            format(program_name, opts.seed, i + 1,
+                   prop_name, table.concat(prop_args, " ")))
    end
-   math.randomseed(assert(tonumber(seed)))
+   math.randomseed(opts.seed)
 
-   if not iterations then iterations = 100 end
-
-   if not prop_name then
-      error("No property name specified")
-   end
-
-   prop = require(prop_name)
-   if prop.handle_prop_args then
-      prop_info = prop.handle_prop_args(prop_args)
-   else
-      assert(#prop_args == 0,
-             "Property does not take options "..prop_name)
-      prop_info = nil
-   end
-end
-
-function initialize_from_command_line(args)
-   local options = {}
-   while #args >= 1 and args[1]:match("^%-%-") do
-      local arg, _, val = table.remove(args, 1):match("^%-%-([^=]*)(=(.*))$")
-      assert(arg)
-      if arg == 'seed' then options.seed = assert(tonumber(val))
-      elseif arg == 'iterations' then options.iterations = assert(tonumber(val))
-      else error("Unknown argument: " .. arg) end
-   end
-   if #args < 1 then
-      print("Usage: " ..
-               program_name ..
-               " [--seed=SEED]" ..
-               " [--iterations=ITERATIONS]" ..
-               " property_file [property_specific_args]")
-      os.exit(1)
-   end
-   options.prop_name = table.remove(args, 1)
-   options.prop_args = args
-   initialize(options)
-end
-
-function run(args)
-   initialize_from_command_line(args)
-   if not prop then
-      error("Call initialize() or initialize_from_command_line() first")
-   end
-
-   for i = 1,iterations do
-      -- Wrap property and its arguments in a 0-arity function for xpcall
+   local prop, prop_info = initialize_property(prop_name, prop_args)
+   for i=1,opts.iterations do
+      -- Wrap property and its arguments in a 0-arity function for xpcall.
       local wrap_prop = function() return prop.property(prop_info) end
       local propgen_ok, expected, got = xpcall(wrap_prop, debug.traceback)
       if not propgen_ok then
@@ -99,14 +84,14 @@ function run(args)
              end
           end
           print("Traceback (this is reliable):")
-          print(expected) -- This is an error code and traceback in this case
+          print(expected) -- This is an error code and traceback in this case.
           rerun_usage(i)
-          os.exit(1)
+          main.exit(1)
       end
       if not utils.equals(expected, got) then
           print_gitrev_if_available()
           print("The property was falsified.")
-          -- If the property file has extra info available, show it
+          -- If the property file has extra info available, show it.
           if prop.print_extra_information then
              prop.print_extra_information()
           else
@@ -116,10 +101,10 @@ function run(args)
              utils.pp(got)
           end
           rerun_usage(i)
-          os.exit(1)
+          main.exit(1)
       end
    end
-   print(iterations.." iterations succeeded.")
+   print(opts.iterations.." iterations succeeded.")
 
    if prop.cleanup then prop.cleanup() end
 end
