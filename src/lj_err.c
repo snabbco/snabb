b28c891a614bd00649d450cf686ed8d8f8cd0891
Mike Pall <mike>
2017-04-07 12:12:03 +0200

    Refactor with LUA_OK.
    Contributed by François Perrad.

--- src/lj_err.c
+++ src/lj_err.c
@@ -127,7 +127,7 @@ LJ_NOINLINE void lj_err_throw(lua_State *L, int errcode)
   global_State *g = G(L);
   lj_trace_abort(g);
   setmref(g->jit_base, NULL);
-  L->status = 0;
+  L->status = LUA_OK;
   {
     void *cf = err_unwind(L, NULL, errcode);
     if (cframe_unwind_ff(cf))
