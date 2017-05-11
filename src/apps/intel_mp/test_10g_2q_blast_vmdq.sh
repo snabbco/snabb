12d15228f07e73e2a7d1df7aad79917ee66faa78
Asumu Takikawa <asumu@igalia.com>
2017-05-11 19:59:50 +0000

    Adjust test to test that both vmdq pools get pkts

--- src/apps/intel_mp/test_10g_2q_blast_vmdq.sh
+++ src/apps/intel_mp/test_10g_2q_blast_vmdq.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+#
+# Test VMDq mode with two pools with different MACs
+
 SNABB_SEND_BLAST=true ./testsend.snabb $SNABB_PCI_INTEL1 0 source2.pcap &
 BLAST=$!
 
@@ -6,5 +9,10 @@ SNABB_RECV_SPINUP=2 SNABB_RECV_DURATION=5 ./testvmdqrecv.snabb $SNABB_PCI_INTEL0
 SNABB_RECV_SPINUP=2 SNABB_RECV_DURATION=5 ./testvmdqrecv.snabb $SNABB_PCI_INTEL0 "12:34:56:78:9a:bc" 1 4 > results.1
 
 kill -9 $BLAST
-test `cat results.0 | grep "^RXDGPC" | awk '{print $2}'` -gt 10000
+test `cat results.* | grep "^RXDGPC" | awk '{print $2}'` -gt 10000
+
+# both queues should see packets
+test \( $? -eq 0 \) -a \( `cat results.0 | grep -m 1 fpb | awk '{print $9}'` -gt 0 \)
+test \( $? -eq 0 \) -a \( `cat results.1 | grep -m 1 fpb | awk '{print $9}'` -gt 0 \)
+
 exit $?
