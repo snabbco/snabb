5ea4b741ae08a2935c825613047d7cb5ab97159d
Luke Gorrie <luke@snabb.co>
2017-05-22 10:28:30 +0000

    lj_auditlog: use msgpack format for audit log

--- src/lj_auditlog.c
+++ src/lj_auditlog.c
@@ -8,7 +8,38 @@
 
 #include "lj_auditlog.h"
 
+/* File where the audit log is written. */
 FILE *fp;
+
+/* -- msgpack writer - see http://msgpack.org/index.html ------------------ */
+
+/* XXX assumes little endian cpu. */
+
+static void fixmap(int size) {
+  fputc(0x80|size, fp);         /* map header with size */
+};
+
+static void str_16(const char *s) {
+  uint16_t biglen = __builtin_bswap16(strlen(s));
+  fputc(0xda, fp);                        /* string header */
+  fwrite(&biglen, sizeof(biglen), 1, fp); /* string length */
+  fputs(s, fp);                           /* string contents */
+}
+
+static void uint_64(uint64_t n) {
+  uint64_t big = __builtin_bswap64(n);
+  fputc(0xcf, fp);                  /* uint 64 header */
+  fwrite(&big, sizeof(big), 1, fp); /* value */
+}
+
+static void bin_32(void *ptr, int n) {
+  uint32_t biglen = __builtin_bswap32(n);
+  fputc(0xc6, fp);                        /* array 32 header */
+  fwrite(&biglen, sizeof(biglen), 1, fp); /* length */
+  fwrite(ptr, n, 1, fp);                  /* data */
+}
+
+/* -- low-level object logging API ---------------------------------------- */
 
 /* Ensure that the log file is open. */
 static void ensure_log_open()
@@ -23,9 +54,15 @@ static void ensure_log_open()
 static void log(const char *type, void *ptr, unsigned int size)
 {
   ensure_log_open();
-  fprintf(fp, "type=%s address=%p size=%d data:\n", type, ptr, size);
-  fwrite(ptr, size, 1, fp);
+  fixmap(5);
+  str_16("type");    /* = */ str_16("memory");
+  str_16("hint");    /* = */ str_16(type);
+  str_16("address"); /* = */ uint_64((uint64_t)ptr);
+  str_16("size");    /* = */ uint_64(size);
+  str_16("data");    /* = */ bin_32(ptr, size);
 }
+
+/* -- high-level LuaJIT object logging ------------------------------------ */
 
 /* Log a trace that has just been compiled. */
 void lj_auditlog_trace_stop(jit_State *J, GCtrace *T)
