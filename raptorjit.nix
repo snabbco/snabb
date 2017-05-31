0a554696d9a2bb069157d4c7d2d15b5772f56b2c
Luke Gorrie <luke@snabb.co>
2017-05-31 11:00:18 +0000

    Generate separate DWARF debug info
    The build now produces a ".dwo" file containing DWARF debug
    information. This contains a binary encoding of internal information
    about RaptorJIT e.g. the layout of all its internal data structures.
    
    The DWARF information is for use by debugging tools that need to
    understand JIT internals.
    
    The DWARF file is currently generated with gcc. I would prefer to use
    clang, for the sake of consistency with the rest of the build, but I
    seem to get _much_ more debug information from gcc so I stick with
    that for now.
    
    The nix build expression outputs the debug information as
    lib/raptorjit.dwo

--- raptorjit.nix
+++ raptorjit.nix
@@ -9,10 +9,14 @@ mkDerivation rec {
   name = "raptorjit-${version}";
   inherit version;
   src = source;
-  buildInputs = [ luajit ];  # LuaJIT to bootstrap DynASM
+  buildInputs = [
+      luajit                # LuaJIT to bootstrap DynASM
+      gcc6                  # GCC for generating DWARF info
+    ];
+  dontStrip = true;         # No extra stripping (preserve debug info)
   installPhase = ''
-    mkdir -p $out/bin
-    cp src/luajit $out/bin/raptorjit
+    install -D src/luajit $out/bin/raptorjit
+    install -D src/lj_dwarf.dwo $out/lib/raptorjit.dwo
   '';
 
   enableParallelBuilding = true;  # Do 'make -j'
