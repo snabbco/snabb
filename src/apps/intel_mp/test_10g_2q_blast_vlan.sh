7c15e248505309e20f429eee72bd39634f8de0b7
Asumu Takikawa <asumu@igalia.com>
2017-06-06 23:45:19 +0000

    Add waits to avoid race conditions in vmdq tests

--- src/apps/intel_mp/test_10g_2q_blast_vlan.sh
+++ src/apps/intel_mp/test_10g_2q_blast_vlan.sh
@@ -6,8 +6,10 @@ SNABB_SEND_BLAST=true ./testsend.snabb $SNABB_PCI_INTEL1 0 source-vlan.pcap &
 BLAST=$!
 
 SNABB_RECV_SPINUP=2 SNABB_RECV_DURATION=5 ./testvmdqrecv.snabb $SNABB_PCI_INTEL0 "90:72:82:78:c9:7a" 1 0 0 > results.0 &
+PID1=$!
 SNABB_RECV_SPINUP=2 SNABB_RECV_DURATION=5 ./testvmdqrecv.snabb $SNABB_PCI_INTEL0 "90:72:82:78:c9:7a" 2 1 4 > results.1
 
+wait $PID1
 kill -9 $BLAST
 [[ `cat results.* | grep "^RXDGPC" | awk '{print $2}'` -gt 10000 ]] &&\
 # both queues should see packets
