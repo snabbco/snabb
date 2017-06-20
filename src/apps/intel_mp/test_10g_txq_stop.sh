886c65d605c8f103491d770e4701b50770c24d99
Asumu Takikawa <asumu@igalia.com>
2017-06-20 22:39:24 +0000

    Fix teardown test for this branch
    The test depended on some modifications from another branch
    that disabled app link presence tests.

--- src/apps/intel_mp/test_10g_txq_stop.sh
+++ src/apps/intel_mp/test_10g_txq_stop.sh
@@ -2,6 +2,7 @@
 
 -- Test to make sure the app shuts down propertly on reconfiguration
 
+local basic_apps = require("apps.basic.basic_apps")
 local intel      = require("apps.intel_mp.intel_mp")
 local lib        = require("core.lib")
 
@@ -15,6 +16,10 @@ config.app(c, "n1", intel.Intel,
              rxq = 0,
              txq = 0,
              wait_for_link = true })
+config.app(c, "source", basic_apps.Source)
+config.app(c, "sink", basic_apps.Sink)
+config.link(c, "source.output -> n1.input")
+config.link(c, "n1.output -> sink.input")
 
 engine.configure(c)
 engine.main({ duration = 1 })
