726cc9c256e502ea1ed3ff0e244ad5c9e4335580
Luke Gorrie <luke@snabb.co>
2017-07-08 10:11:23 +0000

    raptorjit.nix: preBuild = 'make bootstrapclean'
    Start with 'make bootstrapclean' in builds under nix. This removes the
    generated code that lives in-tree in the repository to ensure that it
    will be rebuilt. This means that the nix build is not dependent on the
    in-tree code being up-to-date.
    
    Nix provides all of the build dependencies so there is no issue with
    rebuilding the generated code.

--- raptorjit.nix
+++ raptorjit.nix
@@ -9,6 +9,9 @@ mkDerivation rec {
   name = "raptorjit-${version}";
   inherit version;
   src = source;
+  preBuild = ''
+    make bootstrapclean
+  '';
   buildInputs = [ luajit ];  # LuaJIT to bootstrap DynASM
   installPhase = ''
     mkdir -p $out/bin
