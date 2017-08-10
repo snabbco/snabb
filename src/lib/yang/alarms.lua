1723026ff7886868f4d6b1253186f5d38a17d07c
Diego Pino Garcia <dpino@igalia.com>
2017-08-10 16:47:38 +0000

    Load alarm inventory and fetch alarm state

--- src/lib/yang/alarms.lua
+++ src/lib/yang/alarms.lua
@@ -1,5 +1,50 @@
 module(..., package.seeall)
 
+local data = require('lib.yang.data')
+local util = require('lib.yang.util')
+
+local state = {
+   alarm_inventory = {},
+}
+
+function get_state ()
+   return {
+      alarm_inventory = state.alarm_inventory,
+   }
+end
+
+-- Single point to access alarm type keys.
+alarm_type_keys = {}
+
+function alarm_type_keys:fetch (alarm_type_id, alarm_type_qualifier)
+   self.cache = self.cache or {}
+   local function lookup (alarm_type_id, alarm_type_qualifier)
+      if not self.cache[alarm_type_id] then
+         self.cache[alarm_type_id] = {}
+      end
+      return self.cache[alarm_type_id][alarm_type_qualifier]
+   end
+   assert(alarm_type_id)
+   alarm_type_qualifier = alarm_type_qualifier or ''
+   local key = lookup(alarm_type_id, alarm_type_qualifier)
+   if not key then
+      key = {alarm_type_id=alarm_type_id, alarm_type_qualifier=alarm_type_qualifier}
+      self.cache[alarm_type_id][alarm_type_qualifier] = key
+   end
+   return key
+end
+
+local function load_default_configuration ()
+   local content = require("apps.lwaftr.alarms.lwaftr_default_alarms.inc")
+   local conf = data.load_state_for_schema_by_name('ietf-alarms', content)
+   return conf.alarms
+end
+
+local default = load_default_configuration()
+state.alarm_inventory = default.alarm_inventory
+
+---
+
 function raise_alarm (key, args)
    print('raise_alarm')
    assert(type(key) == 'table')
@@ -9,4 +54,19 @@ end
 function clear_alarm (key)
    print('clear alarm')
    assert(type(key) == 'table')
+end
+
+---
+
+function selftest ()
+   print("selftest: alarms")
+   local function table_size (t)
+      local size = 0
+      for _ in pairs(t) do size = size + 1 end
+      return size
+   end
+
+   assert(table_size(state.alarm_inventory.alarm_type) > 0)
+
+   print("ok")
 end
--- src/lib/yang/state.lua
+++ src/lib/yang/state.lua
@@ -1,6 +1,7 @@
 -- Use of this source code is governed by the Apache 2.0 license; see COPYING.
 module(..., package.seeall)
 
+local alarms = require("lib.yang.alarms")
 local lib = require("core.lib")
 local shm = require("core.shm")
 local xpath = require("lib.yang.path")
@@ -104,9 +105,16 @@ function state_reader_from_schema_by_name(schema_name)
 end
 state_reader_from_schema_by_name = util.memoize(state_reader_from_schema_by_name)
 
+local function append_alarms (state)
+   state.softwire_state = state.softwire_state or {}
+   state.softwire_state.alarms = alarms.get_state() or {}
+end
+
 function read_state(schema_name, pid)
    local reader = state_reader_from_schema_by_name(schema_name)
-   return reader(flatten(find_counters(pid)))
+   local state = reader(flatten(find_counters(pid)))
+   append_alarms(state)
+   return state
 end
 
 function selftest ()
