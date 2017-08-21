4486728f43d59709ac2cca3e2e99fbab04a450aa
Max Rottenkolber <max@mr.gy>
2017-08-21 17:36:48 +0200

    apps.interlink.receiver: don’t forward packets until connected

--- src/apps/interlink/receiver.lua
+++ src/apps/interlink/receiver.lua
@@ -26,6 +26,7 @@ end
 
 function Receiver:pull ()
    local o, r, n = self.output.output, self.interlink, 0
+   if not o then return end -- don’t forward packets until connected
    while not interlink.empty(r) and n < engine.pull_npackets do
       link.transmit(o, interlink.extract(r))
       n = n + 1
