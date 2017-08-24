d2022fb4277963229dedef1e767271250399e128
Andy Wingo <wingo@igalia.com>
2017-08-24 12:55:30 +0200

    Remove unused definitions from lwtypes

--- src/apps/lwaftr/lwtypes.lua
+++ src/apps/lwaftr/lwtypes.lua
@@ -12,21 +12,7 @@ local ethernet_header_type = ffi.typeof([[
 ethernet_header_ptr_type = ffi.typeof("$*", ethernet_header_type)
 ethernet_header_size = ffi.sizeof(ethernet_header_type)
 
-local ethernet_vlan_header_type = ffi.typeof([[
-   struct {
-      uint8_t  ether_dhost[6];
-      uint8_t  ether_shost[6];
-      struct {
-         uint16_t tpid;
-         uint16_t tag;
-      } vlan;
-      uint16_t ether_type;
-   }
-]])
-ethernet_vlan_header_ptr_type = ffi.typeof("$*", ethernet_vlan_header_type)
-ethernet_vlan_header_size = ffi.sizeof(ethernet_vlan_header_type)
-
-local ipv4hdr_t = ffi.typeof[[
+local ipv4_header_type = ffi.typeof[[
 struct {
   uint16_t ihl_v_tos; // ihl:4, version:4, tos(dscp:6 + ecn:2)
   uint16_t total_length;
@@ -39,8 +25,8 @@ struct {
   uint8_t  dst_ip[4];
 } __attribute__((packed))
 ]]
-ipv4_header_ptr_type = ffi.typeof("$*", ipv4hdr_t)
-ipv4_header_size = ffi.sizeof(ipv4hdr_t)
+ipv4_header_ptr_type = ffi.typeof("$*", ipv4_header_type)
+ipv4_header_size = ffi.sizeof(ipv4_header_type)
 
 local ipv6_ptr_type = ffi.typeof([[
    struct {
@@ -54,20 +40,3 @@ local ipv6_ptr_type = ffi.typeof([[
 ]])
 ipv6_header_ptr_type = ffi.typeof("$*", ipv6_ptr_type)
 ipv6_header_size = ffi.sizeof(ipv6_ptr_type)
-
-local udp_header_t = ffi.typeof[[
-struct {
-  uint16_t    src_port;
-  uint16_t    dst_port;
-  uint16_t    len;
-  uint16_t    checksum;
-} __attribute__((packed))
-]]
-udp_header_ptr_type = ffi.typeof("$*", udp_header_t)
-udp_header_size = ffi.sizeof(udp_header_t)
-
-lwaftr_id_type = ffi.typeof[[
-struct {
-  char value[256];
-}
-]]
