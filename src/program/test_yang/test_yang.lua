c34e0bcc996b67b8650ac6314a9e145838e2ec0d
Rajkumar <raj@mettlenetworks.com>
2017-08-28 14:02:30 +0530

    YANG list with multiple keys

--- src/program/test_yang/test_yang.lua
+++ src/program/test_yang/test_yang.lua
@@ -8,6 +8,11 @@ function run (parameters)
    local schema = schema.load_schema_by_name(schema_name)
    local conf = yang.load_configuration(parameters[1],{schema_name=schema_name, verbose = true})
 
+   local test_list = conf.test_list.entry
+   for k,v in pairs(test_list) do
+      print (k)
+   end
+
    local c = config.new()
    engine.configure(c)
    engine.main({duration=1})
