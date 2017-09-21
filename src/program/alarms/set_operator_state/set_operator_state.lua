631f73a06c33150e2856f823fe676613af4f694d
Diego Pino Garcia <dpino@igalia.com>
2017-09-21 09:39:41 +0000

    Customize usage for each alarms command

--- src/program/alarms/set_operator_state/set_operator_state.lua
+++ src/program/alarms/set_operator_state/set_operator_state.lua
@@ -3,13 +3,18 @@ module(..., package.seeall)
 
 local common = require("program.config.common")
 
-local function usage(exit_code)
+function show_usage(command, status, err_msg)
+   if err_msg then print('error: '..err_msg) end
    print(require("program.alarms.set_operator_state.README_inc"))
-   main.exit(exit_code)
+   main.exit(status)
+end
+
+local function fatal()
+   show_usage(nil, 1)
 end
 
 local function parse_args (args)
-   if #args < 4 or #args > 5 then usage(1) end
+   if #args < 4 or #args > 5 then fatal() end
    local alarm_type_id, alarm_type_qualifier = (args[3]):match("([%w]+)/([%w]+)")
    if not alarm_type_id then
       alarm_type_id, alarm_type_qualifier = args[3], ''
@@ -32,7 +37,8 @@ end
 
 function run(args)
    local l_args = parse_args(args)
-   local opts = { command='set-alarm-operator-state', with_path=false, is_config=false }
+   local opts = { command='set-alarm-operator-state', with_path=false, is_config=false,
+                  usage = show_usage }
    args = common.parse_command_line(args, opts)
    local response = common.call_leader(
       args.instance_id, 'set-alarm-operator-state',
