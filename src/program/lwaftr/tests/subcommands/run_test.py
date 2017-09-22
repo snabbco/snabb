8aec91209521d7f2701e63c681f4b765c1b2c6bb
Jessica Tallon <tsyesika@igalia.com>
2017-09-22 16:16:12 +0200

    Add test for migration using --on-a-stick command
    When --on-a-stick command is used with a single instance defined it
    should migrate the instance to work in a stick  (replace the device
    with the one provided in the flag and remove the device in
    `external-device`). It just checks that it works by not crashing and
    says it's going to do it in the output.

--- src/program/lwaftr/tests/subcommands/run_test.py
+++ src/program/lwaftr/tests/subcommands/run_test.py
@@ -4,7 +4,7 @@ Test the "snabb lwaftr run" subcommand. Needs NIC names.
 
 import unittest
 
-from test_env import DATA_DIR, SNABB_CMD, BaseTestCase, nic_names
+from test_env import DATA_DIR, SNABB_CMD, BaseTestCase, nic_names, ENC
 
 
 SNABB_PCI0, SNABB_PCI1 = nic_names()
@@ -25,6 +25,21 @@ class TestRun(BaseTestCase):
         output = self.run_cmd(self.cmd_args)
         self.assertIn(b'link report', output,
             b'\n'.join((b'OUTPUT', output)))
+
+    def test_run_on_a_stick_migration(self):
+        # The LwAFTR should be abel to migrate from non-on-a-stick -> on-a-stick
+        run_cmd = list(self.cmd_args)[:-4]
+        run_cmd.extend((
+            "--on-a-stick",
+            SNABB_PCI0
+        ))
+
+        # The best way to check is to see if it's what it's saying it'll do.
+        output = self.run_cmd(run_cmd).decode(ENC)
+        self.assertIn("Migrating instance", output)
+
+        migration_line = [l for l in output.split("\n") if "Migrating" in l][0]
+        self.assertIn(SNABB_PCI0, migration_line)
 
 if __name__ == '__main__':
     unittest.main()
