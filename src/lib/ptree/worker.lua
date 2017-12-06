49651f81a9adc2b7e1df79bba49f284ef89a6e30
Andy Wingo <wingo@igalia.com>
2017-12-06 17:36:59 +0100

    Rework ptree worker to be library, not app

--- src/lib/ptree/worker.lua
+++ src/lib/ptree/worker.lua
@@ -2,30 +2,43 @@
 
 module(...,package.seeall)
 
-local S = require("syscall")
-local ffi = require("ffi")
-local yang = require("lib.yang.yang")
-local rpc = require("lib.yang.rpc")
-local app = require("core.app")
-local shm = require("core.shm")
-local app_graph = require("core.config")
-local channel = require("lib.ptree.channel")
+local S            = require("syscall")
+local engine       = require("core.app")
+local app_graph    = require("core.config")
+local counter      = require("core.counter")
+local histogram    = require('core.histogram')
+local lib          = require('core.lib')
+local timer        = require('core.timer')
+local channel      = require("lib.ptree.channel")
 local action_codec = require("lib.ptree.action_codec")
-local alarm_codec = require("lib.ptree.alarm_codec")
+local alarm_codec  = require("lib.ptree.alarm_codec")
 
-local Worker = {
-   config = {
-      Hz = {default=1000},
-   }
+local Worker = {}
+
+local worker_config_spec = {
+   duration = {},
+   measure_latency = {default=true},
+   no_report = {default=false},
+   report = {default={showapps=true,showlinks=true}},
+   Hz = {default=1000},
 }
 
-function Worker:new (conf)
+function new_worker (conf)
+   local conf = lib.parse(conf, worker_config_spec)
    local ret = setmetatable({}, {__index=Worker})
    ret.period = 1/conf.Hz
-   ret.next_time = app.now()
+   ret.duration = conf.duration or 1/0
+   ret.no_report = conf.no_report
+   ret.next_time = engine.now()
    ret.channel = channel.create('config-worker-channel', 1e6)
    ret.alarms_channel = alarm_codec.get_channel()
    ret.pending_actions = {}
+
+   ret.breathe = engine.breathe
+   if conf.measure_latency then
+      local latency = histogram.create('engine/latency.histogram', 1e-6, 1e0)
+      ret.breathe = latency:wrap_thunk(ret.breathe, engine.now)
+   end
    return ret
 end
 
@@ -44,11 +57,11 @@ function Worker:commit_pending_actions()
       local name, args = unpack(action)
       if name == 'call_app_method_with_blob' then
          if #to_apply > 0 then
-            app.apply_config_actions(to_apply)
+            engine.apply_config_actions(to_apply)
             to_apply = {}
          end
          local callee, method, blob = unpack(args)
-         local obj = assert(app.app_table[callee])
+         local obj = assert(engine.app_table[callee])
          assert(obj[method])(obj, blob)
       elseif name == "shutdown" then
          self:shutdown()
@@ -59,7 +72,7 @@ function Worker:commit_pending_actions()
          table.insert(to_apply, action)
       end
    end
-   if #to_apply > 0 then app.apply_config_actions(to_apply) end
+   if #to_apply > 0 then engine.apply_config_actions(to_apply) end
    self.pending_actions = {}
    if should_flush then require('jit').flush() end
 end
@@ -79,30 +92,27 @@ function Worker:handle_actions_from_manager()
    end
 end
 
-function Worker:pull ()
-   if app.now() < self.next_time then return end
-   self.next_time = app.now() + self.period
-   self:handle_actions_from_manager()
+function Worker:main ()
+   local stop = engine.now() + self.duration
+   repeat
+      self.breathe()
+      if self.next_time < engine.now() then
+         self.next_time = engine.now() + self.period
+         self:handle_actions_from_manager()
+         timer.run()
+      end
+      if not engine.busywait then engine.pace_breathing() end
+   until stop < engine.now()
+   counter.commit()
+   if not self.no_report then engine.report(self.report) end
 end
 
 function main (opts)
-   local app_graph = require('core.config')
-   local engine = require('core.app')
-
-   if opts == nil then opts = {} end
-
-   local graph = app_graph.new()
-   app_graph.app(graph, "worker", Worker, {})
-   engine.configure(graph)
-   engine.main(opts)
+   return new_worker(opts):main()
 end
 
 function selftest ()
    print('selftest: lib.ptree.worker')
-   local c = config.new()
-   config.app(c, "worker", Worker, {})
-   engine.configure(c)
-   engine.main({ duration = 0.0001, report = {showapps=true,showlinks=true}})
-   engine.configure(config.new())
+   main({duration=0.0001})
    print('selftest: ok')
 end
