4189da6cb788952bef9d3f78802838ccfd28517a
Luke Gorrie <luke@snabb.co>
2018-01-17 13:05:44 +0000

    Just use the default gcc instead of clang 4.0.0.
    It was a bit of a flight of fancy to use a pinned version of clang.
    This would make sense if we were really depending on hyper-specific
    behaviour of the C compiler, like some automatic SIMD parallization,
    but in reality we're not. So don't be fussy and just keep it simple.

--- raptorjit.nix
+++ raptorjit.nix
@@ -3,13 +3,14 @@
 { pkgs, source, version }:
 
 with pkgs;
-with llvmPackages_4.stdenv;  # Use clang 4.0
+with stdenv;
 
 mkDerivation rec {
   name = "raptorjit-${version}";
   inherit version;
   src = source;
   buildInputs = [ luajit ];  # LuaJIT to bootstrap DynASM
+  dontStrip = true;
   installPhase = ''
     mkdir -p $out/bin
     cp src/raptorjit $out/bin/raptorjit
