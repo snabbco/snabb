ddd90d2030b8a4624d1cace9264cb8a3e250c2ce
Michael G <m@gdyr.co>
2018-02-09 11:00:35 +0000

    Naive i350 fixes

--- src/apps/intel_mp/intel_mp.lua
+++ src/apps/intel_mp/intel_mp.lua
@@ -603,6 +603,7 @@ function Intel:load_registers(key)
 end
 function Intel:load_queue_registers(key)
   local v = reg[key]
+  if v.inherit then self:load_registers(v.inherit) end
   if v.txq and self.txq then
     register.define(v.txq, self.r, self.base, self.txq)
   end
@@ -1088,7 +1089,9 @@ function Intel1g:init_phy ()
    self.r.SW_FW_SYNC:clr(bits { SW_PHY_SM = 1 })
    self:unlock_fw_sem()
 
-   self.r.EEMNGCTL:wait(bits { CFG_DONE0 = 18 })
+-- NOTE: CFG_DONE0 does not necessarily correspond to PCI .0.
+-- E.g. on a PE720 2xI350 module, bits 20 & 21 are set for PCI .0 and .1 (not 18 & 19).
+--   self.r.EEMNGCTL:wait(bits { CFG_DONE0 = 18 })
 
    --[[
    self:lock_fw_sem()
