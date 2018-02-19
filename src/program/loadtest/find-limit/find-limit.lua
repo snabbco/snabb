d9ac3666f766231cac19192af8929a1b9a8fef5e
Jessica Tallon <tsyesika@igalia.com>
2018-02-19 12:00:57 +0000

    Fix find-limit by checking actual throughput rather than requested.
    This also includes some changes to increase the default warmup time and
    perform a brief warm-up before each load we apply.

--- src/program/loadtest/find-limit/find-limit.lua
+++ src/program/loadtest/find-limit/find-limit.lua
@@ -13,7 +13,7 @@ local numa = require("lib.numa")
 local promise = require("program.loadtest.promise")
 
 local WARM_UP_BIT_RATE = 1e9
-local WARM_UP_TIME = 1
+local WARM_UP_TIME = 5
 
 local function fatal (msg)
    print(msg)
@@ -31,22 +31,28 @@ local function find_limit(tester, max_bitrate, precision, duration, retry_count)
    end
 
    -- lo and hi are bitrates, in bits per second.
-   local function bisect(lo, hi, iter)
-      local function continue(cur, result)
+   local function bisect(lo, hi, iter, actual)
+      local function continue(cur, result, actual)
          if result then
             print("Success.")
-            return bisect(cur, hi, 1)
+            return bisect(cur, hi, 1, actual)
          elseif iter <= retry_count then
             print("Failed; "..(retry_count - iter).. " retries remaining.")
-            return bisect(lo, hi, iter + 1)
+            return bisect(lo, hi, iter + 1, actual)
          else
             print("Failed.")
-            return bisect(lo, cur, 1)
+            return bisect(lo, cur, 1, actual)
          end
       end
       local cur = round((lo + hi) / 2)
-      if cur == lo or cur == hi then return lo end
-      return tester.measure(cur, duration):
+      if cur == lo or cur == hi then
+	 print(round(actual or lo) * 1e-9)
+	 return lo
+      end
+
+      -- We need to 
+
+      return tester.start_load(cur, duration):
          and_then(continue, cur)
    end
    return bisect(0, round(max_bitrate), 1)
@@ -126,32 +132,16 @@ function run(args)
                rxdrop = nic_app:rxdrop() }
    end
 
-   local function print_diff(diff, duration)
-      local function bitrate(packets, bytes)
-         -- 7 bytes preamble, 1 start-of-frame, 4 CRC, 12 interframe gap.
-         local overhead = 7 + 1 + 4 + 12
-         return (bytes + packets * overhead) * 8 / duration
-      end
-      local tx_mpps = diff.txpackets / duration / 1e6
-      local tx_gbps = bitrate(diff.txpackets, diff.txbytes) / 1e9
-      local rx_mpps = diff.rxpackets / duration / 1e6
-      local rx_gbps = bitrate(diff.rxpackets, diff.rxbytes) / 1e9
-      local lost_packets = diff.txpackets - diff.rxpackets - diff.rxdrop
-      local lost_percent = lost_packets / diff.txpackets * 100
-      print(string.format('    TX %d packets (%f MPPS), %d bytes (%f Gbps)',
-                          diff.txpackets, tx_mpps, diff.txbytes, tx_gbps))
-      print(string.format('    RX %d packets (%f MPPS), %d bytes (%f Gbps)',
-                          diff.rxpackets, rx_mpps, diff.rxbytes, rx_gbps))
-      print(string.format('    Loss: %d ingress drop + %d packets lost (%f%%)',
-                          diff.rxdrop, lost_packets, lost_percent))
+   local function print_stats(s)
    end
 
    local function check_results(diff)
+      local tx_bitrate = diff.tx_gbps * 1e9
       if opts.exec then
          -- Could pass on some arguments to this string.
-         return os.execute(opts.exec) == 0
+         return os.execute(opts.exec) == 0, tx_bitrate
       else
-         return diff.rxpackets == diff.txpackets and diff.rxdrop == 0
+         return diff.rxpackets == diff.txpackets and diff.rxdrop == 0, tx_bitrate
       end
    end
 
@@ -172,25 +162,59 @@ function run(args)
       return tester.generate_load(WARM_UP_BIT_RATE, WARM_UP_TIME)
    end
 
+   local function compute_bitrate(packets, bytes, duration)
+      -- 7 bytes preamble, 1 start-of-frame, 4 CRC, 12 interframe gap.
+      local overhead = 7 + 1 + 4 + 12
+      return (bytes + packets * overhead) * 8 / duration
+   end
+
+   function tester.start_load(bitrate, duration)
+      return tester.generate_load(WARM_UP_BIT_RATE, 1):
+	 and_then(promise.Wait, 0.002):
+	 and_then(tester.measure, bitrate, duration)
+   end
+   
    function tester.measure(bitrate, duration)
       local gbps_bitrate = bitrate/1e9
       local start_counters = read_counters()
-      local function report()
+      local function compute_stats()
          local end_counters = read_counters()
-         local diff = {}
+         local s = {}
          for k,v in pairs(start_counters) do
-            diff[k] = tonumber(end_counters[k] - start_counters[k])
+            s[k] = tonumber(end_counters[k] - start_counters[k])
          end
-         print_diff(diff, duration)
-         return diff
+	 s.applied_gbps = gbps_bitrate
+	 s.tx_mpps = s.txpackets / duration / 1e6
+	 s.tx_gbps = compute_bitrate(s.txpackets, s.txbytes, duration) / 1e9
+	 s.rx_mpps = s.rxpackets / duration / 1e6
+	 s.rx_gbps = compute_bitrate(s.rxpackets, s.rxbytes, duration) / 1e9
+	 s.lost_packets = s.txpackets - s.rxpackets - s.rxdrop
+	 s.lost_percent = s.lost_packets / s.txpackets * 100
+	 print(string.format('    TX %d packets (%f MPPS), %d bytes (%f Gbps)',
+			     s.txpackets, s.tx_mpps, s.txbytes, s.tx_gbps))
+	 print(string.format('    RX %d packets (%f MPPS), %d bytes (%f Gbps)',
+			     s.rxpackets, s.rx_mpps, s.rxbytes, s.rx_gbps))
+	 print(string.format('    Loss: %d ingress drop + %d packets lost (%f%%)',
+			     s.rxdrop, s.lost_packets, s.lost_percent))
+         return s
+      end
+      local function verify_load(s)
+	 if s.tx_gbps < 0.5 * s.applied_gbps then
+	    print("Invalid result.")
+	    return tester.start_load(bitrate, duration)
+	 else
+	    return check_results(s)
+	 end
       end
       print(string.format('Applying %f Gbps of load.', gbps_bitrate))
       return tester.generate_load(bitrate, duration):
          -- Wait 2ms for packets in flight to arrive
          and_then(promise.Wait, 0.002):
-         and_then(report):
-         and_then(check_results)
+	 and_then(compute_stats):
+	 and_then(verify_load)
    end
+
+   io.stdout:setvbuf("line")
 
    engine.busywait = true
    local is_done = false
