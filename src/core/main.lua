snabbco/snabb

commit 59caa02ca5d101bf8ae2b61d3c2b1536d86c3730

apps.interlink: shutdown handler to release stale attachments
Add calls to shutdown routines for the interlink receiver and transmitter apps
in cleanup handler of core.main (run by the supervisor.) This is to ensure the
release of inter-process links attached to by Snabb processes that exit
uncleanly.

src/core/main.lua
@@ -172,17 +172,19 @@ end
 
 -- Cleanup after Snabb process.
 function shutdown (pid)
+   -- simple pcall helper to print error and continue
+   local function safely (f)
+      local ok, err = pcall(f)
+      if not ok then print(err) end
+   end
+   -- Run cleanup hooks
+   safely(function () require("apps.interlink.receiver").shutdown(pid) end)
+   safely(function () require("apps.interlink.transmitter").shutdown(pid) end)
    -- Parent process performs additional cleanup steps.
    -- (Parent is the process whose 'group' folder is not a symlink.)
    local st, err = S.lstat(shm.root.."/"..pid.."/group")
    local is_parent = st and st.isdir
    if is_parent then
-      -- simple pcall helper to print error and continue
-      local function safely (f)
-         local ok, err = pcall(f)
-         if not ok then print(err) end
-      end
-      -- Run cleanup hooks
       safely(function () require("lib.hardware.pci").shutdown(pid) end)
       safely(function () require("core.memory").shutdown(pid) end)
    end
@@ -229,6 +231,7 @@ if assert(S.fork()) ~= 0 then
    -- parent process: run snabb
    xpcall(main, handler)
 else
+   initialize()
    -- child process: supervise parent & perform cleanup
    -- Subscribe to SIGHUP on parent death
    S.prctl("set_name", "[snabb sup]")

src/lib/interlink.lua
@@ -185,17 +185,17 @@ end
 function detach_receiver (r, name)
    detach(r, name,
           -- Reset: detach from queue with active transmitter (DXUP -> TXUP.)
-          function () return sync.cas(r.state, DXUP, TXUP) end,
+          function (r) return sync.cas(r.state, DXUP, TXUP) end,
           -- Shutdown: deallocate no longer used (RXUP -> DOWN.)
-          function () return sync.cas(r.state, RXUP, DOWN) end)
+          function (r) return sync.cas(r.state, RXUP, DOWN) end)
 end
 
 function detach_transmitter (r, name)
    detach(r, name,
           -- Reset: detach from queue with ready receiver (DXUP -> RXUP.)
-          function () return sync.cas(r.state, DXUP, RXUP) end,
+          function (r) return sync.cas(r.state, DXUP, RXUP) end,
           -- Shutdown: deallocate no longer used queue (TXUP -> DOWN.)
-          function () return sync.cas(r.state, TXUP, DOWN) end)
+          function (r) return sync.cas(r.state, TXUP, DOWN) end)
 end
 
 -- Queue operations follow below.
@@ -242,4 +242,8 @@ end
 function pull (r)
    -- NB: no need for memory barrier on x86 (see push.)
    r.read = r.nread
+end
+
+function open (name, readonly)
+   return shm.open(name, "struct interlink", readonly)
 end

src/apps/interlink/receiver.lua
@@ -10,7 +10,9 @@ local Receiver = {name="apps.interlink.Receiver"}
 function Receiver:new (_, name)
    local self = {}
    self.shm_name = "group/interlink/"..name
+   self.backlink = "interlink/receiver/"..name
    self.interlink = interlink.attach_receiver(self.shm_name)
+   shm.alias(self.backlink, self.shm_name)
    return setmetatable(self, {__index=Receiver})
 end
 
@@ -26,6 +28,22 @@ end
 
 function Receiver:stop ()
    interlink.detach_receiver(self.interlink, self.shm_name)
+   shm.unlink(self.backlink)
+end
+
+-- Detach receivers to prevent leaking interlinks opened by pid.
+--
+-- This is an internal API function provided for cleanup during
+-- process termination.
+function Receiver.shutdown (pid)
+   for _, name in ipairs(shm.children("/"..pid.."/interlink/receiver")) do
+      local backlink = "/"..pid.."/interlink/receiver/"..name
+      local shm_name = "/"..pid.."/group/interlink/"..name
+      -- Call protected in case /<pid>/group is already unlinked.
+      local ok, r = pcall(interlink.open, shm_name)
+      if ok then interlink.detach_receiver(r, shm_name) end
+      shm.unlink(backlink)
+   end
 end
 
 return Receiver

src/apps/interlink/transmitter.lua
@@ -10,7 +10,9 @@ local Transmitter = {name="apps.interlink.Transmitter"}
 function Transmitter:new (_, name)
    local self = {}
    self.shm_name = "group/interlink/"..name
+   self.backlink = "interlink/transmitter/"..name
    self.interlink = interlink.attach_transmitter(self.shm_name)
+   shm.alias(self.backlink, self.shm_name)
    return setmetatable(self, {__index=Transmitter})
 end
 
@@ -26,6 +28,22 @@ end
 
 function Transmitter:stop ()
    interlink.detach_transmitter(self.interlink, self.shm_name)
+   shm.unlink(self.backlink)
+end
+
+-- Detach transmitters to prevent leaking interlinks opened by pid.
+--
+-- This is an internal API function provided for cleanup during
+-- process termination.
+function Transmitter.shutdown (pid)
+   for _, name in ipairs(shm.children("/"..pid.."/interlink/transmitter")) do
+      local backlink = "/"..pid.."/interlink/transmitter/"..name
+      local shm_name = "/"..pid.."/group/interlink/"..name
+      -- Call protected in case /<pid>/group is already unlinked.
+      local ok, r = pcall(interlink.open, shm_name)
+      if ok then interlink.detach_transmitter(r, shm_name) end
+      shm.unlink(backlink)
+   end
 end
 
 return Transmitter

commit 154d63c111525c8f0ac9400b8d24a107f6a0d7be

lib.interlink / core.main: fix shutdown bugs
59caa02ca (apps.interlink: shutdown handler to release stale attachments)
introduced a bug where the supervisor processes would create directories under
SNABB_SHM_ROOT. This happened because lib.interlink depended on core.link and
core.packet, and I mistakenly added a call to initialize() to main in order to
resolve these dependencies.

The dependencies existed because detaching possibly involves freeing any
packets remaining on the queue, and the queue size was based on link.max (for
no particular reason.)

Besides introducing this bug, this approach was wrong for another reason: the
supervisor has no packet freelist, and hence there is no point in freeing
packets within the shutdown code.

This commit removes the unnecessary dependency on core.link, and makes sure
that the dependency on core.packet is only exercised when it makes sense to do
so. E.g., when detaching from queues in the supervisor no freeing of packets
takes place. Finally, the call to initialize() in the supervisor is reverted.

src/core/main.lua
@@ -231,7 +231,6 @@ if assert(S.fork()) ~= 0 then
    -- parent process: run snabb
    xpcall(main, handler)
 else
-   initialize()
    -- child process: supervise parent & perform cleanup
    -- Subscribe to SIGHUP on parent death
    S.prctl("set_name", "[snabb sup]")

src/lib/interlink.lua
@@ -63,7 +63,7 @@ local band = require("bit").band
 local waitfor = require("core.lib").waitfor
 local sync = require("core.sync")
 
-local SIZE = link.max + 1
+local SIZE = 1024
 local CACHELINE = 64 -- XXX - make dynamic
 local INT = ffi.sizeof("int")
 
@@ -176,7 +176,10 @@ local function detach (r, name, reset, shutdown)
          if reset(r) then return true
          -- Alternatively, attempt to shutdown and deallocate queue.
          elseif shutdown(r) then
-            while not empty(r) do
+            -- If detach is called by the supervisor (due to an abnormal exit)
+            -- the packet module will not be loaded (and there will be no
+            -- freelist to put the packets into.)
+            while packet and not empty(r) do
                packet.free(extract(r))
             end
             shm.unlink(name)
@@ -206,7 +209,7 @@ end
 -- Queue operations follow below.
 
 local function NEXT (i)
-   return band(i + 1, link.max)
+   return band(i + 1, SIZE - 1)
 end
 
 function full (r)