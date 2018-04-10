abf656bc97d650ae675e736916de7845dfa5ab02
Fabian Bonk <mail@fabianbonk.de>
2018-04-10 21:02:45 +0200

    add numa compatibility

--- src/program/echo/echo.lua
+++ src/program/echo/echo.lua
@@ -2,17 +2,19 @@ module(..., package.seeall)
 
 local echo = require("apps.echo.echo")
 local intel = require("apps.intel.intel_app")
+local numa = require("lib.numa")
 
 function run(parameters)
   print("running echo")
 
   if #parameters > 3 then
-    print("Usage: echo <pci-addr> [chain-length] [duration]\nexiting...")
+    print("Usage: echo <pci-addr> [chain-length] [duration] [cpu]\nexiting...")
     main.exit(1)
   end
 
   local pciaddr = parameters[1]
   local chainlen = tonumber(parameters[2]) or 1
+  local cpu = tonumber(parameters[3])
 
   if chainlen < 1 then
     print("chain-length < 1, defaulting to 1")
@@ -34,6 +36,7 @@ function run(parameters)
   config.link(c, "intel.tx -> echo1.input")
   config.link(c, string.format("echo%d.output -> intel.rx", chainlen))
 
+  if cpu then numa.bind_to_cpu(cpu) end
   engine.configure(c)
   engine.busywait = true
   engine.main{duration = tonumber(parameters[3])}
