419c943b8e72a11a21631c0b63ac60ad5de70f13
Diego Pino Garcia <dpino@igalia.com>
2018-04-16 15:39:05 +0000

    Fix error message

--- src/program/alarms/set_operator_state/set_operator_state.lua
+++ src/program/alarms/set_operator_state/set_operator_state.lua
@@ -3,14 +3,14 @@ module(..., package.seeall)
 
 local common = require("program.config.common")
 
-function show_usage(status, err_msg)
+function show_usage(program, status, err_msg)
    if err_msg then print('error: '..err_msg) end
    print(require("program.alarms.set_operator_state.README_inc"))
    main.exit(status)
 end
 
 local function fatal()
-   show_usage(1)
+   show_usage('set-operator-state', 1)
 end
 
 local function parse_args (args)
@@ -33,7 +33,7 @@ end
 
 function run(args)
    local opts = { command='set-alarm-operator-state', with_path=false, is_config=false,
-                  usage = show_usage, failsafe = true }
+                  usage=show_usage, failsafe=true }
    local args, cdr = common.parse_command_line(args, opts)
    local l_args = parse_args(cdr)
    local response = common.call_leader(
