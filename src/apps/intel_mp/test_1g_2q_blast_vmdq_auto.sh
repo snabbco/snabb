2bd39aee5d84cc74be4343438f20f4d257c2a68e
Max Rottenkolber <max@mr.gy>
2018-09-17 13:54:38 +0200

    intel_mp: fix 1G blast selftest via use of RateLimitedRepeater
    The lack of backpressure in basic_apps.Repeater caused the testsend.snabb
    to predict unpredictable/unbalanced packet distributions, causing test cases on
    slower 1G NICs to fail.
    
    This changes testsend.snabb to use lwaftr’s RateLimitedRepeater, and sets an
    appropriate rate for the 1G tests.

--- src/apps/intel_mp/test_1g_2q_blast_vmdq_auto.sh
+++ src/apps/intel_mp/test_1g_2q_blast_vmdq_auto.sh
@@ -2,7 +2,7 @@
 #
 # Test VMDq with automatic pool selection
 
-SNABB_SEND_BLAST=true ./testsend.snabb $SNABB_PCI_INTEL1G1 0 source2.pcap &
+SNABB_SEND_BLAST=true SNABB_SEND_BLAST_RATE=1e9 ./testsend.snabb $SNABB_PCI_INTEL1G1 0 source2.pcap &
 BLAST=$!
 
 SNABB_RECV_SPINUP=2 SNABB_RECV_DURATION=5 ./testvmdqrecv.snabb $SNABB_PCI_INTEL1G0 "90:72:82:78:c9:7a" nil nil nil > results.0 &
@@ -18,7 +18,7 @@ kill -9 $BLAST
 
 # both queues should see packets
 [[ `cat results.* | grep "^GPRC" | awk '{print $2}'` -gt 10000 ]] &&\
-[[ `cat results.0 | grep -m 1 bpp | awk '{print $11}'` -gt 0 ]] &&\
-[[ `cat results.1 | grep -m 1 bpp | awk '{print $11}'` -gt 0 ]]
+[[ `cat results.0 | grep -m 1 fpb | awk '{print $9}'` -gt 0 ]] &&\
+[[ `cat results.1 | grep -m 1 fpb | awk '{print $9}'` -gt 0 ]]
 
 exit $?
