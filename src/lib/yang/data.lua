1bff543236f2819a1106afc7814034e16b72ed2c
Max Rottenkolber <max@mr.gy>
2018-10-16 18:30:52 +0200

    lib.yang.path_data: add unique restriction consistency checker
    Implements checking of unique restrictions for list statements, see:
    https://tools.ietf.org/html/rfc6020#section-7.8.3

--- src/lib/yang/data.lua
+++ src/lib/yang/data.lua
@@ -223,7 +223,8 @@ function data_grammar_from_schema(schema, is_config)
       return {type='table', keys=keys, values=values,
               string_key=table_string_key(keys),
               key_ctype=struct_ctype(keys),
-              value_ctype=struct_ctype(values)}
+              value_ctype=struct_ctype(values),
+              unique = node.unique}
    end
    function handlers.leaf(node, for_key)
       if node.config ~= is_config and not for_key then return end
