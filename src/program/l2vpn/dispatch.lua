42da08f4f0c9d844f221442279f5b1abcfe88f95
Alexander Gall <alexander.gall@gmail.com>
2018-12-17 17:42:18 +0100

    l2vpn: rewrite dispatch module to use perfect hashing

--- src/program/l2vpn/dispatch.lua
+++ src/program/l2vpn/dispatch.lua
@@ -1,3 +1,5 @@
+-- Use of this source code is governed by the Apache 2.0 license; see COPYING.
+
 -- This app is a multiplexer/demultiplexer based on the IP source and
 -- destination address of a packet.  It has a well-known port called
 -- "south" that connects to the network and carries the multiplexed
@@ -12,93 +14,117 @@ module(..., package.seeall)
 local ffi = require("ffi")
 local lib = require("core.lib")
 local ethernet = require("lib.protocol.ethernet")
-local C = ffi.C
+local ctable = require("lib.ctable_perfect")
 
 dispatch = {}
 
-local dispatch_config_params = {
+local params = {
    afi = { required = true },
-   links = { default = {} }
-}
-local link_config_params = {
-   src = { required = true },
-   dst = { required = true }
+   links = {
+      keysof =
+         {
+            src = { required = true },
+            dst = { required = true }
+         }
+   }
 }
 
 local afs = {
    ipv4 = {
-      class = require("lib.protocol.ipv4"),
       offset = 12,
-      size = 8,
-      type = ffi.typeof[[
-        union {
-          struct {
-            uint8_t src[4];
-            uint8_t dst[4];
-          } addrs;
-          uint8_t bytes[8];
+      key_t = ffi.typeof([[
+        struct {
+          uint8_t src[4];
+          uint8_t dst[4];
         }
-      ]]
+      ]])
    },
    ipv6 = {
-      class = require("lib.protocol.ipv6"),
       offset = 8,
-      size = 32,
-      type = ffi.typeof[[
-        union {
-          struct {
-            uint8_t src[16];
-            uint8_t dst[16];
-          } addrs;
-          uint8_t bytes[32];
+      key_t = ffi.typeof([[
+        struct {
+          uint8_t src[16];
+          uint8_t dst[16];
         }
-      ]]
+      ]])
   }
 }
-function dispatch:new (arg)
+
+function dispatch:new (args)
    local o = {}
-   local conf = lib.parse(arg, dispatch_config_params)
+   local conf = lib.parse(args, params)
    local af = afs[conf.afi]
    assert(af, "Invalid address family identifier "..conf.afi)
-   o._offset = ethernet:sizeof() + af.offset
-   o._size = af.size
 
-   o._targets = {}
-   for name, link in pairs(conf.links) do
-      local conf = lib.parse(link, link_config_params)
-      local template = af.type()
-      C.memcpy(template.addrs.src, conf.src, ffi.sizeof(conf.src))
-      C.memcpy(template.addrs.dst, conf.dst, ffi.sizeof(conf.dst))
-      table.insert(o._targets, { template = template, link = name })
+   local key_t = af.key_t
+   o.offset = ethernet:sizeof() + af.offset
+   o.key_ptr_t = ffi.typeof("$*", key_t)
+   o.discard = link.new("dispatch_discard")
+
+   o.links, o.keys_by_name = {}, {}
+   local keys = {}
+   for name, addrs in pairs(conf.links) do
+      local key = key_t()
+      key.src = addrs.src
+      key.dst = addrs.dst
+      table.insert(keys, key)
+      table.insert(o.links, name)
+      o.keys_by_name[name] = key
    end
-   o._ntargets = #o._targets
+   o.ctab = ctable.new({
+         key_type = key_t,
+         value_type = ffi.typeof("struct link *"),
+         keys = keys,
+         default_value = o.discard })
+   o.nlinks = #o.links
 
    return setmetatable(o, { __index = dispatch })
+end
+
+function dispatch:link ()
+   for name, l in pairs(self.output) do
+      if type(name) == "string" and name ~= "south" then
+         local key = assert(self.keys_by_name[name])
+         self.ctab:update(key, l)
+      end
+   end
+end
+
+local function input (self, i, sout)
+   local name = self.links[i]
+
+   local lin = self.input[name]
+   for _ = 1, link.nreadable(lin) do
+      link.transmit(sout, link.receive(lin))
+   end
 end
 
 function dispatch:push()
    local sin = self.input.south
    local sout = self.output.south
 
-   for i = 1, self._ntargets do
-      local t = self._targets[i]
-      local tout = self.output[t.link]
-      for _ = 1, link.nreadable(sin) do
-         local p = link.receive(sin)
-         if C.memcmp(t.template, p.data + self._offset, self._size) == 0 then
-            link.transmit(tout, p)
-         else
-            link.transmit(sin, p)
-         end
-      end
-
-      local tin = self.input[t.link]
-      for _ = 1, link.nreadable(tin) do
-         link.transmit(sout, link.receive(tin))
-      end
+   for _ = 1, link.nreadable(sin) do
+      local p = link.receive(sin)
+      local key = ffi.cast(self.key_ptr_t, p.data + self.offset)
+      local entry = self.ctab:lookup_ptr(ffi.cast("uint8_t *", key))
+      link.transmit(entry.value, p)
    end
 
-   for _ = 1, link.nreadable(sin) do
-      packet.free(link.receive(sin))
+   local discard = self.discard
+   for _ = 1, link.nreadable(discard) do
+      packet.free(link.receive(discard))
+   end
+
+   if self.nlinks >= 3 then
+      for i = 1, self.nlinks do
+         input(self, i, sout)
+      end
+   else
+      if self.nlinks >= 1 then
+         input(self, 1, sout)
+      end
+      if self.nlinks >= 2 then
+         input(self, 2, sout)
+      end
    end
 end
