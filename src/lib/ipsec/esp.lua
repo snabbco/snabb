snabbco/snabb

commit ba81db5c61e65a640a1f8956013caf54e18e2414

lib.ipsec,apps.ipsec: add IPv4 transport mode

src/lib/ipsec/esp.lua
@@ -22,6 +22,7 @@ local header = require("lib.protocol.header")
 local datagram = require("lib.protocol.datagram")
 local ethernet = require("lib.protocol.ethernet")
 local ipv6 = require("lib.protocol.ipv6")
+local ipv4 = require("lib.protocol.ipv4")
 local esp = require("lib.protocol.esp")
 local esp_tail = require("lib.protocol.esp_tail")
 local aes_128_gcm = require("lib.ipsec.aes_128_gcm")
@@ -32,7 +33,7 @@ local C = ffi.C
 local logger = lib.logger_new({ rate = 32, module = 'esp' })
 local band = bit.band
 
-local htons, htonl, ntohl = lib.htons, lib.htonl, lib.ntohl
+local htons, htonl, ntohs, ntohl = lib.htons, lib.htonl, lib.ntohs, lib.ntohl
 
 require("lib.ipsec.track_seq_no_h")
 local window_t = ffi.typeof("uint8_t[?]")
@@ -40,6 +41,7 @@ local window_t = ffi.typeof("uint8_t[?]")
 PROTOCOL = 50 -- https://tools.ietf.org/html/rfc4303#section-2
 
 local ipv6_ptr_t = ffi.typeof("$ *", ipv6:ctype())
+local ipv4_ptr_t = ffi.typeof("$ *", ipv4:ctype())
 local function ipv6_fl (ip) return bit.lshift(ntohl(ip.v_tc_fl), 12) end
 
 local esp_header_ptr_t = ffi.typeof("$ *", esp:ctype())
@@ -51,6 +53,12 @@ local ESP_SIZE = esp:sizeof()
 local ESP_TAIL_SIZE = esp_tail:sizeof()
 
 local TRANSPORT6_PAYLOAD_OFFSET = ETHERNET_SIZE + IPV6_SIZE
+
+-- Offsets in units of uint16_t
+local IPV4_CSUM_OFFSET = ffi.offsetof(ipv4:ctype(), 'checksum')/2
+local IPV4_LENGTH_OFFSET = ffi.offsetof(ipv4:ctype(), 'total_length')/2
+-- Covers TTL (unchanged) and protocol
+local IPV4_TTL_OFFSET = ffi.offsetof(ipv4:ctype(), 'ttl')/2
 
 -- NB: `a' must be a power of two
 local function padding (a, l) return bit.band(-l, a-1) end
@@ -144,6 +152,42 @@ function encrypt:encapsulate_transport6 (p)
    return p
 end
 
+function encrypt:encapsulate_transport4 (p)
+   local ip = ffi.cast(ipv4_ptr_t, p.data + ETHERNET_SIZE)
+   local ihl = bit.band(bit.rshift(ntohs(ip.ihl_v_tos), 8), 0x0f)
+   local ip_header_length = ihl*4
+   local transport_payload_offset = ETHERNET_SIZE + ip_header_length
+
+   if p.length < transport_payload_offset then return nil end
+
+   local payload = p.data + transport_payload_offset
+   local payload_length = p.length - transport_payload_offset
+   local pad_length = self:padding(payload_length)
+   local overhead = self.ESP_OVERHEAD + pad_length
+   p = packet.resize(p, p.length + overhead)
+
+   local tail = payload + payload_length + pad_length
+   self:encode_esp_trailer(tail, ip.protocol, pad_length)
+
+   local ctext_length = payload_length + pad_length + ESP_TAIL_SIZE
+   self:encrypt_payload(payload, ctext_length)
+
+   local ctext = payload + ESP_SIZE + self.cipher.IV_SIZE
+   C.memmove(ctext, payload, ctext_length + self.cipher.AUTH_SIZE)
+
+   self:encode_esp_header(payload)
+
+   local ptr = ffi.cast("uint16_t *", ip)
+   C.checksum_update_incremental_16(ptr + IPV4_CSUM_OFFSET,
+                                    ptr + IPV4_LENGTH_OFFSET,
+                                    ip_header_length + payload_length + overhead)
+   C.checksum_update_incremental_16(ptr + IPV4_CSUM_OFFSET,
+                                    ptr + IPV4_TTL_OFFSET,
+                                    bit.bor(bit.lshift(ip.ttl, 8), PROTOCOL))
+
+   return p
+end
+
 -- Encapsulation in tunnel mode is performed as follows:
 -- (In tunnel mode, the input packet must be an IP frame already stripped of
 -- its Ethernet header.)
@@ -199,7 +243,7 @@ function decrypt:new (conf)
    return setmetatable(o, {__index=decrypt})
 end
 
-function decrypt:decrypt_payload (ptr, length, ip)
+function decrypt:decrypt_payload (ptr, length, ip, audit_fn)
    -- NB: bounds check is performed by caller
    local esp_header = ffi.cast(esp_header_ptr_t, ptr)
    local iv_start = ptr + ESP_SIZE
@@ -226,7 +270,7 @@ function decrypt:decrypt_payload (ptr, length, ip)
    end
 
    if error then
-      self:audit(error, ntohl(esp_header.spi), seq_low, ip)
+      audit_fn(self, error, ntohl(esp_header.spi), seq_low, ip)
       return nil
    end
 
@@ -257,7 +301,7 @@ function decrypt:decapsulate_transport6 (p)
    local payload_length = p.length - TRANSPORT6_PAYLOAD_OFFSET
 
    local ptext_start, ptext_length, next_header =
-      self:decrypt_payload(payload, payload_length, ip)
+      self:decrypt_payload(payload, payload_length, ip, self.audit_v6)
 
    if not ptext_start then return nil end
 
@@ -266,6 +310,36 @@ function decrypt:decapsulate_transport6 (p)
 
    C.memmove(payload, ptext_start, ptext_length)
    p = packet.resize(p, TRANSPORT6_PAYLOAD_OFFSET + ptext_length)
+
+   return p
+end
+
+function decrypt:decapsulate_transport4 (p)
+   local ip = ffi.cast(ipv4_ptr_t, p.data + ETHERNET_SIZE)
+   local ihl = bit.band(bit.rshift(ntohs(ip.ihl_v_tos), 8), 0x0f)
+   local ip_header_length = ihl*4
+   local transport_payload_offset = ETHERNET_SIZE + ip_header_length
+
+   if p.length - transport_payload_offset < self.MIN_SIZE then return nil end
+
+   local payload = p.data + transport_payload_offset
+   local payload_length = p.length - transport_payload_offset
+
+   local ptext_start, ptext_length, protocol =
+      self:decrypt_payload(payload, payload_length, ip, self.audit_v4)
+
+   if not ptext_start then return nil end
+
+   local ptr = ffi.cast("uint16_t *", ip)
+   C.checksum_update_incremental_16(ptr + IPV4_CSUM_OFFSET,
+                                    ptr + IPV4_LENGTH_OFFSET,
+                                    ip_header_length + ptext_length)
+   C.checksum_update_incremental_16(ptr + IPV4_CSUM_OFFSET,
+                                    ptr + IPV4_TTL_OFFSET,
+                                    bit.bor(bit.lshift(ip.ttl, 8), protocol))
+
+   C.memmove(payload, ptext_start, ptext_length)
+   p = packet.resize(p, transport_payload_offset + ptext_length)
 
    return p
 end
@@ -292,7 +366,7 @@ function decrypt:decapsulate_tunnel (p)
    return p, next_header
 end
 
-function decrypt:audit (reason, spi, seq, ip)
+function decrypt:audit_v6 (reason, spi, seq, ip)
    if not self.auditing then return end
    -- The information RFC4303 says we SHOULD log:
    logger:log(("Rejected packet (spi=%d, seq=%d, "
@@ -302,6 +376,18 @@ function decrypt:audit (reason, spi, seq, ip)
                  ip and ipv6:ntop(ip.src_ip) or "unknown",
                  ip and ipv6:ntop(ip.dst_ip) or "unknown",
                  ip and ipv6_fl(ip) or 0,
+                 reason))
+end
+
+function decrypt:audit_v4 (reason, spi, seq, ip)
+   if not self.auditing then return end
+   -- The information RFC4303 says we SHOULD log:
+   logger:log(("Rejected packet (spi=%d, seq=%d, "
+                  .."src_ip=%s, dst_ip=%s, "
+                  .."reason=%q)")
+         :format(spi, seq,
+                 ip and ipv4:ntop(ip.src_ip) or "unknown",
+                 ip and ipv4:ntop(ip.dst_ip) or "unknown",
                  reason))
 end
 

src/apps/ipsec/esp.lua
@@ -12,10 +12,11 @@ local ipsec_shm = require("lib.ipsec.shm")
 local counter = require("core.counter")
 local ethernet = require("lib.protocol.ethernet")
 local ipv6 = require("lib.protocol.ipv6")
+local ipv4 = require("lib.protocol.ipv4")
 local siphash = require("lib.hash.siphash")
 require("lib.ipsec.shm_h")
 
-Transport6 = {
+Transport = {
    config = {
       spi = {required=true},
       aead = {default="aes-gcm-16-icv"},
@@ -34,7 +35,7 @@ Transport6 = {
    }
 }
 
-function Transport6:new (conf)
+function Transport:new (conf)
    local self = {}
    assert(conf.transmit_salt ~= conf.receive_salt,
           "Refusing to operate with transmit_salt == receive_salt")
@@ -52,10 +53,10 @@ function Transport6:new (conf)
       resync_threshold = conf.resync_threshold,
       resync_attempts = conf.resync_attempts,
       auditing = conf.auditing}
-   return setmetatable(self, {__index = Transport6})
+   return setmetatable(self, {__index = Transport})
 end
 
-function Transport6:push ()
+function Transport:push ()
    if self.ike_check and self.ike_check() then
       self:maybe_update_keys()
    end
@@ -64,14 +65,34 @@ function Transport6:push ()
    local input = self.input.decapsulated
    local output = self.output.encapsulated
    if self.encrypt then
-      for _=1,link.nreadable(input) do
-         local p = link.receive(input)
-         local p_enc = self.encrypt:encapsulate_transport6(p)
-         if p_enc then
-            link.transmit(output, p_enc)
-         else
-            packet.free(p)
-            counter.add(self.shm.txerrors)
+      -- Parametrization of the call to encapsulate_transport() per
+      -- address family would introduce a variability that will
+      -- ultimately lead to the formation of a side trace at that
+      -- point when traffic of both kinds is present, which will
+      -- favour the address family for which the loop happens to get
+      -- compiled first.  We work around this by duplicating the code
+      -- with calls to fixed functions.
+      if self.afi == "ipv4" then
+         for _=1,link.nreadable(input) do
+            local p = link.receive(input)
+            local p_enc = self.encrypt:encapsulate_transport4(p)
+            if p_enc then
+               link.transmit(output, p_enc)
+            else
+               packet.free(p)
+               counter.add(self.shm.txerrors)
+            end
+         end
+      else
+         for _=1,link.nreadable(input) do
+            local p = link.receive(input)
+            local p_enc = self.encrypt:encapsulate_transport6(p)
+            if p_enc then
+               link.transmit(output, p_enc)
+            else
+               packet.free(p)
+               counter.add(self.shm.txerrors)
+            end
          end
       end
    else
@@ -85,14 +106,27 @@ function Transport6:push ()
    local input = self.input.encapsulated
    local output = self.output.decapsulated
    if self.decrypt then
-      for _=1,link.nreadable(input) do
-         local p = link.receive(input)
-         local p_dec = self.decrypt:decapsulate_transport6(p)
-         if p_dec then
-            link.transmit(output, p_dec)
-         else
-            packet.free(p)
-            counter.add(self.shm.rxerrors)
+      if self.afi == "ipv4" then
+         for _=1,link.nreadable(input) do
+            local p = link.receive(input)
+            local p_dec = self.decrypt:decapsulate_transport4(p)
+            if p_dec then
+               link.transmit(output, p_dec)
+            else
+               packet.free(p)
+               counter.add(self.shm.rxerrors)
+            end
+         end
+      else
+         for _=1,link.nreadable(input) do
+            local p = link.receive(input)
+            local p_dec = self.decrypt:decapsulate_transport6(p)
+            if p_dec then
+               link.transmit(output, p_dec)
+            else
+               packet.free(p)
+               counter.add(self.shm.rxerrors)
+            end
          end
       end
    else
@@ -103,7 +137,33 @@ function Transport6:push ()
    end
 end
 
-Transport6_IKE = setmetatable(
+local ip_config = {
+   v6 = {
+      afi = "ipv6",
+      afi_class = ipv6,
+      hash_buf = ffi.new[[
+         struct {
+            uint8_t remote[16];
+            uint8_t local[16];
+         } __attribute__ ((__packed__))
+      ]]
+   },
+   v4 = {
+      afi = "ipv4",
+      afi_class = ipv4,
+      hash_buf = ffi.new[[
+         struct {
+            uint8_t remote[4];
+            uint8_t local[4];
+         } __attribute__ ((__packed__))
+      ]]
+   }
+}
+
+Transport6 = setmetatable(ip_config.v6, {__index = Transport})
+Transport4 = setmetatable(ip_config.v4, {__index = Transport})
+
+Transport_IKE = setmetatable(
    {
       config = {
          aead = {default="aes-gcm-16-icv"},
@@ -114,35 +174,29 @@ Transport6_IKE = setmetatable(
          resync_attempts = {},
          auditing = {}
       },
-   }, { __index = Transport6 })
+   }, { __index = Transport })
 
-function Transport6_IKE:new (conf)
-   local self = {
-      ike_check = lib.throttle(conf.ike_check_interval),
-      conf = conf,
-      -- Cache of current SAs
-      sas = { ['in'] = { spi = 0 }, out = { spi = 0 } }
-   }
+function Transport_IKE:new (conf)
+   local self = setmetatable(
+      {
+         ike_check = lib.throttle(conf.ike_check_interval),
+         conf = conf,
+         -- Cache of current SAs
+         sas = { ['in'] = { spi = 0 }, out = { spi = 0 } }
+      }, {__index = self})
 
    -- The hash over the source/destination addresses is used as a
    -- rendez-vous point with the Strongswan IKE daemon in the "ipsec"
    -- subdirectory of SNABB_SHM_ROOT.  The daemon calculates the same
    -- hash over the source and destination traffic-selectors.
-   local buf = ffi.new[[
-      union {
-         struct {
-            uint8_t remote[16];
-            uint8_t local[16];
-         } addrs;
-         uint8_t blob[32];
-      }]]
-   buf.addrs.remote = ipv6:pton(conf.remote_address)
-   buf.addrs['local'] = ipv6:pton(conf.local_address)
+   self.hash_buf.remote = self.afi_class:pton(conf.remote_address)
+   self.hash_buf['local'] = self.afi_class:pton(conf.local_address)
    -- Static key used by the Strongswan Charon IKE daemon
    local key = ffi.new("uint8_t[16]", 0, 1, 2, 3, 4, 5, 6, 7,
                        8, 9, 10, 11, 12, 13, 14, 15)
-   local hash_fn = siphash.make_hash({ size = 32, standard = true, key = key })
-   local hash = hash_fn(buf)
+   local hash_fn = siphash.make_hash({ size = ffi.sizeof(self.hash_buf),
+                                       standard = true, key = key })
+   local hash = hash_fn(self.hash_buf)
    local path = "/ipsec/"..bit.tohex(hash)
    -- It is crucial that we don't pick up the keys used by a previous
    -- invocation, since that would break the aes-gcm cipher (re-use of
@@ -152,10 +206,10 @@ function Transport6_IKE:new (conf)
                                     { ['in'] = { ipsec_shm },
                                        out = {ipsec_shm } })
 
-   return setmetatable(self, {__index = Transport6_IKE})
+   return self
 end
 
-function Transport6_IKE:maybe_update_key (dir, update_fn)
+function Transport_IKE:maybe_update_key (dir, update_fn)
    local cache = self.sas[dir]
    local sa = self.sa_frame[dir]
    if sa.spi == 0 then
@@ -204,10 +258,13 @@ local function update_key_out (self, spi, key, salt)
    end
 end
 
-function Transport6_IKE:maybe_update_keys ()
+function Transport_IKE:maybe_update_keys ()
    self:maybe_update_key('in', update_key_in)
    self:maybe_update_key('out', update_key_out)
 end
+
+Transport6_IKE = setmetatable(ip_config.v6, {__index = Transport_IKE})
+Transport4_IKE = setmetatable(ip_config.v4, {__index = Transport_IKE})
 
 Tunnel6 = {
    config = {

commit a2338f8e7c4f8ee068987918d680758ce0969fa7

lib.ipsec.esp: fix regression introduced in ba81db5c6

src/lib/ipsec/esp.lua
@@ -356,7 +356,7 @@ function decrypt:decapsulate_tunnel (p)
    if p.length < self.MIN_SIZE then return nil end
 
    local ptext_start, ptext_length, next_header =
-      self:decrypt_payload(p.data, p.length)
+      self:decrypt_payload(p.data, p.length, nil, self.audit_v6)
 
    if not ptext_start then return nil end
 