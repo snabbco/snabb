5a74c150f2ee80252d4aa36b046ab786501e70ea
Luke Gorrie <luke@snabb.co>
2019-11-08 17:32:12 +0100

    ci.nix: Switch to pinned nixpkgs
    <nixpkgs> is not defined on hercules-ci.

--- ci.nix
+++ ci.nix
@@ -1,8 +1,12 @@
 # Nix expression for CI builds via hercules-ci.com.
-# Tests run on Snabb owner/operated hardware.
+# Tests run on Snabb owned/operated hardware.
 # Currently simple example to compile Snabb.
 
-with import <nixpkgs> {};
+with import (builtins.fetchGit {
+  name = "nixpkgs-release-19.09";
+  url = https://github.com/nixos/nixpkgs/;
+  rev = "23af4044501b161a23fea47f8ab0b6f0efca5a6f";
+}) {};
 {
   build = stdenv.mkDerivation {
     name = "snabb";
