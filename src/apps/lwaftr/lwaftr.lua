snabbco/snabb

commit 75a3373eee0c2aeee4f513c9e1bcdc97e482e6f7

Revert "lwaftr: keep full config in worker setup"
This reverts commit d4a1083588add7e7107084d83c2742635490ffb4.

Instead we attach instance device/queue_id meta-data to the
worker configs using a setmetatable, and ensure that all
config manipulation based on the worker queue id is performed
within the manager process.

Specifically, this means moving select_instance() from
apps.lwaftr.lwaftr to apps.lwaftr.lwutil and calling it from
program.lwaftr.setup. (I also took the liberty to make this
function pure to avoid future confusion on my end.)

src/apps/lwaftr/lwaftr.lua
@@ -339,21 +339,6 @@ local function drop(pkt)
    packet.free(pkt)
 end
 
-local function select_instance(conf)
-   local function table_merge(t1, t2)
-      local ret = {}
-      for k,v in pairs(t1) do ret[k] = v end
-      for k,v in pairs(t2) do ret[k] = v end
-      return ret
-   end
-   local device, id, queue = lwutil.parse_instance(conf)
-   conf.softwire_config.external_interface = table_merge(
-      conf.softwire_config.external_interface, queue.external_interface)
-   conf.softwire_config.internal_interface = table_merge(
-      conf.softwire_config.internal_interface, queue.internal_interface)
-   return conf
-end
-
 LwAftr = { yang_schema = 'snabb-softwire-v2' }
 -- Fields:
 --   - direction: "in", "out", "hairpin", "drop";
@@ -420,9 +405,8 @@ LwAftr.shm = {
 function LwAftr:new(conf)
    if conf.debug then debug = true end
    local o = setmetatable({}, {__index=LwAftr})
-   conf = select_instance(conf).softwire_config
+   conf = conf.softwire_config
    o.conf = conf
-
    o.binding_table = bt.load(conf.binding_table)
    o.inet_lookup_queue = bt.BTLookupQueue.new(o.binding_table)
    o.hairpin_lookup_queue = bt.BTLookupQueue.new(o.binding_table)

src/apps/lwaftr/lwutil.lua
@@ -21,6 +21,7 @@ local ntohs = lib.ntohs
 
 -- Return device PCI address, queue ID, and queue configuration.
 function parse_instance(conf)
+   assert(conf.worker_config, "conf missing instance/queue metadata.")
    local device = conf.worker_config.device
    local id = conf.worker_config.queue_id
    local queue = conf.softwire_config.instance[device].queue[id]
@@ -47,6 +48,25 @@ function num_queues(conf)
       n = n + 1
    end
    return n
+end
+
+function select_instance(conf)
+   local function table_merge(t1, t2)
+      local ret = {}
+      for k,v in pairs(t1) do ret[k] = v end
+      for k,v in pairs(t2) do ret[k] = v end
+      return ret
+   end
+   local device, id, queue = parse_instance(conf)
+   local copy = {softwire_config={}}
+   for k,v in pairs(conf.softwire_config) do
+      copy.softwire_config[k] = v
+   end
+   copy.softwire_config.external_interface = table_merge(
+      conf.softwire_config.external_interface, queue.external_interface)
+   copy.softwire_config.internal_interface = table_merge(
+      conf.softwire_config.internal_interface, queue.internal_interface)
+   return copy
 end
 
 function get_ihl_from_offset(pkt, offset)

src/lib/yang/snabb-softwire-v2.yang
@@ -886,18 +886,5 @@ module snabb-softwire-v2 {
     }
   }
 
-  container worker-config {
-    description
-    "Worker process configuration state. The contained leaves are used only
-     internally. Setting them has no effect.";
-     
-    leaf device {
-      type string;
-    }
-    leaf queue-id {
-      type uint8;
-    }
-  }
-
   uses state-counters;
 }

src/program/lwaftr/setup.lua
@@ -87,7 +87,7 @@ function lwaftr_app(c, conf)
               { address = convert_ipv4(iexternal_interface.ip) })
    config.app(c, "icmpechov6", ipv6_echo.ICMPEcho,
               { address = iinternal_interface.ip })
-   config.app(c, "lwaftr", lwaftr.LwAftr, conf)
+   config.app(c, "lwaftr", lwaftr.LwAftr, lwutil.select_instance(conf))
    config.app(c, "fragmenterv4", ipv4_fragment.Fragmenter,
               { mtu=gexternal_interface.mtu })
    config.app(c, "fragmenterv6", ipv6_fragment.Fragmenter,
@@ -827,8 +827,8 @@ local function compute_worker_configs(conf)
       for id, _ in pairs(queues.queue) do
          local worker_id = string.format('%s/%s', device, id)
          local worker_config = make_copy()
-         worker_config.worker_config = {device=device, queue_id=id}
-         ret[worker_id] = worker_config
+         local meta = {worker_config = {device=device, queue_id=id}}
+         ret[worker_id] = setmetatable(worker_config, {__index=meta})
       end
    end
    return ret

commit 18fc9f5bff0e5021163a57c88a742e90badc6830

lwaftr: fix bugs introduced in 75a3373e
NB: extra leaves got stripped by config serialization.
Merge interface configs in lwaftr:new, pass valid-per-schema config to
app but remove all but the respective instance/queue.

Update lwutil.parse_instance to fall back to old behavior
(pre d4a10835).

src/apps/lwaftr/lwaftr.lua
@@ -405,7 +405,7 @@ LwAftr.shm = {
 function LwAftr:new(conf)
    if conf.debug then debug = true end
    local o = setmetatable({}, {__index=LwAftr})
-   conf = conf.softwire_config
+   conf = lwutil.merge_instance(conf).softwire_config
    o.conf = conf
    o.binding_table = bt.load(conf.binding_table)
    o.inet_lookup_queue = bt.BTLookupQueue.new(o.binding_table)

src/apps/lwaftr/lwutil.lua
@@ -7,6 +7,7 @@ local bit = require("bit")
 local ffi = require("ffi")
 local lib = require("core.lib")
 local cltable = require("lib.cltable")
+local binary = require("lib.yang.binary")
 
 local band = bit.band
 local cast = ffi.cast
@@ -21,11 +22,23 @@ local ntohs = lib.ntohs
 
 -- Return device PCI address, queue ID, and queue configuration.
 function parse_instance(conf)
-   assert(conf.worker_config, "conf missing instance/queue metadata.")
-   local device = conf.worker_config.device
-   local id = conf.worker_config.queue_id
-   local queue = conf.softwire_config.instance[device].queue[id]
-   return device, id, queue
+   if conf.worker_config then
+      local device = conf.worker_config.device
+      local id = conf.worker_config.queue_id
+      local queue = conf.softwire_config.instance[device].queue[id]
+      return device, id, queue
+   else
+      local device, id
+      for dev in pairs(conf.softwire_config.instance) do
+         assert(not device, "Config contains more than one device")
+         device = dev
+      end
+      for queue in pairs(conf.softwire_config.instance[device].queue) do
+         assert(not id, "Config contains more than one queue")
+         id = queue
+      end
+      return device, id, conf.softwire_config.instance[device].queue[id]
+   end
 end
 
 function is_on_a_stick(device, queue)
@@ -51,17 +64,34 @@ function num_queues(conf)
 end
 
 function select_instance(conf)
+   local copier = binary.config_copier_for_schema_by_name('snabb-softwire-v2')
+   local device, id = parse_instance(conf)
+   local copy = copier(conf)()
+   local instance = copy.softwire_config.instance
+   for other_device, queues in pairs(conf.softwire_config.instance) do
+      if other_device ~= device then
+         instance[other_device] = nil
+      else
+         for other_id, _ in pairs(queues.queue) do
+            if other_id ~= id then
+               instance[device].queue[other_id] = nil
+            end
+         end
+      end
+   end
+   return copy
+end
+
+function merge_instance (conf)
    local function table_merge(t1, t2)
       local ret = {}
       for k,v in pairs(t1) do ret[k] = v end
       for k,v in pairs(t2) do ret[k] = v end
       return ret
    end
-   local device, id, queue = parse_instance(conf)
-   local copy = {softwire_config={}}
-   for k,v in pairs(conf.softwire_config) do
-      copy.softwire_config[k] = v
-   end
+   local copier = binary.config_copier_for_schema_by_name('snabb-softwire-v2')
+   local copy = copier(conf)()
+   local _, _, queue = parse_instance(conf)
    copy.softwire_config.external_interface = table_merge(
       conf.softwire_config.external_interface, queue.external_interface)
    copy.softwire_config.internal_interface = table_merge(