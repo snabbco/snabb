snabbco/snabb

commit fece51f0da0878bce6b4b63a780a6c39b45cbdd7

intel_avf: fix truncation of outbound packets

src/apps/intel_avf/intel_avf.lua
@@ -398,9 +398,11 @@ function Intel_avf:push ()
    self:reclaim_txdesc()
    while not empty(li) and self.tx_desc_free > 0 do
       local p = receive(li)
+      -- NB: need to extend size for 4 byte CRC (not clear from the spec.)
+      local size = lshift(4ULL+p.length, SIZE_SHIFT)
       self.txdesc[ self.tx_next ].address = tophysical(p.data)
       self.txqueue[ self.tx_next ] = p
-      self.txdesc[ self.tx_next ].cmd_type_offset_bsz = RS_EOP + p.length * 2^SIZE_SHIFT
+      self.txdesc[ self.tx_next ].cmd_type_offset_bsz = RS_EOP + size
       self.tx_next = self:ringnext(self.tx_next)
       self.tx_desc_free = self.tx_desc_free - 1
    end

src/apps/intel_avf/tests/back2back/test.snabb
@@ -24,7 +24,7 @@ local counter = require("core.counter")
 
 local c = config.new()
 config.app(c, "synth", synth.Synth, {
-       sizes = {64,128,192,256,384,512,1024},
+       sizes = {64,67,128,133,192,256,384,512,777,1024},
        src=src,
        dst=dst,
        random_payload = true

commit 9c83c2561cb43d46bc88cb81e8cdd7386cba851d

intel_avf: fix rx/tx packet lengths
This fixes a bug originally introduced in fece51f0 where sent packets
where with 4 bytes too many as a trailer.

Reverts the hence bogus 355051b0.

The original bug I believe comes from a misunderstood shift truncation
which is fixed by a 0ULL+ before shift.
The original 4ULL+ "fix" was likely a fever dream of mine where I
thought AVF expects us to specify the packet size+4 (i.e., include
space for the CRC?), and only fixed the real issue (shift truncation) by
accident.

src/apps/intel_avf/intel_avf.lua
@@ -686,8 +686,7 @@ function IO:transmit (li)
    self:reclaim_txdesc()
    while not empty(li) and cxq.tx_desc_free > 0 do
       local p = receive(li)
-      -- NB: need to extend size for 4 byte CRC (not clear from the spec.)
-      local size = lshift(4ULL+p.length, SIZE_SHIFT)
+      local size = lshift(0ULL+p.length, SIZE_SHIFT) -- NB: extend to 64 bit before shift
       cxq.txdesc[ cxq.tx_next ].address = tophysical(p.data)
       cxq.txqueue[ cxq.tx_next ] = p
       cxq.txdesc[ cxq.tx_next ].cmd_type_offset_bsz = bor(RS_EOP_IL2TAG1, size, L2TAG1)
@@ -705,8 +704,7 @@ function IO:receive (lo)
    local cxq = self.cxq
    while band(cxq.rxdesc[cxq.rx_tail].write.status_err_type_len, 0x01) == 1 and pkts < engine.pull_npackets do
       local p = cxq.rxqueue[cxq.rx_tail]
-      -- NB: truncate 4 byte CRC
-      p.length = rshift(cxq.rxdesc[cxq.rx_tail].write.status_err_type_len, 38) - 4
+      p.length = rshift(cxq.rxdesc[cxq.rx_tail].write.status_err_type_len, 38)
       transmit(lo, p)
 
       local np = packet.allocate()