snabbco/snabb

commit de3a22548ac9284b43505392a95f4eb264d863ab

ipfix probe_ptree: key rss groups / instances in snabbflow-state with id instead of pid

src/lib/ptree/support/snabb-snabbflow-v1.lua
@@ -60,10 +60,11 @@ end
 local function collect_ipfix_states (pid, rss_links)
    local states = {}
    for _, app in ipairs(shm.children("/"..pid.."/apps")) do
-      local exporter = app:match("ipfix_([%w_]+)")
+      local exporter, instance = app:match("^ipfix_([%w_]+)_(%d+)$")
       if exporter then
          local stats = shm.open_frame("/"..pid.."/apps/"..app)
          local state = {
+            id = tonumber(instance),
             pid = pid,
             observation_domain = tonumber(counter.read(stats.observation_domain)),
             packets_received = counter.read(stats.received_packets),
@@ -71,7 +72,7 @@ local function collect_ipfix_states (pid, rss_links)
             template_packets_transmitted = counter.read(stats.template_packets),
             sequence_number = counter.read(stats.sequence_number)
          }
-         state.template = collect_template_states(pid, app, exporter)
+         state.template = collect_template_states(pid, app, exporter..instance)
          rss_links[find_rss_link(pid, app)] = state.observation_domain
          states[exporter] = state
       end
@@ -81,14 +82,23 @@ end
 
 function collect_rss_states (pid, rss_links)
    local states = {}
+   local id
+   for _, app in ipairs(shm.children("/"..pid.."/apps")) do
+      local rss_group = tonumber(app:match("^rss(%d+)$"))
+      if rss_group then
+         id = tonumber(rss_group)
+         break
+      end
+   end
    for _, link in ipairs(shm.children("/"..pid.."/links")) do
       for rss_link, _ in pairs(rss_links) do
-         if (link:match("^"..rss_link) and link:match("^rss.")) -- embedded link
+         if (link:match("^"..rss_link) and link:match("^rss%d+.")) -- embedded link
          or (link:match("-> *"..rss_link:gsub("%.output$", ".input").."$")) -- interlink
          then
             local stats = shm.open_frame("/"..pid.."/links/"..link)
             local observation_domain = rss_links[rss_link]
             states[observation_domain] = {
+               id = id,
                pid = pid,
                txdrop = counter.read(stats.txdrop)
             }
@@ -170,20 +180,21 @@ local function process_states (pids)
       for _, ipfix_state in ipairs(states) do
          local observation_domain = ipfix_state.observation_domain
          local rss_state = rss_states[observation_domain]
-         if not state.rss_group[rss_state.pid] then
-            state.rss_group[rss_state.pid] = {
+         if not state.rss_group[rss_state.id] then
+            state.rss_group[rss_state.id] = {
+               id = rss_state.id,
                pid = rss_state.pid,
                exporter = {}
             }
          end
-         local rss_group = state.rss_group[rss_state.pid]
+         local rss_group = state.rss_group[rss_state.id]
          if not rss_group.exporter[exporter] then
             rss_group.exporter[exporter] = {
                instance = {}
             }
          end
          local instances = rss_group.exporter[exporter].instance
-         instances[ipfix_state.pid] = ipfix_state
+         instances[ipfix_state.id] = ipfix_state
       end
    end
    return {snabbflow_state=state}

src/lib/yang/snabb-snabbflow-v1.yang
@@ -524,10 +524,16 @@ module snabb-snabbflow-v1 {
     }
 
     list rss-group {
-      key pid;
+      key id;
       description
         "Statistics for the RSS group workers defined in
         /snabbflow-config/rss/hardware-scaling.";
+
+      leaf id {
+        type numeric-id;
+        description
+          "RSS group identifier.";
+      }
 
       uses worker-state;
 
@@ -544,9 +550,15 @@ module snabb-snabbflow-v1 {
         }
 
         list instance {
-          key pid;
+          key id;
           description
             "Statistics for exporter instance workers for this RSS group.";
+
+          leaf id {
+            type numeric-id;
+            description
+              "Exporter instance identifier (within this RSS group).";
+          }
 
           uses worker-state;
           uses exporter-state;

src/program/ipfix/lib.lua
@@ -369,9 +369,11 @@ function run_rss(config, inputs, outputs, duration, busywait, cpu, jit, log_date
    clear_jit_options(jit)
 end
 
-function configure_rss_graph (config, inputs, outputs, log_date)
+function configure_rss_graph (config, inputs, outputs, log_date, rss_group)
    local graph = app_graph.new()
-   app_graph.app(graph, "rss", rss.rss, config)
+
+   local rss_name = "rss"..(rss_group or '')
+   app_graph.app(graph, rss_name, rss.rss, config)
 
    -- An input describes a physical interface
    local tags, in_app_specs = {}, {}
@@ -393,7 +395,7 @@ function configure_rss_graph (config, inputs, outputs, log_date)
          link_name = "vlan"..tag
       end
       app_graph.link(graph, input_name.."."..in_link.output
-                        .." -> rss."..link_name)
+                        .." -> "..rss_name.."."..link_name)
    end
 
    -- An output describes either an interlink or a complete ipfix app
@@ -402,7 +404,7 @@ function configure_rss_graph (config, inputs, outputs, log_date)
          -- Keys
          --   link_name  name of the link
          app_graph.app(graph, output.link_name, Transmitter)
-         app_graph.link(graph, "rss."..output.link_name.." -> "
+         app_graph.link(graph, rss_name.."."..output.link_name.." -> "
                            ..output.link_name..".input")
       else
          -- Keys
@@ -411,7 +413,7 @@ function configure_rss_graph (config, inputs, outputs, log_date)
          --   instance   # of embedded instance
          output.args.instance = output.instance or output.args.instance
          local graph = configure_graph(output.args, graph)
-         app_graph.link(graph, "rss."..output.link_name
+         app_graph.link(graph, rss_name.."."..output.link_name
                            .." -> ipfix_"..output.args.instance..".input")
       end
    end

src/program/ipfix/probe_ptree/probe_ptree.lua
@@ -222,9 +222,9 @@ function setup_workers (config)
             local iconfig = lib.deepcopy(config)
 
             -- This is used to disambiguate multiple instances of the
-            -- ipfix app (possible using multiple instances of the same
+            -- ipfix app (possibly using multiple instances of the same
             -- template) within a single worker.
-            iconfig.instance = name
+            iconfig.instance = name.."_"..i
 
             local rss_link
             local class = flow_director.class[name]
@@ -297,8 +297,9 @@ function setup_workers (config)
             continue = class.continue
          })
       end
-      workers["rss"..rss_group] =
-         probe.configure_rss_graph(rss_config, inputs, outputs, ipfix.log_date)
+      workers["rss"..rss_group] = probe.configure_rss_graph(
+         rss_config, inputs, outputs, ipfix.log_date, rss_group
+      )
    end
 
    -- Create a trivial app graph that only contains the control apps

commit acf52dd443119d7913abb5ffae442c46a92ad2b8

ipfix probe_ptree: fix bug introduced in de3a2254

src/lib/ptree/support/snabb-snabbflow-v1.lua
@@ -83,7 +83,7 @@ local function collect_ipfix_states (pid, rss_links)
             template_packets_transmitted = counter.read(stats.template_packets),
             sequence_number = counter.read(stats.sequence_number)
          }
-         state.template = collect_template_states(pid, app, exporter..instance)
+         state.template = collect_template_states(pid, app, exporter.."_"..instance)
          rss_links[find_rss_link(pid, app)] = state.observation_domain
          states[exporter] = state
       end