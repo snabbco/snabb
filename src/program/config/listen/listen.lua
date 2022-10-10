snabbco/snabb

commit bfa68ba1bd4ccf7afe973ed21cafac9a0dcb72b5

program.config: do not exit on error in call_leader
This is to enable programmatic usage of this module.

The change requires callsites of call_leader  parse_command_line to check
and handle returned errors.

src/program/config/listen/listen.lua
@@ -91,6 +91,9 @@ end
 
 function run(args)
    args = common.parse_command_line(args, { command='listen' })
+   if args.error then
+      common.print_and_exit(args)
+   end
    local caller = rpc.prepare_caller('snabb-config-leader-v1')
    local leader = common.open_socket_or_die(args.instance_id)
    attach_listener(leader, caller, args.schema_name, args.revision_date)

src/program/alarms/compress/compress.lua
@@ -35,6 +35,9 @@ function run(args)
    local opts = { command='compress-alarms', with_path=false, is_config=false,
                   usage=show_usage }
    args = common.parse_command_line(args, opts)
+   if args.error then
+      common.print_and_exit(args)
+   end
    local response = common.call_leader(
       args.instance_id, 'compress-alarms',
       { schema = args.schema_name, revision = args.revision,

src/program/alarms/set_operator_state/set_operator_state.lua
@@ -35,6 +35,9 @@ function run(args)
    local opts = { command='set-alarm-operator-state', with_path=false, is_config=false,
                   usage=show_usage, allow_extra_args=true }
    local args, cdr = common.parse_command_line(args, opts)
+   if args.error then
+      common.print_and_exit(args)
+   end
    local l_args = parse_args(cdr)
    local response = common.call_leader(
       args.instance_id, 'set-alarm-operator-state',

src/program/config/add/add.lua
@@ -7,6 +7,9 @@ function run(args)
    local opts = { command='add', with_path=true, with_value=true,
                   is_config=true }
    args = common.parse_command_line(args, opts)
+   if args.error then
+      common.print_and_exit(args)
+   end
    local response = common.call_leader(
       args.instance_id, 'add-config',
       { schema = args.schema_name, revision = args.revision_date,

src/program/config/common.lua
@@ -97,6 +97,9 @@ function parse_command_line(args, opts)
    if #args == 0 then err() end
    ret.instance_id = table.remove(args, 1)
    local descr = call_leader(ret.instance_id, 'describe', {})
+   if descr.error then
+      return descr
+   end
    if not ret.schema_name then
       if opts.require_schema then err("missing --schema arg") end
       ret.schema_name = descr.default_schema
@@ -136,17 +139,15 @@ function parse_command_line(args, opts)
    return ret, args
 end
 
-function open_socket_or_die(instance_id)
+function open_socket(instance_id)
    S.signal('pipe', 'ign')
    local socket = assert(S.socket("unix", "stream"))
    local tail = instance_id..'/config-leader-socket'
    local by_name = S.t.sockaddr_un(shm.root..'/by-name/'..tail)
    local by_pid = S.t.sockaddr_un(shm.root..'/'..tail)
    if not socket:connect(by_name) and not socket:connect(by_pid) then
-      io.stderr:write(
-         "Could not connect to config leader socket on Snabb instance '"..
-            instance_id.."'.\n")
-      main.exit(1)
+      socket:close()
+      return nil
    end
    return file.fdopen(socket, 'rdwr')
 end
@@ -194,7 +195,14 @@ end
 
 function call_leader(instance_id, method, args)
    local caller = rpc.prepare_caller('snabb-config-leader-v1')
-   local socket = open_socket_or_die(instance_id)
+   local socket = open_socket(instance_id)
+   if not socket then
+      return {
+         status = 1,
+         error = ("Could not connect to config leader socket on Snabb instance %q")
+            :format(instance_id)
+      }
+   end
    -- FIXME: stream call and response.
    local msg, parse_reply = rpc.prepare_call(caller, method, args)
    send_message(socket, msg)

src/program/config/get/get.lua
@@ -6,6 +6,9 @@ local common = require("program.config.common")
 function run(args)
    local opts = { command='get', with_path=true, is_config=true }
    args = common.parse_command_line(args, opts)
+   if args.error then
+      common.print_and_exit(args)
+   end
    local response = common.call_leader(
       args.instance_id, 'get-config',
       { schema = args.schema_name, revision = args.revision_date,

src/program/config/get_state/get_state.lua
@@ -6,6 +6,9 @@ local common = require("program.config.common")
 function run(args)
    local opts = { command='get-state', with_path=true, is_config=false }
    args = common.parse_command_line(args, opts)
+   if args.error then
+      common.print_and_exit(args)
+   end
    local response = common.call_leader(
       args.instance_id, 'get-state',
       { schema = args.schema_name, revision = args.revision_date,

src/program/config/load/load.lua
@@ -6,6 +6,9 @@ local common = require("program.config.common")
 function run(args)
    local opts = { command='load', with_config_file=true, is_config = false}
    args = common.parse_command_line(args, opts)
+   if args.error then
+      common.print_and_exit(args)
+   end
    local response = common.call_leader(
       args.instance_id, 'set-config',
       { schema = args.schema_name, revision = args.revision_date,

commit 18d2631f4dcf8e099230583d0b707fd681da2a72

snabb config listen: fix bug introduced in bfa68ba1bd4ccf7afe973ed21cafac9a0dcb72b5

src/program/config/listen/listen.lua
@@ -95,7 +95,14 @@ function run(args)
       common.print_and_exit(args)
    end
    local caller = rpc.prepare_caller('snabb-config-leader-v1')
-   local leader = common.open_socket_or_die(args.instance_id)
+   local leader = common.open_socket(args.instance_id)
+   if not leader then
+      io.stderr:write(
+         ("Could not connect to config leader socket on Snabb instance %q\n")
+            :format(args.instance_id)
+      )
+      main.exit(1)
+   end
    attach_listener(leader, caller, args.schema_name, args.revision_date)
    
    local handler = require('lib.fibers.file').new_poll_io_handler()