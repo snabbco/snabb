snabbco/snabb

commit feaf250bf5dafdf8971588170904abb374141044

lib.ptree.ptree: unconditionally call :stop() after main loop finishes

src/lib/ptree/ptree.lua
@@ -982,13 +982,12 @@ function Manager:main (duration)
          now = C.get_monotonic_time()
       end
    end
-   if self.shutdown then self:stop() end
+   self:stop()
 end
 
 function main (opts, duration)
    local m = new_manager(opts)
    m:main(duration)
-   m:stop()
 end
 
 function selftest ()

commit d8be2c4fb8b1bcfed0e8ca3b25ec6672f38444a8

lib.ptree: restore old behavior broken in feaf250bf5dafdf8971588170904abb374141044

src/lib/ptree/ptree.lua
@@ -982,12 +982,15 @@ function Manager:main (duration)
          now = C.get_monotonic_time()
       end
    end
-   self:stop()
+   if self.shutdown then self:stop() end
 end
 
 function main (opts, duration)
    local m = new_manager(opts)
    m:main(duration)
+   if not m.shutdown then
+      m:stop()
+   end
 end
 
 function selftest ()